1c9f5a05e776f50133e56e74b43aa72790370479
Guillaume PIERRE <guillaume.pierre@rte-france.com>
2020-04-02 08:35:02 +0200

    Unit-testing Matrix : enhance of "load from file to buffer" mocking + adding tests

--- tests/src/libs/antares/array/matrix-bypass-load.h
+++ tests/src/libs/antares/array/matrix-bypass-load.h
@@ -57,65 +57,63 @@ public:
 };
 
 
+template<class T = double, class ReadWriteT = T>
+class fake_fuffer_factory
+{
+public:
+	fake_fuffer_factory() :
+		buffer_precision_(0),
+		buffer_print_dimensions_(false)
+	{}
+
+	void matrix_to_build_buffer_with(Matrix_easy_to_fill<T, ReadWriteT> * mtx) { mtx_to_build_buffer_with_ = mtx; }
+	void set_precision(uint precision) { buffer_precision_ = precision; }
+	void print_dimensions(bool print_dims) { buffer_print_dimensions_ = print_dims; }
+	Clob* build_buffer()
+	{
+		Clob* buffer_to_return = new Clob;
+		Antares::UnitTests::PredicateIdentity predicate;
+		mtx_to_build_buffer_with_->saveToFileDescriptor(*buffer_to_return, buffer_precision_, buffer_print_dimensions_, predicate);
+
+		return buffer_to_return;
+	}
+
+private:
+	uint buffer_precision_;
+	bool buffer_print_dimensions_;
+	Matrix_easy_to_fill<T, ReadWriteT> * mtx_to_build_buffer_with_;
+
+};
+
 
 template<class T = double, class ReadWriteT = T>
-class Matrix_mock_load_to_buffer : public Matrix_easy_to_fill<T, ReadWriteT>
+class Matrix_mock_load_to_buffer : public Matrix<T, ReadWriteT>
 {
 public:
 	Matrix_mock_load_to_buffer() :
-		Matrix_easy_to_fill(),
-		fake_mtx_heigth_(0),
-		fake_mtx_width_(0),
-		fake_mtx_precision_(0),
-		fake_mtx_print_dimensions_(false),
+		Matrix(),
 		fake_mtx_error_when_loading_(IO::errNone)
 	{};
 
 	Matrix_mock_load_to_buffer(uint height, uint width) :
-		Matrix_easy_to_fill<T, ReadWriteT>(height, width),
-		fake_mtx_heigth_(0),
-		fake_mtx_width_(0),
-		fake_mtx_precision_(0),
-		fake_mtx_print_dimensions_(false),
+		Matrix<T, ReadWriteT>(height, width),
 		fake_mtx_error_when_loading_(IO::errNone)
 	{};
 
 	Matrix_mock_load_to_buffer(uint height, uint width, const vector<T>& vec) :
-		Matrix_easy_to_fill<T, ReadWriteT>(height, width, vec),
-		fake_mtx_heigth_(0),
-		fake_mtx_width_(0),
-		fake_mtx_precision_(0),
-		fake_mtx_print_dimensions_(false),
+		Matrix<T, ReadWriteT>(height, width, vec),
 		fake_mtx_error_when_loading_(IO::errNone)
 	{};
 	
 	virtual IO::Error loadFromFileToBuffer(BufferType & buffer, const AnyString& filename, uint64 hardlimit) const
 	{
-		Matrix_easy_to_fill<double, double> fake_mtx(fake_mtx_heigth_, fake_mtx_width_, fake_mtx_vec_);
-		Antares::UnitTests::PredicateIdentity predicate;
-		fake_mtx.saveToFileDescriptor(buffer, fake_mtx_precision_, fake_mtx_print_dimensions_, predicate);
-
 		return fake_mtx_error_when_loading_;
 	}
 
-	void fake_matrix_to_load(uint height, uint width, vector<ReadWriteT> v) 
-	{	
-		fake_mtx_vec_ = v;
-		fake_mtx_heigth_ = height;
-		fake_mtx_width_ = width;
-	}
-
-	void fake_mtx_to_be_loaded__set_precision(uint precision) { fake_mtx_precision_ = precision; }
-	void fake_mtx_to_be_loaded__print_dimensions(bool print_dims) { fake_mtx_print_dimensions_ = print_dims; }
-	void error_when_loading_fake_mtx(IO::Error err) { fake_mtx_error_when_loading_ = err; }
+	void error_when_loading_from_file(IO::Error err) { fake_mtx_error_when_loading_ = err; }
 
 public:
-	vector<ReadWriteT> fake_mtx_vec_;
-	uint fake_mtx_heigth_;
-	uint fake_mtx_width_;
 	IO::Error fake_mtx_error_when_loading_;
-	uint fake_mtx_precision_;
-	bool fake_mtx_print_dimensions_;
 };
 
 #endif	// __ANTARES_LIBS_ARRAY_MATRIX_BYPASS_LOAD_H__
--- tests/src/libs/antares/array/tests-matrix-load.cpp
+++ tests/src/libs/antares/array/tests-matrix-load.cpp
@@ -16,21 +16,73 @@ using namespace Yuni;
 // ================================
 // ===  Matrix<double, double>  ===
 // ================================
-BOOST_AUTO_TEST_SUITE(test_suite_1)
+BOOST_AUTO_TEST_SUITE(coeffs_are_double__load_from_double)
+
+
+BOOST_AUTO_TEST_CASE(fake_file_is_empty___loaded_matrix_has_only_0s)
+{
+	// Creating a buffer mocking the result of : IO::File::LoadFromFile(...)
+	fake_fuffer_factory<double, double> buffer_factory;
+	Matrix_easy_to_fill<double, double> mtx_0;
+	buffer_factory.matrix_to_build_buffer_with(&mtx_0);
+	buffer_factory.set_precision(0);
+	buffer_factory.print_dimensions(false);
+
+	Clob* fake_buffer = buffer_factory.build_buffer();
+
+	// Testing load
+	Matrix_mock_load_to_buffer<double, double> mtx(2, 2);
+	mtx.error_when_loading_from_file(IO::errNone);
+	mtx.loadFromCSVFile("path/to/a/file", 2, 2, Matrix<double, double>::optNone, fake_buffer);
+
+	BOOST_REQUIRE_EQUAL(mtx.containsOnlyZero(), true);
+}
+
+BOOST_AUTO_TEST_CASE(fake_file_not_empty__mtx_is_empty___mtx_get_file_sizes)
+{
+	// Creating a buffer mocking the result of : IO::File::LoadFromFile(...)
+	fake_fuffer_factory<double, double> buffer_factory;
+	Matrix_easy_to_fill<double, double> mtx_0(2, 3, { 1.5, -2.44444, 3.66666, 0, 8.559, -5.5555 });
+	buffer_factory.matrix_to_build_buffer_with(&mtx_0);
+	buffer_factory.set_precision(2);
+	buffer_factory.print_dimensions(false);
+
+	Clob* fake_buffer = buffer_factory.build_buffer();
+
+	// Testing load
+	Matrix_mock_load_to_buffer<double, double> mtx;
+	mtx.error_when_loading_from_file(IO::errNone);
+	mtx.loadFromCSVFile("path/to/a/file", 0, 0, Matrix<double, double>::optNone, fake_buffer);
+
+	BOOST_REQUIRE_EQUAL(mtx.height, 2);
+	BOOST_REQUIRE_EQUAL(mtx.width, 3);
+	BOOST_REQUIRE_EQUAL(mtx.entry[0][0], 1.5);
+	BOOST_REQUIRE_EQUAL(mtx.entry[1][0], -2.44);
+	BOOST_REQUIRE_EQUAL(mtx.entry[2][0], 3.67);
+	BOOST_REQUIRE_EQUAL(mtx.entry[0][1], 0);
+	BOOST_REQUIRE_EQUAL(mtx.entry[1][1], 8.56);
+	BOOST_REQUIRE_EQUAL(mtx.entry[2][1], -5.56);
+}
+
+// gp : tester lorsque le buffer contient size:nxm
+// gp : tester les histoires de jit : mise à jour du jit quand on charge la matrice
+//		Mais pour ça il faut mettre JIT::enabled à true
 
 BOOST_AUTO_TEST_CASE(test_1)
 {
-	
-	Matrix_mock_load_to_buffer<double, double> mtx(2, 2);
-
 	// Creating a buffer mocking the result of : IO::File::LoadFromFile(...)
-	mtx.fake_matrix_to_load(2, 2, { 1.5, -2.44444, 3.66666, 0 });
-	mtx.fake_mtx_to_be_loaded__set_precision(2);
-	mtx.fake_mtx_to_be_loaded__print_dimensions(false);
-	mtx.error_when_loading_fake_mtx(IO::errNone);
+	fake_fuffer_factory<double, double> buffer_factory;
+	Matrix_easy_to_fill<double, double> mtx_0(2, 2, { 1.5, -2.44444, 3.66666, 0 });
+	buffer_factory.matrix_to_build_buffer_with(&mtx_0);
+	buffer_factory.set_precision(2);
+	buffer_factory.print_dimensions(false);
 
-	Clob* buffer = new Clob;
-	mtx.loadFromCSVFile("path/to/a/file", 2, 2, Matrix<double, double>::optNone, buffer);
+	Clob* fake_buffer = buffer_factory.build_buffer();
+
+	// Testing load
+	Matrix_mock_load_to_buffer<double, double> mtx(2, 2);
+	mtx.error_when_loading_from_file(IO::errNone);
+	mtx.loadFromCSVFile("path/to/a/file", 2, 2, Matrix<double, double>::optNone, fake_buffer);
 
 	BOOST_REQUIRE_EQUAL(mtx.entry[0][0], 1.5);
 	BOOST_REQUIRE_EQUAL(mtx.entry[1][0], -2.44);
