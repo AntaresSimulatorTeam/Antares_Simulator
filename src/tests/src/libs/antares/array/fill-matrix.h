78b5adc4a4d93bd24addb115eff0e56755546155
Guillaume PIERRE <guillaume.pierre@rte-france.com>
2020-07-28 13:04:01 +0200

    Compilation on linux and CI : attempt 6
    Still the problem of inheriting a template class and how to use the parent class members in the child class in such a configuration.

--- src/tests/src/libs/antares/array/fill-matrix.h
+++ src/tests/src/libs/antares/array/fill-matrix.h
@@ -19,12 +19,12 @@ public:
 	Matrix_easy_to_fill(uint height, uint width, const vector<T>& vec) : Matrix<T, ReadWriteT>()
 	{
 		BOOST_REQUIRE_EQUAL(height * width, vec.size());
-		reset(width, height, true);
+		this->reset(width, height, true);
 		uint count = 0;
 		for (uint j = 0; j < height; j++)
 			for (uint i = 0; i < width; i++)
 			{
-				entry[i][j] = vec[count];
+				this->entry[i][j] = vec[count];
 				count++;
 			}
 	}
