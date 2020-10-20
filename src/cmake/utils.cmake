e5390c72f3fb3dd29c71a34bf261e5d3ca9d6318
KERLOCH Jean-Marie <jmkerloch@silicom.fr>
2020-10-20 12:27:16 +0200

    Add macro to copy dependency libs to target directory Add option in dependency build to avoid build if dependency found (update for sirius and ortools)

--- src/cmake/utils.cmake
+++ src/cmake/utils.cmake
@@ -97,3 +97,19 @@ function(build_git_dependency)
   message(STATUS "Building ${GIT_DEP_NAME}: ...DONE")
 endfunction()
 
+macro(copy_dependency deps target)
+
+    if( MSVC )
+        if("${CMAKE_BUILD_TYPE}" STREQUAL "release")
+
+            get_target_property( DEP_SHARED_LIB_PATH ${deps} IMPORTED_LOCATION_RELEASE )
+        else()
+
+            get_target_property( DEP_SHARED_LIB_PATH ${deps} IMPORTED_LOCATION_DEBUG )
+        endif()
+
+        # Copy the shared lib file
+        add_custom_command(TARGET ${target} POST_BUILD COMMAND ${CMAKE_COMMAND} -E copy ${DEP_SHARED_LIB_PATH} $<TARGET_FILE_DIR:${target}>)
+    endif()
+
+endmacro()
