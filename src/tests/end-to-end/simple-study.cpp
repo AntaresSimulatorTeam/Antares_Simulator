c7661c430b6b20736ed1ab83d3243183aa2a208f
KERLOCH Jean-Marie <jmkerloch@silicom.fr>
2020-11-05 14:05:12 +0100

    [skip ci] disable tests to check for error in CI

--- src/tests/end-to-end/simple-study.cpp
+++ src/tests/end-to-end/simple-study.cpp
@@ -203,6 +203,7 @@ void cleanSimulation(Study::Ptr pStudy, Solver::Simulation::ISimulation< Solver:
 //Very simple test with one area and one load and one year
 BOOST_AUTO_TEST_CASE(one_mc_year_one_ts)
 {
+    
 	//Create study
 	Study::Ptr pStudy = new Study(true); // for the solver
 
@@ -247,6 +248,7 @@ BOOST_AUTO_TEST_CASE(one_mc_year_one_ts)
 //Very simple test with one area and one load and two year
 BOOST_AUTO_TEST_CASE(two_mc_year_one_ts)
 {
+    /*
 	//Create study
 	Study::Ptr pStudy = new Study(true); // for the solver
 
@@ -286,12 +288,14 @@ BOOST_AUTO_TEST_CASE(two_mc_year_one_ts)
 
 	//Clean simulation
 	cleanSimulation(pStudy, simulation);
+    */
 }
 
 
 //Very simple test with one area and one load and two year and two identical TS
 BOOST_AUTO_TEST_CASE(two_mc_year_two_ts_identical)
 {
+    /*
 	//Create study
 	Study::Ptr pStudy = new Study(true); // for the solver
 
@@ -333,6 +337,7 @@ BOOST_AUTO_TEST_CASE(two_mc_year_two_ts_identical)
 
 	//Clean simulation
 	cleanSimulation(pStudy, simulation);
+    */
 }
 
 
@@ -340,6 +345,7 @@ BOOST_AUTO_TEST_CASE(two_mc_year_two_ts_identical)
 //Very simple test with one area and one load and two year and two TS with different load
 BOOST_AUTO_TEST_CASE(two_mc_year_two_ts)
 {
+    /*
 	//Create study
 	Study::Ptr pStudy = new Study(true); // for the solver
 
@@ -394,6 +400,7 @@ BOOST_AUTO_TEST_CASE(two_mc_year_two_ts)
 
 	//Clean simulation
 	cleanSimulation(pStudy, simulation);
+    */
 }
 
 
@@ -401,6 +408,7 @@ BOOST_AUTO_TEST_CASE(two_mc_year_two_ts)
 //Very simple test with one area and one load and two year with different load and weight
 BOOST_AUTO_TEST_CASE(two_mc_year_two_ts_different_weight)
 {
+    /*
 	//Create study
 	Study::Ptr pStudy = new Study(true); // for the solver
 
@@ -473,6 +481,8 @@ BOOST_AUTO_TEST_CASE(two_mc_year_two_ts_different_weight)
 
 	//Clean simulation
 	cleanSimulation(pStudy, simulation);
+    
+    */
 }
 
 BOOST_AUTO_TEST_SUITE_END()
