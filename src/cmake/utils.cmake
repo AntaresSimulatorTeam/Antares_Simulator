69ed06cf5a29827b73bf1ca040cffec12fe65629
jmkerloch <jmkerloch@silicom.fr>
2020-12-07 15:43:25 +0100

    add libsirius_solver.so copy also on linux

--- src/cmake/utils.cmake
+++ src/cmake/utils.cmake
@@ -99,7 +99,6 @@ endfunction()
 
 macro(copy_dependency deps target)
 
-    if( MSVC )
         if("${CMAKE_BUILD_TYPE}" STREQUAL "release")
 
             get_target_property( DEP_SHARED_LIB_PATH ${deps} IMPORTED_LOCATION_RELEASE )
@@ -110,6 +109,6 @@ macro(copy_dependency deps target)
 
         # Copy the shared lib file
         add_custom_command(TARGET ${target} POST_BUILD COMMAND ${CMAKE_COMMAND} -E copy ${DEP_SHARED_LIB_PATH} $<TARGET_FILE_DIR:${target}>)
-    endif()
 
 endmacro()
+
