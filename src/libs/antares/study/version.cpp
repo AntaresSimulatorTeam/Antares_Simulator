ab95f7834d7dae331aa326874c845ef2789ede9f
Guillaume PIERRE <guillaume.pierre@rte-france.com>
2020-12-10 08:27:17 +0100

    [skip ci] : update version number

--- src/libs/antares/study/version.cpp
+++ src/libs/antares/study/version.cpp
@@ -112,7 +112,8 @@ namespace Data
 		// The list should remain ordered in the reverse order for performance reasons
 		switch (v)
 		{
-			case versionFutur:    return ">7.2";
+			case versionFutur:    return ">8.0";
+			case version800:      return "8.0";
 			case version720:      return "7.2";
 			case version710:      return "7.1";
 			case version700:      return "7.0";
@@ -156,7 +157,8 @@ namespace Data
 		// The list should remain ordered in the reverse order for performance reasons
 		switch (v)
 		{
-			case versionFutur:		return L">7.2";
+			case versionFutur:		return L">8.0";
+			case version800:		return L"8.0";
 			case version720:		return L"7.2";
 			case version710:		return L"7.1";
 			case version700:		return L"7.0";
@@ -200,6 +202,7 @@ namespace Data
 		// The list should remain ordered in the reverse order for performance reasons
 		switch (version)
 		{
+			case 800: return version800;
 			case 720: return version720;
 			case 710: return version710;
 			case 700: return version700;
