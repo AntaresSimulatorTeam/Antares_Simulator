6607171a7d7bf08625800a48ca96f13f0b015533
KERLOCH Jean-Marie <jmkerloch@silicom.fr>
2021-01-13 14:38:30 +0100

    Change pytest command line to be like antares-solver

--- src/tests/examples/conftest.py
+++ src/tests/examples/conftest.py
@@ -1,7 +1,7 @@
 import pytest
 
 def pytest_addoption(parser):
-    parser.addoption("--use-ortools", action="store", default="False")
+    parser.addoption("--use-ortools", action="store_true", default=False)
     parser.addoption("--ortools-solver", action="store", default="sirius")
     parser.addoption("--solver-path", action="store")
 
@@ -11,7 +11,7 @@ def ortools_solver(request):
 
 @pytest.fixture()
 def use_ortools(request):
-    return request.config.getoption("--use-ortools")=="True"
+    return request.config.getoption("--use-ortools")
     
 @pytest.fixture()
 def solver_path(request):
