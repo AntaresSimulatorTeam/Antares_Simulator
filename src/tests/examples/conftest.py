637b14b0059821d06763208f3099bd4ddca228bd
KERLOCH Jean-Marie <jmkerloch@silicom.fr>
2021-01-13 13:19:06 +0100

    Remove unused pytest option

--- src/tests/examples/conftest.py
+++ src/tests/examples/conftest.py
@@ -4,7 +4,6 @@ def pytest_addoption(parser):
     parser.addoption("--use-ortools", action="store", default="False")
     parser.addoption("--ortools-solver", action="store", default="sirius")
     parser.addoption("--solver-path", action="store")
-    parser.addoption("--study-path", action="store")
 
 @pytest.fixture()
 def ortools_solver(request):
@@ -17,8 +16,4 @@ def use_ortools(request):
 @pytest.fixture()
 def solver_path(request):
     return request.config.getoption("--solver-path")
-    
-    
-@pytest.fixture()
-def study_path(request):
-    return request.config.getoption("--study-path")
+
