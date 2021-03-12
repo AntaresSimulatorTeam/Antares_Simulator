c9aeb2d5028619886bd11802744e781fb6e4daa7
Florian Omnes <florian@omnes.cc>
2021-03-12 15:51:18 +0100

    Use BOOST_CHECK instead of BOOST_TEST
    for compatibility reasons

--- src/tests/end-to-end/simple-study.cpp
+++ src/tests/end-to-end/simple-study.cpp
@@ -42,9 +42,9 @@ void checkVariable(
 	/*Get value*/
 	typename Antares::Solver::Variable::Storage<VCard>::ResultsType* result = nullptr;
 	simulation->variables.retrieveResultsForArea<VCard>(&result, pArea);
-	BOOST_TEST(std::fabs(result->avgdata.hourly[0] - expectedHourlyValue) < 0.001);
-	BOOST_TEST(std::fabs(result->avgdata.daily[0] - expectedHourlyValue * 24) < 0.001);
-	BOOST_TEST(std::fabs(result->avgdata.weekly[0] - expectedHourlyValue * 24 * 7) <0.001);
+	BOOST_CHECK(std::fabs(result->avgdata.hourly[0] - expectedHourlyValue) < 0.001);
+	BOOST_CHECK(std::fabs(result->avgdata.daily[0] - expectedHourlyValue * 24) < 0.001);
+	BOOST_CHECK(std::fabs(result->avgdata.weekly[0] - expectedHourlyValue * 24 * 7) <0.001);
 }
 
 void prepareStudy(Study::Ptr pStudy, int nbYears)
