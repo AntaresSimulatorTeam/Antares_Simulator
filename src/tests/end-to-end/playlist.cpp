06710434c629a6224a425ad3dc50678cdfdcb0cf
Florian Omnes <florian@omnes.cc>
2021-03-12 18:05:09 +0100

    Add write tests vor v810

--- src/tests/end-to-end/playlist.cpp
+++ src/tests/end-to-end/playlist.cpp
@@ -7,6 +7,7 @@
 
 #include <iostream>
 #include <fstream>
+#include <streambuf>
 
 /* GCC */
 #ifdef __GNUC__
@@ -34,15 +35,29 @@ namespace fs = std::filesystem;
 class TemporaryFile
 {
 private:
-    fs::path mPath;
+    const fs::path mPath;
 
 public:
-    TemporaryFile(std::string content)
+    TemporaryFile() : mPath(fs::temp_directory_path() / "generaldata.ini")
     {
-        mPath = fs::temp_directory_path() / "generaldata.ini";
+    }
+
+    void write(const std::string& content)
+    {
         std::ofstream ofs(mPath);
         // dump content into temporary file
         ofs << content;
+    }
+
+    std::string read()
+    {
+        std::ifstream in(mPath);
+        if (in)
+        {
+            return (
+              std::string((std::istreambuf_iterator<char>(in)), std::istreambuf_iterator<char>()));
+        }
+        return "error";
     }
 
     ~TemporaryFile()
@@ -56,11 +71,16 @@ public:
     }
 };
 
+namespace {
+  const std::size_t& npos = std::string::npos;
+}
+
 TEST_CASE("Read v800 active years", "[v800]")
 {
     using namespace Antares::Data;
     Parameters p;
-    TemporaryFile ini_file(
+    TemporaryFile ini_file;
+    ini_file.write(
       R"([general]
 mode = Adequacy
 horizon = 2000
@@ -104,7 +124,8 @@ TEST_CASE("Read v810 active years", "[v810]")
 {
     using namespace Antares::Data;
     Parameters p;
-    TemporaryFile ini_file(
+    TemporaryFile ini_file;
+    ini_file.write(
       R"([general]
 mode = Adequacy
 horizon = 2000
@@ -148,8 +169,8 @@ TEST_CASE("Read v810 weights", "[v810]")
 {
     using namespace Antares::Data;
     Parameters p;
-    TemporaryFile ini_file(
-      R"([general]
+    TemporaryFile ini_file;
+    ini_file.write(R"([general]
 mode = Adequacy
 horizon = 2000
 nbyears = 5
@@ -185,4 +206,51 @@ playlist_weight_year_1=3.3
     size_t idx = 0;
     for (float w : {1.1f, 3.3f, 1.f, 1.f, 2.f})
         CHECK(p.yearsWeight[idx++] == Approx(w).margin(1e-6));
+}
+
+TEST_CASE("Write v810 active years", "[v810]")
+{
+    using namespace Antares::Data;
+    Parameters p;
+    p.nbYears = 5;
+    p.yearsFilter = {true, true, false, true, true};
+    p.yearsWeight = std::vector<float>(5, 1.f);
+    p.userPlaylist = true;
+    p.mode = stdmEconomy;
+
+    p.variablesPrintInfo.add(new VariablePrintInfo("name", 0, 0, 0));
+    p.include.unfeasibleProblemBehavior = UnfeasibleProblemBehavior::ERROR_MPS;
+
+    TemporaryFile ini_file;
+    p.saveToFile(ini_file.path(), 810);
+    const std::string content = ini_file.read();
+    // I/O error
+    REQUIRE(content != "error");
+
+    REQUIRE(content.find("playlist_active_years = 0,1,3,4") != ::npos);
+    // All weights are 1, they should not be written
+    REQUIRE(content.find("playlist_weight_year") == ::npos);
+}
+
+TEST_CASE("Write v810 weights", "[v810]")
+{
+    using namespace Antares::Data;
+    Parameters p;
+    p.nbYears = 5;
+    p.yearsFilter = std::vector<bool>(5);
+    p.yearsWeight = std::vector<float>{1.f, 2.f, 3.f, 4.f, 0.f};
+    p.userPlaylist = true;
+    p.mode = stdmEconomy;
+    p.variablesPrintInfo.add(new VariablePrintInfo("name", 0, 0, 0));
+    p.include.unfeasibleProblemBehavior = UnfeasibleProblemBehavior::ERROR_MPS;
+
+    TemporaryFile ini_file;
+    p.saveToFile(ini_file.path(), 810);
+    const std::string content = ini_file.read();
+    // I/O error
+    REQUIRE(content != "error");
+    // Weight 0 is 1.f, it should not be written
+    REQUIRE(content.find("playlist_weight_year_0") == ::npos);
+    // Weight 1 is 2.f, it should be written
+    REQUIRE(content.find("playlist_weight_year_1") != ::npos);
 }
