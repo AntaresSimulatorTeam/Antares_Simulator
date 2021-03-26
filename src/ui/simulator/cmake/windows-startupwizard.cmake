fded277c4650c7bb61438c85fdf5dc6de5ffd7db
Guillaume PIERRE <guillaume.pierre@rte-france.com>
2021-03-26 14:47:58 +0100

    Remove license - GUI : removing proxy connection window

--- src/ui/simulator/cmake/windows-startupwizard.cmake
+++ src/ui/simulator/cmake/windows-startupwizard.cmake
@@ -3,8 +3,6 @@
 set(SRC_UI_WINDOWS_STARTUP_WIZARD
 	windows/startupwizard.h
 	windows/startupwizard.cpp
-	windows/proxy/proxysetup.cpp
-	windows/proxy/proxysetup.h
 )
 add_Library(libantares-ui-windows-startupwizard ${SRC_UI_WINDOWS_STARTUP_WIZARD})
 
