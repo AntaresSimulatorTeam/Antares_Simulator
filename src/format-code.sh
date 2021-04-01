f9e5bb62fb2a2e95b866460aca7fd6306e0efff7
Florian Omnes <florian@omnes.cc>
2021-04-01 14:31:51 +0200

    Update format-code to exclude generator files

--- src/format-code.sh
+++ src/format-code.sh
@@ -1,16 +1,17 @@
 #!/bin/bash
 
 SOURCE_DIRS="analyzer/ ext/ internet/ libs/ solver/ tools/ ui/"
+SOURCE_FILES=$(find $SOURCE_DIRS -type f \( -name "*.h" -or -name "*.cpp" -or -name "*.h" -or -name "*.hxx" -or -name "*.hpp" -or -name "*.cxx" -or -name "*.c" \) -and -not \( -iname "sqlite3.c" -or -name "*.h.generator.hpp" -or -name "*.hxx.generator.hpp" -or -name "*.generator.commonstypes.hpp" \))
 
 # Remove ^M, etc.
 if ! [ -x "$(command -v dos2unix)" ]; then
     echo 'Warning: dos2unix is not installed. Skipping' >&2
 else
-    find $SOURCE_DIRS -type f \( -name "*.h" -or -name "*.cpp" -or -name "*.h" -or -name "*.hxx" -or -name "*.hpp" -or -name "*.cxx" -or -name "*.c" ! -iname "sqlite3.c" \) | xargs dos2unix
+    echo "$SOURCE_FILES" | xargs dos2unix
 fi
 
 if ! [ -x "$(command -v clang-format-10)" ]; then
     echo 'Warning: clang-format-10 is not installed. Skipping' >&2
 else
-    find $SOURCE_DIRS -type f \( -name "*.h" -or -name "*.cpp" -or -name "*.h" -or -name "*.hxx" -or -name "*.hpp" -or -name "*.cxx" -or -name "*.c" ! -iname "sqlite3.c" \) | xargs clang-format-10 -i --verbose
+    echo "$SOURCE_FILES" | xargs clang-format-10 -i --verbose
 fi
