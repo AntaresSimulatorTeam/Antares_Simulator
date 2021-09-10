097e3501b772b69734eb551b81af2c93cb90dfd4
Florian OMNES <florian.omnes@rte-france.com>
2021-09-10 14:46:17 +0200

    Fix encoding

--- src/solver/simulation/solver.utils.h
+++ src/solver/simulation/solver.utils.h
@@ -44,33 +44,33 @@ namespace Simulation
 {
 struct setOfParallelYears
 {
-    // Un lot d'annï¿½e ï¿½ exï¿½cuter en parallï¿½le.
-    // En fonction d'une ï¿½ventuelle play-list, certaines seront jouï¿½es et d'autres non.
+    // Un lot d'année à exécuter en parallèle.
+    // En fonction d'une éventuelle play-list, certaines seront jouées et d'autres non.
 
 public:
-    // Numeros des annees en parallele pour ce lot (certaines ne seront pas jouï¿½es en cas de
-    // play-list "trouï¿½e")
+    // Numeros des annees en parallele pour ce lot (certaines ne seront pas jouées en cas de
+    // play-list "trouée")
     std::vector<unsigned int> yearsIndices;
 
-    // Une annee doit-elle ï¿½tre rejouï¿½e ?
+    // Une annee doit-elle être rejouée ?
     std::map<uint, bool> yearFailed;
 
-    // Associe le numero d'une annï¿½e jouï¿½e ï¿½ l'indice de l'espace
+    // Associe le numero d'une année jouée à l'indice de l'espace
     std::map<unsigned int, unsigned int> performedYearToSpace;
 
-    // L'inverse : pour une annï¿½e jouï¿½e, associe l'indice de l'espace au numero de l'annï¿½e
+    // L'inverse : pour une année jouée, associe l'indice de l'espace au numero de l'année
     std::map<unsigned int, unsigned int> spaceToPerformedYear;
 
-    // Pour chaque annï¿½e, est-elle la premiï¿½re ï¿½ devoir ï¿½tre jouï¿½e dans son lot d'annï¿½es ?
+    // Pour chaque année, est-elle la première à devoir être jouée dans son lot d'années ?
     std::map<unsigned int, bool> isFirstPerformedYearOfASet;
 
-    // Pour chaque annï¿½e du lot, est-elle jouï¿½e ou non ?
+    // Pour chaque année du lot, est-elle jouée ou non ?
     std::map<unsigned int, bool> isYearPerformed;
 
-    // Nbre d'annï¿½es en parallele vraiment jouï¿½es pour ce lot
+    // Nbre d'années en parallele vraiment jouées pour ce lot
     unsigned int nbPerformedYears;
 
-    // Nbre d'annï¿½es en parallele jouï¿½es ou non pour ce lot
+    // Nbre d'années en parallele jouées ou non pour ce lot
     unsigned int nbYears;
 
     // Regenere-t-on des times series avant de jouer les annees du lot courant
