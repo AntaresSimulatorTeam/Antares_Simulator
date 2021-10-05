ad716450be43ee607faaf5de2e495fb8b3329986
Andrea Sgattoni <andrea.sgattoni@gmail.com>
2021-10-05 17:37:20 +0200

    feature: test pdf build

--- docs/pdf-doc-generation-with-sphinx/source/conf.py
+++ docs/pdf-doc-generation-with-sphinx/source/conf.py
@@ -28,6 +28,11 @@ author = 'RTE'
 # extensions coming with Sphinx (named 'sphinx.ext.*') or your custom
 # ones.
 extensions = ['myst_parser']
+myst_enable_extensions = [
+    "amsmath",
+    "dollarmath",
+]
+
 
 # Add any paths that contain templates here, relative to this directory.
 templates_path = ['_templates']
@@ -51,4 +56,4 @@ html_sidebars = {
 # Add any paths that contain custom static files (such as style sheets) here,
 # relative to this directory. They are copied after the builtin static files,
 # so a file named "default.css" will overwrite the builtin "default.css".
-html_static_path = ['_static']
+# html_static_path = ['_static']
