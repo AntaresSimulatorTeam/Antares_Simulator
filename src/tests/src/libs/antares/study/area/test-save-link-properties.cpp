1f1b5874745e197c42d617ea4f1431f9c484d87d
Guillaume PIERRE <guillaume.pierre@rte-france.com>
2021-10-14 10:51:23 +0200

    Test links property save : first commit (do not compile)

---
+++ src/tests/src/libs/antares/study/area/test-save-link-properties.cpp
@@ -0,0 +1,15 @@
+#define BOOST_TEST_MODULE test save link properties.ini
+
+#define WIN32_LEAN_AND_MEAN
+
+#include <boost/test/included/unit_test.hpp>
+
+#include <area.h>
+
+using namespace Antares::Data;
+
+BOOST_AUTO_TEST_CASE(my_first_test)
+{
+	Area area;
+
+}
