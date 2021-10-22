60866b9259a7264af241b78987acced106f220cd
Florian OMNES <florian.omnes@rte-france.com>
2021-10-22 15:58:53 +0200

    Replace lambda function by static function

--- src/solver/infeasible-diag/report.cpp
+++ src/solver/infeasible-diag/report.cpp
@@ -2,22 +2,21 @@
 #include <antares/logs.h>
 #include <algorithm>
 
-void InfeasibleProblemReport::append(const std::string& constraintName,
-                                double value) {
+void InfeasibleProblemReport::append(const std::string& constraintName, double value)
+{
     // TODO check prior existence of constraintName
     mConstraints.push_back({constraintName, value});
 }
 
-void InfeasibleProblemReport::printLargest(int n)
+static bool compareConstraints(const std::pair<std::string, double>& a,
+                               const std::pair<std::string, double>& b)
 {
-  std::sort(std::begin(mConstraints),
-            std::end(mConstraints),
-            [](const std::pair<std::string, double>& a,
-               const std::pair<std::string, double>& b)
-            {
-              return a.second > b.second;
-            });
-  for (int ii = 0; ii < n; ++ii)
-    Antares::logs.info() << mConstraints[ii].first << ": " << mConstraints[ii].second;
+    return a.second > b.second;
 }
 
+void InfeasibleProblemReport::printLargest(int n)
+{
+    std::sort(std::begin(mConstraints), std::end(mConstraints), compareConstraints);
+    for (int ii = 0; ii < n; ++ii)
+        Antares::logs.info() << mConstraints[ii].first << ": " << mConstraints[ii].second;
+}
