455dfab743fd1ebf0ed5c4b629215f79ba0280a3
Florian Omnès <florian.omnes@rte-france.com>
2021-11-02 16:36:37 +0100

    Fix build on CentOS 7 (#435)
    * Fix build on CentOS 7
    
    * Fix previous commit
    
    * Fix previous commit (bis)
    
    * Fix previous commit (ter)
    
    * Fix previous commit (4)

--- src/tests/src/libs/antares/study/area/test-save-area-optimization-ini.cpp
+++ src/tests/src/libs/antares/study/area/test-save-area-optimization-ini.cpp
@@ -3,7 +3,6 @@
 #define WIN32_LEAN_AND_MEAN
 
 #include <boost/test/included/unit_test.hpp>
-#include <filesystem>
 #include <string>
 
 #include <study.h>
@@ -13,7 +12,6 @@
 #include "files-helper.h"
 
 using namespace Antares::Data;
-namespace fs = std::filesystem;
 
 const string generatedIniFileName = "optimization.ini";
 const string referenceIniFileName = "optimization-reference.ini";
