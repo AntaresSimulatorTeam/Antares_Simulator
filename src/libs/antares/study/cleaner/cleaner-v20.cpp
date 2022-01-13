8c5dd6cb2c2253b6bf4c8f4a1d80984f359e04d0
Florian OMNES <florian.omnes@rte-france.com>
2022-01-13 14:01:28 +0100

    Fix code smell

--- src/libs/antares/study/cleaner/cleaner-v20.cpp
+++ src/libs/antares/study/cleaner/cleaner-v20.cpp
@@ -232,7 +232,7 @@ bool PreflightVersion20(StudyCleaningInfos* infos)
     e.add("input/thermal/areas.ini");
 
     // Also exclude custom files/folders provided by the user
-    infos->customExclude.words(":", [&e](AnyString& word) {
+    infos->customExclude.words(":", [&e](const AnyString& word) {
         e.add(word);
         return true;
     });
