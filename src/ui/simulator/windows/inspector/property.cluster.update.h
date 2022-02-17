86b838664b93e90556bc39493249522a99c18fc2
Florian OMNES <florian.omnes@rte-france.com>
2022-02-17 14:27:49 +0100

    Add 'explicit' keyword

--- src/ui/simulator/windows/inspector/property.cluster.update.h
+++ src/ui/simulator/windows/inspector/property.cluster.update.h
@@ -12,7 +12,7 @@ namespace Inspector
 class ClusterUpdater
 {
 public:
-    ClusterUpdater(Frame& frame);
+    explicit ClusterUpdater(Frame& frame);
     bool changeName(const wxVariant& value);
     bool changeGroup(const wxVariant& value);
     bool changeUnit(const wxVariant& value);
