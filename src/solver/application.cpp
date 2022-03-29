14a2a35146075d911607e521f57d68495bee5dbb
Guillaume PIERRE <guillaume.pierre@rte-france.com>
2022-03-29 18:17:05 +0200

    Antares solver options : fix a bug on Windows

--- src/solver/application.cpp
+++ src/solver/application.cpp
@@ -176,8 +176,8 @@ void Application::prepare(int argc, char* argv[])
     auto parser = CreateParser(pSettings, options);
 
     // Parse the command line arguments
-    if (!parser(argc, argv))
-        throw Error::CommandLineArguments(parser.errors());
+    if (!parser->operator()(argc, argv))
+        throw Error::CommandLineArguments(parser->errors());
 
     if (options.displayVersion)
     {
