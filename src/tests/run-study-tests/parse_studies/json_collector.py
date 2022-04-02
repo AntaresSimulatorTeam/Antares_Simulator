24774e70bf10defe6351fbdc990e24a203ad6b9b
Guillaume PIERRE <guillaume.pierre@rte-france.com>
2022-04-02 20:13:37 +0200

    [skip ci] read tests from json : handling errors when retreaving data from the json file

--- src/tests/run-study-tests/parse_studies/json_collector.py
+++ src/tests/run-study-tests/parse_studies/json_collector.py
@@ -1,43 +1,99 @@
 import json
-from jsonschema import validate
+from jsonschema import validate, Draft7Validator
+from pathlib import Path
 import os.path
 
-class checksJsonReader:
+class error:
+    def __init__(self, type, items = []):
+        self.type = type
+        self.items = items
+
+    def print(self):
+        print("  Error type : %s" % self.type)
+        for item in self.items:
+            print("    ==> %s" % item)
+
+
+
+class checksRetriever:
     def __init__(self, study_path):
         self.study_path = study_path
+
         self.json_file = study_path / "check-config.json"
+        self.json_file_content = None
+
+        self.json_schema_file = "./parse_studies/json_schema.json"
+        self.json_schema_content = None
 
         self.test_pairs = []
         self.test_ids = []
+        self.errors = []
 
-    def json_file_exists(self):
-        return os.path.isfile(self.json_file)
+    def run(self):
+        if not self.json_file_exists():
+            return
 
-    def read(self):
-        with open(self.json_file, "r") as json_file:
-            json_file_content = json.load(json_file)
+        if not self.read_json_file():
+            return
 
-        with open("./parse_studies/json_schema.json", "r") as file:
-            json_schema = json.load(file)
+        if not self.read_json_schema():
+            return
 
-        validate_json(json_file_content, json_schema)
+        if not self.validate_json():
+            return
 
-        for test in json_file_content:
+        for test in self.json_file_content:
             self.test_ids.append(test["name"])
             self.test_pairs.append((self.study_path, test["checks"]))
 
+    def json_file_exists(self) -> bool:
+        if not os.path.isfile(self.json_file):
+            self.errors.append(error("json file does not exist"))
+            return False
+        return True
+
+    def read_json_file(self) -> bool:
+        try:
+            with open(self.json_file, "r") as json_file:
+                self.json_file_content = json.load(json_file)
+        except json.decoder.JSONDecodeError as err:
+            self.errors.append(error("json file ill formed", [err]))
+            return False
+        return True
+
+    def read_json_schema(self) -> bool:
+        try:
+            with open(self.json_schema_file, "r") as file:
+                self.json_schema_content = json.load(file)
+        except json.decoder.JSONDecodeError as err:
+            self.errors.append(error("json schema file ill formed", [err]))
+            return False
+        return True
+
+    def validate_json(self) -> bool:
+        v = Draft7Validator(self.json_schema_content)
+        errors = [err.message for err in sorted(v.iter_errors(self.json_file_content), key=str)]
+        if len(errors) > 0:
+            self.errors.append(error("json file not valid under schema", errors))
+            return False
+        return True
+
+    def has_errors(self) -> bool:
+        if len(self.errors) > 0:
+            return True
+        return False
+
+    def print_errors(self):
+        print("\n---")
+        print("Skipping checks on : '%s', because :" % os.path.basename(self.study_path))
+        for err in self.errors:
+            err.print()
+
     def get_ids(self):
         return self.test_ids
+
     def get_checks(self):
         return self.test_pairs
-
-
-def validate_json(json_file_content, json_schema):
-    try:
-        validate(json_file_content, json_schema)
-    except Exception as valid_err:
-        print("Validation KO: {}".format(valid_err))
-        raise valid_err
 
 
 class jsonCollector:
@@ -48,11 +104,13 @@ class jsonCollector:
 
     def collect(self):
         for study_path in self.study_paths_:
-            checks_json_reader = checksJsonReader(study_path)
-            if checks_json_reader.json_file_exists():
-                checks_json_reader.read()
-                self.pairs_.extend(checks_json_reader.get_checks())
-                self.test_ids_.extend(checks_json_reader.get_ids())
+            checks_retriever = checksRetriever(study_path)
+            checks_retriever.run()
+            if checks_retriever.has_errors():
+                checks_retriever.print_errors()
+            else:
+                self.pairs_.extend(checks_retriever.get_checks())
+                self.test_ids_.extend(checks_retriever.get_ids())
 
     def pairs(self):
         return self.pairs_
@@ -61,16 +119,14 @@ class jsonCollector:
         return self.test_ids_
 
 
-"""
+
 # ================
 # For validation
 # ================
 ROOT_FOLDER = Path('../../resources/study-collection-sample').resolve()
+# print("ROOT_FOLDER : %s" % ROOT_FOLDER)
 
-study_paths = [ROOT_FOLDER / "short-tests" / "001 One node - passive"]
-json_collector = jsonCollector(study_paths)
-json_collector.collect()
-
-print("pairs :", json_collector.pairs())
-print("testIds :", json_collector.testIds())
-"""
+if __name__ == "__main__":
+    study_paths = [ROOT_FOLDER / "short-tests" / "hydro initialization 1"]
+    json_collector = jsonCollector(study_paths)
+    json_collector.collect()
