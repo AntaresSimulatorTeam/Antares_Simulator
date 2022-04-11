94c102540c54da6384665b8a3973f0488f405e32
Florian OMNES <florian.omnes@rte-france.com>
2022-04-11 17:37:18 +0200

    First try : ubuntu-system.yml

--- src/tests/run-study-tests/conftest.py
+++ src/tests/run-study-tests/conftest.py
@@ -4,6 +4,7 @@ def pytest_addoption(parser):
     parser.addoption("--use-ortools", action="store_true", default=False)
     parser.addoption("--ortools-solver", action="store", default="sirius")
     parser.addoption("--solver-path", action="store")
+    parser.addoption("--batch-path", action="store")
 
 @pytest.fixture()
 def ortools_solver(request):
@@ -17,3 +18,6 @@ def use_ortools(request):
 def solver_path(request):
     return request.config.getoption("--solver-path")
 
+@pytest.fixture()
+def study_batch_path(request):
+    return request.config.getoption("--batch-path")
--- src/tests/run-study-tests/read_json_and_test_studies.py
+++ src/tests/run-study-tests/read_json_and_test_studies.py
@@ -4,7 +4,7 @@ from parse_studies.json_collector import jsonCollector
 from check_on_results.create_checks import create_checks
 
 
-ROOT_FOLDER = Path('../resources/study-collection-sample').resolve()
+ROOT_FOLDER = Path('../resources').resolve()
 
 study_paths = look_for_studies(ROOT_FOLDER)
 json_collector = jsonCollector(study_paths)
