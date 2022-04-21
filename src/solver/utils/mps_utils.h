4c46353c6032a5a740490020eedd77508111ac06
Florian OMNES <florian.omnes@rte-france.com>
2022-04-21 16:56:01 +0200

    Place MPS writers in mps_utils.{h,cpp}

---
+++ src/solver/utils/mps_utils.h
@@ -0,0 +1,6 @@
+#pragma once
+
+void OPT_dump_spx_fixed_part(const PROBLEME_SIMPLEXE* Pb, uint numSpace);
+void OPT_dump_spx_variable_part(const PROBLEME_SIMPLEXE* Pb, uint numSpace);
+
+void OPT_EcrireJeuDeDonneesLineaireAuFormatMPS(void*, uint);
