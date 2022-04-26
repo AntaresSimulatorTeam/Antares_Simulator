b2e36cc741384c1389f721c999679c702dae8110
Florian OMNES <florian.omnes@rte-france.com>
2022-04-26 13:23:51 +0200

    Add json tests, change path
    TODO : ROOT_FOLDER shoud be provided from the command-line as a parameter.

--- src/tests/run-study-tests/test_from_json.py
+++ src/tests/run-study-tests/test_from_json.py
@@ -4,12 +4,13 @@ from parse_studies.json_collector import jsonCollector
 from check_on_results.create_checks import create_checks
 
 
-ROOT_FOLDER = Path('../resources').resolve()
+ROOT_FOLDER = Path('../resources/short-tests').resolve()
 
 study_paths = look_for_studies(ROOT_FOLDER)
 json_collector = jsonCollector(study_paths)
 json_collector.collect()
 
+@pytest.mark.json
 @pytest.mark.parametrize('study_path,test_check_data', json_collector.pairs(), ids=json_collector.testIds())
 def test(study_path, test_check_data, check_runner):
     checks = create_checks(study_path, test_check_data, simulation=check_runner.get_simulation())
