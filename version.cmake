fb9a589580cb8cabfa926d28ffecd0bd2ed94fdf
Florian OMNES <florian.omnes@rte-france.com>
2022-06-03 16:09:02 +0200

    Move version to root file

---
+++ version.cmake
@@ -0,0 +1,13 @@
+# Version
+set(ANTARES_VERSION_HI	      8)
+set(ANTARES_VERSION_LO	      2)
+set(ANTARES_VERSION_REVISION  2)
+set(ANTARES_VERSION_YEAR      2022)
+
+set(ANTARES_PUBLISHER "RTE France")
+set(ANTARES_WEBSITE "https://antares.rte-france.com")
+set(ANTARES_ONLINE_DOC "https://antares-simulator.readthedocs.io/")
+
+# Beta release
+set(ANTARES_BETA 0)
+set(ANTARES_RC 0)
