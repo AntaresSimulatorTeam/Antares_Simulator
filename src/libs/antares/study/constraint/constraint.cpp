f0c2c1570482152c58fa8337237f094515c446b7
Florian OMNES <florian.omnes@rte-france.com>
2022-06-27 13:55:07 +0200

    Pass by const ref

--- src/libs/antares/study/constraint/constraint.cpp
+++ src/libs/antares/study/constraint/constraint.cpp
@@ -222,11 +222,10 @@ void BindingConstraint::removeAllOffsets()
 
 uint Antares::Data::BindingConstraint::enabledClusterCount() const
 {
-  return std::count_if(pClusterWeights.begin(),
-                       pClusterWeights.end(),
-                       [](const clusterWeightMap::value_type i) {
-                       return i.first->enabled && !i.first->mustrun;
-                       });
+    return std::count_if(
+      pClusterWeights.begin(), pClusterWeights.end(), [](const clusterWeightMap::value_type& i) {
+          return i.first->enabled && !i.first->mustrun;
+      });
 }
 
 bool BindingConstraint::removeLink(const AreaLink* lnk)
