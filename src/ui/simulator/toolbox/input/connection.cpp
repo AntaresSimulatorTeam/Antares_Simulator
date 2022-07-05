7bf13df62f110d05d8c4ab4baec227de2dc6baad
Florian OMNES <florian.omnes@rte-france.com>
2022-07-05 12:16:23 +0200

    [GUI] Replace "Flat" view with "By area" view

--- src/ui/simulator/toolbox/input/connection.cpp
+++ src/ui/simulator/toolbox/input/connection.cpp
@@ -173,6 +173,121 @@ void Connections::internalBuildSubControls()
     sizer->Layout();
 }
 
+static void addUpstreamDownstream(wxTreeItemId& rootId,
+                                  wxTreeCtrl* listbox,
+                                  const Data::AreaList& areas,
+                                  size_t layerID)
+{
+    auto localRootId = listbox->AppendItem(rootId, wxString(wxT("Upstream / Downstream")), 2);
+    listbox->SetItemBold(localRootId, true);
+    {
+        for (const auto& namedArea : areas)
+        {
+            Data::Area* area = namedArea.second;
+            if (area->isVisibleOnLayer(layerID))
+            {
+                wxTreeItemId id;
+                // Foreach Interconnection for the area
+                const Data::AreaLink::Map::iterator end = area->links.end();
+                for (Data::AreaLink::Map::iterator i = area->links.begin(); i != end; ++i)
+                {
+                    Data::AreaLink* lnk = i->second;
+
+                    if (lnk->isVisibleOnLayer(layerID))
+                    {
+                        if (!id)
+                        {
+                            // We have to create the item corresponding to the area
+                            id = listbox->AppendItem(
+                              localRootId,
+                              wxString() << wxT(' ') << wxStringFromUTF8(area->name) << wxT(' '),
+                              1,
+                              1);
+                            listbox->SetItemBold(id, true);
+                        }
+                        // Adding the item for the interconnection
+                        /* wxTreeItemId itemID = */ listbox->AppendItem(
+                          id, /*parent*/
+                          // caption
+                          wxString() << wxT(' ') << wxStringFromUTF8(lnk->with->name) << wxT(' '),
+                          0,
+                          0,
+                          new TreeLeaf(lnk));
+                    }
+                }
+            }
+        }
+    }
+}
+
+static void addByArea(wxTreeItemId& rootId,
+                      wxTreeCtrl* listbox,
+                      const Data::AreaList& areas,
+                      size_t layerID)
+{
+    // Root Node
+    auto localRootId = listbox->AppendItem(rootId, wxString(wxT("By area")), 2);
+    listbox->SetItemBold(localRootId, true);
+
+    // For each area, list the links related to it. For each link, the area is either
+    // its origin or extremity.
+    using ListOfLinks
+      = std::vector<std::pair<Data::AreaLink* /*pointer to link*/,
+                              bool /*true if area is the origin of the link, false otherwise*/>>;
+    // 1. Build hierarchy
+    std::map<Data::AreaName, ListOfLinks> areaToListOfLinks;
+    for (const auto& namedArea : areas)
+    {
+        Data::Area* area = namedArea.second;
+        if (area->isVisibleOnLayer(layerID))
+        {
+            for (const auto& namedLink : area->links)
+            {
+                Data::AreaLink* lnk = namedLink.second;
+                if (lnk->isVisibleOnLayer(layerID))
+                {
+                    areaToListOfLinks[area->name].push_back({lnk, true});
+                    areaToListOfLinks[lnk->with->name].push_back({lnk, false});
+                }
+            }
+        }
+    }
+    // 2. Create nodes, etc.
+    for (const auto& al : areaToListOfLinks)
+    {
+        // Reference to the area
+        wxTreeItemId id;
+        // Foreach Interconnection for the area
+        const auto end = al.second.end();
+        for (auto i = al.second.begin(); i != end; ++i)
+        {
+            Data::AreaLink* lnk = i->first;
+            const bool isAreaOriginOfLink = i->second;
+
+            if (!id)
+            {
+                // We have to create the item corresponding to the area
+                id = listbox->AppendItem(localRootId,
+                                         wxString()
+                                           << wxT(' ') << wxStringFromUTF8(al.first) << wxT(' '),
+                                         1,
+                                         1);
+                listbox->SetItemBold(id, true);
+            }
+            // Adding the item for the interconnection
+            /* wxTreeItemId itemID = */ listbox->AppendItem(
+              id, /*parent*/
+              // caption
+              wxString() << wxT(' ')
+                         << wxStringFromUTF8(isAreaOriginOfLink ? lnk->with->name : lnk->from->name)
+                         << wxT(' '),
+              0,
+              0,
+              new TreeLeaf(lnk));
+        }
+    }
+}
+
 void Connections::update()
 {
     assert(pListbox);
@@ -212,73 +327,12 @@ void Connections::update()
     // The current ROOT node ID
     wxTreeItemId rootId = pListbox->AddRoot(wxString(wxT("Links")));
     pListbox->SetItemBold(rootId, true);
-    wxTreeItemId localRootId;
 
     // UPSTREAM / DOWNSTREEAM
-    // Root Node
-    localRootId = pListbox->AppendItem(rootId, wxString(wxT("By area")), 2);
-    pListbox->SetItemBold(localRootId, true);
+    addUpstreamDownstream(rootId, pListbox, study.areas, layerID);
 
-    // For each area, list the links related to it. For each link, the area is either
-    // its origin or extremity.
-    using ListOfLinks
-      = std::vector<std::pair<Data::AreaLink* /*pointer to link*/,
-                              bool /*true if area is the origin of the link, false otherwise*/>>;
-    std::map<Data::AreaName, ListOfLinks> areaToListOfLinks;
-    {
-        for (const auto& namedArea : study.areas)
-        {
-            Data::Area* area = namedArea.second;
-            if (area->isVisibleOnLayer(layerID))
-            {
-                for (const auto& namedLink : area->links)
-                {
-                    Data::AreaLink* lnk = namedLink.second;
-                    if (lnk->isVisibleOnLayer(layerID))
-                    {
-                        areaToListOfLinks[area->name].push_back({lnk, true});
-                        areaToListOfLinks[lnk->with->name].push_back({lnk, false});
-                    }
-                }
-            }
-        }
-    }
-
-    {
-        for (const auto& al : areaToListOfLinks)
-        {
-            // Reference to the area
-            wxTreeItemId id;
-            // Foreach Interconnection for the area
-            const auto end = al.second.end();
-            for (auto i = al.second.begin(); i != end; ++i)
-            {
-                Data::AreaLink* lnk = i->first;
-                const bool isAreaOriginOfLink = i->second;
-
-                if (!id)
-                {
-                    // We have to create the item corresponding to the area
-                    id = pListbox->AppendItem(
-                      localRootId,
-                      wxString() << wxT(' ') << wxStringFromUTF8(al.first) << wxT(' '),
-                      1,
-                      1);
-                    pListbox->SetItemBold(id, true);
-                }
-                // Adding the item for the interconnection
-                /* wxTreeItemId itemID = */ pListbox->AppendItem(
-                  id, /*parent*/
-                  // caption
-                  wxString() << wxT(
-                    ' ') << wxStringFromUTF8(isAreaOriginOfLink ? lnk->with->name : lnk->from->name)
-                             << wxT(' '),
-                  0,
-                  0,
-                  new TreeLeaf(lnk));
-            }
-        }
-    }
+    // By area
+    addByArea(rootId, pListbox, study.areas, layerID);
 
     // Expand all items and subitems
     pListbox->SetQuickBestSize(true);
