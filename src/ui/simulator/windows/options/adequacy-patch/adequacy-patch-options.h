f56b69aa8d04fc22c7538adac9642eae2c21d750
Milos A <milos.andjelkovic@redstork-solutions.com>
2022-08-25 17:16:14 +0200

    Add the "explicit" keyword to AdequacyPatchOptions constructor

--- src/ui/simulator/windows/options/adequacy-patch/adequacy-patch-options.h
+++ src/ui/simulator/windows/options/adequacy-patch/adequacy-patch-options.h
@@ -52,7 +52,7 @@ public:
     **
     ** \param parent The parent window
     */
-    AdequacyPatchOptions(wxWindow* parent);
+    explicit AdequacyPatchOptions(wxWindow* parent);
     //! Destructor
     virtual ~AdequacyPatchOptions();
     //@}
