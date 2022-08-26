4e10580c47f77725c361125818e338a8d0c4e899
Florian OMNES <florian.omnes@rte-france.com>
2022-08-26 14:48:03 +0200

    Apply PR remarks + renaming

--- src/libs/antares/benchmarking/file_content.cpp
+++ src/libs/antares/benchmarking/file_content.cpp
@@ -4,31 +4,31 @@ using namespace std;
 
 namespace Benchmarking
 {
-	FileContent::iterator FileContent::firstSection()
-	{
-		return sections_.begin();
-	}
-
-	FileContent::iterator FileContent::endSections()
-	{
-		return sections_.end();
-	}
-
-	void FileContent::addItemToSection(const string& section, const string& key, int value)
-	{
-        std::lock_guard<std::mutex> guard(pMutex);
-		sections_[section][key] = to_string(value);
-	}
-
-	void FileContent::addItemToSection(const string& section, const string& key, const string& value)
-	{
-        std::lock_guard<std::mutex> guard(pMutex);
-		sections_[section][key] = value;
-	}
-
-	void FileContent::addDurationItem(const string& name, unsigned int duration, int nbCalls)
-	{
-		addItemToSection("durations_ms", name, duration);
-		addItemToSection("number_of_calls", name, nbCalls);
-	}
+FileContent::iterator FileContent::begin()
+{
+    return sections_.begin();
 }
+
+FileContent::iterator FileContent::end()
+{
+    return sections_.end();
+}
+
+void FileContent::addItemToSection(const string& section, const string& key, int value)
+{
+    std::lock_guard<std::mutex> guard(pSectionsMutex);
+    sections_[section][key] = to_string(value);
+}
+
+void FileContent::addItemToSection(const string& section, const string& key, const string& value)
+{
+    std::lock_guard<std::mutex> guard(pSectionsMutex);
+    sections_[section][key] = value;
+}
+
+void FileContent::addDurationItem(const string& name, unsigned int duration, int nbCalls)
+{
+    addItemToSection("durations_ms", name, duration);
+    addItemToSection("number_of_calls", name, nbCalls);
+}
+} // namespace Benchmarking
