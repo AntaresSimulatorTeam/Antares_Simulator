8bdcc306df984cfdbb3ecf0a6a7665e0445aef39
Guillaume PIERRE <guillaume.pierre@rte-france.com>
2022-10-07 16:03:58 +0200

    Remove swap : removing option for swap folder from yby-aggregator

--- src/tools/yby-aggregator/main.cpp
+++ src/tools/yby-aggregator/main.cpp
@@ -271,7 +271,6 @@ static void ReadCommandLineOptions(int argc, char** argv)
 {
     String::Vector optOutputs;
     uint optJobs = FindNbProcessors();
-    String optSwap;
     String::Vector optTimes;
     String::Vector optColumns;
     String::Vector optDatum;
@@ -307,17 +306,6 @@ static void ReadCommandLineOptions(int argc, char** argv)
                     "jobs",
                     String() << "The number of jobs to run simultaneously (default: " << optJobs
                              << ", 4 should be a maxmimum due to i/o performance considerations)");
-        // --swap
-        options.add(optSwap,
-                    ' ',
-                    "swap-folder",
-#ifdef ANTARES_SWAP_SUPPORT
-                    String("Folder where the swap files will be written. (default: '")
-                      << Antares::memory.cacheFolder() << "')"
-#else
-                    "Folder where the swap files will be written. This option has no effect"
-#endif
-        );
 
         options.addParagraph("\nMisc.");
 
@@ -371,17 +359,6 @@ static void ReadCommandLineOptions(int argc, char** argv)
             optJobs = 1;
         queueService.maximumThreadCount(optJobs);
     }
-
-#ifdef ANTARES_SWAP_SUPPORT
-    // Changing the swap folder
-    if (not optSwap.empty())
-    {
-        logs.info() << "  memory pool: scratch folder:" << optSwap;
-        Antares::memory.cacheFolder(optSwap);
-    }
-    else
-        logs.info() << "  memory pool: scratch folder:" << Antares::memory.cacheFolder();
-#endif
 
     // Starting !
     {
