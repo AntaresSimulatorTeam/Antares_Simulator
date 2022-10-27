91adf0b62c65130fe68e1c00b91aacb916a3294a
Vincent Payet <vincent.payet@rte-france.com>
2022-10-27 13:45:49 +0200

    [TEST] Finish script and added reference result

--- src/tests/kirchhoff-cbuilder/test_kirchoff.py
+++ src/tests/kirchhoff-cbuilder/test_kirchoff.py
@@ -1,18 +1,34 @@
 import subprocess
 import sys
 
+nb_test = 3
+
+files = [[0 for x in range(2)] for y in range(nb_test)]
+files[0][0] = "/home/payetvin/Projects/Antares_Simulator/src/tests/resources/Antares_Simulator_Tests/medium-tests/039 Multistage study-4-Kirchhoff"
+files[0][1] = "reference/39"
+files[1][0] = "/home/payetvin/Projects/Antares_Simulator/src/tests/resources/Antares_Simulator_Tests/medium-tests/043 Multistage study-8-Kirchhoff"
+files[1][1] = "reference/43"
+files[2][0] = "/home/payetvin/Projects/Antares_Simulator/src/tests/resources/Antares_Simulator_Tests/long-tests/079 Zero  Power Balance - Type 1"
+files[2][1] = "reference/79"
+
+
 def run_and_compare(path, ref_path):
     res = subprocess.run([sys.argv[1], path])
     if res.returncode != 0:
-        print("Exec of this study failed: " + path)
+        print("\nExec of this study failed: " + path)
         exit(1)
 
-    out_path = path + "input/bindingconstraints/"
+    out_path = path + "/input/bindingconstraints/"
     res = subprocess.run(["diff", "-bur", out_path, ref_path])
     if res.returncode != 0:
-        print("Wrong results for study: " + path)
+        print("\nWrong results for study: " + path)
         exit(1)
 
+for i in range(nb_test):
+    run_and_compare(files[i][0], files[i][1])
 
-subprocess.run(["ls",".."])
-run_and_compare("/home/payetvin/Projects/Antares_Simulator/src/tests/resources/Antares_Simulator_Tests/medium-tests/039 Multistage study-4-Kirchhoff", "39")
+print()
+for i in range(nb_test):
+    print("Test OK: " + files[i][0])
+
+print("\nAll tests succeeded, exiting now")
