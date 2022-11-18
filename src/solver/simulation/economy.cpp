eccb3cdc93ae1cdc5e89fd0c525b6cf98eb63833
Florian Omnès <florian.omnes@rte-france.com>
2022-11-18 14:50:50 +0100

    Remove EconomyWeeklyOptimization::initializeProblemeHebdo (#954)
    * Remove EconomyWeeklyOptimization::initializeProblemeHebdo
    
    * [skip ci] Remove useless Ptr=
    
    * Add 'explicit' keyword

--- src/solver/simulation/economy.cpp
+++ src/solver/simulation/economy.cpp
@@ -50,25 +50,27 @@ enum
     nbHoursInAWeek = 168,
 };
 
-// Interface class + factory
-void EconomyWeeklyOptimization::initializeProblemeHebdo(PROBLEME_HEBDO** problemesHebdo)
+EconomyWeeklyOptimization::EconomyWeeklyOptimization(PROBLEME_HEBDO** problemesHebdo) :
+ pProblemesHebdo(problemesHebdo)
 {
-    pProblemesHebdo = problemesHebdo;
 }
 
-EconomyWeeklyOptimization::Ptr EconomyWeeklyOptimization::create(bool adqPatchEnabled)
+EconomyWeeklyOptimization::Ptr EconomyWeeklyOptimization::create(bool adqPatchEnabled,
+                                                                 PROBLEME_HEBDO** problemesHebdo)
 {
-    using EcoWeeklyPtr = EconomyWeeklyOptimization::Ptr;
     if (adqPatchEnabled)
-        return EcoWeeklyPtr(new AdequacyPatchOptimization());
+        return std::make_unique<AdequacyPatchOptimization>(problemesHebdo);
     else
-        return EcoWeeklyPtr(new NoAdequacyPatchOptimization());
+        return std::make_unique<NoAdequacyPatchOptimization>(problemesHebdo);
 
     return nullptr;
 }
 
 // Adequacy patch
-AdequacyPatchOptimization::AdequacyPatchOptimization() = default;
+AdequacyPatchOptimization::AdequacyPatchOptimization(PROBLEME_HEBDO** problemesHebdo) :
+ EconomyWeeklyOptimization(problemesHebdo)
+{
+}
 void AdequacyPatchOptimization::solve(Variable::State& state, int hourInTheYear, uint numSpace)
 {
     auto problemeHebdo = pProblemesHebdo[numSpace];
@@ -96,7 +98,10 @@ void AdequacyPatchOptimization::solve(Variable::State& state, int hourInTheYear,
 }
 
 // No adequacy patch
-NoAdequacyPatchOptimization::NoAdequacyPatchOptimization() = default;
+NoAdequacyPatchOptimization::NoAdequacyPatchOptimization(PROBLEME_HEBDO** problemesHebdo) :
+ EconomyWeeklyOptimization(problemesHebdo)
+{
+}
 void NoAdequacyPatchOptimization::solve(Variable::State&, int, uint numSpace)
 {
     auto problemeHebdo = pProblemesHebdo[numSpace];
@@ -154,8 +159,6 @@ bool Economy::simulationBegin()
             memset(pProblemesHebdo[numSpace], '\0', sizeof(PROBLEME_HEBDO));
             SIM_InitialisationProblemeHebdo(study, *pProblemesHebdo[numSpace], 168, numSpace);
 
-            assert((uint)nbHoursInAWeek == (uint)pProblemesHebdo[numSpace]->NombreDePasDeTemps
-                   && "inconsistency");
             if ((uint)nbHoursInAWeek != (uint)pProblemesHebdo[numSpace]->NombreDePasDeTemps)
             {
                 logs.fatal() << "internal error";
@@ -163,7 +166,8 @@ bool Economy::simulationBegin()
             }
         }
 
-        weeklyOptProblem = EconomyWeeklyOptimization::create(study.parameters.adqPatch.enabled);
+        weeklyOptProblem
+          = EconomyWeeklyOptimization::create(study.parameters.adqPatch.enabled, pProblemesHebdo);
 
         SIM_InitialisationResultats();
     }
@@ -172,11 +176,6 @@ bool Economy::simulationBegin()
     {
         for (uint numSpace = 0; numSpace < pNbMaxPerformedYearsInParallel; numSpace++)
             pProblemesHebdo[numSpace]->TypeDOptimisation = OPTIMISATION_LINEAIRE;
-
-        if (weeklyOptProblem)
-        {
-            weeklyOptProblem->initializeProblemeHebdo(pProblemesHebdo);
-        }
     }
 
     pStartTime = study.calendar.days[study.parameters.simulationDays.first].hours.first;
--- src/solver/simulation/economy.h
+++ src/solver/simulation/economy.h
@@ -48,24 +48,24 @@ class EconomyWeeklyOptimization
 public:
     using Ptr = std::unique_ptr<EconomyWeeklyOptimization>;
     virtual void solve(Variable::State& state, int hourInTheYear, uint numSpace) = 0;
-    void initializeProblemeHebdo(PROBLEME_HEBDO** pProblemesHebdo);
-    static Ptr create(bool adqPatchEnabled);
+    static Ptr create(bool adqPatchEnabled, PROBLEME_HEBDO** pProblemesHebdo);
 
 protected:
+    explicit EconomyWeeklyOptimization(PROBLEME_HEBDO** pProblemesHebdo);
     PROBLEME_HEBDO** pProblemesHebdo;
 };
 
 class AdequacyPatchOptimization : public EconomyWeeklyOptimization
 {
 public:
-    AdequacyPatchOptimization();
+    explicit AdequacyPatchOptimization(PROBLEME_HEBDO** problemesHebdo);
     void solve(Variable::State& state, int hourInTheYear, uint numSpace) override;
 };
 
 class NoAdequacyPatchOptimization : public EconomyWeeklyOptimization
 {
 public:
-    NoAdequacyPatchOptimization();
+    explicit NoAdequacyPatchOptimization(PROBLEME_HEBDO** problemesHebdo);
     void solve(Variable::State&, int, uint numSpace) override;
 };
 
