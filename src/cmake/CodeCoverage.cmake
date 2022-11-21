c99a6e7c03a042983b988ae26a545e523994b428
kathvargasr <113925565+kathvargasr@users.noreply.github.com>
2022-11-21 15:07:45 +0100

    reducing report scope

--- src/cmake/CodeCoverage.cmake
+++ src/cmake/CodeCoverage.cmake
@@ -68,7 +68,7 @@ function(code_coverage)
         COMMAND ${CMAKE_COMMAND} -E make_directory ${Coverage_OUTPUT_DIR}/reports
         COMMAND ${GCOVR_CMD} ${CMAKE_CURRENT_BINARY_DIR}
         --gcov-executable "${GCOV_CMD}"
-        --root ${CMAKE_SOURCE_DIR}
+        --root ${CMAKE_SOURCE_DIR}/libs/antares
         --keep --object-directory ${Coverage_OUTPUT_DIR}/reports
         --html --html-details --output ${Coverage_OUTPUT_DIR}/index.html --sonarqube ${Coverage_OUTPUT_DIR}/coverage.xml
         ${GCOVR_OPTIONS}
