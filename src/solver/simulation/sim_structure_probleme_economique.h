a09cd78f56c53e5b962b75b23bf68c4ebc1b87e8
Vincent Payet <vincent.payet@rte-france.com>
2022-11-22 16:03:52 +0100

    [REFAC] More correction of PR 903

--- src/solver/simulation/sim_structure_probleme_economique.h
+++ src/solver/simulation/sim_structure_probleme_economique.h
@@ -710,41 +710,5 @@ public:
 };
 
 namespace Antares::Solver::Variable { class State; } // foward declaration
-// hourly CSR problem structure
-class HOURLY_CSR_PROBLEM
-{
-private:
-    void calculateCsrParameters();
-    void resetProblem();
-    void buildProblemVariables();
-    void setVariableBounds();
-    void buildProblemConstraintsLHS();
-    void buildProblemConstraintsRHS();
-    void setProblemCost();
-    void solveProblem(uint week, int year);
-public:
-    void run(uint week, const Antares::Solver::Variable::State& state);
-    
-    int hourInWeekTriggeredCsr;
-    double belowThisThresholdSetToZero;
-    PROBLEME_HEBDO* pWeeklyProblemBelongedTo;
-    HOURLY_CSR_PROBLEM(int hourInWeek, PROBLEME_HEBDO* pProblemeHebdo)
-    {
-        hourInWeekTriggeredCsr = hourInWeek;
-        pWeeklyProblemBelongedTo = pProblemeHebdo;
-        belowThisThresholdSetToZero
-          = pProblemeHebdo->adqPatchParams->ThresholdCSRVarBoundsRelaxation;
-    };
-    std::map<int, int> numberOfConstraintCsrEns;
-    std::map<int, int> numberOfConstraintCsrAreaBalance;
-    std::map<int, int> numberOfConstraintCsrFlowDissociation;
-    std::map<int, int> numberOfConstraintCsrHourlyBinding; // length is number of binding constraint
-                                                           // contains interco 2-2
-
-    std::map<int, double> rhsAreaBalanceValues;
-    std::set<int> varToBeSetToZeroIfBelowThreshold; // place inside only ENS and Spillage variable
-    std::set<int> ensSet; // place inside only ENS inside adq-patch
-    std::set<int> linkSet; // place inside only links between to zones inside adq-patch
-};
 
 #endif
