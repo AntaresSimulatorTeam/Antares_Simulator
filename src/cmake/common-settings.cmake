6f2c38b2a8b13c86e852fb15367c1dc6133dc45c
Florian OMNES <florian.omnes@rte-france.com>
2022-11-30 09:49:10 +0100

    Simplify previous commit

--- src/cmake/common-settings.cmake
+++ src/cmake/common-settings.cmake
@@ -218,13 +218,14 @@ endmacro()
 
 macro(executable_strip TARGET)
 	if("${CMAKE_BUILD_TYPE}" STREQUAL "release")
+        set(EXEC_NAME $<TARGET_FILE:${TARGET}>)
 		if(NOT MSVC)
 			if(WIN32)
-				add_custom_command(TARGET ${TARGET} POST_BUILD COMMAND ${CMAKE_STRIP} $<TARGET_FILE:${TARGET}>
-					COMMENT "Stripping the executable '$<TARGET_FILE:${TARGET}>'")
+				add_custom_command(TARGET ${TARGET} POST_BUILD COMMAND ${CMAKE_STRIP} ${EXEC_NAME}
+					COMMENT "Stripping the executable '${EXEC_NAME}'")
 			else()
-				add_custom_command(TARGET ${TARGET} POST_BUILD COMMAND ${CMAKE_STRIP} --strip-all $<TARGET_FILE:${TARGET}>
-					COMMENT "Stripping the executable '$<TARGET_FILE:${TARGET}>'")
+				add_custom_command(TARGET ${TARGET} POST_BUILD COMMAND ${CMAKE_STRIP} --strip-all ${EXEC_NAME}
+					COMMENT "Stripping the executable '${EXEC_NAME}'")
 			endif()
 		endif()
 	endif()
