fd67d6f4399c051a8868c055d39e37528a26d9bc
Milos A <milos.andjelkovic@redstork-solutions.com>
2022-12-15 18:12:37 +0100

    profitByPlants - update with marginalCostPerHourTs

--- src/solver/variable/economy/profitByPlant.h
+++ src/solver/variable/economy/profitByPlant.h
@@ -299,18 +299,22 @@ public:
         uint hourInTheYear = state.hourInTheYear;
 
         // Thermal cluster profit
-        if(cluster->costgeneration == Data::setManually)
+        uint tsIndex
+          = Yuni::Math::Min(state.timeseriesIndex->ThermiqueParPalier[cluster->areaWideIndex],
+                            cluster->marginalCostPerHourTs.size() - 1);
+        if (cluster->costgeneration == Data::setManually)
             pValuesForTheCurrentYear[numSpace][cluster->areaWideIndex].hour[hourInTheYear]
             = (hourlyClusterProduction - cluster->PthetaInf[hourInTheYear])
                 * (-areaMarginalCosts[hourInTheWeek]
-                - cluster->marginalCost
-                    * cluster->modulation[Data::thermalModulationCost][hourInTheYear]);
-        else{
+                   - cluster->marginalCost
+                       * cluster->modulation[Data::thermalModulationCost][hourInTheYear]);
+        else
+        {
             pValuesForTheCurrentYear[numSpace][cluster->areaWideIndex].hour[hourInTheYear]
             = (hourlyClusterProduction - cluster->PthetaInf[hourInTheYear])
                 * (-areaMarginalCosts[hourInTheWeek]
-                    - cluster->marginalCostPerHour[hourInTheYear]
-                    * cluster->modulation[Data::thermalModulationCost][hourInTheYear]);            
+                   - cluster->marginalCostPerHourTs[tsIndex][hourInTheYear]
+                       * cluster->modulation[Data::thermalModulationCost][hourInTheYear]);
         }
         // Next item in the list
         NextType::hourForEachThermalCluster(state, numSpace);
