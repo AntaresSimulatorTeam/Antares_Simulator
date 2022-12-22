19bbd27c61657faad3fe859bf23e3d1da5606d0f
payetvin <113102157+payetvin@users.noreply.github.com>
2022-12-22 16:00:20 +0100

    Use MPS writer from solvers (#1023)
    * [DEV] Refactored mps writing #1
    
    * [DEV] Include ortools_utils functions
    
    * [DEV] Fix static functions
    
    * [DEV] Remove fs header
    
    * [DEV] Fix unfeasible problems
    
    * [DEV] Improve convert problem function
    
    * Fix variable type (#1037)
    
    * Feature/mps writer minor improvements (#1038)
    
    * [CI] Try short tests
    
    * [CI] Fix yaml syntax
    
    * [CI] Fix bash syntax
    
    * [CI] Fix run
    
    * [CI] Fix url batch
    
    * [CI] Revert tests
    
    Co-authored-by: Florian Omnès <florian.omnes@rte-france.com>

--- src/solver/utils/ortools_utils.h
+++ src/solver/utils/ortools_utils.h
@@ -27,6 +27,9 @@ std::list<std::string> getAvailableOrtoolsSolverName();
  */
 MPSolver* MPSolverFactory(const Antares::Optimization::PROBLEME_SIMPLEXE_NOMME *probleme, const std::string & solverName);
 
+std::string generateTempPath(const std::string& filename);
+void removeTemporaryFile(const std::string& tmpPath);
+
 
 class OrtoolsUtils
 {
