5ed066cb7d890993810073216cf23e5d1a3e3dd7
Jason Marechal <jason.marechal+github@gmail.com>
2023-01-03 11:39:38 +0100

    Clean up

--- src/ext/yuni/src/yuni/cmake/core/core.cmake
+++ src/ext/yuni/src/yuni/cmake/core/core.cmake
@@ -273,7 +273,6 @@ add_library(yuni-static-core STATIC
 		core/static/remove.h
 		core/static/typedef.h
 		core/static/types.h
-		core/stl/hash-combine.h
 		core/string/escape.h
 		core/string/escape.hxx
 		core/string/iterator.hxx
