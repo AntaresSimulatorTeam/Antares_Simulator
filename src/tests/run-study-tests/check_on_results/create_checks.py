12d2c1c32c594a7d8d49e4ed0c3ecd5e080da792
Guillaume PIERRE <guillaume.pierre@rte-france.com>
2023-01-12 16:03:39 +0100

    Move mps tests to tests system : moving tests (finalizing)

--- src/tests/run-study-tests/check_on_results/create_checks.py
+++ src/tests/run-study-tests/check_on_results/create_checks.py
@@ -3,6 +3,7 @@ from check_on_results.output_compare import output_compare
 from check_on_results.integrity_compare import integrity_compare
 from check_on_results.check_hydro_level import check_hydro_level
 from check_on_results.unfeasible_problem import unfeasible_problem, weeks_in_year
+from check_on_results.compare_mps_files import compare_mps_files
 from check_on_results.tolerances import get_tolerances
 
 
@@ -16,6 +17,8 @@ def create_checks(study_path, checks_data = {}, simulation = None):
         checks.add(create_check_hydro_level(study_path, checks_data["hydro_level"]))
     if "unfeasible_problem" in checks_data:
         checks.add(create_unfeasible_problem(study_path, checks_data["unfeasible_problem"], simulation))
+    if "mps_compare" in checks_data:
+        checks.add(create_mps_compare(study_path))
 
     return checks
 
@@ -62,4 +65,10 @@ def make_list_of_weeks(weeks_in_year_data):
     for item in weeks_in_year_data:
         checks_on_weeks.append(weeks_in_year(year=item["year"], weeks=item["weeks"]))
     return checks_on_weeks
+
+# --------------------------------------
+# mps_compare object creation
+# --------------------------------------
+def create_mps_compare(study_path):
+    return compare_mps_files(study_path)
 
