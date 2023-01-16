16f72c84e5bb8d8fd5d2ec87baa795928e5ab187
Florian OMNES <florian.omnes@rte-france.com>
2023-01-16 10:31:42 +0100

    Clean up headers

--- src/solver/optimisation/adq_patch_local_matching.cpp
+++ src/solver/optimisation/adq_patch_local_matching.cpp
@@ -1,13 +1,76 @@
 #include "adq_patch_local_matching.h"
 
-
 namespace Antares
 {
 namespace Data
 {
 namespace AdequacyPatch
 {
-ntcSetToZeroStatus_AdqPatchStep1 getNTCtoZeroStatus(PROBLEME_HEBDO* ProblemeHebdo, int Interco)
+/*!
+ * Determines restriction type for transmission links for first step of adequacy patch, when start
+ * node is inside adq path (type 2).
+ *
+ * @param ExtremityNodeAdequacyPatchType uint: The adq type of the node at the end of the link.
+ *
+ * @return uint from an enumeration that describes the type of restrictions to put on this link for
+ * adq purposes.
+ */
+static ntcSetToZeroStatus_AdqPatchStep1 SetNTCForAdequacyFirstStepOriginNodeInsideAdq(
+  AdequacyPatchMode ExtremityNodeAdequacyPatchType)
+{
+    switch (ExtremityNodeAdequacyPatchType)
+    {
+    case physicalAreaInsideAdqPatch:
+    case physicalAreaOutsideAdqPatch:
+        return setToZero;
+    default:
+        return leaveLocalValues;
+    }
+}
+
+/*!
+ * Determines restriction type for transmission links for first step of adequacy patch, when start
+ * node is outside adq path (type 1).
+ *
+ * @param ExtremityNodeAdequacyPatchType uint: The adq type of the node at the end of the link.
+ *
+ * @param setToZeroNTCfromOutToIn_AdqPatch bool: Switch to cut links from nodes outside adq patch
+ * (type 1) towards nodes inside adq patch (type 2).
+ *
+ * @param setToZeroNTCfromOutToOut_AdqPatch bool: Switch to cut links between nodes outside adq
+ * patch (type 1).
+ *
+ * @return uint from an enumeration that describes the type of restrictions to put on this link for
+ * adq purposes.
+ */
+static ntcSetToZeroStatus_AdqPatchStep1 getNTCtoZeroStatusOriginNodeOutsideAdq(
+  AdequacyPatchMode ExtremityNodeAdequacyPatchType,
+  bool setToZeroNTCfromOutToIn_AdqPatch,
+  bool setToZeroNTCfromOutToOut_AdqPatch)
+{
+    switch (ExtremityNodeAdequacyPatchType)
+    {
+    case physicalAreaInsideAdqPatch:
+        return setToZeroNTCfromOutToIn_AdqPatch ? setToZero : setExtremityOriginToZero;
+    case physicalAreaOutsideAdqPatch:
+        return setToZeroNTCfromOutToOut_AdqPatch ? setToZero : leaveLocalValues;
+    default:
+        return leaveLocalValues;
+    }
+}
+
+/*!
+ * Determines restriction type for transmission links for first step of adequacy patch.
+ *
+ * @param ProblemeHebdo PROBLEME_HEBDO*: Weekly problem structure.
+ *
+ * @param Interco int: Index of the link.
+ *
+ * @return uint from an enumeration that describes the type of restrictions to put on this link for
+ * adq purposes.
+ */
+static ntcSetToZeroStatus_AdqPatchStep1 getNTCtoZeroStatus(PROBLEME_HEBDO* ProblemeHebdo,
+                                                           int Interco)
 {
     AdequacyPatchMode OriginNodeAdequacyPatchType
       = ProblemeHebdo->adequacyPatchRuntimeData.originAreaMode[Interco];
@@ -27,35 +90,6 @@ ntcSetToZeroStatus_AdqPatchStep1 getNTCtoZeroStatus(PROBLEME_HEBDO* ProblemeHebd
         return getNTCtoZeroStatusOriginNodeOutsideAdq(ExtremityNodeAdequacyPatchType,
                                                       setToZeroNTCfromOutToIn_AdqPatch,
                                                       setToZeroNTCfromOutToOut_AdqPatch);
-    default:
-        return leaveLocalValues;
-    }
-}
-
-ntcSetToZeroStatus_AdqPatchStep1 SetNTCForAdequacyFirstStepOriginNodeInsideAdq(
-  AdequacyPatchMode ExtremityNodeAdequacyPatchType)
-{
-    switch (ExtremityNodeAdequacyPatchType)
-    {
-    case physicalAreaInsideAdqPatch:
-    case physicalAreaOutsideAdqPatch:
-        return setToZero;
-    default:
-        return leaveLocalValues;
-    }
-}
-
-ntcSetToZeroStatus_AdqPatchStep1 getNTCtoZeroStatusOriginNodeOutsideAdq(
-  AdequacyPatchMode ExtremityNodeAdequacyPatchType,
-  bool setToZeroNTCfromOutToIn_AdqPatch,
-  bool setToZeroNTCfromOutToOut_AdqPatch)
-{
-    switch (ExtremityNodeAdequacyPatchType)
-    {
-    case physicalAreaInsideAdqPatch:
-        return setToZeroNTCfromOutToIn_AdqPatch ? setToZero : setExtremityOriginToZero;
-    case physicalAreaOutsideAdqPatch:
-        return setToZeroNTCfromOutToOut_AdqPatch ? setToZero : leaveLocalValues;
     default:
         return leaveLocalValues;
     }
@@ -105,4 +139,3 @@ void setNTCbounds(double& Xmax,
 } // namespace AdequacyPatch
 } // end namespace Data
 } // namespace Antares
-
