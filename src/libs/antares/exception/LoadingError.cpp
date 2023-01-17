061d2c7dfe74b8322408eb7c1ded5d2dd2507a58
Florian OMNES <florian.omnes@rte-france.com>
2023-01-17 15:41:42 +0100

    Check for incompatible hurdle cost options

--- src/libs/antares/exception/LoadingError.cpp
+++ src/libs/antares/exception/LoadingError.cpp
@@ -133,6 +133,11 @@ NoAreaInsideAdqPatchMode::NoAreaInsideAdqPatchMode() :
 {
 }
 
+IncompatibleHurdleCostCSR::IncompatibleHurdleCostCSR() :
+ LoadingError("Incompatible options include.hurdleCost and curtailmentSharing.includeHurdleCost")
+{
+}
+
 IncompatibleOutputOptions::IncompatibleOutputOptions(const std::string& text) : LoadingError(text)
 {
 }
