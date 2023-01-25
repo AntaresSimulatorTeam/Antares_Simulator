0e79f37fe1782d45b4a8697291bdec5b5be9d369
guilpier-code <62292552+guilpier-code@users.noreply.github.com>
2023-01-25 14:10:34 +0100

    Add mps tests to tests system (#1084)
    * Move mps tests to tests system : refactoring + add class "compare_mps_files" + new test file
    
    A new check is added and tests are added in a test standalone file
    - Refactoring : renaming + add comments + moving a piece of code (function find_simulation_folder)
    - add class "compare_mps_files" : see file compare_mps_files.py
    - new test file : see file test_mps_compare.py
    
    * Move mps tests to tests system : moving tests (finalizing)
    
    * Move mps tests to tests system : removing explicit run of new tests from CI
    
    These 2 new tests are now integrated in the usual tests system (marked as "json" tests for pytest), they don't differ from any other tests
    
    * [skip ci] Move mps tests to tests system : adding documentation to testing script system
    
    * [skip ci] Move mps tests to tests system : the doc has to be finished
    
    * [skip ci] Move mps tests to tests system : the doc finalization
    
    * Move mps tests to tests system : enables new tests on CI
    
    * Move mps tests to tests system : doc improvements

--- src/tests/run-study-tests/utils/find_output.py
+++ src/tests/run-study-tests/utils/find_output.py
@@ -1,5 +1,7 @@
 from re import match
 from os.path import isdir, basename
+from os import walk
+from pathlib import Path
 
 from utils.assertions import check
 
@@ -18,8 +20,16 @@ class output_folder_finder:
     def get(self):
         return self.found_dir_path
 
-def find_output_folder(study_path):
+def find_dated_output_folder(study_path):
+    # From the study root folder, search and return full path of the simulation folder,
+    # which is of form : yyyymmdd-hhmm<mode> (ex : 20230105-0944eco)
     output_folder_finder_ = output_folder_finder(study_path)
     check(output_folder_finder_.find(), "Output folder not found")
     return output_folder_finder_.get()
+
+def find_simulation_folder(output_dir):
+    # Return full path of the ouptut simulation path (can be "adequacy", "economy" or "adequacy-draft")
+    for root, dirs, files in walk(output_dir):
+        if basename(root) in ["adequacy", "economy", "adequacy-draft"]:
+            return Path(root)
 
