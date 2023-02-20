27b0c05ea21617c5cf511e5c1688bc03d832de69
Florian OMNES <florian.omnes@rte-france.com>
2023-02-20 17:55:50 +0100

    Remove useless comparison
    std::numeric_limits<std::size_t>::max() / 8 - 1 = 2305843009213693950
    std::numeric_limits<int>::max() = 2147483647

--- src/solver/optimisation/opt_alloc_probleme_a_optimiser.cpp
+++ src/solver/optimisation/opt_alloc_probleme_a_optimiser.cpp
@@ -193,12 +193,6 @@ static void optimisationAllocateProblem(PROBLEME_HEBDO* problemeHebdo, const int
     logs.info() << " Expected Number of Non-zero terms in Problem Matrix : " << NbTermes;
     logs.info();
 
-    if ((uint)NbTermes > (std::numeric_limits<std::size_t>::max() / 8) - 1)
-    {
-        logs.fatal() << "Optimisation problem too large to be allocated.";
-        AntaresSolverEmergencyShutdown();
-    }
-
     OPT_AllocateFromNumberOfVariableConstraints(problemeHebdo->ProblemeAResoudre, NbTermes);
 
     NbIntervalles
@@ -238,12 +232,6 @@ void OPT_AugmenterLaTailleDeLaMatriceDesContraintes(PROBLEME_ANTARES_A_RESOUDRE*
     logs.info() << " Expected Number of Non-zero terms in Problem Matrix : increased to : "
                 << NbTermes;
     logs.info();
-
-    if (NbTermes > (std::numeric_limits<std::size_t>::max() / 8) - 1)
-    {
-        logs.fatal() << "Optimisation problem too large to be allocated.";
-        AntaresSolverEmergencyShutdown();
-    }
 
     ProblemeAResoudre->CoefficientsDeLaMatriceDesContraintes = (double*)MemRealloc(
       ProblemeAResoudre->CoefficientsDeLaMatriceDesContraintes, NbTermes * sizeof(double));
