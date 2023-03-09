0ad98a894f19177154e088e013440eaedeeb7990
Vincent Payet <vincent.payet@rte-france.com>
2023-03-09 16:36:32 +0100

    [DEV] Add properties.cpp and unit.cpp

---
+++ src/libs/antares/study/parts/short-term-storage/unit.cpp
@@ -0,0 +1,70 @@
+/*
+** Copyright 2007-2023 RTE
+** Authors: Antares_Simulator Team
+**
+** This file is part of Antares_Simulator.
+**
+** Antares_Simulator is free software: you can redistribute it and/or modify
+** it under the terms of the GNU General Public License as published by
+** the Free Software Foundation, either version 3 of the License, or
+** (at your option) any later version.
+**
+** There are special exceptions to the terms and conditions of the
+** license as they are applied to this software. View the full text of
+** the exceptions in file COPYING.txt in the directory of this software
+** distribution
+**
+** Antares_Simulator is distributed in the hope that it will be useful,
+** but WITHOUT ANY WARRANTY; without even the implied warranty of
+** MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+** GNU General Public License for more details.
+**
+** You should have received a copy of the GNU General Public License
+** along with Antares_Simulator. If not, see <http://www.gnu.org/licenses/>.
+**
+** SPDX-License-Identifier: licenceRef-GPL3_WITH_RTE-Exceptions
+*/
+#include <antares/logs.h>
+
+#include "unit.h"
+
+namespace Antares::Data::ShortTermStorage
+{
+
+bool Unit::loadFromSection(const IniFile::Section& section)
+{
+    if (!section.firstProperty)
+        return false;
+
+    for (auto* property = section.firstProperty; property; property = property->next)
+    {
+        if (property->key.empty())
+        {
+            logs.warning() << "Loading units: `" << section.name << "`: Invalid key/value";
+            continue;
+        }
+        if (!properties.loadKey(property))
+        {
+            logs.warning() << "Loading units: `" << section.name << "`/`"
+                << property->key << "`: The property is unknown and ignored";
+        }
+    }
+
+    return true;
+}
+
+void Unit::printProperties()
+{
+    logs.notice() << "name : " << properties.name;
+    logs.notice() << "injectionnominalcapacity : " << properties.injectionCapacity;
+    logs.notice() << "withdrawalnominalcapacity : " << properties.withdrawalCapacity;
+    logs.notice() << "reservoircapacity : " << properties.capacity;
+    logs.notice() << "initiallevel : " << properties.initialLevel;
+    logs.notice() << "efficiency : " << properties.efficiencyFactor;
+    logs.notice() << "storagecycle : " << properties.cycleDuration;
+    for (auto& i : Properties::stStoragePropertyGroupEnum)
+        if (i.second == properties.group)
+            logs.notice() << "group : " << i.first;
+}
+
+} // namespace Antares::Data::ShortTermStorage
