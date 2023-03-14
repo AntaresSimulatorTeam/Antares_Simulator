d8fda2f63bc275e4bf74db12690d44a6d392763d
Jason Marechal <jason.marechal+github@gmail.com>
2023-03-14 16:08:28 +0100

    Refactor test: generate file in TEMP instead of datafiles

--- src/tests/src/libs/antares/study/constraint/test_constraint.cpp
+++ src/tests/src/libs/antares/study/constraint/test_constraint.cpp
@@ -3,17 +3,41 @@
 //
 #define BOOST_TEST_MODULE constraints
 #include <boost/test/unit_test.hpp>
+#include <fstream>
 #include "antares/study/constraint.h"
 #include "antares/study/area/area.h"
 #include "antares/study/study.h"
+#include <filesystem>
 
 using namespace Antares::Data;
+namespace fs = std::filesystem;
+
 BOOST_AUTO_TEST_CASE( load_basic_attributes ) {
     Study study;
 
     StudyLoadOptions options;
     BindConstList bindingConstraints;
-    const bool loading_ok = bindingConstraints.loadFromFolder(study, options, ".");
+
+    auto tmp_dir = fs::temp_directory_path();
+    auto working_tmp_dir = tmp_dir / std::tmpnam(nullptr);
+    fs::create_directories(working_tmp_dir);
+
+    std::ofstream constraints(working_tmp_dir / "bindingconstraints.ini");
+    constraints << "[1]\n"
+                << "name = dummy_name\n"
+                <<"id = dummy_id\n"
+                << "enabled = false\n"
+                << "type = hourly\n"
+                << "operator = equal\n"
+                << "filter-year-by-year = annual\n"
+                << "filter-synthesis = hourly\n"
+                << "comments = dummy_comment"
+                   ;
+    constraints.close();
+    std::ofstream rhs(working_tmp_dir / "dummy_id.txt");
+    rhs.close();
+
+    const bool loading_ok = bindingConstraints.loadFromFolder(study, options, working_tmp_dir.c_str());
 
     BOOST_CHECK_EQUAL(loading_ok, true);
     BOOST_CHECK_EQUAL(bindingConstraints.size(), 1);
