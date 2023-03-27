1a15017abadf9573b16a60cf0c43f25b2e8fad18
Vincent Payet <vincent.payet@rte-france.com>
2023-03-27 11:29:39 +0200

    [TESTS] Add testing for loading series files

--- src/tests/src/libs/antares/study/short-term-storage-input/short-term-storage-input.cpp
+++ src/tests/src/libs/antares/study/short-term-storage-input/short-term-storage-input.cpp
@@ -3,10 +3,15 @@
 #define WIN32_LEAN_AND_MEAN
 
 #include <boost/test/included/unit_test.hpp>
+#include <yuni/io/file.h>
+#include <filesystem>
+#include <fstream>
 
 #include <study.h>
 
 #include "series.h"
+
+#define SEP Yuni::IO::Separator
 
 using namespace std;
 using namespace Antares::Data;
@@ -19,6 +24,31 @@ void resizeFillVectors(ShortTermStorage::Series& series, double value, unsigned 
     series.inflows.resize(size, value);
     series.lowerRuleCurve.resize(size, value);
     series.upperRuleCurve.resize(size, value);
+}
+
+void createIndividualFileSeries(const std::string& path, double value, unsigned int size)
+{
+    std::ofstream outfile;
+    outfile.open(path, std::ofstream::out | std::ofstream::trunc);
+
+    for (unsigned int i = 0; i < size; i++)
+    {
+        outfile << value << std::endl;
+    }
+
+    outfile.close();
+}
+
+void createFileSeries(double value, unsigned int size)
+{
+    std::filesystem::path tmpDir = std::filesystem::temp_directory_path();
+    std::string folder = tmpDir;
+
+    createIndividualFileSeries(folder + SEP + "PMAX-injection.txt", value, size);
+    createIndividualFileSeries(folder + SEP + "PMAX-withdrawal.txt", value, size);
+    createIndividualFileSeries(folder + SEP + "inflows.txt", value, size);
+    createIndividualFileSeries(folder + SEP + "lower-rule-curve.txt", value, size);
+    createIndividualFileSeries(folder + SEP + "upper-rule-curve.txt", value, size);
 }
 
 // =================
@@ -54,9 +84,28 @@ BOOST_AUTO_TEST_CASE(check_vector_sizes)
 
     resizeFillVectors(series, 0.0, 8760);
     BOOST_CHECK(series.validate());
+}
 
-    std::cout << series.inflows.size();
+BOOST_AUTO_TEST_CASE(check_folder_loading)
+{
+    std::filesystem::path tmpDir = std::filesystem::temp_directory_path();
+    std::string folder = tmpDir;
 
+    createFileSeries(1.0, 8760);
+
+    BOOST_CHECK(series.loadFromFolder(folder));
+    BOOST_CHECK(series.validate());
+}
+
+BOOST_AUTO_TEST_CASE(check_folder_loading_too_big)
+{
+    std::filesystem::path tmpDir = std::filesystem::temp_directory_path();
+    std::string folder = tmpDir;
+
+    createFileSeries(1.0, 9000);
+
+    BOOST_CHECK(series.loadFromFolder(folder));
+    BOOST_CHECK(series.validate());
 }
 
 BOOST_AUTO_TEST_SUITE_END()
