36129d244f1951479bfe81cd98672476489f44aa
Vincent Payet <vincent.payet@rte-france.com>
2023-03-31 12:19:23 +0200

    [CLEAN] series cluster_list

--- src/libs/antares/study/parts/thermal/cluster_list.cpp
+++ src/libs/antares/study/parts/thermal/cluster_list.cpp
@@ -104,14 +104,6 @@ bool ThermalClusterList::loadFromFolder(Study& study, const AnyString& folder, A
                     continue;
                 }
 
-                if (study.header.version < 390)
-                {
-                    // We may have some strange name/id in older studies
-                    // temporary reverting to the old naming convention
-                    cluster->pID = cluster->name();
-                    cluster->pID.toLower();
-                }
-
                 // Keeping the current value of 'mustrun' somewhere else
                 cluster->mustrunOrigin = cluster->mustrun;
 
@@ -128,192 +120,25 @@ bool ThermalClusterList::loadFromFolder(Study& study, const AnyString& folder, A
                 // v5.0 format
                 // allow startup cost between [-5 000 000 ;-5 000 000] (was [-50 000;50 000])
 
-                if (study.header.version <= 500)
-                {
-                    // cluster->marketBidCost = Math::Round(cluster->marketBidCost, 3);
-
-                    // Market bid cost
-                    if (Math::Abs(cluster->marketBidCost) < 5.e-3)
-                        cluster->marketBidCost = 0.;
-                    else
-                    {
-                        if (Math::Abs(cluster->marketBidCost) > 5.e4)
-                            (cluster->marketBidCost > 0) ? cluster->marketBidCost = 5.e4
-                                                         : cluster->marketBidCost = -5.e4;
-                        else
-                            cluster->marketBidCost = Math::Round(cluster->marketBidCost, 3);
-                    }
-
-                    // Marginal cost
-                    if (Math::Abs(cluster->marginalCost) < 5.e-3)
-                        cluster->marginalCost = 0.;
-                    else
-                    {
-                        if (Math::Abs(cluster->marginalCost) > 5.e4)
-                            (cluster->marginalCost > 0) ? cluster->marginalCost = 5.e4
-                                                        : cluster->marginalCost = -5.e4;
-                        else
-                            cluster->marginalCost = Math::Round(cluster->marginalCost, 3);
-                    }
-
-                    // Spread cost - no negative values, must be 0 or >= 0.005
-                    if (cluster->spreadCost < 5.e-3)
-                        cluster->spreadCost = 0.;
-                    else
-                    {
-                        if (cluster->spreadCost > 5.e4)
-                            cluster->spreadCost = 5.e4;
-                        else
-                            cluster->spreadCost = Math::Round(cluster->spreadCost, 3);
-                    }
-
-                    // Fixed cost
-                    if (Math::Abs(cluster->fixedCost) < 5.e-3)
-                        cluster->fixedCost = 0.;
-                    else
-                    {
-                        if (Math::Abs(cluster->fixedCost) > 5.e4)
-                            (cluster->fixedCost > 0) ? cluster->fixedCost = 5.e4
-                                                     : cluster->fixedCost = -5.e4;
-                        else
-                            cluster->fixedCost = Math::Round(cluster->fixedCost, 3);
-                    }
-
-                    // Startup cost
-                    if (Math::Abs(cluster->startupCost) < 5.e-3)
-                        cluster->startupCost = 0.;
-                    else
-                    {
-                        if (Math::Abs(cluster->startupCost) > 5.e6)
-                            (cluster->startupCost > 0) ? cluster->startupCost = 5.e6
-                                                       : cluster->startupCost = -5.e6;
-                        else
-                            cluster->startupCost = Math::Round(cluster->startupCost, 3);
-                    }
-
-                    // Before v3.5, the marginal cost and the market bid cost were the same
-                    // (and was named 'operatingCost')
-                    // Rounding to 3 decimal places
-                    if (study.header.version < 350)
-                        cluster->marginalCost = cluster->marketBidCost;
-
-                    //	if (not Math::Zero(it->weeklyMinimumCapacity))
-                    //		it->minUpDownTime = 168;
-                    //	else
-                    //	{
-                    //		if (not Math::Zero(it->dailyMinimumCapacity))
-                    //			it->minUpDownTime = 24;
-                    //		else
-                    //			it->minUpDownTime = 1;
-                    //	}
-                    //	it->minStablePower =
-                    //		Math::Max(it->hourlyMinimumCapacity,
-                    //			Math::Max(it->dailyMinimumCapacity,
-                    // it->weeklyMinimumCapacity));
-                }
-                // Backward compatibility
-                // switch (it->minUpDownTime)
-                // {
-                //	case 1:
-                //		it->hourlyMinimumCapacity = it->minStablePower;
-                //		it->dailyMinimumCapacity  = 0;
-                //		it->weeklyMinimumCapacity = 0;
-                //		break;
-                //	case 24:
-                //		it->hourlyMinimumCapacity = 0;//it->minStablePower;
-                //		it->dailyMinimumCapacity  = it->minStablePower;
-                //		it->weeklyMinimumCapacity = 0;
-                //		break;
-                //	case 168:
-                //		it->hourlyMinimumCapacity = 0;//it->minStablePower;
-                //		it->dailyMinimumCapacity  = 0;//it->minStablePower;
-                //		it->weeklyMinimumCapacity = it->minStablePower;
-                //		break;
-                //	default:
-                //		logs.error() << "Invalid 'Min. Up/Down time' for the cluster '" <<
-                // it->name() << "'"; 		it->hourlyMinimumCapacity = it->minStablePower;
-                //		it->dailyMinimumCapacity  = 0;
-                //		it->weeklyMinimumCapacity = 0;
-                // }
-
                 // Modulation
                 modulationFile.clear() << folder << SEP << ".." << SEP << ".." << SEP << "prepro"
                                        << SEP << cluster->parentArea->id << SEP << cluster->id()
                                        << SEP << "modulation." << study.inputExtension;
 
-                if (study.header.version < 350)
-                {
-                    auto& modulation = cluster->modulation;
-                    // Before v3.5, the market bid modulation is missing
-                    bool r = modulation.loadFromCSVFile(
-                      modulationFile,
-                      2,
-                      HOURS_PER_YEAR,
-                      Matrix<>::optImmediate | Matrix<>::optMarkAsModified);
-                    if (r and modulation.width == 2)
-                    {
-                        modulation.resizeWithoutDataLost(thermalModulationMax, modulation.height);
-                        // Copy of the modulation cost into the market bid modulation
-                        // modulation.fillColumn(2, 1.);
-                        modulation.pasteToColumn(thermalModulationMarketBid,
-                                                 modulation[thermalModulationCost]);
-                        modulation.fillColumn(thermalMinGenModulation, 0.);
-                    }
-                    else
-                    {
-                        modulation.reset(thermalModulationMax, HOURS_PER_YEAR);
-                        modulation.fill(1.);
-                    }
-                    modulation.markAsModified();
-                    ret = ret and r;
-                }
-                else if (study.header.version <= 450)
-                {
-                    auto& modulation = cluster->modulation;
-                    // Before v4.5, the modulation generation relative is missing
-                    bool r = cluster->modulation.loadFromCSVFile(
-                      modulationFile,
-                      3,
-                      HOURS_PER_YEAR,
-                      Matrix<>::optImmediate | Matrix<>::optMarkAsModified);
 
-                    if (r and modulation.width == 3)
-                    {
-                        modulation.resizeWithoutDataLost(thermalModulationMax, modulation.height);
-                    }
-                    else
-                    {
-                        modulation.reset(thermalModulationMax, HOURS_PER_YEAR);
-                        modulation.fill(1.);
-                    }
-                    // switch thermalModulationMarketBid and thermalModulationCapacity
-                    // They have a wrong order
-                    modulation.pasteToColumn(thermalMinGenModulation,
-                                             modulation[thermalModulationMarketBid]);
-                    modulation.pasteToColumn(thermalModulationMarketBid,
-                                             modulation[thermalModulationCapacity]);
-                    modulation.pasteToColumn(thermalModulationCapacity,
-                                             modulation[thermalMinGenModulation]);
-                    modulation.fillColumn(thermalMinGenModulation, 0.);
-                    modulation.markAsModified();
-                    ret = ret and r;
-                }
-                else
+                enum
                 {
-                    enum
-                    {
-                        options = Matrix<>::optFixedSize,
-                    };
-                    bool r = cluster->modulation.loadFromCSVFile(
-                      modulationFile, thermalModulationMax, HOURS_PER_YEAR, options);
-                    if (not r and study.usedByTheSolver)
-                    {
-                        cluster->modulation.reset(thermalModulationMax, HOURS_PER_YEAR);
-                        cluster->modulation.fill(1.);
-                        cluster->modulation.fillColumn(thermalMinGenModulation, 0.);
-                    }
-                    ret = ret and r;
+                    options = Matrix<>::optFixedSize,
+                };
+                bool r = cluster->modulation.loadFromCSVFile(
+                        modulationFile, thermalModulationMax, HOURS_PER_YEAR, options);
+                if (not r and study.usedByTheSolver)
+                {
+                    cluster->modulation.reset(thermalModulationMax, HOURS_PER_YEAR);
+                    cluster->modulation.fill(1.);
+                    cluster->modulation.fillColumn(thermalMinGenModulation, 0.);
                 }
+                ret = ret and r;
 
                 // Special operations when not ran from the interface (aka solver)
                 if (study.usedByTheSolver)
