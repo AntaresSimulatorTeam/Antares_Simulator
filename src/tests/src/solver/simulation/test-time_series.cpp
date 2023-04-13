03dab945d4ae5c64128c5d63200aae266646bbb4
Jason Marechal <jason.marechal+github@gmail.com>
2023-04-13 09:42:18 +0200

    Test that reading different TS size fail [PASS]

--- src/tests/src/solver/simulation/test-time_series.cpp
+++ src/tests/src/solver/simulation/test-time_series.cpp
@@ -38,73 +38,79 @@ void CheckEqual(Matrix<Ta>& a, Matrix<Tb>& b) {
     }
 }
 
-BOOST_AUTO_TEST_CASE(load_binding_constraints_timeseries) {
+struct Fixture {
+    Fixture() {
+
+        auto tmp_dir = fs::temp_directory_path();
+        working_tmp_dir = tmp_dir / std::tmpnam(nullptr);
+        fs::create_directories(working_tmp_dir);
+
+        std::ofstream constraints(working_tmp_dir / "bindingconstraints.ini");
+        constraints << "[1]\n"
+                    << "name = dummy_name\n"
+                    <<"id = dummy_id\n"
+                    << "enabled = false\n"
+                    << "type = hourly\n"
+                    << "operator = equal\n"
+                    << "filter-year-by-year = annual\n"
+                    << "filter-synthesis = hourly\n"
+                    << "comments = dummy_comment\n"
+                    << "group = dummy_group\n"
+                ;
+        constraints.close();
+
+        initializeStudy(study);
+
+        expected_lower_bound_series.resize(3, 8784);
+        expected_upper_bound_series.resize(3, 8784);
+        expected_equality_series.resize(3, 8784);
+
+        expected_lower_bound_series.fillColumn(0, 0.3);
+        expected_lower_bound_series.fillColumn(1, 0.5);
+        expected_lower_bound_series.fillColumn(2, 1);
+
+        expected_upper_bound_series.fillColumn(0, 0.2);
+        expected_upper_bound_series.fillColumn(1, 0.6);
+        expected_upper_bound_series.fillColumn(2, 0);
+
+        expected_equality_series.fillColumn(0, 0.1);
+        expected_equality_series.fillColumn(1, 0.55);
+        expected_equality_series.fillColumn(2, 0.9);
+        expected_equality_series[0][8763] = 1;
+
+        fs::create_directories(working_tmp_dir / "dummy_group");
+        expected_lower_bound_series.saveToCSVFile((working_tmp_dir / "dummy_group" / "dummy_group_lt.txt").c_str());
+        expected_upper_bound_series.saveToCSVFile((working_tmp_dir / "dummy_group" /"dummy_group_gt.txt").c_str());
+        expected_equality_series.saveToCSVFile((working_tmp_dir / "dummy_group" /"dummy_group_eq.txt").c_str());
+    };
     Study study;
-
     StudyLoadOptions options;
+    std::filesystem::path working_tmp_dir;
     BindingConstraintsList bindingConstraints;
+    Matrix<double> expected_lower_bound_series;
+    Matrix<double> expected_upper_bound_series;
+    Matrix<double> expected_equality_series;
+};
 
-    auto tmp_dir = fs::temp_directory_path();
-    auto working_tmp_dir = tmp_dir / std::tmpnam(nullptr);
-    fs::create_directories(working_tmp_dir);
+BOOST_FIXTURE_TEST_SUITE(BC_TimeSeries, Fixture)
 
-    std::ofstream constraints(working_tmp_dir / "bindingconstraints.ini");
-    constraints << "[1]\n"
-                << "name = dummy_name\n"
-                <<"id = dummy_id\n"
-                << "enabled = false\n"
-                << "type = hourly\n"
-                << "operator = equal\n"
-                << "filter-year-by-year = annual\n"
-                << "filter-synthesis = hourly\n"
-                << "comments = dummy_comment\n"
-                << "group = dummy_group\n"
-            ;
-    constraints.close();
-
-    initializeStudy(study);
-
-    Matrix expected_lower_bound_series;
-    Matrix expected_upper_bound_series;
-    Matrix expected_equality_series;
-
-    expected_lower_bound_series.resize(3, 8784);
-    expected_upper_bound_series.resize(3, 8784);
-    expected_equality_series.resize(3, 8784);
-
-    expected_lower_bound_series.fillColumn(0, 0.3);
-    expected_lower_bound_series.fillColumn(1, 0.5);
-    expected_lower_bound_series.fillColumn(2, 1);
-
-    expected_upper_bound_series.fillColumn(0, 0.2);
-    expected_upper_bound_series.fillColumn(1, 0.6);
-    expected_upper_bound_series.fillColumn(2, 0);
-
-    expected_equality_series.fillColumn(0, 0.1);
-    expected_equality_series.fillColumn(1, 0.55);
-    expected_equality_series.fillColumn(2, 0.9);
-    expected_equality_series[0][8763] = 1;
-
-    fs::create_directories(working_tmp_dir / "dummy_group");
-    expected_lower_bound_series.saveToCSVFile((working_tmp_dir / "dummy_group" / "dummy_group_lt.txt").c_str());
-    expected_upper_bound_series.saveToCSVFile((working_tmp_dir / "dummy_group" /"dummy_group_gt.txt").c_str());
-    expected_equality_series.saveToCSVFile((working_tmp_dir / "dummy_group" /"dummy_group_eq.txt").c_str());
-
+BOOST_AUTO_TEST_CASE(load_binding_constraints_timeseries) {
     const bool loading_ok = bindingConstraints.loadFromFolder(study, options, working_tmp_dir.c_str());
     BOOST_CHECK_EQUAL(loading_ok, true);
-    auto ts = bindingConstraints.time_series["dummy_group"];
-    auto& tss = bindingConstraints.time_series["dummy_group"];
-    auto lb = ts.lower_bound_series;
-    auto& lbb = ts.lower_bound_series;
     CheckEqual(bindingConstraints.time_series["dummy_group"].lower_bound_series, expected_lower_bound_series);
     CheckEqual(bindingConstraints.time_series["dummy_group"].upper_bound_series, expected_upper_bound_series);
     CheckEqual(bindingConstraints.time_series["dummy_group"].equality_series, expected_equality_series);
 }
 
 BOOST_AUTO_TEST_CASE(verify_all_series_have_the_same_width) {
-    BOOST_CHECK(false);
+        expected_upper_bound_series.resize(1, 8784);
+        expected_upper_bound_series.saveToCSVFile((working_tmp_dir / "dummy_group" /"dummy_group_gt.txt").c_str());
+        const bool loading_ok = bindingConstraints.loadFromFolder(study, options, working_tmp_dir.c_str());
+        BOOST_CHECK_EQUAL(loading_ok, false);
 }
 
 BOOST_AUTO_TEST_CASE(verify_all_bc_in_a_group_have_same_type) {
     BOOST_CHECK(false);
 }
+
+BOOST_AUTO_TEST_SUITE_END()
