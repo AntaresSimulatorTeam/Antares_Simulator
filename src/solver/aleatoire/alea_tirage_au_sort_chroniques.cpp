287a4941020fae599be51419d2f75e519c6a9f14
Jason Marechal <jason.marechal+github@gmail.com>
2023-05-09 14:49:59 +0200

    Load time_series empty files as a column of 0s

--- src/solver/aleatoire/alea_tirage_au_sort_chroniques.cpp
+++ src/solver/aleatoire/alea_tirage_au_sort_chroniques.cpp
@@ -191,7 +191,8 @@ static void InitializeTimeSeriesNumbers_And_ThermalClusterProductionCost(
     //Setting 0 for time_series of width 0 is done when using the value.
     //To do this here we would have to check every BC for its width
     for (const auto& [group_name, _] : study.bindingConstraints.time_series_numbers) {
-        assert(year < study.bindingConstraints.time_series_numbers[group_name].timeseriesNumbers.height);
+        auto number_of_ts_numbers = study.bindingConstraints.time_series_numbers[group_name].timeseriesNumbers.height; //Supposely number of time_series
+        assert(year < number_of_ts_numbers || number_of_ts_numbers == 1); //If only 1 ts_number we suppose only one TS. Any "year" will be converted to "0" later
         NumeroChroniquesTireesParGroup[numSpace][group_name] = study.bindingConstraints.time_series_numbers[group_name].timeseriesNumbers[0][year];
     }
 }
