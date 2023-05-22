3b0ebcae0dda7c3a61b58b6e5258566e448f5c3a
Vincent Payet <vincent.payet@rte-france.com>
2023-05-22 11:34:08 +0200

    [DEV] Outfile for each study and test

--- src/tests/performance/performance.py
+++ src/tests/performance/performance.py
@@ -6,22 +6,23 @@ from pathlib import Path
 
 class StudyList(object):
 
-    def __init__(self, path):
+    def __init__(self, name, path):
+        self.name = name
         self.path = Path(path).resolve()
 
     def run_valgrind_base(self, exe):
-        valgrindOutFile = "--log-file=valgrind.out"
-        res = subprocess.run(["valgrind", valgrindOutFile, exe, self.path])
+        outFile = "--log-file=" + self.name + ".valgrind.out"
+        res = subprocess.run(["valgrind", outFile, exe, self.path])
         assert (res.returncode == 0), "The exec failed for study: " + str(self.path)
 
     def run_valgrind_massif(self, exe):
-        massifOutFile = "valgrind.massif"
-        res = subprocess.run(["valgrind" , "--tool=massif", "--massif-out-file=" + massifOutFile, exe, self.path])
+        outFile = "--massif-out-file=" + self.name + ".valgrind.massif"
+        res = subprocess.run(["valgrind" , "--tool=massif", outFile, exe, self.path])
         assert (res.returncode == 0), "The exec failed for study: " + str(self.path)
 
     def run_time(self, exe):
-        massifOutFile = "time.txt"
-        res = subprocess.run(["/bin/time" , "-o", timeOutFile, "-v", exe, self.path])
+        outFile = self.name + ".time.txt"
+        res = subprocess.run(["/bin/time" , "-o", outFile, "-v", exe, self.path])
         assert (res.returncode == 0), "The exec failed for study: " + str(self.path)
 
     def run_all(self, exe):
@@ -31,22 +32,13 @@ class StudyList(object):
 
 
 study_list = []
-study_list.append(StudyList("../resources/Antares_Simulator_Tests/short-tests/001 One node - passive/"))
-study_list.append(StudyList("../resources/Antares_Simulator_Tests/medium-tests/043 Multistage study-8-Kirchhoff"))
-study_list.append(StudyList("../resources/Antares_Simulator_Tests/long-tests/079 Zero  Power Balance - Type 1"))
+study_list.append(StudyList("short", "../resources/Antares_Simulator_Tests/short-tests/001 One node - passive/"))
+study_list.append(StudyList("medium", "../resources/Antares_Simulator_Tests/medium-tests/043 Multistage study-8-Kirchhoff"))
+study_list.append(StudyList("long", "../resources/Antares_Simulator_Tests/long-tests/079 Zero  Power Balance - Type 1"))
 
-def valgrind_short(solver_path):
-    study_list[0].run_all(solver_path)
-
-def valgrind_medium(solver_path):
-    study_list[1].run_all(solver_path)
-
-def valgrind_long(solver_path):
-    study_list[2].run_all(solver_path)
 
 def performance(solver_path):
-    valgrind_short(solver_path)
-    valgrind_medium(solver_path)
-    valgrind_long(solver_path)
+    for i in range(study_list.length()
+        study_list[i].run_all(solver_path)
 
 performance("/home/payetvin/Antares_Simulator/_build_debug/solver/antares-8.6-solver")
