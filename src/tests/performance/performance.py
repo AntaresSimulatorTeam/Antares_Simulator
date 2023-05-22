411da53de9ba6a0e6d550f9f7f4719e6addaa039
Vincent Payet <vincent.payet@rte-france.com>
2023-05-22 14:56:55 +0200

    [DEV] Use /bin/time instead of valgrind, functions to get values implem

--- src/tests/performance/performance.py
+++ src/tests/performance/performance.py
@@ -10,26 +10,22 @@ class StudyList(object):
     def __init__(self, name, path):
         self.name = name
         self.path = Path(path).resolve()
+        self.outFile = name + "-metrics.txt"
 
-    def run_valgrind_base(self, exe):
-        outFile = "--log-file=" + self.name + ".valgrind.out"
-        res = subprocess.run(["valgrind", outFile, exe, self.path])
+    def run_metrics(self, exe):
+        res = subprocess.run(["/bin/time" , "-o", self.outFile, "-v", exe, self.path])
         assert (res.returncode == 0), "The exec failed for study: " + str(self.path)
 
-    def run_valgrind_massif(self, exe):
-        outFile = "--massif-out-file=" + self.name + ".valgrind.massif"
-        res = subprocess.run(["valgrind" , "--tool=massif", outFile, exe, self.path])
-        assert (res.returncode == 0), "The exec failed for study: " + str(self.path)
+    def get_memory(self):
+        ps = subprocess.run(["sed -n -e 's/^.*Maximum.*: //p' " + self.outFile], shell=True, capture_output=True)
+        return int(ps.stdout)
 
-    def run_time(self, exe):
-        outFile = self.name + ".time.txt"
-        res = subprocess.run(["/bin/time" , "-o", outFile, "-v", exe, self.path])
-        assert (res.returncode == 0), "The exec failed for study: " + str(self.path)
+    def get_time(self):
+        ps = subprocess.run(["sed -n -e 's/^.*User time.*: //p' " + self.outFile], shell=True, capture_output=True)
+        ps2 = subprocess.run(["sed -n -e 's/^.*System time.*: //p' " + self.outFile], shell=True, capture_output=True)
+        return float(ps.stdout) + float(ps2.stdout)
 
-    def run_all(self, exe):
-        self.run_valgrind_base(exe)
-        self.run_valgrind_massif(exe)
-        self.run_time(exe)
+
 
 
 study_list = []
@@ -40,6 +36,8 @@ study_list.append(StudyList("long", "../resources/Antares_Simulator_Tests/long-t
 
 def performance(solver_path):
     for studies in study_list:
-        studies.run_all(solver_path)
+        studies.run_metrics(solver_path)
 
 performance("/home/payetvin/Antares_Simulator/_build_debug/solver/antares-8.6-solver")
+print(study_list[0].get_memory())
+print(study_list[0].get_time())
