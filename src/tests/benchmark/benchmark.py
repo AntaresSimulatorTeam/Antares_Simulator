01174833051cd4503c391e145017677d16438e9d
payetvin <113102157+payetvin@users.noreply.github.com>
2023-06-02 16:38:11 +0200

    Performance metrics (#1306)
    * [DEV] Create performance.py, run valgrind and massif
    
    * [DEV] Improvements
    
    * [DEV] Outfile for each study and test
    
    * [FIX] Loop
    
    * [DEV] Use /bin/time instead of valgrind, functions to get values implem
    
    * [DEV] create_json working
    
    * [DEV] Create yml
    
    * [DEV] update branch
    
    * [DEV] Test upload page
    
    * [DEV] Test upload page
    
    * [CI] Test page.yml
    
    * [CI] Add gh-pages.yml
    
    * [CI] Fix gh-pages.yml
    
    * [CI] Fix gh-pages.yml 2
    
    * [DEV] Json file for all studies
    
    * [DEV] Add section in dict
    
    * [DEV] Remove yml
    
    * [DEV] execution_info.ini in JSON
    
    * [DEV] Use python to find match in files for time and memory
    
    * [FIX] Code smells
    
    * [FIX] Comments
    
    * [FIX] Renamed performance.py into benchmark.py
    
    * [FIX] Renamed dir perfomance into benchmark
    
    * [FIX] Florian comments
    
    * [FIX] Remove comments on run_metrics
    
    * [DEV] Give solver path in args
    
    * [DEV] Add file studiesToBenchmark.json
    
    * [DEV] New sections, removed time_s, time of execution.ini in seconds
    
    * [DEV] Add shebang and made file executable, add verif for number of args
    
    * [DEV] Directory abstraction, remove metrics.txt file after using it
    
    * [GIT] Add results.json to .gitignore

---
+++ src/tests/benchmark/benchmark.py
@@ -0,0 +1,110 @@
+#!/usr/bin/python3
+import configparser
+import subprocess
+import datetime
+import json
+import sys
+import os
+import re
+
+from pathlib import Path
+
+# needed to find the execution_info.ini
+def get_latest_output_folder(path):
+    output_path_study = os.path.join(path, "output")
+    files = os.listdir(output_path_study)
+    paths = [os.path.join(output_path_study, basename) for basename in files]
+    return max(paths, key=os.path.getctime)
+
+# used to get the memory and time from the /bin/time -v file
+def search_patern_in_file(file_name, pattern):
+    with open(file_name) as f:
+        s = f.read()
+        m = re.search(pattern, s)
+        if m:
+            return m.groups(1)[1]
+        else:
+            raise Exception("Sorry, no numbers below zero")
+
+def get_git_revision_hash() -> str:
+    return subprocess.check_output(['git', 'rev-parse', 'HEAD']).decode('ascii').strip()
+
+class StudyList(object):
+
+    def __init__(self, name, path):
+        self.name = name                        # study name, used in JSON and out_file
+        self.path = Path(path).resolve()        # study path
+        self.out_file = name + "-metrics.txt"   # name of the /bin/time -v file
+
+    def run_metrics(self, exe):
+        res = subprocess.run(["/bin/time" , "-o", self.out_file, "-v", exe, self.path])
+        assert (res.returncode == 0), "The exec failed for study: " + str(self.path)
+
+    def get_memory(self):
+        memory = int(search_patern_in_file(self.out_file, "Maximum(.*): (.*)"))
+        os.remove(self.out_file) # clean file after we get data
+        return memory
+
+    def create_json(self):
+        return { 'peak_memory_mb' : self.get_memory() / 1024 } # convert to Mb
+
+    # read the [sections] from the execution_info.ini file in the study output
+    def read_execution_ini(self, json_data):
+        output_folder = get_latest_output_folder(self.path)
+        ini_path = os.path.join(output_folder, "execution_info.ini")
+
+        config = configparser.ConfigParser()
+        config.read(ini_path)
+        json_data["durations_s"] = dict(config.items('durations_ms'))
+        json_data["optimization problem"] = dict(config.items('optimization problem'))
+        json_data["study"] = dict(config.items('study'))
+
+        # convert ms into seconds
+        durations = json_data['durations_s']
+        for i in durations:
+            durations[i] = float(durations[i]) / 1000
+
+
+# load the JSON list containing studies to benchmark
+def read_study_list():
+    study_list = []
+    with open("studiesToBenchmark.json", "r") as f:
+        studies = json.load(f)
+        for name in studies:
+            study_list.append(StudyList(name, studies[name]))
+    return study_list
+
+
+def main(solver_path):
+    # init and general data for the JSON
+    results = {}
+    results["commit-id"] = get_git_revision_hash()
+
+    date = datetime.datetime.now()
+    results["date"] = date.strftime("%x %X")
+
+    # Execution and results for each study
+    for studies in read_study_list():
+        studies.run_metrics(solver_path)
+        results[studies.name] = studies.create_json()
+        studies.read_execution_ini(results[studies.name])
+
+    # Writing the JSON
+    with open("results.json", "w") as f:
+        f.write(json.dumps(results))
+
+
+if len(sys.argv) < 2:
+    raise Exception("Not enough arguments, need solver path")
+
+solver_path = sys.argv[1]
+if not os.path.isfile(solver_path):
+    raise Exception("Invalid solver path, file not found")
+
+
+# change current directory to the script directory
+abspath = os.path.abspath(__file__)
+dname = os.path.dirname(abspath)
+os.chdir(dname)
+
+main(solver_path)
