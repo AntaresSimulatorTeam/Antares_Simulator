5e6c50ba848e755115d5ec484b65a617c470cf9f
Jason Marechal <jason.marechal+github@gmail.com>
2023-06-05 17:31:58 +0200

    Test for legacy studies

--- src/tests/src/libs/antares/study/constraint/test_constraint.cpp
+++ src/tests/src/libs/antares/study/constraint/test_constraint.cpp
@@ -157,3 +157,49 @@ BOOST_AUTO_TEST_CASE(BC_load_range_type) {
     BOOST_CHECK_CLOSE(bc_gt->TimeSeries()[1][30], 0.6, 0.0001);
     BOOST_CHECK_CLOSE(bc_gt->TimeSeries()[2][8783], 0.8, 0.0001);
 }
+
+BOOST_AUTO_TEST_CASE(BC_load_legacy) {
+    Study study;
+    study.areaAdd("area1");
+    study.areaAdd("area2");
+    study.areaAdd("area3");
+
+    StudyLoadOptions options;
+    BindingConstraintsList bindingConstraints;
+
+    auto tmp_dir = fs::temp_directory_path();
+    auto working_tmp_dir = tmp_dir / std::tmpnam(nullptr);
+    fs::create_directories(working_tmp_dir);
+
+    std::ofstream constraints(working_tmp_dir / "bindingconstraints.ini");
+    constraints << "[1]\n"
+                << "name = dummy_name\n"
+                <<"id = dummy_id\n"
+                << "enabled = false\n"
+                << "type = hourly\n"
+                << "operator = less\n"
+                << "filter-year-by-year = annual\n"
+                << "filter-synthesis = hourly\n"
+                << "comments = dummy_comment\n"
+                << "area1%area2 = 1.000000\n"
+                << "area2%area3 = -1.000000\n"
+                << "area3%area1 = 2.000000\n"
+            ;
+    constraints.close();
+    std::ofstream lt(working_tmp_dir / "dummy_id.txt");
+    for (int i = 0; i < 8784; ++i) {
+        lt << "0.2\t0.4\t0.6\n";
+    }
+    lt.close();
+
+    study.header.version = version860;
+    const bool loading_ok = bindingConstraints.loadFromFolder(study, options, working_tmp_dir.string());
+
+    BOOST_CHECK_EQUAL(loading_ok, true);
+    BOOST_CHECK_EQUAL(bindingConstraints.size(), 1);
+
+    auto bc_lt = (*bindingConstraints.begin());
+    BOOST_CHECK_CLOSE(bc_lt->TimeSeries()[0][0], 0.2, 0.0001);
+    BOOST_CHECK_CLOSE(bc_lt->TimeSeries()[1][30], 0.4, 0.0001);
+    BOOST_CHECK_CLOSE(bc_lt->TimeSeries()[2][8783], 0.6, 0.0001);
+}
