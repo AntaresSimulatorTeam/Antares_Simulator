da62f466a5c1122ee443ee4bd381d2dcd05b6981
Jason Marechal <jason.marechal@gmail.com>
2023-06-13 14:42:18 +0200

    Give same name and id to both constraints to ensure they are processed in the same order as before

--- src/libs/antares/study/binding_constraint/BindingConstraintLoader.cpp
+++ src/libs/antares/study/binding_constraint/BindingConstraintLoader.cpp
@@ -150,7 +150,8 @@ BindingConstraintLoader::load(EnvForLoading env) {
     if (bc->operatorType() == BindingConstraint::opBoth) {
         auto greater_bc = std::make_shared<BindingConstraint>();
         greater_bc->copyFrom(bc.get());
-        greater_bc->name(bc->pID); //Need the same id to load time series file properly
+        greater_bc->name(bc->name());
+        greater_bc->pID = bc->pID;
         bc->operatorType(BindingConstraint::opLess);
         greater_bc->operatorType(BindingConstraint::opGreater);
         if (loadTimeSeries(env, bc.get()) && loadTimeSeries(env, greater_bc.get())) {
