6190bf13d4764936409f2b784c465bf24344ddd1
Jason Marechal <jason.marechal+github@gmail.com>
2023-06-28 16:30:24 +0200

    RED

---
+++ src/libs/antares/study/binding_constraint/BindingConstraintGroupRepository.h
@@ -0,0 +1,14 @@
+//
+// Created by marechaljas on 28/06/23.
+//
+
+#pragma once
+
+namespace Antares::Data {
+
+class BindingConstraintGroupRepository {
+public:
+    [[no_discard]] unsigned size();
+};
+
+} // DAta
