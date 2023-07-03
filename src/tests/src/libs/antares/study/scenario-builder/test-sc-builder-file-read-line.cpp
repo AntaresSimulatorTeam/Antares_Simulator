8067c9fd95ab190e2960ec2eb83163499fddb256
NikolaIlic <nikola.ilic@redstork-solutions.com>
2023-07-03 13:41:29 +0200

    Testing sc buildr file read

--- src/tests/src/libs/antares/study/scenario-builder/test-sc-builder-file-read-line.cpp
+++ src/tests/src/libs/antares/study/scenario-builder/test-sc-builder-file-read-line.cpp
@@ -309,7 +309,7 @@ BOOST_AUTO_TEST_CASE(on_rn_cluster32_of_area3_and_on_year_2__solar_TS_number_4_i
 
 
 // ========================
-// Tests on Hydro levels
+// Tests on Hydro initial levels
 // ========================
 BOOST_AUTO_TEST_CASE(on_area1_and_on_year_17__hydro_level_0_123_is_chosen__reading_OK)
 {
@@ -390,6 +390,54 @@ BOOST_AUTO_TEST_CASE(on_link_area2_area3_and_on_year_19__ntc_TS_number_6_is_chos
 
 	BOOST_CHECK(my_rule.apply());
 	BOOST_CHECK_EQUAL(link_23->timeseriesNumbers[0][yearNumber.to<uint>()], tsNumber.to<uint>() - 1);
+}
+
+// ========================
+// Tests on Hydro final levels
+// ========================
+BOOST_AUTO_TEST_CASE(on_area1_and_on_year_8__hydro_level_0_342_is_chosen__reading_OK)
+{
+        AreaName yearNumber = "8";
+        String level = "0.342";
+        AreaName::Vector splitKey = {"hfl", "area 1", yearNumber};
+        my_rule.readLine(splitKey, level, false);
+
+        BOOST_CHECK_EQUAL(my_rule.hydroFinalLevels.get_value(yearNumber.to<uint>(), area_1->index),
+                          level.to<double>());
+
+        BOOST_CHECK(my_rule.apply());
+        BOOST_CHECK_EQUAL(study->scenarioFinalHydroLevels[area_1->index][yearNumber.to<uint>()],
+                          level.to<double>());
+}
+
+BOOST_AUTO_TEST_CASE(on_area2_and_on_year_1__hydro_level_2_4_is_chosen_level_lowered_to_1__reading_OK)
+{
+        AreaName yearNumber = "1";
+        String level = "2.4";
+        AreaName::Vector splitKey = {"hfl", "area 2", yearNumber};
+        BOOST_CHECK(my_rule.readLine(splitKey, level, false));
+
+        BOOST_CHECK_EQUAL(my_rule.hydroFinalLevels.get_value(yearNumber.to<uint>(), area_2->index),
+                          1.);
+
+        BOOST_CHECK(my_rule.apply());
+        BOOST_CHECK_EQUAL(study->scenarioFinalHydroLevels[area_2->index][yearNumber.to<uint>()],
+                          1.);
+}
+
+BOOST_AUTO_TEST_CASE(on_area3_and_on_year_3__hydro_level_neg_5_2_is_chosen__level_raised_to_0__reading_OK)
+{
+        AreaName yearNumber = "3";
+        String level = "-5.2";
+        AreaName::Vector splitKey = {"hfl", "area 3", yearNumber};
+        BOOST_CHECK(my_rule.readLine(splitKey, level, false));
+
+        BOOST_CHECK_EQUAL(my_rule.hydroFinalLevels.get_value(yearNumber.to<uint>(), area_3->index),
+                          0.);
+
+        BOOST_CHECK(my_rule.apply());
+        BOOST_CHECK_EQUAL(study->scenarioFinalHydroLevels[area_3->index][yearNumber.to<uint>()],
+                          0.);
 }
 
 // ========================
