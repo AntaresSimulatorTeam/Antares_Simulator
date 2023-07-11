57f628d055adbb687550157336605bf3b000221e
Florian Omnes <florian@omnes.cc>
2023-07-11 14:25:02 +0200

    Apply clang-format

--- src/tests/src/libs/antares/study/thermal-price-definition/thermal-price-definition.cpp
+++ src/tests/src/libs/antares/study/thermal-price-definition/thermal-price-definition.cpp
@@ -25,88 +25,92 @@ static std::filesystem::path getFolder()
 // Use RAII to simplify teardown
 struct ThermalIniFile
 {
-  explicit ThermalIniFile()
-  {
-    auto folder = getFolder();
+    explicit ThermalIniFile()
+    {
+        auto folder = getFolder();
 
-    std::ofstream outfile(folder / "list.ini", std::ofstream::out | std::ofstream::trunc);
+        std::ofstream outfile(folder / "list.ini", std::ofstream::out | std::ofstream::trunc);
 
-    outfile << "[area]" << std::endl;
-    outfile << "name = area" << std::endl;
-    outfile << "group = Lignite" << std::endl;
-    outfile << "unitcount = 4" << std::endl;
-    outfile << "nominalcapacity = 900.000000" << std::endl;
-    outfile << "min-stable-power = 400.000000" << std::endl;
-    outfile << "min-up-time = 24" << std::endl;
-    outfile << "min-down-time = 24" << std::endl;
-    outfile << "co2 = 2.000000" << std::endl;
-    outfile << "marginal-cost = 23.000000" << std::endl;
-    outfile << "fixed-cost = 1700.000000" << std::endl;
-    outfile << "startup-cost = 70000.000000" << std::endl;
-    outfile << "market-bid-cost = 35.000000" << std::endl;
-    outfile << "costgeneration = useCostTimeseries" << std::endl;
-    outfile << "efficiency = 36.00000" << std::endl;
-    outfile << "variableomcost = 12.120000" << std::endl;
+        outfile << "[area]" << std::endl;
+        outfile << "name = area" << std::endl;
+        outfile << "group = Lignite" << std::endl;
+        outfile << "unitcount = 4" << std::endl;
+        outfile << "nominalcapacity = 900.000000" << std::endl;
+        outfile << "min-stable-power = 400.000000" << std::endl;
+        outfile << "min-up-time = 24" << std::endl;
+        outfile << "min-down-time = 24" << std::endl;
+        outfile << "co2 = 2.000000" << std::endl;
+        outfile << "marginal-cost = 23.000000" << std::endl;
+        outfile << "fixed-cost = 1700.000000" << std::endl;
+        outfile << "startup-cost = 70000.000000" << std::endl;
+        outfile << "market-bid-cost = 35.000000" << std::endl;
+        outfile << "costgeneration = useCostTimeseries" << std::endl;
+        outfile << "efficiency = 36.00000" << std::endl;
+        outfile << "variableomcost = 12.120000" << std::endl;
 
-    outfile.flush();
-  }
+        outfile.flush();
+    }
 
-  ~ThermalIniFile() noexcept
-  {
-    auto folder = getFolder();
-    std::filesystem::remove(folder / "list.ini");
-  }
+    ~ThermalIniFile() noexcept
+    {
+        auto folder = getFolder();
+        std::filesystem::remove(folder / "list.ini");
+    }
 };
 
 struct SeriesFile
 {
-  SeriesFile(const std::string& name, std::size_t size) : name_(name)
-  {
-    auto folder = getFolder();
-    std::ofstream outfile(folder / name, std::ofstream::out | std::ofstream::trunc);
-
-    for (std::size_t i = 0; i < size; i++)
+    SeriesFile(const std::string& name, std::size_t size) : name_(name)
     {
-        outfile << 1 << std::endl;
-    }
-  }
+        auto folder = getFolder();
+        std::ofstream outfile(folder / name, std::ofstream::out | std::ofstream::trunc);
 
-  ~SeriesFile()
-  {
-    auto folder = getFolder();
-    std::filesystem::remove(folder / name_);
-  }
+        for (std::size_t i = 0; i < size; i++)
+        {
+            outfile << 1 << std::endl;
+        }
+    }
+
+    ~SeriesFile()
+    {
+        auto folder = getFolder();
+        std::filesystem::remove(folder / name_);
+    }
+
 private:
-  const std::string name_;
+    const std::string name_;
 };
 
 struct CO2CostFile : private SeriesFile
 {
-  CO2CostFile(std::size_t size) : SeriesFile("CO2Cost.txt", size) {}
+    CO2CostFile(std::size_t size) : SeriesFile("CO2Cost.txt", size)
+    {
+    }
 };
 
 struct FuelCostFile : private SeriesFile
 {
-  FuelCostFile(std::size_t size) : SeriesFile("fuelCost.txt", size) {}
+    FuelCostFile(std::size_t size) : SeriesFile("fuelCost.txt", size)
+    {
+    }
 };
 
-void fillThermalEconomicTimeSeries(ThermalCluster *c)
+void fillThermalEconomicTimeSeries(ThermalCluster* c)
 {
     c->thermalEconomicTimeSeries[0].productionCostTs.fill(1);
     c->thermalEconomicTimeSeries[0].marketBidCostPerHourTs.fill(1);
     c->thermalEconomicTimeSeries[0].marginalCostPerHourTs.fill(1);
 }
 
-
 // =================
 // The fixture
 // =================
 struct Fixture : private ThermalIniFile
 {
-    Fixture(const Fixture & f) = delete;
-    Fixture(const Fixture && f) = delete;
-    Fixture & operator= (const Fixture & f) = delete;
-    Fixture& operator= (const Fixture && f) = delete;
+    Fixture(const Fixture& f) = delete;
+    Fixture(const Fixture&& f) = delete;
+    Fixture& operator=(const Fixture& f) = delete;
+    Fixture& operator=(const Fixture&& f) = delete;
     Fixture()
     {
         area = study->areaAdd("area");
@@ -122,7 +126,6 @@ struct Fixture : private ThermalIniFile
     Study::Ptr study = std::make_shared<Study>();
 };
 
-
 // ==================
 // Tests section
 // ==================
@@ -136,7 +139,6 @@ BOOST_AUTO_TEST_CASE(ThermalClusterList_loadFromFolder_basic)
     BOOST_CHECK(clusterList.mapping["area"]->costgeneration == useCostTimeseries);
     BOOST_CHECK(clusterList.mapping["area"]->fuelEfficiency == 36.0);
     BOOST_CHECK(clusterList.mapping["area"]->variableomcost == 12.12);
-
 }
 
 BOOST_AUTO_TEST_CASE(EconomicInputData_loadFromFolder_basic)
@@ -210,7 +212,7 @@ BOOST_AUTO_TEST_CASE(checkFuelAndCo2_failing_different_economic_input_width)
     l.add(area);
 
     BOOST_CHECK_THROW(Antares::Check::checkFuelAndCo2ColumnNumber(l),
-            Error::IncompatibleFuelOrCo2CostColumns);
+                      Error::IncompatibleFuelOrCo2CostColumns);
 
     area->thermal.list.mapping["area"]->ecoInput.fuelcost.width = 1;
     area->thermal.list.mapping["area"]->ecoInput.co2cost.width = 1;
@@ -245,10 +247,10 @@ BOOST_AUTO_TEST_CASE(ThermalCluster_costGenManualCalculationOfMarketBidAndMargin
 {
     clusterList.loadFromFolder(*study, folder, area);
     clusterList.mapping["area"]->costGenManualCalculationOfMarketBidAndMarginalCostPerHour();
-    BOOST_CHECK_EQUAL(clusterList.mapping["area"]->thermalEconomicTimeSeries[0]
-        .marketBidCostPerHourTs[2637], 35);
-    BOOST_CHECK_EQUAL(clusterList.mapping["area"]->thermalEconomicTimeSeries[0]
-        .marginalCostPerHourTs[6737], 23);
+    BOOST_CHECK_EQUAL(
+      clusterList.mapping["area"]->thermalEconomicTimeSeries[0].marketBidCostPerHourTs[2637], 35);
+    BOOST_CHECK_EQUAL(
+      clusterList.mapping["area"]->thermalEconomicTimeSeries[0].marginalCostPerHourTs[6737], 23);
 }
 
 BOOST_AUTO_TEST_CASE(ThermalCluster_costGenTimeSeriesCalculationOfMarketBidAndMarginalCostPerHour)
@@ -263,10 +265,14 @@ BOOST_AUTO_TEST_CASE(ThermalCluster_costGenTimeSeriesCalculationOfMarketBidAndMa
 
     clusterList.mapping["area"]->costGenTimeSeriesCalculationOfMarketBidAndMarginalCostPerHour();
 
-    BOOST_CHECK_CLOSE(clusterList.mapping["area"]->thermalEconomicTimeSeries[0]
-        .marketBidCostPerHourTs[0], 24.12, 0.001);
-    BOOST_CHECK_CLOSE(clusterList.mapping["area"]->thermalEconomicTimeSeries[0]
-        .marketBidCostPerHourTs[2637], 24.12, 0.001);
+    BOOST_CHECK_CLOSE(
+      clusterList.mapping["area"]->thermalEconomicTimeSeries[0].marketBidCostPerHourTs[0],
+      24.12,
+      0.001);
+    BOOST_CHECK_CLOSE(
+      clusterList.mapping["area"]->thermalEconomicTimeSeries[0].marketBidCostPerHourTs[2637],
+      24.12,
+      0.001);
 }
 
 BOOST_AUTO_TEST_CASE(computeMarketBidCost)
