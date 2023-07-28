4206ce1270109e7aab99772fae3a327bd937c85e
Jason Marechal <jason.marechal+github@gmail.com>
2023-07-28 15:32:38 +0200

    Benchmarking

--- src/libs/antares/study/save.cpp
+++ src/libs/antares/study/save.cpp
@@ -26,7 +26,7 @@
 */
 
 #include "study.h"
-#include "../benchmarking.h"
+#include <antares/benchmarking/benchmarking.h>
 #include "scenario-builder/sets.h"
 #include <yuni/core/string/wstring.h>
 #include <fstream>
