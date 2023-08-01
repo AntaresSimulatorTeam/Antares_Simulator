653024c3a13c3045c38533cb14ed843a0377a44b
Guillaume PIERRE <guillaume.pierre@rte-france.com>
2023-08-01 15:29:02 +0200

    Improve tests CR20 : clarify by referencing a cluster variable instead of fetching this cluster in a list every time

--- src/tests/src/libs/antares/study/thermal-price-definition/thermal-price-definition.cpp
+++ src/tests/src/libs/antares/study/thermal-price-definition/thermal-price-definition.cpp
@@ -152,21 +152,25 @@ BOOST_FIXTURE_TEST_SUITE(s, FixtureFull)
 BOOST_AUTO_TEST_CASE(ThermalClusterList_loadFromFolder_basic)
 {
     clusterList.loadFromFolder(*study, folder, area);
-    BOOST_CHECK(clusterList.mapping["some cluster"]->startupCost == 70000.0);
-    BOOST_CHECK(clusterList.mapping["some cluster"]->costgeneration == useCostTimeseries);
-    BOOST_CHECK(clusterList.mapping["some cluster"]->fuelEfficiency == 36.0);
-    BOOST_CHECK(clusterList.mapping["some cluster"]->variableomcost == 12.12);
+    auto cluster = clusterList.mapping["some cluster"];
+
+    BOOST_CHECK(cluster->startupCost == 70000.0);
+    BOOST_CHECK(cluster->costgeneration == useCostTimeseries);
+    BOOST_CHECK(cluster->fuelEfficiency == 36.0);
+    BOOST_CHECK(cluster->variableomcost == 12.12);
 }
 
 BOOST_AUTO_TEST_CASE(checkCo2_checkCO2CostColumnNumber_OK)
 {
     area->thermal.list.loadFromFolder(*study, folder, area);
-    area->thermal.list.mapping["some cluster"]->series = new DataSeriesCommon;
-    area->thermal.list.mapping["some cluster"]->series->timeSeries.reset(3, 8760);
+    auto cluster = area->thermal.list.mapping["some cluster"];
+
+    cluster->series = new DataSeriesCommon;
+    cluster->series->timeSeries.reset(3, 8760);
 
     area->thermal.prepareAreaWideIndexes();
 
-    auto& ecoInput = area->thermal.list.mapping["some cluster"]->ecoInput;
+    auto& ecoInput = cluster->ecoInput;
     ecoInput.co2cost.reset(3, 8760);
 
     BOOST_CHECK_NO_THROW(Antares::Check::checkCO2CostColumnNumber(study->areas));
@@ -175,12 +179,14 @@ BOOST_AUTO_TEST_CASE(checkCo2_checkCO2CostColumnNumber_OK)
 BOOST_AUTO_TEST_CASE(checkCo2_checkCO2CostColumnNumber_KO)
 {
     area->thermal.list.loadFromFolder(*study, folder, area);
-    area->thermal.list.mapping["some cluster"]->series = new DataSeriesCommon;
-    area->thermal.list.mapping["some cluster"]->series->timeSeries.reset(3, 8760);
+    auto cluster = area->thermal.list.mapping["some cluster"];
+
+    cluster->series = new DataSeriesCommon;
+    cluster->series->timeSeries.reset(3, 8760);
 
     area->thermal.prepareAreaWideIndexes();
 
-    auto& ecoInput = area->thermal.list.mapping["some cluster"]->ecoInput;
+    auto& ecoInput = cluster->ecoInput;
     ecoInput.co2cost.reset(2, 8760);
 
     BOOST_CHECK_THROW(Antares::Check::checkCO2CostColumnNumber(study->areas),
@@ -190,14 +196,15 @@ BOOST_AUTO_TEST_CASE(checkCo2_checkCO2CostColumnNumber_KO)
 BOOST_AUTO_TEST_CASE(checkFuelAndCo2_checkColumnNumber_OK)
 {
     area->thermal.list.loadFromFolder(*study, folder, area);
-    area->thermal.list.mapping["some cluster"]->series = new DataSeriesCommon;
-    area->thermal.list.mapping["some cluster"]->series->timeSeries.reset(3, 8760);
+    auto cluster = area->thermal.list.mapping["some cluster"];
+
+    cluster->series = new DataSeriesCommon;
+    cluster->series->timeSeries.reset(3, 8760);
 
     area->thermal.prepareAreaWideIndexes();
 
-    area->thermal.list.mapping["some cluster"]->ecoInput.fuelcost.reset(3, 8760);
-    area->thermal.list.mapping["some cluster"]->ecoInput.co2cost.reset
-      (3, 8760);
+    cluster->ecoInput.fuelcost.reset(3, 8760);
+    cluster->ecoInput.co2cost.reset(3, 8760);
 
     BOOST_CHECK_NO_THROW(Antares::Check::checkFuelCostColumnNumber(study->areas));
     BOOST_CHECK_NO_THROW(Antares::Check::checkCO2CostColumnNumber(study->areas));
@@ -206,12 +213,13 @@ BOOST_AUTO_TEST_CASE(checkFuelAndCo2_checkColumnNumber_OK)
 BOOST_AUTO_TEST_CASE(ThermalCluster_costGenManualCalculationOfMarketBidAndMarginalCostPerHour)
 {
     clusterList.loadFromFolder(*study, folder, area);
-    clusterList.mapping["some cluster"]->costgeneration = Data::setManually;
-    clusterList.mapping["some cluster"]->ComputeCostTimeSeries();
-    BOOST_CHECK_EQUAL(
-      clusterList.mapping["some cluster"]->costsTimeSeries[0].marketBidCostTS[2637], 35);
-    BOOST_CHECK_EQUAL(
-      clusterList.mapping["some cluster"]->costsTimeSeries[0].marginalCostTS[6737], 23);
+    auto cluster = clusterList.mapping["some cluster"];
+
+    cluster->costgeneration = Data::setManually;
+    cluster->ComputeCostTimeSeries();
+
+    BOOST_CHECK_EQUAL(cluster->costsTimeSeries[0].marketBidCostTS[2637], 35);
+    BOOST_CHECK_EQUAL(cluster->costsTimeSeries[0].marginalCostTS[6737], 23);
 }
 
 BOOST_AUTO_TEST_CASE(ThermalCluster_costGenTimeSeriesCalculationOfMarketBidAndMarginalCostPerHour)
@@ -220,25 +228,23 @@ BOOST_AUTO_TEST_CASE(ThermalCluster_costGenTimeSeriesCalculationOfMarketBidAndMa
     TimeSeriesFile co2("CO2Cost.txt", 8760);
 
     clusterList.loadFromFolder(*study, folder, area);
-    clusterList.mapping["some cluster"]->modulation.reset(1, 8760);
-    clusterList.mapping["some cluster"]->ecoInput.loadFromFolder(*study, folder);
-    fillThermalEconomicTimeSeries(clusterList.mapping["some cluster"].get());
+    auto cluster = clusterList.mapping["some cluster"];
 
-    clusterList.mapping["some cluster"]->ComputeCostTimeSeries();
+    cluster->modulation.reset(1, 8760);
+    cluster->ecoInput.loadFromFolder(*study, folder);
+    fillThermalEconomicTimeSeries(cluster.get());
 
-    BOOST_CHECK_CLOSE(
-      clusterList.mapping["some cluster"]->costsTimeSeries[0].marketBidCostTS[0],
-      24.12,
-      0.001);
-    BOOST_CHECK_CLOSE(
-      clusterList.mapping["some cluster"]->costsTimeSeries[0].marketBidCostTS[2637],
-      24.12,
-      0.001);
+    cluster->ComputeCostTimeSeries();
+
+    BOOST_CHECK_CLOSE(cluster->costsTimeSeries[0].marketBidCostTS[0], 24.12, 0.001);
+    BOOST_CHECK_CLOSE(cluster->costsTimeSeries[0].marketBidCostTS[2637], 24.12, 0.001);
 }
 
 BOOST_AUTO_TEST_CASE(computeMarketBidCost)
 {
     clusterList.loadFromFolder(*study, folder, area);
-    BOOST_CHECK_CLOSE(clusterList.mapping["some cluster"]->computeMarketBidCost(1, 2, 1), 24.12, 0.001);
+    auto cluster = clusterList.mapping["some cluster"];
+
+    BOOST_CHECK_CLOSE(cluster->computeMarketBidCost(1, 2, 1), 24.12, 0.001);
 }
 BOOST_AUTO_TEST_SUITE_END()
