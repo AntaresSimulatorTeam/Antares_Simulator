bc24f430307dfe70d7d9635dca108f27b3ac8591
Florian OMNES <florian.omnes@rte-france.com>
2023-08-07 17:22:12 +0200

    Add untracked files

---
+++ src/solver/misc/write-command-line.cpp
@@ -0,0 +1,13 @@
+#include <antares/logs.h>
+#include <sstream>
+
+void WriteCommandLineIntoLogs(int argc, char** argv)
+{
+  std::ostringstream buffer;
+  for (int arg = 0; arg < argc; arg++) {
+      buffer << argv[arg];
+      if (arg + 1 != argc)
+          buffer << " ";
+  }
+  Antares::logs.info() << "  :: command = " << buffer.str();
+}
