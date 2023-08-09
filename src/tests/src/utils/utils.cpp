c8b94f0a626c95b133c195df0e9765576512f72c
Vincent Payet <vincent.payet@rte-france.com>
2023-08-09 10:39:35 +0200

    [DEV] Use filesystem::remove_all

--- src/tests/src/utils/utils.cpp
+++ src/tests/src/utils/utils.cpp
@@ -5,7 +5,7 @@ namespace fs = std::filesystem;
 fs::path generateAndCreateDirName(std::string dirName)
 {
     fs::path working_dir = fs::temp_directory_path() / dirName;
-    fs::remove(working_dir);
+    fs::remove_all(working_dir);
     fs::create_directories(working_dir);
     return working_dir;
 }
