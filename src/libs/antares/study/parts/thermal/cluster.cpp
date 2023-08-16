51f755125e12f3da43e6393095748856eaa43b01
guilpier-code <62292552+guilpier-code@users.noreply.github.com>
2023-08-16 16:36:53 +0200

    Tests end-to-end in memory : simplify tests (part 2) (#1516)
    More utilities to simplify the creation of in-memory studies,
    and the retrieval of results.

--- src/libs/antares/study/parts/thermal/cluster.cpp
+++ src/libs/antares/study/parts/thermal/cluster.cpp
@@ -123,21 +123,21 @@ namespace Antares
 namespace Data
 {
 Data::ThermalCluster::ThermalCluster(Area* parent) :
- Cluster(parent),
- groupID(thermalDispatchGrpOther1),
- mustrun(false),
- mustrunOrigin(false),
- nominalCapacityWithSpinning(0.),
- minStablePower(0.),
- minUpTime(1),
- minDownTime(1),
- spinning(0.),
- forcedVolatility(0.),
- plannedVolatility(0.),
- forcedLaw(thermalLawUniform),
- plannedLaw(thermalLawUniform),
- PthetaInf(HOURS_PER_YEAR, 0),
- costsTimeSeries(1, CostsTimeSeries())
+    Cluster(parent),
+    groupID(thermalDispatchGrpOther1),
+    mustrun(false),
+    mustrunOrigin(false),
+    nominalCapacityWithSpinning(0.),
+    minStablePower(0.),
+    minUpTime(1),
+    minDownTime(1),
+    spinning(0.),
+    forcedVolatility(0.),
+    plannedVolatility(0.),
+    forcedLaw(thermalLawUniform),
+    plannedLaw(thermalLawUniform),
+    PthetaInf(HOURS_PER_YEAR, 0),
+    costsTimeSeries(1, CostsTimeSeries())
 {
     // assert
     assert(parent and "A parent for a thermal dispatchable cluster can not be null");
