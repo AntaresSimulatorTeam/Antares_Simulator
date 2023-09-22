10464113e743ebc6d11494ff0becf8253d554beb
Abdoulbari ZAKIR <azakir@neverhack.com>
2023-09-22 15:34:10 +0200

    update

--- src/solver/optimisation/constraints/MinHydroPower.h
+++ src/solver/optimisation/constraints/MinHydroPower.h
@@ -1,9 +1,17 @@
 #pragma once
 #include "ConstraintBuilder.h"
 
+/*!
+ * represent 'Min Hydraulic Power' constraint type
+ */
 class MinHydroPower : private ConstraintFactory
 {
 public:
     using ConstraintFactory::ConstraintFactory;
+
+    /*!
+     * @brief Add variables to the constraint and update constraints Matrix
+     * @param pays : area
+     */
     void add(int pays);
 };
