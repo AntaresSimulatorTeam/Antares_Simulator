65ee6881681c059bd2ea97eaed5a638fc33c382d
Abdoulbari ZAKIR <azakir@neverhack.com>
2023-09-27 14:22:17 +0200

    MinHydroPower

--- src/solver/optimisation/constraints/MinHydroPower.cpp
+++ src/solver/optimisation/constraints/MinHydroPower.cpp
@@ -1,34 +1,25 @@
 #include "MinHydroPower.h"
 
-void MinHydroPower::add(int pays)
+void MinHydroPower::add(int pays, std::shared_ptr<MinHydroPowerData> data)
 {
-    bool presenceHydro
-      = problemeHebdo->CaracteristiquesHydrauliques[pays].PresenceDHydrauliqueModulable;
-    bool TurbEntreBornes = problemeHebdo->CaracteristiquesHydrauliques[pays].TurbinageEntreBornes;
-    if (presenceHydro
-        && (TurbEntreBornes
-            || problemeHebdo->CaracteristiquesHydrauliques[pays].PresenceDePompageModulable))
+    if (data->presenceHydro && (data->TurbEntreBornes || data->PresenceDePompageModulable))
     {
-        problemeHebdo->NumeroDeContrainteMinEnergieHydraulique[pays]
-          = problemeHebdo->ProblemeAResoudre->NombreDeContraintes;
+        data->NumeroDeContrainteMinEnergieHydraulique[pays] = builder->data->nombreDeContraintes;
 
-        const int NombreDePasDeTempsPourUneOptimisation
-          = problemeHebdo->NombreDePasDeTempsPourUneOptimisation;
+        ConstraintNamer namer(builder->data->NomDesContraintes);
+        namer.UpdateArea(builder->data->NomsDesPays[pays]);
+        namer.UpdateTimeStep(builder->data->weekInTheYear);
+        namer.MinHydroPower(builder->data->nombreDeContraintes);
 
-        ConstraintNamer namer(problemeHebdo->ProblemeAResoudre->NomDesContraintes);
-        namer.UpdateArea(problemeHebdo->NomsDesPays[pays]);
-        namer.UpdateTimeStep(problemeHebdo->weekInTheYear);
-        namer.MinHydroPower(problemeHebdo->ProblemeAResoudre->NombreDeContraintes);
-        for (int pdt = 0; pdt < NombreDePasDeTempsPourUneOptimisation; pdt++)
+        for (int pdt = 0; pdt < data->NombreDePasDeTempsPourUneOptimisation; pdt++)
         {
-            builder.updateHourWithinWeek(pdt);
-            builder.HydProd(pays, 1.0);
+            builder->updateHourWithinWeek(pdt);
+            builder->HydProd(pays, 1.0);
         }
 
-        problemeHebdo->NumeroDeContrainteMinEnergieHydraulique[pays]
-          = problemeHebdo->ProblemeAResoudre->NombreDeContraintes;
-        builder.greaterThan().build();
+        data->NumeroDeContrainteMinEnergieHydraulique[pays] = builder->data->nombreDeContraintes;
+        builder->greaterThan().build();
     }
     else
-        problemeHebdo->NumeroDeContrainteMinEnergieHydraulique[pays] = -1;
+        data->NumeroDeContrainteMinEnergieHydraulique[pays] = -1;
 }
--- src/solver/optimisation/constraints/MinHydroPower.h
+++ src/solver/optimisation/constraints/MinHydroPower.h
@@ -1,18 +1,26 @@
 #pragma once
-#include "ConstraintBuilder.h"
+#include "new_constraint_builder.h"
 
+struct MinHydroPowerData
+{
+    const bool& presenceHydro;
+    const bool& TurbEntreBornes;
+    const bool& PresenceDePompageModulable;
+    const int& NombreDePasDeTempsPourUneOptimisation;
+    std::vector<int>& NumeroDeContrainteMinEnergieHydraulique;
+};
 /*!
  * represent 'Min Hydraulic Power' constraint type
  */
-class MinHydroPower : private ConstraintFactory
+
+class MinHydroPower : private NewConstraintFactory
 {
 public:
-    using ConstraintFactory::ConstraintFactory;
+    using NewConstraintFactory::NewConstraintFactory;
 
     /*!
      * @brief Add variables to the constraint and update constraints Matrix
      * @param pays : area
      */
-    void add(int pays);
+    void add(int pays, std::shared_ptr<MinHydroPowerData> data);
 };
-
