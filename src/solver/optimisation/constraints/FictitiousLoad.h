03c246ae57bf3f7a64a1bdefdc362bf7712025fb
Abdoulbari ZAKIR <azakir@neverhack.com>
2023-09-27 11:52:55 +0200

    new FictitiousLoad

--- src/solver/optimisation/constraints/FictitiousLoad.h
+++ src/solver/optimisation/constraints/FictitiousLoad.h
@@ -1,19 +1,27 @@
 
 #pragma once
-#include "ConstraintBuilder.h"
+#include "new_constraint_builder.h"
+
+struct FictitiousLoadData
+{
+    std::vector<int>& NumeroDeContraintePourEviterLesChargesFictives;
+    const PALIERS_THERMIQUES& PaliersThermiquesDuPays;
+    const std::vector<bool>& DefaillanceNegativeUtiliserHydro;
+};
+
 
 /*!
  * represent 'Fictitious Load' constraint type
  */
-class FictitiousLoad : private ConstraintFactory
+class FictitiousLoad : private NewConstraintFactory
 {
 public:
-    using ConstraintFactory::ConstraintFactory;
+    using NewConstraintFactory::NewConstraintFactory;
 
     /*!
      * @brief Add variables to the constraint and update constraints Matrix
      * @param pdt : timestep
      * @param pays : area
      */
-    void add(int pdt, int pays);
+    void add(int pdt, int pays, std::shared_ptr<FictitiousLoadData> data);
 };
