f308c96a4cc3286965506e1940efdd903a22f564
Abdoulbari ZAKIR <azakir@neverhack.com>
2023-09-27 14:52:50 +0200

    new AreaHydroLevel

--- src/solver/optimisation/constraints/AreaHydroLevel.cpp
+++ src/solver/optimisation/constraints/AreaHydroLevel.cpp
@@ -1,32 +1,28 @@
 #include "AreaHydroLevel.h"
 
-void AreaHydroLevel::add(int pays, int pdt)
+void AreaHydroLevel::add(int pays, int pdt, std::shared_ptr<AreaHydroLevelData> data)
 {
-    CORRESPONDANCES_DES_CONTRAINTES& CorrespondanceCntNativesCntOptim
-      = problemeHebdo->CorrespondanceCntNativesCntOptim[pdt];
-    CorrespondanceCntNativesCntOptim.NumeroDeContrainteDesNiveauxPays[pays]
-      = problemeHebdo->ProblemeAResoudre->NombreDeContraintes;
-    if (problemeHebdo->CaracteristiquesHydrauliques[pays].SuiviNiveauHoraire)
+    data->NumeroDeContrainteDesNiveauxPays[pays] = builder->data->nombreDeContraintes;
+    if (data->SuiviNiveauHoraire)
     {
-        builder.updateHourWithinWeek(pdt).HydroLevel(pays, 1.0);
+        builder->updateHourWithinWeek(pdt).HydroLevel(pays, 1.0);
         if (pdt > 0)
         {
-            builder.updateHourWithinWeek(pdt - 1).HydroLevel(pays, -1.0);
+            builder->updateHourWithinWeek(pdt - 1).HydroLevel(pays, -1.0);
         }
-        ConstraintNamer namer(problemeHebdo->ProblemeAResoudre->NomDesContraintes);
+        ConstraintNamer namer(builder->data->NomDesContraintes);
 
-        namer.UpdateArea(problemeHebdo->NomsDesPays[pays]);
-        namer.UpdateTimeStep(problemeHebdo->weekInTheYear * 168 + pdt);
-        namer.AreaHydroLevel(problemeHebdo->ProblemeAResoudre->NombreDeContraintes);
-        CorrespondanceCntNativesCntOptim.NumeroDeContrainteDesNiveauxPays[pays]
-          = problemeHebdo->ProblemeAResoudre->NombreDeContraintes;
-        builder.updateHourWithinWeek(pdt)
+        namer.UpdateArea(builder->data->NomsDesPays[pays]);
+        namer.UpdateTimeStep(builder->data->weekInTheYear * 168 + pdt);
+        namer.AreaHydroLevel(builder->data->nombreDeContraintes);
+        data->NumeroDeContrainteDesNiveauxPays[pays] = builder->data->nombreDeContraintes;
+        builder->updateHourWithinWeek(pdt)
           .HydProd(pays, 1.0)
-          .Pumping(pays, -problemeHebdo->CaracteristiquesHydrauliques[pays].PumpingRatio)
+          .Pumping(pays, -data->PumpingRatio)
           .Overflow(pays, 1.)
           .equalTo()
           .build();
     }
     else
-        CorrespondanceCntNativesCntOptim.NumeroDeContrainteDesNiveauxPays[pays] = -1;
+        data->NumeroDeContrainteDesNiveauxPays[pays] = -1;
 }
--- src/solver/optimisation/constraints/AreaHydroLevel.h
+++ src/solver/optimisation/constraints/AreaHydroLevel.h
@@ -1,18 +1,26 @@
 #pragma once
-#include "ConstraintBuilder.h"
+#include "new_constraint_builder.h"
+
+struct AreaHydroLevelData
+{
+    std::vector<int>& NumeroDeContrainteDesNiveauxPays;
+    const bool& SuiviNiveauHoraire;
+    const double& PumpingRatio;
+};
 
 /*!
  * represent 'Area Hydraulic Level' constraint type
  */
-class AreaHydroLevel : private ConstraintFactory
+
+class AreaHydroLevel : private NewConstraintFactory
 {
     public:
-    using ConstraintFactory::ConstraintFactory;
+        using NewConstraintFactory::NewConstraintFactory;
 
     /*!
      * @brief Add variables to the constraint and update constraints Matrix
      * @param pdt : timestep
      * @param pays : area
      */
-    void add(int pays, int pdt);
+        void add(int pays, int pdt, std::shared_ptr<AreaHydroLevelData> data);
 };
