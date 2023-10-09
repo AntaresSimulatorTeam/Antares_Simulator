c79f00e99c37d00e0abecc94863f636355f6358a
guilpier-code <62292552+guilpier-code@users.noreply.github.com>
2023-10-09 15:14:36 +0200

    Remove dependency to UI, use RAII to handle resources (#1678)
    * Remove dependency to UI : removal from solver
    
    * Remove dependency to UI : calling the main arguments pre-processing from outside
    
    * Remove dependency to UI : removal from analyzer
    
    * Remove dependency to UI : replace the dependency to UI in other stand alone targets + correct new target include files paths
    
    * Remove dependency to UI : forgot to add header file to repository
    
    * Remove dependency to UI : introduce a RAII to automatically destroy the allocated memory
    
    * Add & use IntoUTF8ArgsTranslator::convert (#1684)
    
    * Add & use IntoUTF8ArgsTranslator::convert
    
    * Fix header inclusion
    
    * Fix windows build
    
    * Fix build
    
    * Fix build (bis)
    
    * Remove ref to argc, make IntoUTF8ArgsTranslator's API homogenous
    
    * Apply clang-format
    
    ---------
    
    Co-authored-by: Florian Omnès <florian.omnes@rte-france.com>

--- src/tools/cleaner/main.cpp
+++ src/tools/cleaner/main.cpp
@@ -28,7 +28,7 @@
 #include <antares/logs/logs.h>
 #include <antares/study/finder.h>
 #include <yuni/core/getopt.h>
-#include "../../ui/common/winmain.hxx"
+#include <antares/args/args_to_utf8.h>
 #include <antares/utils/utils.h>
 #include <antares/study/cleaner.h>
 #include <antares/version.h>
@@ -97,8 +97,8 @@ int main(int argc, char* argv[])
     InitializeDefaultLocale();
 
     Antares::logs.applicationName("cleaner");
-    argv = AntaresGetUTF8Arguments(argc, argv);
-
+    IntoUTF8ArgsTranslator toUTF8ArgsTranslator(argc, argv);
+    std::tie(argc, argv) = toUTF8ArgsTranslator.convert();
     String::Vector optInput;
     bool optPrintOnly = false;
     bool optMrProper = false;
