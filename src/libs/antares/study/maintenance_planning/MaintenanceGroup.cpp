2d2a628ea525b803dcf56e80b1bea94db4ae792b
NikolaIlic <nikola.ilic@redstork-solutions.com>
2023-10-31 12:14:52 +0100

    Comment due to build error

--- src/libs/antares/study/maintenance_planning/MaintenanceGroup.cpp
+++ src/libs/antares/study/maintenance_planning/MaintenanceGroup.cpp
@@ -5,36 +5,36 @@ namespace Antares::Data
 
 bool MaintenanceGroup::saveAreaListToFile(const std::string& filename) const
 {
-    if (!filename)
-        return false;
+    // if (!filename)
+    //     return false;
 
-    std::string data;
-    {
-        // Preparing a new list of areas, sorted by their name
-        using List = std::list<std::string>;
-        List list;
-        {
-            auto end = associatedAreas.end();
-            for (auto i = associatedAreas.begin(); i != end; ++i)
-                list.push_back((i->second)->name.c_str());
-            list.sort();
-        }
-        {
-            auto end = list.end();
-            for (auto i = list.begin(); i != end; ++i)
-                data << *i << '\n';
-        }
-    }
+    // std::string data;
+    // {
+    //     // Preparing a new list of areas, sorted by their name
+    //     using List = std::list<std::string>;
+    //     List list;
+    //     {
+    //         auto end = associatedAreas.end();
+    //         for (auto i = associatedAreas.begin(); i != end; ++i)
+    //             list.push_back((i->second)->name.c_str());
+    //         list.sort();
+    //     }
+    //     {
+    //         auto end = list.end();
+    //         for (auto i = list.begin(); i != end; ++i)
+    //             data << *i << '\n';
+    //     }
+    // }
 
-    // Writing data into the appropriate file
-    std::ofstream file(filename);
-    if (!file.is_open())
-    {
-        file << data;
-        return true;
-    }
-    logs.error() << "impossible to write " << filename;
-    return false;
+    // // Writing data into the appropriate file
+    // std::ofstream file(filename);
+    // if (!file.is_open())
+    // {
+    //     file << data;
+    //     return true;
+    // }
+    // logs.error() << "impossible to write " << filename;
+     return false;
 }
 
 void MaintenanceGroup::name(const std::string& newname)
