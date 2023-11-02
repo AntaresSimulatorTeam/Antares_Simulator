af0c5e42a373b15ef1beb6bcc44738c506bd2942
Milos-RTEi <milos.andjelkovic@redstork-solutions.com>
2023-11-02 16:11:23 +0100

    update TODO-s

--- src/libs/antares/study/maintenance_planning/MaintenanceGroupLoader.h
+++ src/libs/antares/study/maintenance_planning/MaintenanceGroupLoader.h
@@ -33,10 +33,11 @@ private:
 TODO CR27:
     for II phase:
         * check if same area is inside more than 1 Maintenance Group - and throw error
-        * what happens when area name is changed - UI code
-            * we need to change the name in INI file in Maintenance Group
-        * what happens if the area is deleted - UI code
-            * we need to remove that area from all Maintenance Group-s      
+        * what happens when area name is changed - UI code  -- tested area name will be changed in INI file once study is saved - OK
+        * what happens if the area is deleted - UI code - tested all mnt groups containing area name will be removed from INI file once study is saved 
+            - OK?! or not OK! - maybe it is better to remove this area from mnt group? 
+            This will actually happen if you try to load the mnt groups from ini file and the area from INI file do not exist in the model. 
+            That area,with all weights, will be removed from INI file. Mnt Group-s will not be removed - just missing area will be removed from the mnt group.     
 
 
 */ 
