7e49fe69839b9f3193e425003ae9bf5f4efe5859
Abdoulbari ZAKIR <azakir@neverhack.com>
2023-11-10 17:42:17 +0100

    fix

--- src/solver/optimisation/constraints/ExchangeBalance.cpp
+++ src/solver/optimisation/constraints/ExchangeBalance.cpp
@@ -7,13 +7,13 @@ void ExchangeBalance::add(uint32_t pays, std::shared_ptr<ExchangeBalanceData> da
     int interco = data->IndexDebutIntercoOrigine[pays];
     while (interco >= 0)
     {
-        builder->IntercoDirectCost(interco, 1.0);
+        builder->NTCDirect(interco, 1.0);
         interco = data->IndexSuivantIntercoOrigine[interco];
     }
     interco = data->IndexDebutIntercoExtremite[pays];
     while (interco >= 0)
     {
-        builder->IntercoDirectCost(interco, -1.0);
+        builder->NTCDirect(interco, -1.0);
 
         interco = data->IndexSuivantIntercoExtremite[interco];
     }
