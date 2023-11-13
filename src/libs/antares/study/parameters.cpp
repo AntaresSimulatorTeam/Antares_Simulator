17d6dd00560b519239263e2276cd618f6328339e
Jason Marechal <jason.marechal+github@gmail.com>
2023-11-13 13:52:12 +0100

    proper include enum

--- src/libs/antares/study/parameters.cpp
+++ src/libs/antares/study/parameters.cpp
@@ -36,7 +36,7 @@
 #include "../../../solver/variable/economy/all.h"
 
 #include <antares/exception/AssertionError.hpp>
-#include <antares/Enum.hxx>
+#include <antares/Enum.hpp>
 
 using namespace Yuni;
 
