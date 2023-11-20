469f0b845f8de9bc2dc3e5efcc5029d42779c5b1
Milos-RTEi <milos.andjelkovic@redstork-solutions.com>
2023-11-20 16:39:24 +0100

    reset problem

--- src/solver/ts-generator/optimized-thermal-generator/ResetProblem.cpp
+++ src/solver/ts-generator/optimized-thermal-generator/ResetProblem.cpp
@@ -8,6 +8,9 @@ namespace Antares::Solver::TSGenerator
 {
 void OptimizedThermalGenerator::resetProblem()
 {
+    // Clear the solver to reset it for the new problem
+    solver.Clear();
+    var.day.clear();
     return;
 }
 
