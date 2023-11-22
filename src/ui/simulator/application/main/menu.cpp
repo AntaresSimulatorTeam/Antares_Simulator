2b00ff23b2ea3283f7b2d347dbd9fab51d175f83
Jason Marechal <jason.marechal+github@gmail.com>
2023-11-22 11:17:41 +0100

    Fix config.h include path

--- src/ui/simulator/application/main/menu.cpp
+++ src/ui/simulator/application/main/menu.cpp
@@ -33,15 +33,14 @@
 #include "../../toolbox/resources.h"
 #include "internal-data.h"
 #include "internal-ids.h"
-#include "../../../../config.h"
+#include <antares/config/config.h>
 #include "antares/antares/antares.h"
 #include <ui/common/component/frame/registry.h>
 
 using namespace Yuni;
 
-namespace Antares
-{
-namespace Forms
+
+namespace Antares::Forms
 {
 wxMenuBar* ApplWnd::createMenu()
 {
@@ -763,5 +762,5 @@ void ApplWnd::evtOnViewNodalOptimization(wxCommandEvent&)
     pNotebook->select(wxT("nodal"));
 }
 
-} // namespace Forms
-} // namespace Antares
+} // namespace Antares::Forms
+
