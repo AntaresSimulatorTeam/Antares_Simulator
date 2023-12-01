9d45059d57b9ff4fcca3d8c1b322ccf79ba603de
Milos-RTEi <milos.andjelkovic@redstork-solutions.com>
2023-12-01 09:07:24 +0100

    move write results to separate file

---
+++ src/solver/ts-generator/optimized-thermal-generator/PostOptimization/WriteResults.cpp
@@ -0,0 +1,45 @@
+//
+// Created by milos on 14/11/23.
+//
+
+#include "../Main/OptimizedGenerator.h"
+
+namespace Antares::Solver::TSGenerator
+{
+
+// this method is called at the very end
+// after all time-steps and scenarios
+void OptimizedThermalGenerator::writeTsResults()
+{
+    // we need to loop through all the clusters
+    // and write the results
+    // it would be much easier to loop using clusterData
+    // inside of it we already excluded all the non-important clusters
+    // however it is const Data::ThermalCluster*
+    // so we cannot modify cluster values
+
+    for (auto& entryWeightMap : maintenanceGroup_)
+    {
+        auto& area = *(entryWeightMap.first);
+
+        for (auto& clusterEntry : area.thermal.list.mapping)
+        {
+            auto& cluster = *(clusterEntry.second);
+
+            if (!(checkClusterExist(cluster) && cluster.doWeGenerateTS(globalThermalTSgeneration_)
+                  && cluster.optimizeMaintenance))
+                continue;
+
+            if (derated)
+                cluster.series.timeSeries.averageTimeseries();
+
+            if (archive)
+                writeResultsToDisk(area, cluster);
+
+            cluster.calculationOfSpinning();
+        }
+    }
+    return;
+}
+
+} // namespace Antares::Solver::TSGenerator
