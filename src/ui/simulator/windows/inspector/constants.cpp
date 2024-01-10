93b6c05ead881c321df4e30ae1120b9d43dfce85
Vincent Payet <vincent.payet@rte-france.com>
2024-01-10 14:00:03 +0100

    [FIX] compile

--- src/ui/simulator/windows/inspector/constants.cpp
+++ src/ui/simulator/windows/inspector/constants.cpp
@@ -23,7 +23,6 @@
 ** You should have received a copy of the Mozilla Public Licence 2.0
 ** along with Antares_Simulator. If not, see <https://opensource.org/license/mpl-2-0/>.
 */
-#include <antares/xpansion.h>
 #include "constants.h"
 
 namespace Antares
