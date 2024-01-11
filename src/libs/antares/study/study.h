2e22eb8d9f2d2eb99f81db56d9dc9d989c807829
payetvin <113102157+payetvin@users.noreply.github.com>
2024-01-11 16:06:30 +0100

    Clean src/libs/antares/ (#1870)
    Remove directories net/ and object/
    Remove unused functions in stdcxx/

--- src/libs/antares/study/study.h
+++ src/libs/antares/study/study.h
@@ -36,7 +36,6 @@
 #include <antares/writer/i_writer.h>
 
 #include "../antares.h"
-#include "../object/object.h"
 #include "fwd.h"
 
 #include "simulation.h"
@@ -63,7 +62,7 @@ namespace Antares::Data
 */
 
 class UIRuntimeInfo;
-class Study: public Yuni::NonCopyable<Study>, public IObject, public LayerData
+class Study: public Yuni::NonCopyable<Study>, public LayerData
 {
 public:
     using Ptr = std::shared_ptr<Study>;
