d30294273493f75e2672a858e3b3bad7ad9fcc70
github-action-benchmark <github@users.noreply.github.com>
2024-01-25 15:42:14 +0000

    add Python Benchmark with customSmallerIsBetter-benchmark (customSmallerIsBetter) benchmark result for b5b1d408dfc3f3c04c846c33e8c42b965fdb45ae

--- dev/bench/data.js
+++ dev/bench/data.js
@@ -1,5 +1,5 @@
 window.BENCHMARK_DATA = {
-  "lastUpdate": 1706197237855,
+  "lastUpdate": 1706197333692,
   "repoUrl": "https://github.com/AntaresSimulatorTeam/Antares_Simulator",
   "entries": {
     "Python Benchmark with customSmallerIsBetter-benchmark": [
@@ -568,6 +568,580 @@ window.BENCHMARK_DATA = {
           {
             "name": "reservoir-Min-MaxHydroPower execution_time",
             "value": 2235,
+            "unit": "ms"
+          },
+          {
+            "name": "reservoir-Min-MaxHydroPower Memory",
+            "value": 6062757,
+            "unit": "b"
+          }
+        ]
+      },
+      {
+        "commit": {
+          "author": {
+            "email": "azakir@neverhack.com",
+            "name": "Abdoulbari ZAKIR",
+            "username": "a-zakir"
+          },
+          "committer": {
+            "email": "azakir@neverhack.com",
+            "name": "Abdoulbari ZAKIR",
+            "username": "a-zakir"
+          },
+          "distinct": true,
+          "id": "b5b1d408dfc3f3c04c846c33e8c42b965fdb45ae",
+          "message": "test",
+          "timestamp": "2024-01-25T16:19:39+01:00",
+          "tree_id": "9782b0022cfc9489dec29e21f8983c8cfebef98a",
+          "url": "https://github.com/AntaresSimulatorTeam/Antares_Simulator/commit/b5b1d408dfc3f3c04c846c33e8c42b965fdb45ae"
+        },
+        "date": 1706197315533,
+        "tool": "customSmallerIsBetter",
+        "benches": [
+          {
+            "name": "FinalStockEquivalent-cnt First optimization EXP",
+            "value": 1.97115,
+            "unit": "ms"
+          },
+          {
+            "name": "FinalStockEquivalent-cnt First optimization STD",
+            "value": 0.0673077,
+            "unit": "ms"
+          },
+          {
+            "name": "FinalStockEquivalent-cnt First optimization MIN",
+            "value": 1.90385,
+            "unit": "ms"
+          },
+          {
+            "name": "FinalStockEquivalent-cnt First optimization MAX",
+            "value": 2.03846,
+            "unit": "ms"
+          },
+          {
+            "name": "FinalStockEquivalent-cnt Second optimization EXP",
+            "value": 1.42308,
+            "unit": "ms"
+          },
+          {
+            "name": "FinalStockEquivalent-cnt Second optimization STD",
+            "value": 0.134615,
+            "unit": "ms"
+          },
+          {
+            "name": "FinalStockEquivalent-cnt Second optimization MIN",
+            "value": 1.28846,
+            "unit": "ms"
+          },
+          {
+            "name": "FinalStockEquivalent-cnt Second optimization MAX",
+            "value": 1.55769,
+            "unit": "ms"
+          },
+          {
+            "name": "FinalStockEquivalent-cnt execution_time",
+            "value": 6261,
+            "unit": "ms"
+          },
+          {
+            "name": "FinalStockEquivalent-cnt Memory",
+            "value": 125631994,
+            "unit": "b"
+          },
+          {
+            "name": "AreaHydroLevel First optimization EXP",
+            "value": 0.2,
+            "unit": "ms"
+          },
+          {
+            "name": "AreaHydroLevel First optimization STD",
+            "value": 0,
+            "unit": "ms"
+          },
+          {
+            "name": "AreaHydroLevel First optimization MIN",
+            "value": 0.2,
+            "unit": "ms"
+          },
+          {
+            "name": "AreaHydroLevel First optimization MAX",
+            "value": 0.2,
+            "unit": "ms"
+          },
+          {
+            "name": "AreaHydroLevel Second optimization EXP",
+            "value": 0.2,
+            "unit": "ms"
+          },
+          {
+            "name": "AreaHydroLevel Second optimization STD",
+            "value": 0,
+            "unit": "ms"
+          },
+          {
+            "name": "AreaHydroLevel Second optimization MIN",
+            "value": 0.2,
+            "unit": "ms"
+          },
+          {
+            "name": "AreaHydroLevel Second optimization MAX",
+            "value": 0.2,
+            "unit": "ms"
+          },
+          {
+            "name": "AreaHydroLevel execution_time",
+            "value": 1483,
+            "unit": "ms"
+          },
+          {
+            "name": "AreaHydroLevel Memory",
+            "value": 26559789,
+            "unit": "b"
+          },
+          {
+            "name": "tests-ST-storage First optimization EXP",
+            "value": 8,
+            "unit": "ms"
+          },
+          {
+            "name": "tests-ST-storage First optimization STD",
+            "value": 0,
+            "unit": "ms"
+          },
+          {
+            "name": "tests-ST-storage First optimization MIN",
+            "value": 8,
+            "unit": "ms"
+          },
+          {
+            "name": "tests-ST-storage First optimization MAX",
+            "value": 8,
+            "unit": "ms"
+          },
+          {
+            "name": "tests-ST-storage Second optimization EXP",
+            "value": 7,
+            "unit": "ms"
+          },
+          {
+            "name": "tests-ST-storage Second optimization STD",
+            "value": 0,
+            "unit": "ms"
+          },
+          {
+            "name": "tests-ST-storage Second optimization MIN",
+            "value": 7,
+            "unit": "ms"
+          },
+          {
+            "name": "tests-ST-storage Second optimization MAX",
+            "value": 7,
+            "unit": "ms"
+          },
+          {
+            "name": "tests-ST-storage execution_time",
+            "value": 204,
+            "unit": "ms"
+          },
+          {
+            "name": "tests-ST-storage Memory",
+            "value": 8441217,
+            "unit": "b"
+          },
+          {
+            "name": "binding-constraints First optimization EXP",
+            "value": 216.8,
+            "unit": "ms"
+          },
+          {
+            "name": "binding-constraints First optimization STD",
+            "value": 0,
+            "unit": "ms"
+          },
+          {
+            "name": "binding-constraints First optimization MIN",
+            "value": 216.8,
+            "unit": "ms"
+          },
+          {
+            "name": "binding-constraints First optimization MAX",
+            "value": 216.8,
+            "unit": "ms"
+          },
+          {
+            "name": "binding-constraints Second optimization EXP",
+            "value": 121.4,
+            "unit": "ms"
+          },
+          {
+            "name": "binding-constraints Second optimization STD",
+            "value": 0,
+            "unit": "ms"
+          },
+          {
+            "name": "binding-constraints Second optimization MIN",
+            "value": 121.4,
+            "unit": "ms"
+          },
+          {
+            "name": "binding-constraints Second optimization MAX",
+            "value": 121.4,
+            "unit": "ms"
+          },
+          {
+            "name": "binding-constraints execution_time",
+            "value": 9846,
+            "unit": "ms"
+          },
+          {
+            "name": "binding-constraints Memory",
+            "value": 279405665,
+            "unit": "b"
+          },
+          {
+            "name": "reservoir-MaxPumping First optimization EXP",
+            "value": 0,
+            "unit": "ms"
+          },
+          {
+            "name": "reservoir-MaxPumping First optimization STD",
+            "value": 0,
+            "unit": "ms"
+          },
+          {
+            "name": "reservoir-MaxPumping First optimization MIN",
+            "value": 0,
+            "unit": "ms"
+          },
+          {
+            "name": "reservoir-MaxPumping First optimization MAX",
+            "value": 0,
+            "unit": "ms"
+          },
+          {
+            "name": "reservoir-MaxPumping Second optimization EXP",
+            "value": 0,
+            "unit": "ms"
+          },
+          {
+            "name": "reservoir-MaxPumping Second optimization STD",
+            "value": 0,
+            "unit": "ms"
+          },
+          {
+            "name": "reservoir-MaxPumping Second optimization MIN",
+            "value": 0,
+            "unit": "ms"
+          },
+          {
+            "name": "reservoir-MaxPumping Second optimization MAX",
+            "value": 0,
+            "unit": "ms"
+          },
+          {
+            "name": "reservoir-MaxPumping execution_time",
+            "value": 1459,
+            "unit": "ms"
+          },
+          {
+            "name": "reservoir-MaxPumping Memory",
+            "value": 26559917,
+            "unit": "b"
+          },
+          {
+            "name": "025_mps-week First optimization EXP",
+            "value": 0.666667,
+            "unit": "ms"
+          },
+          {
+            "name": "025_mps-week First optimization STD",
+            "value": 0,
+            "unit": "ms"
+          },
+          {
+            "name": "025_mps-week First optimization MIN",
+            "value": 0.666667,
+            "unit": "ms"
+          },
+          {
+            "name": "025_mps-week First optimization MAX",
+            "value": 0.666667,
+            "unit": "ms"
+          },
+          {
+            "name": "025_mps-week Second optimization EXP",
+            "value": 0.666667,
+            "unit": "ms"
+          },
+          {
+            "name": "025_mps-week Second optimization STD",
+            "value": 0,
+            "unit": "ms"
+          },
+          {
+            "name": "025_mps-week Second optimization MIN",
+            "value": 0.666667,
+            "unit": "ms"
+          },
+          {
+            "name": "025_mps-week Second optimization MAX",
+            "value": 0.666667,
+            "unit": "ms"
+          },
+          {
+            "name": "025_mps-week execution_time",
+            "value": 939,
+            "unit": "ms"
+          },
+          {
+            "name": "025_mps-week Memory",
+            "value": 102771771,
+            "unit": "b"
+          },
+          {
+            "name": "Power-fluctuations-minimize-ramping-HydroPowerSmoothingUsingVariationSum First optimization EXP",
+            "value": 6.4,
+            "unit": "ms"
+          },
+          {
+            "name": "Power-fluctuations-minimize-ramping-HydroPowerSmoothingUsingVariationSum First optimization STD",
+            "value": 3.2,
+            "unit": "ms"
+          },
+          {
+            "name": "Power-fluctuations-minimize-ramping-HydroPowerSmoothingUsingVariationSum First optimization MIN",
+            "value": 0,
+            "unit": "ms"
+          },
+          {
+            "name": "Power-fluctuations-minimize-ramping-HydroPowerSmoothingUsingVariationSum First optimization MAX",
+            "value": 8,
+            "unit": "ms"
+          },
+          {
+            "name": "Power-fluctuations-minimize-ramping-HydroPowerSmoothingUsingVariationSum Second optimization EXP",
+            "value": 6.4,
+            "unit": "ms"
+          },
+          {
+            "name": "Power-fluctuations-minimize-ramping-HydroPowerSmoothingUsingVariationSum Second optimization STD",
+            "value": 3.2,
+            "unit": "ms"
+          },
+          {
+            "name": "Power-fluctuations-minimize-ramping-HydroPowerSmoothingUsingVariationSum Second optimization MIN",
+            "value": 0,
+            "unit": "ms"
+          },
+          {
+            "name": "Power-fluctuations-minimize-ramping-HydroPowerSmoothingUsingVariationSum Second optimization MAX",
+            "value": 8,
+            "unit": "ms"
+          },
+          {
+            "name": "Power-fluctuations-minimize-ramping-HydroPowerSmoothingUsingVariationSum execution_time",
+            "value": 658,
+            "unit": "ms"
+          },
+          {
+            "name": "Power-fluctuations-minimize-ramping-HydroPowerSmoothingUsingVariationSum Memory",
+            "value": 1198541,
+            "unit": "b"
+          },
+          {
+            "name": "thermal-fleet-Accurate-unit-commitment First optimization EXP",
+            "value": 19,
+            "unit": "ms"
+          },
+          {
+            "name": "thermal-fleet-Accurate-unit-commitment First optimization STD",
+            "value": 0,
+            "unit": "ms"
+          },
+          {
+            "name": "thermal-fleet-Accurate-unit-commitment First optimization MIN",
+            "value": 19,
+            "unit": "ms"
+          },
+          {
+            "name": "thermal-fleet-Accurate-unit-commitment First optimization MAX",
+            "value": 19,
+            "unit": "ms"
+          },
+          {
+            "name": "thermal-fleet-Accurate-unit-commitment Second optimization EXP",
+            "value": 10,
+            "unit": "ms"
+          },
+          {
+            "name": "thermal-fleet-Accurate-unit-commitment Second optimization STD",
+            "value": 0,
+            "unit": "ms"
+          },
+          {
+            "name": "thermal-fleet-Accurate-unit-commitment Second optimization MIN",
+            "value": 10,
+            "unit": "ms"
+          },
+          {
+            "name": "thermal-fleet-Accurate-unit-commitment Second optimization MAX",
+            "value": 10,
+            "unit": "ms"
+          },
+          {
+            "name": "thermal-fleet-Accurate-unit-commitment execution_time",
+            "value": 173,
+            "unit": "ms"
+          },
+          {
+            "name": "thermal-fleet-Accurate-unit-commitment Memory",
+            "value": 2252209,
+            "unit": "b"
+          },
+          {
+            "name": "025_mps-day First optimization EXP",
+            "value": 0,
+            "unit": "ms"
+          },
+          {
+            "name": "025_mps-day First optimization STD",
+            "value": 0,
+            "unit": "ms"
+          },
+          {
+            "name": "025_mps-day First optimization MIN",
+            "value": 0,
+            "unit": "ms"
+          },
+          {
+            "name": "025_mps-day First optimization MAX",
+            "value": 0,
+            "unit": "ms"
+          },
+          {
+            "name": "025_mps-day Second optimization EXP",
+            "value": 0,
+            "unit": "ms"
+          },
+          {
+            "name": "025_mps-day Second optimization STD",
+            "value": 0,
+            "unit": "ms"
+          },
+          {
+            "name": "025_mps-day Second optimization MIN",
+            "value": 0,
+            "unit": "ms"
+          },
+          {
+            "name": "025_mps-day Second optimization MAX",
+            "value": 0,
+            "unit": "ms"
+          },
+          {
+            "name": "025_mps-day execution_time",
+            "value": 946,
+            "unit": "ms"
+          },
+          {
+            "name": "025_mps-day Memory",
+            "value": 102771771,
+            "unit": "b"
+          },
+          {
+            "name": "Power-fluctuations-minimize-excursions-HydroPowerSmoothingUsingVariationMaxDown First optimization EXP",
+            "value": 5.6,
+            "unit": "ms"
+          },
+          {
+            "name": "Power-fluctuations-minimize-excursions-HydroPowerSmoothingUsingVariationMaxDown First optimization STD",
+            "value": 2.8,
+            "unit": "ms"
+          },
+          {
+            "name": "Power-fluctuations-minimize-excursions-HydroPowerSmoothingUsingVariationMaxDown First optimization MIN",
+            "value": 0,
+            "unit": "ms"
+          },
+          {
+            "name": "Power-fluctuations-minimize-excursions-HydroPowerSmoothingUsingVariationMaxDown First optimization MAX",
+            "value": 7,
+            "unit": "ms"
+          },
+          {
+            "name": "Power-fluctuations-minimize-excursions-HydroPowerSmoothingUsingVariationMaxDown Second optimization EXP",
+            "value": 5.6,
+            "unit": "ms"
+          },
+          {
+            "name": "Power-fluctuations-minimize-excursions-HydroPowerSmoothingUsingVariationMaxDown Second optimization STD",
+            "value": 2.8,
+            "unit": "ms"
+          },
+          {
+            "name": "Power-fluctuations-minimize-excursions-HydroPowerSmoothingUsingVariationMaxDown Second optimization MIN",
+            "value": 0,
+            "unit": "ms"
+          },
+          {
+            "name": "Power-fluctuations-minimize-excursions-HydroPowerSmoothingUsingVariationMaxDown Second optimization MAX",
+            "value": 7,
+            "unit": "ms"
+          },
+          {
+            "name": "Power-fluctuations-minimize-excursions-HydroPowerSmoothingUsingVariationMaxDown execution_time",
+            "value": 589,
+            "unit": "ms"
+          },
+          {
+            "name": "Power-fluctuations-minimize-excursions-HydroPowerSmoothingUsingVariationMaxDown Memory",
+            "value": 1198541,
+            "unit": "b"
+          },
+          {
+            "name": "reservoir-Min-MaxHydroPower First optimization EXP",
+            "value": 0,
+            "unit": "ms"
+          },
+          {
+            "name": "reservoir-Min-MaxHydroPower First optimization STD",
+            "value": 0,
+            "unit": "ms"
+          },
+          {
+            "name": "reservoir-Min-MaxHydroPower First optimization MIN",
+            "value": 0,
+            "unit": "ms"
+          },
+          {
+            "name": "reservoir-Min-MaxHydroPower First optimization MAX",
+            "value": 0,
+            "unit": "ms"
+          },
+          {
+            "name": "reservoir-Min-MaxHydroPower Second optimization EXP",
+            "value": 0,
+            "unit": "ms"
+          },
+          {
+            "name": "reservoir-Min-MaxHydroPower Second optimization STD",
+            "value": 0,
+            "unit": "ms"
+          },
+          {
+            "name": "reservoir-Min-MaxHydroPower Second optimization MIN",
+            "value": 0,
+            "unit": "ms"
+          },
+          {
+            "name": "reservoir-Min-MaxHydroPower Second optimization MAX",
+            "value": 0,
+            "unit": "ms"
+          },
+          {
+            "name": "reservoir-Min-MaxHydroPower execution_time",
+            "value": 2205,
             "unit": "ms"
           },
           {
