8124ab2d6ea823330906757fb9b970132a91a4a2
Abdoulbari ZAKIR <azakir@neverhack.com>
2024-01-25 12:21:53 +0100

    one file per tests unit

--- src/tests/run-study-tests/utils/benchmark_utils.py
+++ src/tests/run-study-tests/utils/benchmark_utils.py
@@ -7,15 +7,20 @@ class CustomBenchmark:
         self.study_path = study_path
         output_path =  find_dated_output_folder(study_path)
         self.custom_benchmark_file = output_path / "custom_benchmark.txt"
-        self.dest = dest
+        self.dest = Path(dest)
     
     def copy(self) : 
         content = ""
         with open(self.custom_benchmark_file, 'r') as f:
             content = f.read()
         
+        empty_dest = False
+        if not self.dest.parent.exists():
+            self.dest.parent.mkdir()
+            empty_dest = True
+
         with open(self.dest, "+a") as dest_file:
-            if  os.stat(self.dest).st_size != 0:
+            if not empty_dest:
                 dest_file.write(",\n")
             dest_file.write(content)
               
