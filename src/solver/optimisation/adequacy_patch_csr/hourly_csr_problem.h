40fbe1d70eb1de9360cfa6526f0d711cbb8d4bfd
Abdoulbari ZAKIR <azakir@neverhack.com>
2024-01-30 11:18:09 +0100

    fix

--- src/solver/optimisation/adequacy_patch_csr/hourly_csr_problem.h
+++ src/solver/optimisation/adequacy_patch_csr/hourly_csr_problem.h
@@ -85,10 +85,10 @@ public:
     explicit HourlyCSRProblem(const AdqPatchParams& adqPatchParams, PROBLEME_HEBDO* p) :
      adqPatchParams_(adqPatchParams),
      problemeHebdo_(p),
-     variableManagerFactory_(problemeHebdo_->CorrespondanceVarNativesVarOptim,
-                             problemeHebdo_->NumeroDeVariableStockFinal,
-                             problemeHebdo_->NumeroDeVariableDeTrancheDeStock,
-                             problemeHebdo_->NombreDePasDeTempsPourUneOptimisation)
+     variableManagerFactory_(p->CorrespondanceVarNativesVarOptim,
+                             p->NumeroDeVariableStockFinal,
+                             p->NumeroDeVariableDeTrancheDeStock,
+                             p->NombreDePasDeTempsPourUneOptimisation)
     {
         double temp = pow(10, -adqPatchParams.curtailmentSharing.thresholdVarBoundsRelaxation);
         belowThisThresholdSetToZero = std::min(temp, 0.1);
