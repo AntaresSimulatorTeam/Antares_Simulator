2e0a400248e268dfe8a4d264e909eba6a879b18b
Jason Marechal <jason.marechal+github@gmail.com>
2024-02-06 14:58:51 +0100

    Export variables name at week level
    Keep timesteps in variable names

--- src/solver/optimisation/opt_optimisation_lineaire.cpp
+++ src/solver/optimisation/opt_optimisation_lineaire.cpp
@@ -1,29 +1,23 @@
-/*
-** Copyright 2007-2023 RTE
-** Authors: Antares_Simulator Team
-**
-** This file is part of Antares_Simulator.
-**
-** Antares_Simulator is free software: you can redistribute it and/or modify
-** it under the terms of the GNU General Public License as published by
-** the Free Software Foundation, either version 3 of the License, or
-** (at your option) any later version.
-**
-** There are special exceptions to the terms and conditions of the
-** license as they are applied to this software. View the full text of
-** the exceptions in file COPYING.txt in the directory of this software
-** distribution
-**
-** Antares_Simulator is distributed in the hope that it will be useful,
-** but WITHOUT ANY WARRANTY; without even the implied warranty of
-** MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
-** GNU General Public License for more details.
-**
-** You should have received a copy of the GNU General Public License
-** along with Antares_Simulator. If not, see <http://www.gnu.org/licenses/>.
-**
-** SPDX-License-Identifier: licenceRef-GPL3_WITH_RTE-Exceptions
-*/
+// /*
+// ** Copyright 2007-2024, RTE (https://www.rte-france.com)
+// ** See AUTHORS.txt
+// ** SPDX-License-Identifier: MPL-2.0
+// ** This file is part of Antares-Simulator,
+// ** Adequacy and Performance assessment for interconnected energy networks.
+// **
+// ** Antares_Simulator is free software: you can redistribute it and/or modify
+// ** it under the terms of the Mozilla Public Licence 2.0 as published by
+// ** the Mozilla Foundation, either version 2 of the License, or
+// ** (at your option) any later version.
+// **
+// ** Antares_Simulator is distributed in the hope that it will be useful,
+// ** but WITHOUT ANY WARRANTY; without even the implied warranty of
+// ** MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+// ** Mozilla Public Licence 2.0 for more details.
+// **
+// ** You should have received a copy of the Mozilla Public Licence 2.0
+// ** along with Antares_Simulator. If not, see <https://opensource.org/license/mpl-2-0/>.
+// *
 
 #include "antares/solver/simulation/sim_structure_probleme_economique.h"
 #include "antares/solver/optimisation/opt_fonctions.h"
@@ -135,8 +129,6 @@ bool runWeeklyOptimization(const OptimizationOptions& options,
             copy(problemeHebdo->ProblemeAResoudre->IndicesColonnes, year_ptr->IndicesColonnes);
 
             copy(problemeHebdo->ProblemeAResoudre->IndicesDebutDeLigne, year_ptr->Mdeb);
-            copy(problemeHebdo->ProblemeAResoudre->NomDesVariables, year_ptr->variables);
-            copy(problemeHebdo->ProblemeAResoudre->NomDesContraintes, year_ptr->constraints);
 
             lps->_constant = year_ptr;
         }
@@ -148,7 +140,8 @@ bool runWeeklyOptimization(const OptimizationOptions& options,
             copy(problemeHebdo->ProblemeAResoudre->CoutLineaire, week_ptr->CoutLineaire);
             copy(problemeHebdo->ProblemeAResoudre->Xmax, week_ptr->Xmax);
             copy(problemeHebdo->ProblemeAResoudre->Xmin, week_ptr->Xmin);
-
+            copy(problemeHebdo->ProblemeAResoudre->NomDesVariables, week_ptr->variables);
+            copy(problemeHebdo->ProblemeAResoudre->NomDesContraintes, week_ptr->constraints);
             copy(problemeHebdo->ProblemeAResoudre->SecondMembre, week_ptr->SecondMembre);
             copy(problemeHebdo->ProblemeAResoudre->Sens, week_ptr->Sens);
             std::string problemName = createMPSfilename(*optPeriodStringGenerator, optimizationNumber);
--- src/solver/simulation/include/antares/solver/simulation/LpsFromAntares.h
+++ src/solver/simulation/include/antares/solver/simulation/LpsFromAntares.h
@@ -1,3 +1,24 @@
+// /*
+// ** Copyright 2007-2024, RTE (https://www.rte-france.com)
+// ** See AUTHORS.txt
+// ** SPDX-License-Identifier: MPL-2.0
+// ** This file is part of Antares-Simulator,
+// ** Adequacy and Performance assessment for interconnected energy networks.
+// **
+// ** Antares_Simulator is free software: you can redistribute it and/or modify
+// ** it under the terms of the Mozilla Public Licence 2.0 as published by
+// ** the Mozilla Foundation, either version 2 of the License, or
+// ** (at your option) any later version.
+// **
+// ** Antares_Simulator is distributed in the hope that it will be useful,
+// ** but WITHOUT ANY WARRANTY; without even the implied warranty of
+// ** MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+// ** Mozilla Public Licence 2.0 for more details.
+// **
+// ** You should have received a copy of the Mozilla Public Licence 2.0
+// ** along with Antares_Simulator. If not, see <https://opensource.org/license/mpl-2-0/>.
+// *
+
 // Je modifie la structure de Manuel en fonction des remarques que je vous ai
 // faites vendredi
 
@@ -97,9 +118,6 @@ public:
     // fera le nommage des contraintes
     // dans Antarès ?
 
-    std::vector<std::string> variables;
-    std::vector<std::string> constraints;
-
     friend class boost::serialization::access;
     template <class Archive>
     void serialize(Archive& ar, [[maybe_unused]] const unsigned int version) {
@@ -113,8 +131,6 @@ public:
         ar& CoefficientsDeLaMatriceDesContraintes;
         ar& SignificationMetierDesVariables;
         ar& SignificationMetierDesContraintes;
-        ar& variables;
-        ar& constraints;
     }
 };
 
@@ -134,6 +150,9 @@ public:
     // NombreDeContraintes
     std::string name;
 
+    std::vector<std::string> variables;
+    std::vector<std::string> constraints;
+
     friend class boost::serialization::access;
     template <class Archive>
     void serialize(Archive& ar, [[maybe_unused]] const unsigned int version) {
@@ -143,6 +162,8 @@ public:
         ar& CoutLineaire;
         ar& SecondMembre;
         ar& name;
+        ar& variables;
+        ar& constraints;
     }
 };
 
