4c47d7d2db875abf441e00146d6f4ce94bcf73b4
Vincent Payet <vincent.payet@rte-france.com>
2024-02-19 10:23:18 +0100

    use all() to reset TS

--- src/solver/ts-generator/generator.cpp
+++ src/solver/ts-generator/generator.cpp
@@ -54,7 +54,7 @@ void ResizeGeneratedTimeSeries(Data::AreaList& areas, Data::Parameters& params)
 
         // Thermal
         bool globalThermalTSgeneration = params.timeSeriesToGenerate & Data::timeSeriesThermal;
-        for (auto& cluster : area.thermal.list.each_enabled())
+        for (auto& cluster : area.thermal.list.all())
         {
             if (cluster->doWeGenerateTS(globalThermalTSgeneration))
                 cluster->series.timeSeries.reset(params.nbTimeSeriesThermal, HOURS_PER_YEAR);
