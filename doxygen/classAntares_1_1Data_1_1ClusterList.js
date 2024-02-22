1450120c58ba9a2adea32fe2e090c4d5c713182a
JasonMarechal25 <JasonMarechal25@users.noreply.github.com>
2024-02-22 09:34:30 +0000

    deploy: b18190483fcb5c67c6ec292e735627cc66c5a88b

--- doxygen/classAntares_1_1Data_1_1ClusterList.js
+++ doxygen/classAntares_1_1Data_1_1ClusterList.js
@@ -1,20 +1,12 @@
 var classAntares_1_1Data_1_1ClusterList =
 [
-    [ "add", "classAntares_1_1Data_1_1ClusterList.html#af3042479d71c182acae32e9e5300d1d8", null ],
-    [ "clear", "classAntares_1_1Data_1_1ClusterList.html#a462a8b9d9639b3c0f9edd4825d02aeb3", null ],
-    [ "each", "classAntares_1_1Data_1_1ClusterList.html#ac8de4cf1f46071ce78ca4c9945e8a265", null ],
-    [ "empty", "classAntares_1_1Data_1_1ClusterList.html#a3da2332939cce887c0d1dfb0d6130049", null ],
     [ "exists", "classAntares_1_1Data_1_1ClusterList.html#a33ef5b2b0a26fa7ac9fd90fc00251932", null ],
-    [ "find", "classAntares_1_1Data_1_1ClusterList.html#ac83cfea4022b181e60f78b98627695b8", null ],
+    [ "findInAll", "classAntares_1_1Data_1_1ClusterList.html#ad0ccafba9922c3ea8b7d74a8c237b9d1", null ],
     [ "forceReload", "classAntares_1_1Data_1_1ClusterList.html#a98b187097df27060de3243eef91e82bc", null ],
     [ "markAsModified", "classAntares_1_1Data_1_1ClusterList.html#a2ca6eedce55b9a8f79ae8406e74d205b", null ],
-    [ "memoryUsage", "classAntares_1_1Data_1_1ClusterList.html#a29a20946dc7f353ec184a4726cefe83b", null ],
+    [ "memoryUsage", "classAntares_1_1Data_1_1ClusterList.html#a28da8f67aafa8fdab4b8e8dc5552f7a9", null ],
     [ "remove", "classAntares_1_1Data_1_1ClusterList.html#ae4b6287ad34a77daeec40dda0c8d22db", null ],
-    [ "removeDisabledClusters", "classAntares_1_1Data_1_1ClusterList.html#a7ae4b1917888accd01bf9b57db2c06bf", null ],
     [ "rename", "classAntares_1_1Data_1_1ClusterList.html#aff81c0c6a4a31ea871dfe6a99b6af7aa", null ],
-    [ "resizeAllTimeseriesNumbers", "classAntares_1_1Data_1_1ClusterList.html#a6ceab28ffeaaaca7af34373dfd8a903c", null ],
-    [ "retrieveTotalCapacityAndUnitCount", "classAntares_1_1Data_1_1ClusterList.html#aebf4acf28c0d0becf175f69862cb49b6", null ],
-    [ "size", "classAntares_1_1Data_1_1ClusterList.html#a41a21dcb10f31cbf44ff9e6e7860be6a", null ],
-    [ "typeID", "classAntares_1_1Data_1_1ClusterList.html#a62f093580139d7e146f6e6a27959ea9d", null ],
-    [ "clusters", "classAntares_1_1Data_1_1ClusterList.html#ae77ca5c14407a09c8de446306ac61e17", null ]
+    [ "resizeAllTimeseriesNumbers", "classAntares_1_1Data_1_1ClusterList.html#a2742ab55510a24092d9cb7105c73bd7e", null ],
+    [ "retrieveTotalCapacityAndUnitCount", "classAntares_1_1Data_1_1ClusterList.html#aebf4acf28c0d0becf175f69862cb49b6", null ]
 ];
