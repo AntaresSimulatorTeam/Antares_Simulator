d0de5031518252930a33e482bfcb0a1f608fb00e
Vincent Payet <vincent.payet@rte-france.com>
2024-02-26 16:45:03 +0100

    add struct settings, add basic parsing

---
+++ src/tools/timeseries-generation/include/antares/timeseries-generation/timeseries-generation.h
@@ -0,0 +1,7 @@
+#pragma once
+
+struct TsGeneratorSettings
+{
+    bool allThermal = false;
+    std::string thermal = "";
+};
--- src/tools/timeseries-generation/main.cpp
+++ src/tools/timeseries-generation/main.cpp
@@ -1,22 +1,57 @@
 #include <memory>
 #include <string>
 
+#include <antares/exception/LoadingError.hpp>
 #include <antares/study/header.h>
 #include <antares/study/study.h>
 #include <antares/logs/logs.h>
+#include <yuni/core/getopt.h>
+
+#include <antares/timeseries-generation/timeseries-generation.h>
 
 using namespace Antares;
+
+std::unique_ptr<Yuni::GetOpt::Parser> createTsGeneratorParser(TsGeneratorSettings& settings)
+{
+    auto parser = std::unique_ptr<Yuni::GetOpt::Parser>(new Yuni::GetOpt::Parser());
+    parser->addParagraph("Antares Time Series generator\n");
+
+    parser->addFlag(settings.allThermal, ' ', "all-thermal", "Generate TS for all thermal clusters");
+
+    parser->addFlag(settings.allThermal, 't', "thermal", "Generate TS for a list of thermal clusters");
+
+
+    return parser;
+}
 
 int main(int argc, char *argv[])
 {
     if (argc < 2)
         return 1;
 
-    std::cout << "hello" << std::endl;
+    TsGeneratorSettings settings;
+
+    auto parser = createTsGeneratorParser(settings);
+    switch (auto ret = parser->operator()(argc, argv); ret)
+    {
+        using namespace Yuni::GetOpt;
+    case ReturnCode::error:
+        throw Error::CommandLineArguments(parser->errors());
+    case ReturnCode::help:
+        // End the program
+        return 0;
+    default:
+        break;
+    }
 
     auto study = std::make_shared<Data::Study>(true);
     Data::StudyLoadOptions studyOptions;
-    study->loadFromFolder(argv[1], studyOptions);
+
+    if (!study->loadFromFolder(argv[1], studyOptions))
+    {
+        logs.error() << "couldn't load study";
+        return 1;
+    }
 
     for (auto& [name, area] : study->areas)
         for (auto& c : area->thermal.list.all())
