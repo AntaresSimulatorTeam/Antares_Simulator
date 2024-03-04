7ad926d661f9b680cfe5abad1028e5c55c16d28a
Vincent Payet <vincent.payet@rte-france.com>
2024-03-04 17:42:12 +0100

    first test ok

--- src/tests/run-study-tests/ts-generator/ts-generator.py
+++ src/tests/run-study-tests/ts-generator/ts-generator.py
@@ -9,15 +9,21 @@ class StudyReference(object):
 
     def __init__(self, path, args):
         self.path = Path(path).resolve()
+        self.args = args
 
     def run_and_compare(self, exe):
-        res = subprocess.run([exe, self.path])
+        res = subprocess.run([exe, "--thermal=" + self.args, self.path])
         assert (res.returncode == 0), "The exec failed for study: " + str(self.path)
 
-        out_path = self.path / "ts-generator"
-        res = subprocess.run(["diff", "-bur", out_path, self.ref_path])
+        out_path = self.path / find_dated_output_folder(self.path) / "ts-generator"
+        ref_path = self.path / "output" / "reference" / "ts-generator"
+        res = subprocess.run(["diff", "-bur", out_path, ref_path])
         assert (res.returncode == 0), "Wrong results for study: " + str(self.path)
 
+
+study_list = []
+study_list.append(StudyReference("../resources/batches/valid-tsgenerator/007 Thermal fleet - Fast unit commitment", "area.base;area.peak"))
+
 @pytest.mark.tsgenerator
-def test1():
-    assert (1 == 1), "test"
+def test1(exe_tsgenerator_path):
+    study_list[0].run_and_compare(exe_tsgenerator_path)
--- src/tools/ts-generator/main.cpp
+++ src/tools/ts-generator/main.cpp
@@ -152,6 +152,6 @@ int main(int argc, char *argv[])
     for (auto& c : clusters)
         logs.debug() << c->id();
 
-    return TSGenerator::GenerateThermalTimeSeries(*study, clusters, 0, *resultWriter);
+    return !TSGenerator::GenerateThermalTimeSeries(*study, clusters, 0, *resultWriter);
 }
 
