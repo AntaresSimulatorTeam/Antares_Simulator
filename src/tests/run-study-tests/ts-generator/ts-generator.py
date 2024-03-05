8172579fecb5a2541c252436e25572faf15fe605
Vincent Payet <vincent.payet@rte-france.com>
2024-03-05 17:36:07 +0100

    use a file in study to get arg list

--- src/tests/run-study-tests/ts-generator/ts-generator.py
+++ src/tests/run-study-tests/ts-generator/ts-generator.py
@@ -7,12 +7,15 @@ from utils.find_output import find_dated_output_folder
 
 class StudyReference(object):
 
-    def __init__(self, path, args):
+    def __init__(self, path):
         self.path = Path(path).resolve()
-        self.args = args
 
     def run_and_compare(self, exe):
-        res = subprocess.run([exe, "--thermal=" + self.args, self.path])
+        clusterToGenFile = open(self.path / "clustersToGen.txt", 'r')
+        line = clusterToGenFile.readline().rstrip() # remove new line char
+        clusterToGenFile.close()
+
+        res = subprocess.run([exe, "--thermal=" + line, self.path])
         assert (res.returncode == 0), "The exec failed for study: " + str(self.path)
 
         out_path = self.path / find_dated_output_folder(self.path) / "ts-generator"
@@ -22,10 +25,10 @@ class StudyReference(object):
 
 
 study_list = []
-study_list.append(StudyReference("../resources/batches/valid-tsgenerator/007 Thermal fleet - Fast unit commitment", "area.base;area.peak"))
-study_list.append(StudyReference("../resources/batches/valid-tsgenerator/079 Zero  Power Balance - Type 1", "6.p;g.sb;b.b;z11.p;&vir-west.dummy;z11.sb"))
+study_list.append(StudyReference("../resources/batches/valid-tsgenerator/007 Thermal fleet - Fast unit commitment"))
+study_list.append(StudyReference("../resources/batches/valid-tsgenerator/079 Zero  Power Balance - Type 1"))
 
 @pytest.mark.tsgenerator
 def test_ts_generator(exe_tsgenerator_path):
     for study in study_list:
-        study .run_and_compare(exe_tsgenerator_path)
+        study.run_and_compare(exe_tsgenerator_path)
