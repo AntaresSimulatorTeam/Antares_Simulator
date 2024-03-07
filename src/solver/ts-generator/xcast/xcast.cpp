7b25576c081c22394e05c05145a76e5d165f745c
Vincent Payet <vincent.payet@rte-france.com>
2024-03-07 16:28:37 +0100

    xcast inf

--- src/solver/ts-generator/xcast/xcast.cpp
+++ src/solver/ts-generator/xcast/xcast.cpp
@@ -150,7 +150,7 @@ void XCast::applyTransferFunction(PredicateT& predicate)
 
                     if (dailyResults[h] >= pj[x] && dailyResults[h] <= pk[x])
                     {
-                        assert(0 == Math::Infinite(b[j]) && "Infinite value");
+                        assert(!std::isinf(b[j]) && "Infinite value");
                         dailyResults[h] = (a[j] * dailyResults[h]) + b[j];
                         last_i = i;
                         break;
@@ -478,7 +478,7 @@ bool XCast::runWithPredicate(PredicateT& predicate, Progression::Task& progressi
 
                     for (uint h = 0; h != HOURS_PER_DAY; ++h)
                     {
-                        assert(0 == Math::Infinite(dailyResults[h]) && "Infinite value");
+                        assert(!std::isinf(dailyResults[h]) && "Infinite value");
                     }
                 }
 #endif
@@ -499,7 +499,7 @@ bool XCast::runWithPredicate(PredicateT& predicate, Progression::Task& progressi
 
                     for (uint h = 0; h != HOURS_PER_DAY; ++h)
                     {
-                        assert(0 == Math::Infinite(dailyResults[h]) && "Infinite value");
+                        assert(!std::isinf(dailyResults[h]) && "Infinite value");
                         dailyResults[h] += (float)column[hourInTheYear + h];
                     }
                 }
@@ -514,7 +514,7 @@ bool XCast::runWithPredicate(PredicateT& predicate, Progression::Task& progressi
 
                     for (uint h = 0; h != HOURS_PER_DAY; ++h)
                     {
-                        assert(0 == Math::Infinite(dailyResults[h]) && "Infinite value");
+                        assert(!std::isinf(dailyResults[h]) && "Infinite value");
                     }
                 }
 #endif
@@ -533,7 +533,7 @@ bool XCast::runWithPredicate(PredicateT& predicate, Progression::Task& progressi
 
                     for (uint h = 0; h != HOURS_PER_DAY; ++h)
                     {
-                        assert(0 == Math::Infinite(dailyResults[h]) && "Infinite value");
+                        assert(!std::isinf(dailyResults[h]) && "Infinite value");
                         dailyResults[h] *= (float)srcData.capacity;
                     }
 
