b53e042de1f64a97a6d29ba697e9c2c6811d4a30
Florian OMNES <florian.omnes@rte-france.com>
2024-03-12 17:14:50 +0100

    cherry pick #1982

--- src/solver/optimisation/post_process_commands.h
+++ src/solver/optimisation/post_process_commands.h
@@ -51,14 +51,18 @@ private:
 
 class DTGmarginForAdqPatchPostProcessCmd : public basePostProcessCommand
 {
+    using AdqPatchParams = Antares::Data::AdequacyPatch::AdqPatchParams;
+
 public:
-    DTGmarginForAdqPatchPostProcessCmd(PROBLEME_HEBDO* problemeHebdo,
+    DTGmarginForAdqPatchPostProcessCmd(const AdqPatchParams& adqPatchParams,
+                                       PROBLEME_HEBDO* problemeHebdo,
                                        AreaList& areas,
                                        unsigned int thread_number);
 
     void execute(const optRuntimeData& opt_runtime_data) override;
 
 private:
+    const AdqPatchParams& adqPatchParams_;
     const AreaList& area_list_;
     unsigned int thread_number_ = 0;
 };
@@ -91,6 +95,7 @@ private:
 class CurtailmentSharingPostProcessCmd : public basePostProcessCommand
 {
     using AdqPatchParams = Antares::Data::AdequacyPatch::AdqPatchParams;
+
 public:
     CurtailmentSharingPostProcessCmd(const AdqPatchParams& adqPatchParams,
                                      PROBLEME_HEBDO* problemeHebdo,
