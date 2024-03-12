9675c6050d7d7b32cdf24362e9731747d6dcce7b
Abdoulbari Zakir <abdoulbari.zakir@gmail.com>
2024-03-12 12:51:41 +0100

    test yaml-cpp

---
+++ src/libs/antares/yaml-parser/main.cpp
@@ -0,0 +1,158 @@
+#include "yaml-cpp/yaml.h"
+#include <iostream>
+#include <fstream>
+#include <string>
+#include <vector>
+
+// our data types
+struct Vec3
+{
+    float x, y, z;
+};
+struct Power
+{
+    std::string name;
+    int damage;
+};
+
+struct Monster
+{
+    std::string name;
+    Vec3 position;
+    std::vector<Power> powers;
+};
+namespace YAML
+{
+template<>
+struct convert<Vec3>
+{
+    static Node encode(const Vec3& rhs)
+    {
+        Node node;
+        node.push_back(rhs.x);
+        node.push_back(rhs.y);
+        node.push_back(rhs.z);
+        return node;
+    }
+
+    static bool decode(const Node& node, Vec3& rhs)
+    {
+        if (!node.IsSequence() || node.size() != 3)
+        {
+            return false;
+        }
+
+        rhs.x = node[0].as<double>();
+        rhs.y = node[1].as<double>();
+        rhs.z = node[2].as<double>();
+        return true;
+    }
+};
+template<>
+struct convert<Power>
+{
+    static Node encode(const Power& rhs)
+    {
+        Node node;
+        node["name"] = rhs.name;
+        node["damage"] = rhs.damage;
+        return node;
+    }
+
+    static bool decode(const Node& node, Power& rhs)
+    {
+        rhs.name = node["name"].as<std::string>();
+        rhs.damage = node["damage"].as<int>();
+        return true;
+    }
+};
+template<>
+struct convert<Monster>
+{
+    static Node encode(const Monster& rhs)
+    {
+        Node node;
+        node["name"] = rhs.name;
+        // node["position"] = node["position"].as<Vec3>();
+        node["position"] = rhs.position;
+        node["powers"] = rhs.powers;
+
+        return node;
+    }
+
+    static bool decode(const Node& node, Monster& rhs)
+    {
+        rhs.name = node["name"].as<std::string>();
+        rhs.position = node["position"].as<Vec3>();
+        const YAML::Node& powers = node["powers"];
+        for (const auto power : powers)
+        {
+            rhs.powers.push_back(power.as<Power>());
+        }
+        return true;
+    }
+};
+} // namespace YAML
+
+// // now the extraction operators for these types
+// void operator>>(const YAML::Node& node, Vec3& v)
+// {
+//     node[0] >> v.x;
+//     node[1] >> v.y;
+//     node[2] >> v.z;
+// }
+
+// void operator>>(const YAML::Node& node, Power& power)
+// {
+//     node["name"] >> power.name;
+//     node["damage"] >> power.damage;
+// }
+
+// void operator>>(const YAML::Node& node, Monster& monster)
+// {
+//     node["name"] >> monster.name;
+//     node["position"] >> monster.position;
+//     const YAML::Node& powers = node["powers"];
+//     for (unsigned i = 0; i < powers.size(); i++)
+//     {
+//         Power power;
+//         powers[i] >> power;
+//         monster.powers.push_back(power);
+//     }
+// }
+
+// int main(int argc, char** argv)
+int main()
+{
+    // YAML::Node doc = YAML::LoadFile(argv[1]);
+    std::string my_yaml = R"(- name: Ogre
+  position: [0, 5, 0]
+  powers:
+    - name: Club
+      damage: 10
+    - name: Fist
+      damage: 8
+- name: Dragon
+  position: [1, 0, 10]
+  powers:
+    - name: Fire Breath
+      damage: 25
+    - name: Claws
+      damage: 15
+- name: Wizard
+  position: [5, -3, 0]
+  powers:
+    - name: Acid Rain
+      damage: 50
+    - name: Staff
+      damage: 3)";
+    YAML::Node doc = YAML::Load(my_yaml);
+    for (unsigned i = 0; i < doc.size(); i++)
+    {
+        Monster monster;
+        monster = doc[i].as<Monster>();
+        std::cout << monster.name << "\n";
+    }
+
+    return 0;
+}
