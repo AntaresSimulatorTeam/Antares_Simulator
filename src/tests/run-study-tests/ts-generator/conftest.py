dbab0bc2ba57e5f012d0d340c7490d6b71920d2c
Vincent Payet <vincent.payet@rte-france.com>
2024-03-13 16:39:24 +0100

    create antares_tools dir

--- src/tests/run-study-tests/ts-generator/conftest.py
+++
@@ -1,8 +0,0 @@
-import pytest
-
-def pytest_addoption(parser):
-    parser.addoption("--exe_tsgenerator_path", action="store", default="")
-
-@pytest.fixture
-def exe_tsgenerator_path(request):
-    return request.config.getoption("--exe_tsgenerator_path")
