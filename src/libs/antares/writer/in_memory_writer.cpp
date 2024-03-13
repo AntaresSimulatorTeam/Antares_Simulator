8cf4ac3b489e2bfa759188679ce94c1db8b89f17
Florian Omnès <26088210+flomnes@users.noreply.github.com>
2024-03-13 11:15:17 +0100

    Add "in memory" implementation for `IResultWriter` (#1927)
    Mostly for debugging purposes, in-memory tests
    
    1. Where a IResultWriter is expected, provide an InMemoryWriter
    2. Read results in memory
    3. Profit
    
    ## TODO
    - [x] In Windows, separators are '\\'. I suggest to replace them by '/'
    for platform-independence.

---
+++ src/libs/antares/writer/in_memory_writer.cpp
@@ -0,0 +1,111 @@
+#include <algorithm>
+
+#include <antares/writer/in_memory_writer.h>
+#include <antares/logs/logs.h>
+
+#include <antares/benchmarking/timer.h>
+#include <antares/benchmarking/DurationCollector.h>
+
+#include <yuni/io/file.h> // Yuni::IO::File::LoadFromFile
+
+namespace Antares::Solver {
+
+namespace
+{
+void logErrorAndThrow [[noreturn]] (const std::string& errorMessage)
+{
+    Antares::logs.error() << errorMessage;
+    throw IOError(errorMessage);
+}
+
+  template<class ContentT>
+  void addToMap(InMemoryWriter::MapType& entries,
+                const std::string& entryPath,
+                ContentT& content,
+                std::mutex& mutex,
+                Benchmarking::IDurationCollector& duration_collector)
+  {
+    std::string entryPathSanitized = entryPath;
+    std::replace(entryPathSanitized.begin(),
+                 entryPathSanitized.end(),
+                 '\\',
+                 '/');
+
+    Benchmarking::Timer timer_wait;
+    std::lock_guard lock(mutex);
+    timer_wait.stop();
+    duration_collector.addDuration("in_memory_wait", timer_wait.get_duration());
+
+    Benchmarking::Timer timer_insert;
+    entries.insert({entryPathSanitized, content});
+    timer_insert.stop();
+    duration_collector.addDuration("in_memory_insert", timer_insert.get_duration());
+  }
+}
+
+
+InMemoryWriter::InMemoryWriter(Benchmarking::IDurationCollector& duration_collector) : pDurationCollector(duration_collector) {}
+
+InMemoryWriter::~InMemoryWriter() = default;
+
+void InMemoryWriter::addEntryFromBuffer(const std::string& entryPath, Yuni::Clob& entryContent)
+{
+   addToMap(pEntries,
+            entryPath,
+            entryContent,
+            pMapMutex,
+            pDurationCollector);
+}
+void InMemoryWriter::addEntryFromBuffer(const std::string& entryPath, std::string& entryContent)
+{
+   addToMap(pEntries,
+            entryPath,
+            entryContent,
+            pMapMutex,
+            pDurationCollector);
+}
+
+void InMemoryWriter::addEntryFromFile(const std::string& entryPath, const std::string& filePath)
+{
+    // Shamelessly copy-pasted from zip_writer.cpp
+    // TODO refactor
+    Yuni::Clob buffer;
+    switch (Yuni::IO::File::LoadFromFile(buffer, filePath.c_str()))
+    {
+        using namespace Yuni::IO;
+    case errNone:
+         addToMap(pEntries,
+                  entryPath,
+                  buffer,
+                  pMapMutex,
+                  pDurationCollector);
+        break;
+    // Since logErrorAndThrow does not return, we don't need 'break's here
+    case errNotFound: logErrorAndThrow(filePath + ": file does not exist");
+    case errReadFailed: logErrorAndThrow("Read failed '" + filePath + "'");
+    case errMemoryLimit: logErrorAndThrow("Size limit hit for file '" + filePath + "'");
+    default: logErrorAndThrow("Unhandled error");
+    }
+}
+void InMemoryWriter::flush()
+{
+    // Nothing to do here
+}
+
+bool InMemoryWriter::needsTheJobQueue() const
+{
+    // We may reconsider if performance is atrocious
+    return false;
+}
+
+void InMemoryWriter::finalize(bool /* verbose */)
+{
+    // Nothing to do here
+}
+
+const InMemoryWriter::MapType& InMemoryWriter::getMap() const
+{
+    return pEntries;
+}
+
+}
