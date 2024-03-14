ab7efdb6ea2103db95197816a9b6bd008230b241
Guillaume PIERRE <guillaume.pierre@rte-france.com>
2024-03-14 17:23:34 +0100

    CR 23 add tests : remove unintentional duplication

--- src/tests/src/libs/antares/study/scenario-builder/test-sc-builder-file-read-line.cpp
+++ src/tests/src/libs/antares/study/scenario-builder/test-sc-builder-file-read-line.cpp
@@ -109,12 +109,6 @@ struct Fixture
 		area_2->hydro.series->resizeMaxPowerTS(nbReadyMadeTS);
 		area_3->hydro.series->resizeMaxPowerTS(nbReadyMadeTS);
 		
-		// Hydro Max Power: set the nb of ready made TS
-		nbReadyMadeTS = 15;
-		area_1->hydro.series->resizeMaxPowerTS(nbReadyMadeTS);
-		area_2->hydro.series->resizeMaxPowerTS(nbReadyMadeTS);
-		area_3->hydro.series->resizeMaxPowerTS(nbReadyMadeTS);
-
 		// Links
 		link_12 = AreaAddLinkBetweenAreas(area_1, area_2, false);
 		link_12->directCapacities.resize(15, 1);
