6f2bc9fe335c488ac7232c36fbfbe2da1f97751c
Jason Marechal <jason.marechal+github@gmail.com>
2024-03-15 10:24:46 +0100

    WIP

--- src/solver/application/application.cpp
+++ src/solver/application/application.cpp
@@ -183,7 +183,9 @@ void Application::startSimulation(Data::StudyLoadOptions options)
 
     logs.callback.connect(this, &Application::onLogMessage);
 
-    pStudy = std::make_shared<Antares::Data::Study>(true /* for the solver */);
+    FileTreeStudyLoader fileTreeStudyLoader(options, pSettings, resultWriter);
+    processCaption(Yuni::String() << "antares: loading \"" << pSettings.studyFolder << "\"");
+    pStudy = fileTreeStudyLoader.load();
 
     pParameters = &(pStudy->parameters);
 
