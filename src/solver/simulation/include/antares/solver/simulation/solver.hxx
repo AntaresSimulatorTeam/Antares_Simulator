b4f129a684ec2ce98aceaae7ae6b02912b3bc8a9
Vincent Payet <vincent.payet@rte-france.com>
2024-03-25 16:27:39 +0100

    fix type

--- src/solver/simulation/include/antares/solver/simulation/solver.hxx
+++ src/solver/simulation/include/antares/solver/simulation/solver.hxx
@@ -491,7 +491,7 @@ void ISimulation<ImplementationType>::regenerateTimeSeries(uint year)
         auto clusters = getAllClustersToGen(study.areas, pData.haveToRefreshTSThermal);
 #define SEP Yuni::IO::Separator
         const std::string savePath = std::string("ts-generator") + SEP + "thermal" + SEP +
-            "mc-l" + std::to_string(year);
+            "mc-" + std::to_string(year);
 #undef SEP
         GenerateThermalTimeSeries(study, clusters, pResultWriter, savePath);
     }
