91aa4ecc0117b1aed9f14edbe71d25ed09a6fb1d
Jason Marechal <jason.marechal+github@gmail.com>
2024-03-27 10:41:21 +0100

    Make ortools private dependency of infeasible_problem_analysis

--- src/solver/optimisation/include/antares/solver/optimisation/opt_export_structure.h
+++ src/solver/optimisation/include/antares/solver/optimisation/opt_export_structure.h
@@ -1,44 +1,34 @@
 /*
-** Copyright 2007-2024, RTE (https://www.rte-france.com)
-** See AUTHORS.txt
-** SPDX-License-Identifier: MPL-2.0
-** This file is part of Antares-Simulator,
-** Adequacy and Performance assessment for interconnected energy networks.
-**
-** Antares_Simulator is free software: you can redistribute it and/or modify
-** it under the terms of the Mozilla Public Licence 2.0 as published by
-** the Mozilla Foundation, either version 2 of the License, or
-** (at your option) any later version.
-**
-** Antares_Simulator is distributed in the hope that it will be useful,
-** but WITHOUT ANY WARRANTY; without even the implied warranty of
-** MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
-** Mozilla Public Licence 2.0 for more details.
-**
-** You should have received a copy of the Mozilla Public Licence 2.0
-** along with Antares_Simulator. If not, see <https://opensource.org/license/mpl-2-0/>.
-*/
-#ifndef __EXPORT_STRUCTURE__
-#define __EXPORT_STRUCTURE__
-
-#include <vector>
-#include <optional>
-#include <string>
-
+ * Copyright 2007-2024, RTE (https://www.rte-france.com)
+ * See AUTHORS.txt
+ * SPDX-License-Identifier: MPL-2.0
+ * This file is part of Antares-Simulator,
+ * Adequacy and Performance assessment for interconnected energy networks.
+ *
+ * Antares_Simulator is free software: you can redistribute it and/or modify
+ * it under the terms of the Mozilla Public Licence 2.0 as published by
+ * the Mozilla Foundation, either version 2 of the License, or
+ * (at your option) any later version.
+ *
+ * Antares_Simulator is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+ * Mozilla Public Licence 2.0 for more details.
+ *
+ * You should have received a copy of the Mozilla Public Licence 2.0
+ * along with Antares_Simulator. If not, see <https://opensource.org/license/mpl-2-0/>.
+ */
+#pragma once
 #include <yuni/yuni.h>
 
 #include "antares/antares/Enum.hpp"
 #include "antares/writer/i_writer.h"
 #include <antares/study/study.h>
-
-namespace Antares
-{
-namespace Data
+namespace Antares::Data
 {
 
 class Study;
 } // namespace Data
-} // namespace Antares
 
 struct PROBLEME_HEBDO;
 
@@ -47,5 +37,3 @@ void OPT_ExportInterco(Antares::Solver::IResultWriter& writer,
 void OPT_ExportAreaName(Antares::Solver::IResultWriter& writer,
                         const std::vector<const char*>& areaNames);
 void OPT_ExportStructures(PROBLEME_HEBDO* problemeHebdo, Antares::Solver::IResultWriter& writer);
-
-#endif
