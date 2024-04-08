165a057511754ff82b61e787d52b8c633699613b
Jason Marechal <jason.marechal+github@gmail.com>
2024-04-08 16:57:19 +0200

    Fix post rebase

---
+++ src/ext/yuni/src/yuni/core/system/cpu.h
@@ -0,0 +1,50 @@
+/*
+ * Copyright 2007-2024, RTE (https://www.rte-france.com)
+ * See AUTHORS.txt
+ * SPDX-License-Identifier: MPL-2.0
+ * This file is part of Antares-Simulator,
+ * Adequacy and Performance assessment for interconnected energy networks.
+ *
+ * Antares_Simulator is free software: you can redistribute it and/or modify
+ * it under the terms of the Mozilla Public Licence 2.0 as published by
+ * the Mozilla Foundation, either version 2 of the License, or
+ * (at your option) any later version.
+ *
+ * Antares_Simulator is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+ * Mozilla Public Licence 2.0 for more details.
+ *
+ * You should have received a copy of the Mozilla Public Licence 2.0
+ * along with Antares_Simulator. If not, see <https://opensource.org/license/mpl-2-0/>.
+ */
+
+/*
+** This file is part of libyuni, a cross-platform C++ framework (http://libyuni.org).
+**
+** This Source Code Form is subject to the terms of the Mozilla Public License
+** v.2.0. If a copy of the MPL was not distributed with this file, You can
+** obtain one at http://mozilla.org/MPL/2.0/.
+**
+** github: https://github.com/libyuni/libyuni/
+** gitlab: https://gitlab.com/libyuni/libyuni/ (mirror)
+*/
+#pragma once
+#include "../../yuni.h"
+
+namespace Yuni
+{
+namespace System
+{
+namespace CPU
+{
+/*!
+** \brief Get the number of system CPU
+**
+** \return The number of CPU (logic or not), 1 when the value is unknown
+*/
+YUNI_DECL uint Count();
+
+} // namespace CPU
+} // namespace System
+} // namespace Yuni
