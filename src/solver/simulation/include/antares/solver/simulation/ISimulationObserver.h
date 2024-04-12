f8ffd0da322bf5817d7919c26f21b6d906d772b4
Jason Marechal <jason.marechal+github@gmail.com>
2024-04-12 15:02:17 +0200

    Document ISimulationObserver

--- src/solver/simulation/include/antares/solver/simulation/ISimulationObserver.h
+++ src/solver/simulation/include/antares/solver/simulation/ISimulationObserver.h
@@ -26,22 +26,40 @@
 
 namespace Antares::Solver::Simulation
 {
+
+/**
+ * @class ISimulationObserver
+ * @brief The ISimulationObserver class is an interface for observing the simulation.
+ * @details It declares the notifyHebdoProblem method.
+ */
 class ISimulationObserver
 {
 public:
     virtual ~ISimulationObserver() = default;
+    /**
+     * @brief The notifyHebdoProblem method is used to notify of a problem during the simulation.
+     * @param problemeHebdo A pointer to a PROBLEME_HEBDO object representing the problem.
+     * @param optimizationNumber The number of the optimization.
+     * @param name The name of the problem.
+     */
     virtual void notifyHebdoProblem(const PROBLEME_HEBDO* problemeHebdo,
                                     int optimizationNumber,
                                     std::string_view name)
       = 0;
 };
 
+/**
+ * @class NullSimulationObserver
+ * @brief The NullSimulationObserver class is a null object for the ISimulationObserver interface.
+ * @details It overrides the notifyHebdoProblem method with an empty implementation.
+ */
 class NullSimulationObserver : public ISimulationObserver
 {
 public:
     ~NullSimulationObserver() override = default;
     void notifyHebdoProblem(const PROBLEME_HEBDO*, int, std::string_view) override
     {
+        //null object pattern
     }
 };
 } // namespace Antares::Solver::Simulation
