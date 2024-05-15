0adcc22a7cd7ce92d4f9c3f16855856646c93249
Guillaume PIERRE <guillaume.pierre@rte-france.com>
2024-05-15 09:39:29 +0200

    Remove yuni from inifile : adding more tests on reading a stream to populate a Inifile object

--- src/tests/src/libs/antares/inifile/test_inifile_io.cpp
+++ src/tests/src/libs/antares/inifile/test_inifile_io.cpp
@@ -16,7 +16,7 @@ struct Fixture
 };
 
 
-BOOST_AUTO_TEST_SUITE(building_inicontent_from_stream)
+BOOST_AUTO_TEST_SUITE(populating_inicontent_from_stream)
 
 BOOST_FIXTURE_TEST_CASE(one_section_with_one_property, Fixture)
 {
@@ -31,20 +31,77 @@ BOOST_FIXTURE_TEST_CASE(one_section_with_one_property, Fixture)
 	BOOST_CHECK(section->find("some key"));
 }
 
+BOOST_FIXTURE_TEST_CASE(nothing_in_the_stream___no_section_created, Fixture)
+{
+	std::istringstream input_stream(ini_content);
+
+	BOOST_CHECK(my_inifile.readStream(input_stream));
+	BOOST_CHECK(my_inifile.empty());
+}
+
 BOOST_FIXTURE_TEST_CASE(properties_without_parent_section___ini_content_remains_empty, Fixture)
 {
 	ini_content += "key-1 = value-1\n";
 	ini_content += "key-2 = value-2";
 	std::istringstream input_stream(ini_content);
 
-	BOOST_CHECK(my_inifile.readStream(input_stream));	// Reading the stream causes no trouble
+	BOOST_CHECK(my_inifile.readStream(input_stream));
 	BOOST_CHECK(my_inifile.empty());
+}
+
+BOOST_FIXTURE_TEST_CASE(a_section_has_no_property___the_section_is_created_and_remains_empty, Fixture)
+{
+	ini_content += "[some section]";
+	std::istringstream input_stream(ini_content);
+
+	BOOST_CHECK(my_inifile.readStream(input_stream));
+	section = my_inifile.find("some section");
+	BOOST_CHECK(section->empty());
 }
 
 BOOST_FIXTURE_TEST_CASE(a_property_has_2_equal_signs___reading_stream_fails, Fixture)
 {
 	ini_content += "[some section]\n";
 	ini_content += "key-1 = value-1 key-2 = value-2";
+	std::istringstream input_stream(ini_content);
+
+	BOOST_CHECK(!my_inifile.readStream(input_stream));
+}
+
+
+BOOST_FIXTURE_TEST_CASE(two_properties_commented___they_are_not_created, Fixture)
+{
+	ini_content += "[my section]\n";
+	ini_content += "key-1 = value-1\n";
+	ini_content += "# key-2 = value-2";
+	ini_content += "; key-3 = value-3";
+	std::istringstream input_stream(ini_content);
+
+	BOOST_CHECK(my_inifile.readStream(input_stream));
+	section = my_inifile.find("my section");
+	BOOST_CHECK(section->find("key-1"));
+	BOOST_CHECK(section->size() == 1);
+}
+
+BOOST_FIXTURE_TEST_CASE(blank_line_does_not_matter, Fixture)
+{
+	ini_content += "[my section]\n";
+	ini_content += "	\n";
+	ini_content += "key-1 = value-1\n";
+	ini_content += "key-2 = value-2";
+	std::istringstream input_stream(ini_content);
+
+	BOOST_CHECK(my_inifile.readStream(input_stream));
+	section = my_inifile.find("my section");
+	BOOST_CHECK(section->size() == 2);
+	BOOST_CHECK(section->find("key-1") && section->find("key-2"));
+}
+
+BOOST_FIXTURE_TEST_CASE(we_have_a_line_with_unknown_format___reading_stream_fails, Fixture)
+{
+	ini_content += "[my section]\n";
+	ini_content += "Some strange line\n";
+	ini_content += "key-1 = value-1\n";
 	std::istringstream input_stream(ini_content);
 
 	BOOST_CHECK(!my_inifile.readStream(input_stream));
