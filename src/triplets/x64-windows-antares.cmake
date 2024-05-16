e45c9f75be3d6a6fe53cc169c08e8fd89061fb2f
Sylvain Leclerc <sylvain.leclerc@rte-france.com>
2024-05-16 13:44:20 +0200

    Try full static dependencies on windows
    Signed-off-by: Sylvain Leclerc <sylvain.leclerc@rte-france.com>

--- src/triplets/x64-windows-antares.cmake
+++ src/triplets/x64-windows-antares.cmake
@@ -1,11 +1,3 @@
 set(VCPKG_TARGET_ARCHITECTURE x64)
 set(VCPKG_CRT_LINKAGE dynamic)
-set(VCPKG_LIBRARY_LINKAGE dynamic)
-
-# Link zlib and minizip statically
-list(APPEND STATIC_PORTS "zlib.*" "minizip.*")
-foreach (STATIC_PORT IN LISTS STATIC_PORTS)
-    if(PORT MATCHES ${STATIC_PORT})
-        set(VCPKG_LIBRARY_LINKAGE static)
-    endif()
-endforeach ()
+set(VCPKG_LIBRARY_LINKAGE static)
--- src/triplets/x64-windows-release.cmake
+++ src/triplets/x64-windows-release.cmake
@@ -1,15 +1,11 @@
 set(VCPKG_TARGET_ARCHITECTURE x64)
 
 set(VCPKG_CRT_LINKAGE dynamic)
-set(VCPKG_LIBRARY_LINKAGE dynamic)
+set(VCPKG_LIBRARY_LINKAGE static)
 
 # Avoid building debug artifacts
 set(VCPKG_BUILD_TYPE release)
 
-# Link zlib and minizip statically
-list(APPEND STATIC_PORTS "zlib.*" "minizip.*")
-foreach (STATIC_PORT IN LISTS STATIC_PORTS)
-    if(PORT MATCHES ${STATIC_PORT})
-        set(VCPKG_LIBRARY_LINKAGE static)
-    endif()
-endforeach ()
+if (PORT MATCHES "boost-test")
+    set(VCPKG_LIBRARY_LINKAGE dynamic)
+endif ()
