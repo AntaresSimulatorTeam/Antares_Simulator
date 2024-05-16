e526cba250b7ec24eb3214ac4a51c7495196da29
Sylvain Leclerc <sylvain.leclerc@rte-france.com>
2024-05-16 11:15:09 +0200

    Fix build type in triplets
    Signed-off-by: Sylvain Leclerc <sylvain.leclerc@rte-france.com>

--- src/triplets/x64-linux-release.cmake
+++ src/triplets/x64-linux-release.cmake
@@ -4,4 +4,4 @@ set(VCPKG_CRT_LINKAGE dynamic)
 set(VCPKG_LIBRARY_LINKAGE static)
 
 # Avoid building debug artifacts
-set(VCPKG_BUILD_TYPE Release)
+set(VCPKG_BUILD_TYPE release)
--- src/triplets/x64-windows-release.cmake
+++ src/triplets/x64-windows-release.cmake
@@ -5,7 +5,7 @@ set(VCPKG_CRT_LINKAGE dynamic)
 set(VCPKG_LIBRARY_LINKAGE dynamic)
 
 # Avoid building debug artifacts
-set(VCPKG_BUILD_TYPE Release)
+set(VCPKG_BUILD_TYPE release)
 
 # Link zlib and minizip statically
 list(APPEND STATIC_PORTS "zlib.*" "minizip.*")
