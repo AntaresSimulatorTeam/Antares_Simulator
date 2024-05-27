9665f8c85ca8f77b772150a830a2c04533a8f9c3
Abdoulbari Zakir <abdoulbari.zakir@gmail.com>
2024-05-27 14:27:54 +0200

    fix ?

--- src/solver/variable/include/antares/solver/variable/bindConstraints.hxx
+++ src/solver/variable/include/antares/solver/variable/bindConstraints.hxx
@@ -120,6 +120,8 @@ void BindingConstraints<NextT>::simulationEnd()
 template<class NextT>
 void BindingConstraints<NextT>::yearEndBuild(State& state, uint year, uint numSpace)
 {
+    // Next variable
+    NextType::yearEndBuild(state, year, numSpace);
 }
 
 template<class NextT>
