a9fe4d2d0655f5a7a4182e1557bacd74e17d9e3a
Jason Marechal <jason.marechal+github@gmail.com>
2024-05-29 16:03:17 +0200

    Fix test linking

--- src/tests/src/solver/infeasible-problem-analysis/test-unfeasible-problem-analyzer.cpp
+++ src/tests/src/solver/infeasible-problem-analysis/test-unfeasible-problem-analyzer.cpp
@@ -20,7 +20,6 @@
  */
 #define WIN32_LEAN_AND_MEAN
 #define BOOST_TEST_MODULE unfeasible_problem_analyzer
-#define BOOST_TEST_DYN_LINK
 
 #include <boost/test/data/dataset.hpp>
 #include <boost/test/data/test_case.hpp>
