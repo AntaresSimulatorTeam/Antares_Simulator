2dfab715f79c2d5a14bc49f40aec6f304fa10cac
Florian OMNES <florian.omnes@rte-france.com>
2024-06-10 09:42:06 +0200

    Add /D_DISABLE_CONSTEXPR_MUTEX_CONSTRUCTOR

--- src/cmake/common-settings.cmake
+++ src/cmake/common-settings.cmake
@@ -11,7 +11,7 @@ if (NOT WIN32)
 	set(COMMON_GCC_FLAGS "${COMMON_GCC_FLAGS} -Werror=return-type")
 endif()
 set(COMMON_MSVC_FLAGS "/W3 /MP4")
-set(COMMON_MSVC_FLAGS "${COMMON_MSVC_FLAGS} /we4715 /we4716") #adding no return or no return for all code paths as errors
+set(COMMON_MSVC_FLAGS "${COMMON_MSVC_FLAGS} /we4715 /we4716 /D_DISABLE_CONSTEXPR_MUTEX_CONSTRUCTOR") #adding no return or no return for all code paths as errors
 set(ADDITIONAL_C_FLAGS " -Wconversion -Wmissing-prototypes -Wstrict-prototypes")
 set(ADDITIONAL_C_FLAGS "${ADDITIONAL_C_FLAGS} -Wmissing-noreturn -Wpacked -Wredundant-decls -Wbad-function-cast -W -Wcast-align -Wcast-qual -Wsign-compare -fno-exceptions -Wdeclaration-after-statement")
 
