03a51ef544de1498e48c5809254a6566b82c0195
Vincent Payet <vincent.payet@rte-france.com>
2024-06-27 13:58:49 +0200

    try nb weeks = 52

--- src/libs/antares/include/antares/antares/constants.h
+++ src/libs/antares/include/antares/antares/constants.h
@@ -53,7 +53,7 @@
 const unsigned int HOURS_PER_DAY = 24;
 
 const unsigned int MONTHS_PER_YEAR = 12;
-const unsigned int WEEKS_PER_YEAR = 53;
+const unsigned int WEEKS_PER_YEAR = 52;
 const unsigned int DAYS_PER_YEAR = 365;
 const unsigned int HOURS_PER_YEAR = 8760;
 
