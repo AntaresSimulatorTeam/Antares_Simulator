898788f3d60194b7e8d9ab193d46a3df453956c4
Florian OMNES <florian.omnes@rte-france.com>
2024-07-01 10:44:33 +0200

    Fix Yuni headers

--- src/libs/antares/study/include/antares/study/sets.hxx
+++ src/libs/antares/study/include/antares/study/sets.hxx
@@ -176,8 +176,8 @@ bool Sets<T>::saveToFile(const StringT& filename) const
     using namespace Yuni;
     using namespace Antares;
 
-    IO::File::Stream file;
-    if (!file.open(filename, IO::OpenMode::write | IO::OpenMode::truncate))
+    Yuni::IO::File::Stream file;
+    if (!file.open(filename, Yuni::IO::OpenMode::write | Yuni::IO::OpenMode::truncate))
     {
         logs.error() << "I/O Error: " << filename << ": impossible to write the file";
         return false;
