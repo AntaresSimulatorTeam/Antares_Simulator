07420475b526ada849ebc1f33ea20071460daa89
guilpier-code <62292552+guilpier-code@users.noreply.github.com>
2024-07-08 10:30:44 +0200

    Infeasibility analyzer : HydroPower constraint (#2227) [ANT-1825]
    Add constraint that can possibily lead to infeasibilities. HydroPower is the constraint on the sum of generated hydro energy over the week for a given area. The provided RHS in some cases makes the problem infeasible.
    
    ---------
    
    Co-authored-by: Florian OMNES <26088210+flomnes@users.noreply.github.com>

--- src/solver/infeasible-problem-analysis/constraint.cpp
+++ src/solver/infeasible-problem-analysis/constraint.cpp
@@ -26,6 +26,7 @@
 #include <sstream>
 
 #include <boost/algorithm/string/regex.hpp>
+#include <boost/regex.hpp>
 
 namespace
 {
@@ -143,6 +144,10 @@ ConstraintType Constraint::getType() const
     {
         return ConstraintType::hydro_reservoir_level;
     }
+    if (nameComponents_.at(0) == "HydroPower")
+    {
+        return ConstraintType::hydro_production_weekly;
+    }
     if (nameComponents_.at(0) == "Level")
     {
         return ConstraintType::short_term_storage_level;
@@ -194,6 +199,8 @@ std::string Constraint::prettyPrint() const
     case ConstraintType::hydro_reservoir_level:
         return "Hydro reservoir constraint at area '" + getAreaName() + "' at hour "
                + getTimeStepInYear();
+    case ConstraintType::hydro_production_weekly:
+        return "Hydro weekly production at area '" + getAreaName() + "'";
     case ConstraintType::short_term_storage_level:
         return "Short-term-storage reservoir constraint at area '" + getAreaName() + "' in STS '"
                + getSTSName() + "' at hour " + getTimeStepInYear();
