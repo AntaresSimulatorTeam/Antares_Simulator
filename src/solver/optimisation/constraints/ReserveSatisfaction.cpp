a6d0e6ca18cff2f068eb704bcb33d68fd41efce2
h-fournier <hugo.fournier@scalian.com>
2024-07-18 17:25:51 +0200

    Complete the equations 24 by adding the participation of the short term storage to the reserve satisfaction. Does not seem to work, debugging in progress

--- src/solver/optimisation/constraints/ReserveSatisfaction.cpp
+++ src/solver/optimisation/constraints/ReserveSatisfaction.cpp
@@ -2,58 +2,103 @@
 
 void ReserveSatisfaction::add(int pays, int reserve, int pdt, bool isUpReserve)
 {
-    CAPACITY_RESERVATION capacityReservation
+    CAPACITY_RESERVATION thermalCapacityReservation
       = isUpReserve
           ? data.areaReserves.thermalAreaReserves[pays].areaCapacityReservationsUp[reserve]
           : data.areaReserves.thermalAreaReserves[pays].areaCapacityReservationsDown[reserve];
 
-    data.CorrespondanceCntNativesCntOptim[pdt]
-      .NumeroDeContrainteDesContraintesDeBesoinEnReserves[capacityReservation.globalReserveIndex]
+    CAPACITY_RESERVATION STStorageCapacityReservation
+      = isUpReserve
+          ? data.areaReserves.shortTermStorageAreaReserves[pays].areaCapacityReservationsUp[reserve]
+          : data.areaReserves.shortTermStorageAreaReserves[pays]
+              .areaCapacityReservationsDown[reserve];
+
+    data.CorrespondanceCntNativesCntOptim[pdt].NumeroDeContrainteDesContraintesDeBesoinEnReserves
+      [thermalCapacityReservation.globalReserveIndex]
       = -1;
     if (!data.Simulation)
     {
         // 24
 
-        // Sum(P_θ) = S + J^+ -J^-
-        // P_θ : Participation power from cluster θ to the reserve res
+        // Sum(P_θ) + Sum(P_B) = S + J^+ -J^-
+        // P_θ : Participation power from thermal cluster θ to the reserve res
+        // P_B : Participation power from Short Term Storage cluster B to the reserve res
         // S : Internal reserve res need for the area (second membre)
         // J^+ : Amount of internal excess reserve for the reserve res
         // J^- : Amount of internal unsatisfied reserve for the reserve res
 
         builder.updateHourWithinWeek(pdt);
 
-        for (size_t cluster = 0; cluster < capacityReservation.AllReservesParticipation.size();
+        // Thermal clusters reserve participation
+        for (size_t cluster = 0;
+             cluster < thermalCapacityReservation.AllReservesParticipation.size();
              cluster++)
         {
-            if (capacityReservation.AllReservesParticipation[cluster].maxPower != CLUSTER_NOT_PARTICIPATING)
+            if (thermalCapacityReservation.AllReservesParticipation[cluster].maxPower
+                != CLUSTER_NOT_PARTICIPATING)
                 builder.RunningThermalClusterReserveParticipation(
-                  capacityReservation.AllReservesParticipation[cluster]
+                  thermalCapacityReservation.AllReservesParticipation[cluster]
                     .globalIndexClusterParticipation,
                   1);
         }
 
-        builder.InternalUnsatisfiedReserve(capacityReservation.globalReserveIndex, 1)
-          .InternalExcessReserve(capacityReservation.globalReserveIndex, -1)
+        // Short Term Storage clusters reserve participation
+        for (size_t cluster = 0;
+             cluster < STStorageCapacityReservation.AllReservesParticipation.size();
+             cluster++)
+        {
+            if ((STStorageCapacityReservation.AllReservesParticipation[cluster].maxTurbining
+                 != CLUSTER_NOT_PARTICIPATING)
+                || (STStorageCapacityReservation.AllReservesParticipation[cluster].maxPumping
+                    != CLUSTER_NOT_PARTICIPATING))
+            {
+                if (isUpReserve)
+                    builder.STStorageClusterReserveUpParticipation(
+                      STStorageCapacityReservation.AllReservesParticipation[cluster]
+                        .globalIndexClusterParticipation,
+                      1);
+                else
+                    builder.STStorageClusterReserveDownParticipation(
+                      STStorageCapacityReservation.AllReservesParticipation[cluster]
+                        .globalIndexClusterParticipation,
+                      1);
+            }
+        }
+
+        builder.InternalUnsatisfiedReserve(thermalCapacityReservation.globalReserveIndex, 1)
+          .InternalExcessReserve(thermalCapacityReservation.globalReserveIndex, -1)
           .equalTo();
         data.CorrespondanceCntNativesCntOptim[pdt]
-          .NumeroDeContrainteDesContraintesDeBesoinEnReserves[capacityReservation.globalReserveIndex]
+          .NumeroDeContrainteDesContraintesDeBesoinEnReserves[thermalCapacityReservation
+                                                                .globalReserveIndex]
           = builder.data.nombreDeContraintes;
         ConstraintNamer namer(builder.data.NomDesContraintes);
         const int hourInTheYear = builder.data.weekInTheYear * 168 + pdt;
         namer.UpdateTimeStep(hourInTheYear);
         namer.UpdateArea(builder.data.NomsDesPays[pays]);
         namer.ReserveSatisfaction(builder.data.nombreDeContraintes,
-                                  capacityReservation.reserveName);
+                                  thermalCapacityReservation.reserveName);
         builder.build();
     }
     else
     {
         int nbTermes = 0;
-        for (size_t cluster = 0; cluster < capacityReservation.AllReservesParticipation.size();
+        for (size_t cluster = 0;
+             cluster < thermalCapacityReservation.AllReservesParticipation.size();
              cluster++)
         {
-            if (capacityReservation.AllReservesParticipation[cluster].maxPower
+            if (thermalCapacityReservation.AllReservesParticipation[cluster].maxPower
                 != CLUSTER_NOT_PARTICIPATING)
+                nbTermes++;
+        }
+        for (size_t cluster = 0;
+             cluster < STStorageCapacityReservation.AllReservesParticipation.size();
+             cluster++)
+        {
+            if ((STStorageCapacityReservation.AllReservesParticipation[cluster].maxTurbining
+                 != CLUSTER_NOT_PARTICIPATING)
+                || (STStorageCapacityReservation.AllReservesParticipation[cluster].maxPumping
+                    != CLUSTER_NOT_PARTICIPATING))
                 nbTermes++;
         }
 
