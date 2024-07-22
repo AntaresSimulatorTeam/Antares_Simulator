98169b66ffa458fc5e8b628a7a050ddfafdbe1a3
Jason Marechal <jason.marechal+github@gmail.com>
2024-07-22 12:00:42 +0200

    Use uniqie_ptr

--- src/libs/antares/study/study.extra.cpp
+++ src/libs/antares/study/study.extra.cpp
@@ -1,23 +1,23 @@
 /*
-** Copyright 2007-2024, RTE (https://www.rte-france.com)
-** See AUTHORS.txt
-** SPDX-License-Identifier: MPL-2.0
-** This file is part of Antares-Simulator,
-** Adequacy and Performance assessment for interconnected energy networks.
-**
-** Antares_Simulator is free software: you can redistribute it and/or modify
-** it under the terms of the Mozilla Public Licence 2.0 as published by
-** the Mozilla Foundation, either version 2 of the License, or
-** (at your option) any later version.
-**
-** Antares_Simulator is distributed in the hope that it will be useful,
-** but WITHOUT ANY WARRANTY; without even the implied warranty of
-** MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
-** Mozilla Public Licence 2.0 for more details.
-**
-** You should have received a copy of the Mozilla Public Licence 2.0
-** along with Antares_Simulator. If not, see <https://opensource.org/license/mpl-2-0/>.
-*/
+ * Copyright 2007-2024, RTE (https://www.rte-france.com)
+ * See AUTHORS.txt
+ * SPDX-License-Identifier: MPL-2.0
+ * This file is part of Antares-Simulator,
+ * Adequacy and Performance assessment for interconnected energy networks.
+ *
+ * Antares_Simulator is free software: you can redistribute it and/or modify
+ * it under the terms of the Mozilla Public Licence 2.0 as published by
+ * the Mozilla Foundation, either version 2 of the License, or
+ * (at your option) any later version.
+ *
+ * Antares_Simulator is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+ * Mozilla Public Licence 2.0 for more details.
+ *
+ * You should have received a copy of the Mozilla Public Licence 2.0
+ * along with Antares_Simulator. If not, see <https://opensource.org/license/mpl-2-0/>.
+ */
 
 #include "antares/study/study.h"
 
@@ -38,7 +38,7 @@ void Study::scenarioRulesCreate()
     scenarioRules.reset();
 
     // When ran from the solver, the scenario builder must be present
-    scenarioRules = std::make_shared<ScenarioBuilder::Sets>();
+    scenarioRules = std::make_unique<ScenarioBuilder::Sets>();
     scenarioRules->loadFromStudy(*this);
 }
 
@@ -52,7 +52,7 @@ void Study::scenarioRulesLoadIfNotAvailable()
     if (!scenarioRules)
     {
         // When ran from the solver, the scenario builder must be present
-        scenarioRules = std::make_shared<ScenarioBuilder::Sets>();
+        scenarioRules = std::make_unique<ScenarioBuilder::Sets>();
         scenarioRules->loadFromStudy(*this);
     }
 }
