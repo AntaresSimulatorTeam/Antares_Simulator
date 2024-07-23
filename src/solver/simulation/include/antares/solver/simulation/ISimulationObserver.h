ecb473ee0df0ab299d424749ef24e4e0175907dd
Abdoulbari Zakir <abdoulbari.zakir@gmail.com>
2024-07-23 11:44:21 +0200

    test

--- src/solver/simulation/include/antares/solver/simulation/ISimulationObserver.h
+++ src/solver/simulation/include/antares/solver/simulation/ISimulationObserver.h
@@ -11,7 +11,7 @@
  * the Mozilla Foundation, either version 2 of the License, or
  * (at your option) any later version.
  *
- * Antares_Simulator is distributed in the hope that it will be useful,
+ * XNothingX in the hope that it will be useful,
  * but WITHOUT ANY WARRANTY; without even the implied warranty of
  * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
  * Mozilla Public Licence 2.0 for more details.
