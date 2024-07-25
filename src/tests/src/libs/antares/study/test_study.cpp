c3878f0b8a5b7382e5d4a5e5cbb5a0f4c0d922ae
Vincent Payet <vincent.payet@rte-france.com>
2024-07-25 09:46:04 +0200

    use add link

--- src/tests/src/libs/antares/study/test_study.cpp
+++ src/tests/src/libs/antares/study/test_study.cpp
@@ -301,13 +301,14 @@ BOOST_FIXTURE_TEST_CASE(check_filename_limit, OneAreaStudy)
     BOOST_CHECK(study->checkForFilenameLimits(false));
     BOOST_CHECK(study->checkForFilenameLimits(true, "abc"));
 
-#ifdef YUNI_OS_WINDOWS
+//#ifdef YUNI_OS_WINDOWS
     std::string area1name(128, 'a');
     std::string area2name(128, 'b');
     study->areas[0]->name = area1name;
-    study->areaAdd(area2name);
-    BOOST_CHECK(!study->checkForFilenameLimits(true)); // areaname limits size to 128
-#endif
+    auto areaB = study->areaAdd(area2name);
+    AreaAddLinkBetweenAreas(areaA, areaB);
+    BOOST_CHECK(!study->checkForFilenameLimits(true));
+//#endif
 }
 
 BOOST_AUTO_TEST_SUITE_END() // version
