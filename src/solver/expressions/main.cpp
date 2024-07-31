0c26c870174b024a0105e815c72a7323ce152a69
Florian OMNES <florian.omnes@rte-france.com>
2024-07-31 13:19:43 +0200

    Add `visit` helper

--- src/solver/expressions/main.cpp
+++ src/solver/expressions/main.cpp
@@ -4,12 +4,19 @@
 #include <antares/solver/expressions/Add.h>
 #include <antares/solver/expressions/CloneVisitor.h>
 #include <antares/solver/expressions/Negate.h>
+#include <antares/solver/expressions/Node.h>
 #include <antares/solver/expressions/Parameter.h>
 #include <antares/solver/expressions/PrintVisitor.h>
 #include <antares/solver/expressions/StringVisitor.h>
 
-int main()
+template<class V>
+std::any visit(Antares::Solver::Expressions::Node& node)
+{
+    V visitor;
+    return node.accept(visitor);
+}
 
+int main()
 {
     using namespace Antares::Solver::Expressions;
     auto* p1 = new Parameter("hello");
@@ -17,21 +24,13 @@ int main()
     auto* neg = new Negate(p2);
     Add root(p1, neg);
 
-    {
-        PrintVisitor printVisitor;
-        root.accept(printVisitor);
-    }
+    visit<PrintVisitor>(root);
+
     std::cout << std::endl;
-    {
-        StringVisitor stringVisitor;
-        std::cout << std::any_cast<std::string>(root.accept(stringVisitor));
-    }
+    std::cout << std::any_cast<std::string>(visit<StringVisitor>(root));
     std::cout << std::endl;
-    {
-        CloneVisitor cloneVisitor;
-        std::any clone = root.accept(cloneVisitor);
-        PrintVisitor printVisitor;
-        std::unique_ptr<Node> root2(std::any_cast<Node*>(clone));
-        root2->accept(printVisitor);
-    }
+
+    std::any clone = visit<CloneVisitor>(root);
+    std::unique_ptr<Node> root2(std::any_cast<Node*>(clone));
+    visit<PrintVisitor>(*root2);
 }
