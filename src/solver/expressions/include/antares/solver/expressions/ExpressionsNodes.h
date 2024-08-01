ed02673632dfb2e168acce962400941ad0bee7d6
Abdoulbari Zakir <abdoulbari.zakir@gmail.com>
2024-08-01 14:58:06 +0200

    fix

--- src/solver/expressions/include/antares/solver/expressions/ExpressionsNodes.h
+++ src/solver/expressions/include/antares/solver/expressions/ExpressionsNodes.h
@@ -22,4 +22,3 @@
 #include <antares/solver/expressions/Add.h>
 #include <antares/solver/expressions/Leaf.h>
 #include <antares/solver/expressions/Negate.h>
-#include <antares/solver/expressions/Parameter.h>
