b50899f6b5860e5b2d0ffc1b78a30f90338e5aa3
Abdoulbari Zakir <abdoulbari.zakir@gmail.com>
2024-08-07 15:32:02 +0200

    add clone & print visitor test

--- src/tests/src/solver/expressions/test_expressions.cpp
+++ src/tests/src/solver/expressions/test_expressions.cpp
@@ -106,6 +106,18 @@ BOOST_FIXTURE_TEST_CASE(eval_Add_And_Negation_Nodes, Registry<Node>)
     BOOST_CHECK_EQUAL(eval, num1 - num2);
 }
 
+BOOST_FIXTURE_TEST_CASE(Negative_of_AddNode, Registry<Node>)
+{
+    int num1 = 1428.0;
+    int num2 = 8241.0;
+    Node* add_node = create<AddNode>(create<LiteralNode>(num1), create<LiteralNode>(num2));
+    Node* neg = create<NegationNode>(add_node);
+    EvalVisitor evalVisitor;
+    double eval = evalVisitor.dispatch(*neg);
+
+    BOOST_CHECK_EQUAL(eval, -(num1 + num2));
+}
+
 BOOST_FIXTURE_TEST_CASE(print_port_field_node, Registry<Node>)
 {
     PortFieldNode pt_fd("august", "2024");
@@ -113,4 +125,30 @@ BOOST_FIXTURE_TEST_CASE(print_port_field_node, Registry<Node>)
     const auto printed = printVisitor.dispatch(pt_fd);
 
     BOOST_CHECK_EQUAL(printed, "august.2024");
+}
+
+BOOST_FIXTURE_TEST_CASE(cloneVisitor_With_Add_Neg_ComponentVariableNode, Registry<Node>)
+{
+    std::string cpvar_name("var"), cpvar_id("id1");
+    std::string cp_para_name("par"), cp_para_id("id2");
+    ComponentVariableNode cpv(cpvar_id, cpvar_name);
+    ComponentParameterNode cpp(cp_para_id, cp_para_name);
+    int num1 = 22.0, num2 = 8;
+    // num1+num2
+    Node* edge = create<AddNode>(create<LiteralNode>(num1), create<LiteralNode>(num2));
+    // -(num1+num2)
+    Node* negative_edge = create<NegationNode>(edge);
+    // -(num1+num2)+var.id1
+    Node* add_node = create<AddNode>(negative_edge, &cpv);
+    // -(-(num1+num2)+var.id1)+id2.par ==
+    // -(-(22.000000+8.000000)+id1.var)+id2.par
+    Node* root = create<AddNode>(create<NegationNode>(add_node), &cpp);
+
+    PrintVisitor printVisitor;
+    const auto printed = printVisitor.dispatch(*root);
+
+    BOOST_CHECK_EQUAL(printed, "-(-(22.000000+8.000000)+id1.var)+id2.par");
+    CloneVisitor cloneVisitor(*this);
+    Node* cloned = cloneVisitor.dispatch(*root);
+    BOOST_CHECK_EQUAL(printed, printVisitor.dispatch(*cloned));
 }
