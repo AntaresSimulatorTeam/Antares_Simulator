07fba7ead50d5a79a50df8e7752654a146d48beb
Jason Marechal <jason.marechal+github@gmail.com>
2024-08-08 16:52:43 +0200

    Install sirius lib for clients

--- src/cmake/utils.cmake
+++ src/cmake/utils.cmake
@@ -1,33 +1,34 @@
 macro(copy_dependency deps target)
 
-        if("${CMAKE_BUILD_TYPE}" STREQUAL "Release")
-            get_target_property( DEP_SHARED_LIB_PATH ${deps} IMPORTED_LOCATION_RELEASE )
-        else()
-            get_target_property( DEP_SHARED_LIB_PATH ${deps} IMPORTED_LOCATION_DEBUG )
-        endif()
+    if ("${CMAKE_BUILD_TYPE}" STREQUAL "Release")
+        get_target_property(DEP_SHARED_LIB_PATH ${deps} IMPORTED_LOCATION_RELEASE)
+    else ()
+        get_target_property(DEP_SHARED_LIB_PATH ${deps} IMPORTED_LOCATION_DEBUG)
+    endif ()
 
-	if (NOT "${DEP_SHARED_LIB_PATH}" STREQUAL "DEP_SHARED_LIB_PATH-NOTFOUND")
+    if (NOT "${DEP_SHARED_LIB_PATH}" STREQUAL "DEP_SHARED_LIB_PATH-NOTFOUND")
 
-            # Copy the shared lib file
-            add_custom_command(TARGET ${target} POST_BUILD COMMAND ${CMAKE_COMMAND} -E copy ${DEP_SHARED_LIB_PATH} $<TARGET_FILE_DIR:${target}>)
-            
-            # Add to install
-            install(FILES ${DEP_SHARED_LIB_PATH} TYPE BIN)
+        # Copy the shared lib file
+        add_custom_command(TARGET ${target} POST_BUILD COMMAND ${CMAKE_COMMAND} -E copy ${DEP_SHARED_LIB_PATH} $<TARGET_FILE_DIR:${target}>)
 
-	endif()
+        # Add to install
+        install(FILES ${DEP_SHARED_LIB_PATH} TYPE BIN)
+        install(FILES ${DEP_SHARED_LIB_PATH} TYPE LIB)
+
+    endif ()
 
 endmacro()
 
 function(get_linux_lsb_release_information)
     find_program(LSB_RELEASE_EXEC lsb_release)
-    if(NOT LSB_RELEASE_EXEC)
-    
+    if (NOT LSB_RELEASE_EXEC)
+
         message("Could not detect lsb_release executable, can not gather required information. Use of default information (ID : Linux / Version : Unknown / Codename : Unknown")
-        
+
         set(LSB_RELEASE_ID_SHORT "Linux" PARENT_SCOPE)
         set(LSB_RELEASE_VERSION_SHORT "Unknown" PARENT_SCOPE)
-        set(LSB_RELEASE_CODENAME_SHORT "Unknown" PARENT_SCOPE)        
-    else()
+        set(LSB_RELEASE_CODENAME_SHORT "Unknown" PARENT_SCOPE)
+    else ()
         execute_process(COMMAND "${LSB_RELEASE_EXEC}" --short --id OUTPUT_VARIABLE LSB_RELEASE_ID_SHORT OUTPUT_STRIP_TRAILING_WHITESPACE)
         execute_process(COMMAND "${LSB_RELEASE_EXEC}" --short --release OUTPUT_VARIABLE LSB_RELEASE_VERSION_SHORT OUTPUT_STRIP_TRAILING_WHITESPACE)
         execute_process(COMMAND "${LSB_RELEASE_EXEC}" --short --codename OUTPUT_VARIABLE LSB_RELEASE_CODENAME_SHORT OUTPUT_STRIP_TRAILING_WHITESPACE)
@@ -35,8 +36,8 @@ function(get_linux_lsb_release_information)
         set(LSB_RELEASE_ID_SHORT "${LSB_RELEASE_ID_SHORT}" PARENT_SCOPE)
         set(LSB_RELEASE_VERSION_SHORT "${LSB_RELEASE_VERSION_SHORT}" PARENT_SCOPE)
         set(LSB_RELEASE_CODENAME_SHORT "${LSB_RELEASE_CODENAME_SHORT}" PARENT_SCOPE)
-    endif()
-    
+    endif ()
+
 endfunction()
 
 
@@ -44,18 +45,18 @@ function(find_python_module module)
     set(Python3_FIND_REGISTRY "LAST")
     find_package(Python3 COMPONENTS Interpreter)
 
-    if(Python3_Interpreter_FOUND)
+    if (Python3_Interpreter_FOUND)
         execute_process(
-            COMMAND ${Python3_EXECUTABLE} -c "import ${module}"
-            RESULT_VARIABLE EXIT_CODE
-            OUTPUT_QUIET
-            ERROR_QUIET
+                COMMAND ${Python3_EXECUTABLE} -c "import ${module}"
+                RESULT_VARIABLE EXIT_CODE
+                OUTPUT_QUIET
+                ERROR_QUIET
         )
-        
+
         if (${EXIT_CODE} EQUAL 0)
             set(PYTHON_MODULE_${module}_FOUND "true" PARENT_SCOPE)
-        endif()
-        
-    endif()
-    
+        endif ()
+
+    endif ()
+
 endfunction()
