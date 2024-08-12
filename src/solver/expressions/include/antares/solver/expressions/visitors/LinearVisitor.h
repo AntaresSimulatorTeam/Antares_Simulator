cbf6162d83d5f5ef5bdceaf2bbf2553dee03f934
Abdoulbari Zakir <abdoulbari.zakir@gmail.com>
2024-08-12 17:40:30 +0200

    add LinearVisitor

---
+++ src/solver/expressions/include/antares/solver/expressions/visitors/LinearVisitor.h
@@ -0,0 +1,48 @@
+/*
+** Copyright 2007-2024, RTE (https://www.rte-france.com)
+** See AUTHORS.txt
+** SPDX-License-Identifier: MPL-2.0
+** This file is part of Antares-Simulator,
+** Adequacy and Performance assessment for interconnected energy networks.
+**
+** Antares_Simulator is free software: you can redistribute it and/or modify
+** it under the terms of the Mozilla Public Licence 2.0 as published by
+** the Mozilla Foundation, either version 2 of the License, or
+** (at your option) any later version.
+**
+** Antares_Simulator is distributed in the hope that it will be useful,
+** but WITHOUT ANY WARRANTY; without even the implied warranty of
+** MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+** Mozilla Public Licence 2.0 for more details.
+**
+** You should have received a copy of the Mozilla Public Licence 2.0
+** along with Antares_Simulator. If not, see <https://opensource.org/license/mpl-2-0/>.
+*/
+#pragma once
+
+#include "antares/solver/expressions/nodes/NodeVisitor.h"
+
+namespace Antares::Solver::Expressions
+{
+class LinearVisitor: public NodeVisitor<bool>
+{
+public:
+    using Base = NodeVisitor<bool>;
+
+private:
+    bool visit(const AddNode& add) override;
+    bool visit(const SubtractionNode& add) override;
+    bool visit(const MultiplicationNode& add) override;
+    bool visit(const DivisionNode& add) override;
+    bool visit(const EqualNode& add) override;
+    bool visit(const LessThanOrEqualNode& add) override;
+    bool visit(const GreaterThanOrEqualNode& add) override;
+    bool visit(const NegationNode& neg) override;
+    bool visit(const VariableNode& param) override;
+    bool visit(const ParameterNode& param) override;
+    bool visit(const LiteralNode& lit) override;
+    bool visit(const PortFieldNode& port_field_node) override;
+    bool visit(const ComponentVariableNode& component_variable_node) override;
+    bool visit(const ComponentParameterNode& component_parameter_node) override;
+};
+} // namespace Antares::Solver::Expressions
