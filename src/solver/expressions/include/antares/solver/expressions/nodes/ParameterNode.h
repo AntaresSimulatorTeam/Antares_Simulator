157901be4fb1e2f937de2785b0a3beb8d8a635e0
Abdoulbari Zakir <abdoulbari.zakir@gmail.com>
2024-08-19 16:21:54 +0200

    draft time index visitor

--- src/solver/expressions/include/antares/solver/expressions/nodes/ParameterNode.h
+++ src/solver/expressions/include/antares/solver/expressions/nodes/ParameterNode.h
@@ -3,12 +3,14 @@
 #include <string>
 
 #include <antares/solver/expressions/nodes/Leaf.h>
+#include <antares/solver/expressions/nodes/TimeIndex.h>
 
 namespace Antares::Solver::Nodes
 {
-class ParameterNode final: public Leaf<std::string>
+class ParameterNode final: public Leaf<std::string>, public TimeIndex
 {
 public:
-    using Leaf<std::string>::Leaf;
+    explicit ParameterNode(const std::string& value);
+    explicit ParameterNode(const std::string& value, TimeType timeType);
 };
 } // namespace Antares::Solver::Nodes
--- src/solver/expressions/include/antares/solver/expressions/nodes/VariableNode.h
+++ src/solver/expressions/include/antares/solver/expressions/nodes/VariableNode.h
@@ -1,14 +1,17 @@
 #pragma once
 
+#include <include/antares/solver/expressions/nodes/TimeType.h>
 #include <string>
 
 #include <antares/solver/expressions/nodes/Leaf.h>
+#include <antares/solver/expressions/nodes/TimeIndex.h>
 
 namespace Antares::Solver::Nodes
 {
-class VariableNode final: public Leaf<std::string>
+class VariableNode final: public Leaf<std::string>, public TimeIndex
 {
 public:
-    using Leaf<std::string>::Leaf;
+    explicit VariableNode(const std::string& value);
+    explicit VariableNode(const std::string& value, TimeType timeType);
 };
 } // namespace Antares::Solver::Nodes
