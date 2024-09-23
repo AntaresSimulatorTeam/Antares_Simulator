5d756607cfbf4a6c4b3079b253182ac74e3d3cf7
Guillaume PIERRE <guillaume.pierre@rte-france.com>
2024-09-23 16:48:06 +0200

    Remove headers includes : opt_appel_solveur_lineaire.cpp

--- src/solver/optimisation/opt_appel_solveur_lineaire.cpp
+++ src/solver/optimisation/opt_appel_solveur_lineaire.cpp
@@ -19,35 +19,23 @@
  * along with Antares_Simulator. If not, see <https://opensource.org/license/mpl-2-0/>.
  */
 
-#include <yuni/yuni.h>
-
-#include "antares/solver/optimisation/opt_fonctions.h"
-#include "antares/solver/optimisation/opt_structure_probleme_a_resoudre.h"
-#include "antares/solver/simulation/sim_structure_probleme_economique.h"
-#include "antares/solver/simulation/simulation.h"
-#include "antares/solver/utils/basis_status.h"
-
-extern "C"
-{
-#include "spx_definition_arguments.h"
-#include "spx_fonctions.h"
-#include "srs_api.h"
-}
-
 #include <chrono>
+#include <spx_definition_arguments.h>
+#include <spx_fonctions.h>
 
 #include <antares/antares/fatal-error.h>
 #include <antares/logs/logs.h>
+#include "antares/optimization-options/options.h"
 #include "antares/solver/infeasible-problem-analysis/unfeasible-pb-analyzer.h"
+#include "antares/solver/optimisation/opt_structure_probleme_a_resoudre.h"
+#include "antares/solver/simulation/sim_structure_probleme_economique.h"
 #include "antares/solver/utils/filename.h"
 #include "antares/solver/utils/mps_utils.h"
 
 using namespace operations_research;
 
-using namespace Antares;
-using namespace Antares::Data;
-using namespace Yuni;
 using Antares::Solver::IResultWriter;
+using Antares::Solver::Optimization::OptimizationOptions;
 
 class TimeMeasurement
 {
@@ -268,7 +256,7 @@ static SimplexResult OPT_TryToCallSimplex(const OptimizationOptions& options,
             logs.info() << " Solver: Standard resolution failed";
             logs.info() << " Solver: Retry in safe mode"; // second trial w/o scaling
 
-            if (Logs::Verbosity::Debug::enabled)
+            if (false) // To be turned to true if we want to print this
             {
                 logs.info() << " solver: resetting";
             }
