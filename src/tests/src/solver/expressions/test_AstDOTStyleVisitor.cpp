c746f2ab18c21b64bc8c91b36c5441d6954b83cb
guilpier-code <62292552+guilpier-code@users.noreply.github.com>
2024-09-26 14:39:20 +0200

    Visitor AST into DOT : trial for more clarity (#2426)
    Co-authored-by: Florian OMNES <florian.omnes@rte-france.com>

--- src/tests/src/solver/expressions/test_AstDOTStyleVisitor.cpp
+++ src/tests/src/solver/expressions/test_AstDOTStyleVisitor.cpp
@@ -21,8 +21,6 @@
 
 #define WIN32_LEAN_AND_MEAN
 
-#include <variant>
-
 #include <boost/test/unit_test.hpp>
 
 #include <antares/solver/expressions/Registry.hxx>
@@ -149,15 +147,16 @@ legend_VariableNode [ label =" VariableNode: 1"]
     Registry<Node> registry_;
 };
 
-BOOST_FIXTURE_TEST_CASE(tree_with_all_type_node, Fixture)
+BOOST_FIXTURE_TEST_CASE(
+  dot_visitor_is_run_on_complex_expression___resulting_dot_content_as_expected,
+  Fixture)
 {
-    std::ostringstream os;
+    std::ostringstream dotContentStream;
 
     AstDOTStyleVisitor astGraphVisitor;
-    astGraphVisitor(os, makeExpression());
+    astGraphVisitor(dotContentStream, makeExpression());
 
-    // read the content of os
-    BOOST_CHECK_EQUAL(expectedDotContent(), os.str());
+    BOOST_CHECK_EQUAL(dotContentStream.str(), expectedDotContent());
 }
 
 BOOST_FIXTURE_TEST_CASE(AstDOTStyleVisitor_name, Registry<Node>)
