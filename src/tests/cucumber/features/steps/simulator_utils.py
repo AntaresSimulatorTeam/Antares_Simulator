da70910e896da4894c1e1fc53d82f0195628bd0a
Peter Mitri <peter.mitri@rte-france.com>
2024-10-04 14:45:54 +0200

    Cucumber: add short tests & refactor some steps (#2382)

--- src/tests/cucumber/features/steps/simulator_utils.py
+++ src/tests/cucumber/features/steps/simulator_utils.py
@@ -1,35 +1,37 @@
 # Methods to run Antares simulator
 
 import subprocess
-import glob
 import yaml
 from pathlib import Path
 from study_input_handler import study_input_handler
-from output_utils import parse_output_folder_from_logs
+from study_output_handler import study_output_handler
 
 
 def get_solver_path():
     with open("conf.yaml") as file:
-            content = yaml.full_load(file)
+        content = yaml.full_load(file)
     return content.get("antares-solver")
 
-SOLVER_PATH = get_solver_path() # we only need to run this once
+
+SOLVER_PATH = get_solver_path()  # we only need to run this once
 
 
 def run_simulation(context):
-    activate_simu_outputs(context)  # TODO : remove this and update studies instead
+    init_simu(context)
     command = build_antares_solver_command(context)
     print(f"Running command: {command}")
     process = subprocess.Popen(command, stdout=subprocess.PIPE, stderr=subprocess.DEVNULL)
     out, err = process.communicate()
     context.output_path = parse_output_folder_from_logs(out)
     context.return_code = process.returncode
-    context.annual_system_cost = None
-    context.hourly_values = None
+    context.soh = study_output_handler(context.output_path)
 
 
-def activate_simu_outputs(context):
+def init_simu(context):
     sih = study_input_handler(Path(context.study_path))
+    # read metadata
+    context.nbyears = int(sih.get_value(variable="nbyears", file_nick_name="general"))
+    # activate year-by-year results  # TODO : remove this and update studies instead
     sih.set_value(variable="synthesis", value="true", file_nick_name="general")
     sih.set_value(variable="year-by-year", value="true", file_nick_name="general")
 
@@ -45,3 +47,9 @@ def build_antares_solver_command(context):
         command.append('--force-parallel=4')
     return command
 
+
+def parse_output_folder_from_logs(logs: bytes) -> str:
+    for line in logs.splitlines():
+        if b'Output folder : ' in line:
+            return line.split(b'Output folder : ')[1].decode('ascii')
+    raise LookupError("Could not parse output folder in output logs")
--- src/tests/cucumber/features/steps/steps.py
+++ src/tests/cucumber/features/steps/steps.py
@@ -1,14 +1,18 @@
 # Gherkins test steps definitions
 
 import os
+import pathlib
+
 from behave import *
-from simulator_utils import *
+
 from assertions import *
-from context_utils import *
+from simulator_utils import *
+
 
 @given('the study path is "{string}"')
 def study_path_is(context, string):
-    context.study_path = os.path.join("..", "resources", "Antares_Simulator_Tests_NR" , string.replace("/", os.sep))
+    context.study_path = os.path.join("..", "resources", "Antares_Simulator_Tests_NR", string.replace("/", os.sep))
+
 
 @when('I run antares simulator')
 def run_antares(context):
@@ -18,50 +22,104 @@ def run_antares(context):
     context.parallel = False
     run_simulation(context)
 
-@then('the simulation succeeds')
-def simu_success(context):
-    assert context.return_code == 0
-
-@then('the simulation fails')
-def simu_success(context):
-    assert context.return_code != 0
-
-@then('the expected value of the annual system cost is {value}')
-def check_annual_cost_expected(context, value):
-    assert_double_close(float(value), get_annual_system_cost(context)["EXP"], 0.001)
-
-@then('the minimum annual system cost is {value}')
-def check_annual_cost_min(context, value):
-    assert_double_close(float(value), get_annual_system_cost(context)["MIN"], 0.001)
-
-@then('the maximum annual system cost is {value}')
-def check_annual_cost_max(context, value):
-    assert_double_close(float(value), get_annual_system_cost(context)["MAX"], 0.001)
-
-@then('the annual system cost is')
-def check_annual_cost(context):
-    for row in context.table:
-        assert_double_close(float(row["EXP"]), get_annual_system_cost(context)["EXP"], 0.001)
-        assert_double_close(float(row["STD"]), get_annual_system_cost(context)["STD"], 0.001)
-        assert_double_close(float(row["MIN"]), get_annual_system_cost(context)["MIN"], 0.001)
-        assert_double_close(float(row["MAX"]), get_annual_system_cost(context)["MAX"], 0.001)
-
-@then('the simulation takes less than {seconds} seconds')
-def check_simu_time(context, seconds):
-    actual_simu_time = parse_simu_time(context.output_path)
-    assert actual_simu_time <= float(seconds)
-
-@then('in area "{area}", during year {year}, loss of load lasts {lold_hours} hours')
-def check_lold_duration(context, area, year, lold_hours):
-    assert int(lold_hours) == get_hourly_values(context, area.lower(), int(year))["LOLD"].sum()
-
-@then('in area "{area}", unsupplied energy on "{date}" of year {year} is of {lold_value_mw} MW')
-def check_lold_value(context, area, date, year, lold_value_mw):
-    actual_unsp_energ = get_hourly_values_for_specific_hour(context, area.lower(), int(year), date)["UNSP. ENRG"].sum()
-    assert_double_close(float(lold_value_mw), actual_unsp_energ, 0.001)
 
 def after_feature(context, feature):
     # post-processing a test: clean up output files to avoid taking up all the disk space
     if (context.output_path != None):
         pathlib.Path.rmdir(context.output_path)
 
+
+@then('the simulation succeeds')
+def simu_success(context):
+    assert context.return_code == 0
+
+
+@then('the simulation fails')
+def simu_success(context):
+    assert context.return_code != 0
+
+
+@then('the expected value of the annual system cost is {value:g}')
+def check_annual_cost_expected(context, value):
+    assert_double_close(value, context.soh.get_annual_system_cost()["EXP"], 0.001)
+
+
+@then('the minimum annual system cost is {value:g}')
+def check_annual_cost_min(context, value):
+    assert_double_close(value, context.soh.get_annual_system_cost()["MIN"], 0.001)
+
+
+@then('the maximum annual system cost is {value:g}')
+def check_annual_cost_max(context, value):
+    assert_double_close(value, context.soh.get_annual_system_cost()["MAX"], 0.001)
+
+
+@then('the annual system cost is')
+def check_annual_cost(context):
+    for row in context.table:
+        assert_double_close(float(row["EXP"]), context.soh.get_annual_system_cost()["EXP"], 0.001)
+        assert_double_close(float(row["STD"]), context.soh.get_annual_system_cost()["STD"], 0.001)
+        assert_double_close(float(row["MIN"]), context.soh.get_annual_system_cost()["MIN"], 0.001)
+        assert_double_close(float(row["MAX"]), context.soh.get_annual_system_cost()["MAX"], 0.001)
+
+
+@then('the annual system cost is {one_year_value:g}')
+def check_annual_cost(context, one_year_value):
+    assert_double_close(one_year_value, context.soh.get_annual_system_cost()["EXP"], 0.001)
+    assert_double_close(0, context.soh.get_annual_system_cost()["STD"], 0.001)
+    assert_double_close(one_year_value, context.soh.get_annual_system_cost()["MIN"], 0.001)
+    assert_double_close(one_year_value, context.soh.get_annual_system_cost()["MAX"], 0.001)
+
+
+@then('the simulation takes less than {seconds:g} seconds')
+def check_simu_time(context, seconds):
+    assert context.soh.get_simu_time() <= seconds
+
+
+@then('in area "{area}", during year {year:d}, loss of load lasts {lold_hours:d} hours')
+def check_lold_duration(context, area, year, lold_hours):
+    assert lold_hours == context.soh.get_loss_of_load_duration_h(area, year)
+
+
+@then('in area "{area}", unsupplied energy on "{date}" of year {year:d} is of {lold_value_mw:g} MW')
+def check_lold_value(context, area, date, year, lold_value_mw):
+    actual_unsp_energ = context.soh.get_unsupplied_energy_mwh(area, year, date)
+    assert_double_close(lold_value_mw, actual_unsp_energ, 0.001)
+
+
+@then(
+    'in area "{area}", during year {year:d}, hourly production of "{prod_name}" is always {comparator_and_hourly_prod} MWh')
+def check_prod_for_specific_year(context, area, year, prod_name, comparator_and_hourly_prod):
+    expected_prod = float(comparator_and_hourly_prod.split(" ")[-1])
+    actual_hourly_prod = context.soh.get_hourly_prod_mwh(area, year, prod_name)
+    if "greater than" in comparator_and_hourly_prod:
+        ok = actual_hourly_prod >= expected_prod
+    elif "equal to" in comparator_and_hourly_prod:
+        ok = actual_hourly_prod - expected_prod <= 1e-6
+    else:
+        raise NotImplementedError(f"Unknown comparator '{comparator_and_hourly_prod}'")
+    if "zero or" in comparator_and_hourly_prod:
+        ok = ok | (actual_hourly_prod == 0)
+    assert ok.all()
+
+
+@then('in area "{area}", hourly production of "{prod_name}" is always {comparator_and_hourly_prod} MWh')
+def check_prod_for_all_years(context, area, prod_name, comparator_and_hourly_prod):
+    for year in range(1, context.nbyears + 1):
+        check_prod_for_specific_year(context, area, year, prod_name, comparator_and_hourly_prod)
+
+
+@step('in area "{area}", during year {year:d}, total non-proportional cost is {np_cost:g}')
+def check_np_cost_for_specific_year(context, area, year, np_cost):
+    assert_double_close(np_cost, context.soh.get_non_proportional_cost(area, year), 1e-6)
+
+
+@then('in area "{area}", the units of "{prod_name}" produce between {min_p:g} and {max_p:g} MWh hourly')
+def check_pmin_pmax(context, area, prod_name, min_p, max_p):
+    for year in range(1, context.nbyears + 1):
+        actual_hourly_prod = context.soh.get_hourly_prod_mwh(area, year, prod_name)
+        actual_n_dispatched_units = context.soh.get_hourly_n_dispatched_units(area, year, prod_name)
+        assert (actual_hourly_prod <= actual_n_dispatched_units.apply(
+            lambda n: n * max_p)).all(), f"max_p constraint not respected during year {year}"
+        assert (actual_hourly_prod >= actual_n_dispatched_units.apply(
+            lambda n: n * min_p)).all(), f"min_p constraint not respected during year {year}"
