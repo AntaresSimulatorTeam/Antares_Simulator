dd5c453dbba325bf01380b51dd7634dd15eeb543
Vincent Payet <vincent.payet@rte-france.com>
2024-10-07 16:21:49 +0200

    fix compile

--- src/solver/simulation/include/antares/solver/simulation/solver.hxx
+++ src/solver/simulation/include/antares/solver/simulation/solver.hxx
@@ -388,6 +388,7 @@ void ISimulation<ImplementationType>::writeResults(bool synthesis, uint year, ui
     }
 }
 
+template<class ImplementationType>
 uint ISimulation<ImplementationType>::buildSetsOfParallelYears(
   uint firstYear,
   uint endYear,
