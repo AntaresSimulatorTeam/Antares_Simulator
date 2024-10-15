7afdaebd680ccc5d808a85a1a37fddc6eae2ef3b
Juliette-Gerbaux <juliette.gerbaux_externe@rte-france.com>
2024-10-15 09:17:39 +0200

    Update overflow cost according to imput

--- src/solver/optimisation/opt_gestion_des_couts_cas_lineaire.cpp
+++ src/solver/optimisation/opt_gestion_des_couts_cas_lineaire.cpp
@@ -277,10 +277,6 @@ void OPT_InitialiserLesCoutsLineaire(PROBLEME_HEBDO* problemeHebdo,
                 ProblemeAResoudre->CoutLineaire[var] = problemeHebdo->CoutDeDebordement[pays];
                 if (!problemeHebdo->CaracteristiquesHydrauliques[pays].AccurateWaterValue)
                 {
-                    ProblemeAResoudre->CoutLineaire[var] = abs(problemeHebdo
-                                                                 ->CoutDeDefaillanceNegative[pays])
-                                                           * 1.10;
-
                     ProblemeAResoudre->CoutLineaire[var]
                       += problemeHebdo->CaracteristiquesHydrauliques[pays]
                            .WeeklyWaterValueStateRegular;
