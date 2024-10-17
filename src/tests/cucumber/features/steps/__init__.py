0c6a908b9dd1e3d0408d8f495d722c6328e082bf
Peter Mitri <peter.mitri@rte-france.com>
2024-10-17 17:06:09 +0200

    try windows fix
    Signed-off-by: Peter Mitri <peter.mitri@rte-france.com>

--- src/tests/cucumber/features/steps/__init__.py
+++ src/tests/cucumber/features/steps/__init__.py
@@ -1,4 +1,4 @@
-from import_helper import import_steps_from_subdirs, import_steps
+from import_helper import import_steps
 from pathlib import Path
 
 STEPS_DIR_NAME = "steps"
@@ -6,15 +6,3 @@ STEPS_DIR = Path(__file__).parent
 assert STEPS_DIR.name == STEPS_DIR_NAME
 
 import_steps(STEPS_DIR)
-
-# import_steps_from_subdirs("Antares_Simulator/src/tests/cucumber/features/steps")
-
-# import_module(".assertions", "Antares_Simulator.src.tests.cucumber.features.steps")
-# import_module(
-#     ".study_output_handler", "Antares_Simulator.src.tests.cucumber.features.steps"
-# )
-# import_module(
-#     ".study_input_handler", "Antares_Simulator.src.tests.cucumber.features.steps"
-# )
-# import_module(".simulator_utils", "Antares_Simulator.src.tests.cucumber.features.steps")
-# import_module(".steps", "Antares_Simulator.src.tests.cucumber.features.steps")
--- src/tests/cucumber/features/steps/import_helper.py
+++ src/tests/cucumber/features/steps/import_helper.py
@@ -3,6 +3,8 @@ from pkgutil import iter_modules
 from os import walk
 from pathlib import Path
 
+import os
+
 
 def import_steps(steps_dir: Path):
     assert steps_dir.exists()
@@ -14,27 +16,15 @@ def import_steps(steps_dir: Path):
         if directory == str(steps_dir):
             continue
 
-        current_directory = directory + "/"
+        current_directory = directory + os.sep
         print(f"Importing Additional Steps: {current_directory}")
 
         all_modules = [
             module_info[1]
             for module_info in iter_modules(path=[str(current_directory)])
         ]
-        current_directory = current_directory.replace(str(steps_dir) + "/", "")
+        current_directory = current_directory.replace(str(steps_dir) + os.sep, "")
 
         for module in all_modules:
-            module_path = current_directory.replace("/", ".") + module
+            module_path = current_directory.replace(os.sep, ".") + module
             import_module(module_path)
-
-
-def import_steps_from_subdirs(dir_path):
-    for directory in walk(dir_path):
-        current_directory = directory[0] + "/"
-
-        all_modules = [
-            module_info[1] for module_info in iter_modules(path=[current_directory])
-        ]
-
-        for module in all_modules:
-            import_module(current_directory.replace("/", ".") + module)
