26a882526d8ee20c421335b396acb4a73357e563
Peter Mitri <peter.mitri@rte-france.com>
2024-11-20 16:46:05 +0100

    New System model [ANT-2208]
    Signed-off-by: Peter Mitri <peter.mitri@rte-france.com>

---
+++ src/study/system-model/system.cpp
@@ -0,0 +1,69 @@
+/*
+** Copyright 2007-2024, RTE (https://www.rte-france.com)
+** See AUTHORS.txt
+** SPDX-License-Identifier: MPL-2.0
+** This file is part of Antares-Simulator,
+** Adequacy and Performance assessment for interconnected energy networks.
+**
+** Antares_Simulator is free software: you can redistribute it and/or modify
+** it under the terms of the Mozilla Public Licence 2.0 as published by
+** the Mozilla Foundation, either version 2 of the License, or
+** (at your option) any later version.
+**
+** Antares_Simulator is distributed in the hope that it will be useful,
+** but WITHOUT ANY WARRANTY; without even the implied warranty of
+** MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+** Mozilla Public Licence 2.0 for more details.
+**
+** You should have received a copy of the Mozilla Public Licence 2.0
+** along with Antares_Simulator. If not, see <https://opensource.org/license/mpl-2-0/>.
+*/
+
+#include <antares/study/system-model/system.h>
+
+namespace Antares::Study::SystemModel
+{
+System::System(const std::string_view id, std::vector<Component> components):
+    id_(id)
+{
+    // Check that mandatory attributes are not empty
+    if (id.empty())
+    {
+        throw std::invalid_argument("A system can't have an empty id");
+    }
+    if (components.empty())
+    {
+        throw std::invalid_argument("A system must contain at least one component");
+    }
+    std::transform(components.begin(),
+                   components.end(),
+                   std::inserter(components_, components_.end()),
+                   [this](/*Non const to prevent copy*/ Component& component)
+                   {
+                       if (components_.contains(component.Id()))
+                       {
+                           throw std::invalid_argument(
+                             "System has at least two components with the same id ('"
+                             + component.Id() + "'), this is not supported");
+                       }
+                       return std::make_pair(component.Id(), std::move(component));
+                   });
+}
+
+SystemBuilder& SystemBuilder::withId(std::string_view id)
+{
+    id_ = id;
+    return *this;
+}
+
+SystemBuilder& SystemBuilder::withComponents(std::vector<Component>&& components)
+{
+    components_ = std::move(components);
+    return *this;
+}
+
+System SystemBuilder::build() const
+{
+    return System(id_, components_);
+}
+} // namespace Antares::Study::SystemModel
---
+++ src/tests/src/study/system-model/utils.h
@@ -0,0 +1,38 @@
+/*
+ * Copyright 2007-2024, RTE (https://www.rte-france.com)
+ * See AUTHORS.txt
+ * SPDX-License-Identifier: MPL-2.0
+ * This file is part of Antares-Simulator,
+ * Adequacy and Performance assessment for interconnected energy networks.
+ *
+ * Antares_Simulator is free software: you can redistribute it and/or modify
+ * it under the terms of the Mozilla Public Licence 2.0 as published by
+ * the Mozilla Foundation, either version 2 of the License, or
+ * (at your option) any later version.
+ *
+ * Antares_Simulator is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+ * Mozilla Public Licence 2.0 for more details.
+ *
+ * You should have received a copy of the Mozilla Public Licence 2.0
+ * along with Antares_Simulator. If not, see <https://opensource.org/license/mpl-2-0/>.
+ */
+
+#pragma once
+
+#define WIN32_LEAN_AND_MEAN
+
+#include <string>
+
+#include <boost/test/unit_test.hpp>
+
+inline std::function<bool(const std::exception&)> checkMessage(std::string expected_message)
+{
+    auto predicate = [expected_message](const std::exception& e)
+    {
+        BOOST_CHECK_EQUAL(e.what(), expected_message);
+        return true;
+    };
+    return predicate;
+}
