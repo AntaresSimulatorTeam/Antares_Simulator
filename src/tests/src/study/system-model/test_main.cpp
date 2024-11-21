f3c2d6384f0273dbd542cf8f7accbf1a41b15836
Peter Mitri <peter.mitri@rte-france.com>
2024-11-21 12:35:05 +0100

    remove redundant fn definition

--- src/tests/src/study/system-model/test_main.cpp
+++ src/tests/src/study/system-model/test_main.cpp
@@ -23,13 +23,3 @@
 #define WIN32_LEAN_AND_MEAN
 
 #include <boost/test/unit_test.hpp>
-
-std::function<bool(const std::exception&)> checkMessage(std::string expected_message)
-{
-    auto predicate = [expected_message](const std::exception& e)
-    {
-        BOOST_CHECK_EQUAL(e.what(), expected_message);
-        return true;
-    };
-    return predicate;
-}
