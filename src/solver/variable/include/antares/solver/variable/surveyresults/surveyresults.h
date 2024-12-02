52f191750fae10582bfb264e5cd59fe7bc44684a
Vincent Payet <vincent.payet@rte-france.com>
2024-12-02 12:58:06 +0100

    fix header comment

--- src/solver/variable/include/antares/solver/variable/surveyresults/surveyresults.h
+++ src/solver/variable/include/antares/solver/variable/surveyresults/surveyresults.h
@@ -1,4 +1,5 @@
-/*** Copyright 2007-2024, RTE (https://www.rte-france.com)
+/*
+** Copyright 2007-2024, RTE (https://www.rte-france.com)
 ** See AUTHORS.txt
 ** SPDX-License-Identifier: MPL-2.0
 ** This file is part of Antares-Simulator,
