77b8eaf00023abe925387511aef1144d02c9f602
Vincent Payet <vincent.payet@rte-france.com>
2025-01-16 11:25:30 +0100

    use computeAveragesForCurrentYear

--- src/solver/variable/include/antares/solver/variable/economy/dispatchableGeneration.h
+++ src/solver/variable/include/antares/solver/variable/economy/dispatchableGeneration.h
@@ -223,7 +223,7 @@ public:
     {
         for (unsigned int column = 0; column < nbColumns_; column++)
         {
-            pValuesForTheCurrentYear[numSpace][column].computeStatisticsForTheCurrentYear();
+            pValuesForTheCurrentYear[numSpace][column].computeAveragesForCurrentYear();
         }
 
         // Next variable
