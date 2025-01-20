62a54377884d21ec39a39f1e3d19965f76093cff
Guillaume PIERRE <guillaume.pierre@rte-france.com>
2025-01-20 17:04:21 +0100

    Data for new modeler (4.1) : start for testing linear problem data

---
+++ src/tests/src/solver/modeler/dataSeries/testLinearProblemData.cpp
@@ -0,0 +1,13 @@
+#define WIN32_LEAN_AND_MEAN
+
+#include <boost/test/unit_test.hpp>
+
+#include <antares/solver/modeler/api/linearProblemData.h>
+
+using namespace Antares::Solver::Modeler::Api;
+
+BOOST_AUTO_TEST_CASE(dummy_test)
+{
+    LinearProblemData linearProblemData;
+    BOOST_CHECK(true);
+}
