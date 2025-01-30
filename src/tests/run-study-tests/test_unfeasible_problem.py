0ae1b848749c7979922d7465848cd7cd1b1aa35a
Vincent Payet <vincent.payet@rte-france.com>
2025-01-30 14:33:17 +0100

    Add other unfeasible tests

--- src/tests/run-study-tests/test_unfeasible_problem.py
+++ src/tests/run-study-tests/test_unfeasible_problem.py
@@ -65,3 +65,109 @@ def test_unfeasible_problem_01__warning_dry(study_path, check_runner):
                                           checks_on_weeks=warnings_on_weeks,
                                           simulation=check_runner.get_simulation()))
     check_runner.run(checks)
+
+
+#def check_all_unfeasible_options(solver_path, study_path, years_error,years_warning):
+#    output_path = study_path / 'output'
+
+#    st = Study(str(study_path))
+#    st.check_files_existence()
+
+#    # Option error-verbose : mps and error
+#    st.set_variable(variable="include-unfeasible-problem-behavior", value="error-verbose", file_nick_name="general")
+#    result_code = launch_solver(solver_path, study_path)
+#    assert result_code != 0
+#    check_log_values(output_path, years_error, "fatal")
+#    check_mps_availability(output_path, years_error)
+
+#    # Option error-dry : no mps and error
+#    st.set_variable(variable="include-unfeasible-problem-behavior", value="error-dry", file_nick_name="general")
+#    result_code = launch_solver(solver_path, study_path)
+#    assert result_code != 0
+#    check_log_values(output_path, years_error, "fatal")
+#    check_no_mps_available(output_path)
+
+#    # Option warning-verbose : mps and warning
+#    st.set_variable(variable="include-unfeasible-problem-behavior", value="warning-verbose", file_nick_name="general")
+#    result_code = launch_solver(solver_path, study_path)
+#    assert result_code == 0
+#    check_log_values(output_path, years_warning, "warns")
+#    check_mps_availability(output_path, years_warning)
+
+#    # Option warning-dry : no mps and warning
+#    st.set_variable(variable="include-unfeasible-problem-behavior", value="warning-dry", file_nick_name="general")
+#    result_code = launch_solver(solver_path, study_path)
+#    assert result_code == 0
+#    check_log_values(output_path, years_warning, "warns")
+#    check_no_mps_available(output_path)
+
+#@pytest.mark.unfeasible
+#@pytest.mark.parametrize('study_path', [ALL_STUDIES_PATH / "specific-tests" / "unfeasible-problem" / "unfeasible_problem_01"], indirect=True)
+#def test_unfeasible_problem_01(solver_path):
+#    years_warning = {3: [19], 6: [42, 43, 49], 7: [19], 8: [19]}
+#    years_error = {3: [19]}
+#    check_all_unfeasible_options(solver_path, study_path, years_error, years_warning)
+
+#@pytest.mark.unfeasible
+#def test_unfeasible_problem_02(solver_path):
+#    study_path = ALL_STUDIES_PATH / "specific-tests" / "unfeasible-problem" / "unfeasible_problem_02"
+#    years_warning = {6: [22,23,29]}
+#    years_error = {6: [22]}
+#    check_all_unfeasible_options(solver_path, study_path, years_error, years_warning)
+
+#@pytest.mark.unfeasible
+#def test_unfeasible_problem_03(solver_path):
+#    study_path = ALL_STUDIES_PATH / "specific-tests" / "unfeasible-problem" / "unfeasible_problem_03"
+#    years_warning = {3: [19], 6: [42, 43, 49], 7: [19], 8: [19]}
+#    years_error = {3: [19]}
+#    check_all_unfeasible_options(solver_path, study_path, years_error, years_warning)
+
+#@pytest.mark.unfeasible
+#def test_unfeasible_problem_04(solver_path):
+#    study_path = ALL_STUDIES_PATH / "specific-tests" / "unfeasible-problem" / "unfeasible_problem_04"
+#    years_warning = {6: [22, 23, 29]}
+#    years_error = {6: [22]}
+#    check_all_unfeasible_options(solver_path, study_path, years_error, years_warning)
+
+#@pytest.mark.medium
+#def test_unfeasible_problem_05(solver_path):
+#    study_path = ALL_STUDIES_PATH / "specific-tests" / "unfeasible-problem" / "unfeasible_problem_05"
+#    #Not all years with errors are tested, there is more than 200 years with errors
+#    years_warning = {10: [19], 11: [19], 16: [19],29: [19],  41: [42, 43, 49],43: [42, 43, 49],46: [42, 43, 49],142: [42, 43, 49]}
+#    years_error = {10: [19]}
+#    check_all_unfeasible_options(solver_path, study_path, years_error, years_warning)
+
+#@pytest.mark.unfeasible
+#def test_unfeasible_problem_06(solver_path):
+#    study_path = ALL_STUDIES_PATH / "specific-tests" / "unfeasible-problem" / "unfeasible_problem_06"
+#    years_warning = {3: [19], 6: [42, 43, 49], 7: [19], 8: [19]}
+#    years_error = {3: [19]}
+#    check_all_unfeasible_options(solver_path, study_path, years_error, years_warning)
+
+#@pytest.mark.unfeasible
+#def test_unfeasible_problem_07(solver_path):
+#    study_path = ALL_STUDIES_PATH / "specific-tests" / "unfeasible-problem" / "unfeasible_problem_07"
+#    years_warning = {1: [1], 3: [52], 5: [1], 7: [52], 9: [52]}
+#    years_error = {1: [1]}
+#    check_all_unfeasible_options(solver_path, study_path, years_error, years_warning)
+
+#@pytest.mark.unfeasible
+#def test_unfeasible_problem_08(solver_path):
+#    study_path = ALL_STUDIES_PATH / "specific-tests" / "unfeasible-problem" / "unfeasible_problem_08"
+#    years_warning = {3: [51],7: [51],9: [51]}
+#    years_error = {3: [51]}
+#    check_all_unfeasible_options(solver_path, study_path, years_error, years_warning)
+
+#@pytest.mark.unfeasible
+#def test_unfeasible_problem_09(solver_path):
+#    study_path = ALL_STUDIES_PATH / "specific-tests" / "unfeasible-problem" / "unfeasible_problem_09"
+#    years_warning = {1: [1], 3: [52], 5: [1], 7: [52], 9: [52]}
+#    years_error = {1: [1]}
+#    check_all_unfeasible_options(solver_path, study_path, years_error, years_warning)
+
+#@pytest.mark.unfeasible
+#def test_unfeasible_problem_10(solver_path):
+#    study_path = ALL_STUDIES_PATH / "specific-tests" / "unfeasible-problem" / "unfeasible_problem_10"
+#    years_warning = {3: [51],7: [51], 9: [51]}
+#    years_error = {3: [51]}
+#    check_all_unfeasible_options(solver_path, study_path, years_error, years_warning)
