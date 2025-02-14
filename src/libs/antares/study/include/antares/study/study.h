425a3c81808fe65956a90628e9de5001579a50d8
Vincent Payet <vincent.payet@rte-france.com>
2025-02-14 11:12:18 +0100

    Revert "Remove correlation include"
    This reverts commit c17a19c5434dc24e21831f0a66abf900c5355486.

--- src/libs/antares/study/include/antares/study/study.h
+++ src/libs/antares/study/include/antares/study/study.h
@@ -29,6 +29,7 @@
 #include <yuni/job/queue/service.h>
 #include <yuni/thread/thread.h>
 
+#include <antares/correlation/correlation.h>
 #include <antares/date/date.h>
 #include <antares/study/runtime/runtime.h>
 #include <antares/writer/i_writer.h>
--- src/solver/ts-generator/include/antares/solver/ts-generator/xcast/studydata.h
+++ src/solver/ts-generator/include/antares/solver/ts-generator/xcast/studydata.h
@@ -23,6 +23,7 @@
 
 #include <yuni/yuni.h>
 
+#include <antares/correlation/correlation.h>
 #include <antares/study/xcast/xcast.h>
 
 namespace Antares::TSGenerator::XCast
