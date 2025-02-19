80780e693ff2283581be65ee8a2ca4831bb61217
Vincent Payet <vincent.payet@rte-france.com>
2025-02-19 09:53:09 +0100

    Call study saveToFolder

--- src/tests/src/libs/antares/study/test_study.cpp
+++ src/tests/src/libs/antares/study/test_study.cpp
@@ -356,4 +356,14 @@ BOOST_FIXTURE_TEST_CASE(cpu_count, OneAreaStudy)
     BOOST_CHECK_EQUAL(study->getNumberOfCoresPerMode(10, 120), 0);
 }
 
+BOOST_FIXTURE_TEST_CASE(saving_study, OneAreaStudy)
+{
+    auto enabledCluster = std::make_shared<ThermalCluster>(areaA);
+    enabledCluster->setName("Cluster2");
+    enabledCluster->enabled = true;
+    areaA->thermal.list.addToCompleteList(disabledCluster);
+
+    study->saveToFolder(std::filesystem::temp_directory_path().string());
+}
+
 BOOST_AUTO_TEST_SUITE_END() // version
