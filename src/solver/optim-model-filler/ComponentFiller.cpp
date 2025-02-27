ac4eac19bf61903ef6e01d83aa25f0bd7bb115b8
Abdoulbari Zakir <abdoulbari.zakir@gmail.com>
2025-02-27 13:45:17 +0100

    format

--- src/solver/optim-model-filler/ComponentFiller.cpp
+++ src/solver/optim-model-filler/ComponentFiller.cpp
@@ -71,8 +71,7 @@ void VariablesBulkAddition::addVariable(const std::vector<double>& lb,
       dim,
       key,
       [this, &lb, ub, integer, offset](const TimeAndScenario& timeAndScenario,
-                                       const std::string& name)
-      {
+                                       const std::string& name) {
           return linear_problem_.addVariable(lb[timeAndScenario.timestep - offset],
                                              ub,
                                              integer,
@@ -97,8 +96,7 @@ void VariablesBulkAddition::addVariable(double lb,
       dim,
       key,
       [this, lb, &ub, integer, offset](const TimeAndScenario& timeAndScenario,
-                                       const std::string& name)
-      {
+                                       const std::string& name) {
           return linear_problem_.addVariable(lb,
                                              ub[timeAndScenario.timestep - offset],
                                              integer,
