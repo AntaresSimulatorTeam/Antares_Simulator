0b57f3f5bbf9950967b61f5bfc0c47b57fa2607d
Abdoulbari Zakir <abdoulbari.zakir@gmail.com>
2025-04-18 16:02:35 +0200

    fix include

--- src/libs/antares/study/include/antares/study/parts/short-term-storage/HoursCollectorVisitor.h
+++ src/libs/antares/study/include/antares/study/parts/short-term-storage/HoursCollectorVisitor.h
@@ -19,7 +19,7 @@
 ** along with Antares_Simulator. If not, see <https://opensource.org/license/mpl-2-0/>.
 */
 #pragma once
-#include "HoursFieldBaseVisitor.h"
+#include <HoursFieldBaseVisitor.h>
 
 class HoursCollectorVisitor: public HoursFieldBaseVisitor
 {
