30e4d179628637805fbb08056a8ce10fd152b45b
Guillaume PIERRE <guillaume.pierre@rte-france.com>
2025-05-13 16:44:43 +0200

    BC RHS : remove dead code

--- src/solver/simulation/sim_calcul_economique.cpp
+++ src/solver/simulation/sim_calcul_economique.cpp
@@ -27,7 +27,6 @@
 #include <antares/study/study.h>
 #include <antares/utils/utils.h>
 #include "antares/solver/simulation/adequacy_patch_runtime_data.h"
-#include "antares/solver/simulation/sim_binding_constraints.h"
 #include "antares/solver/simulation/sim_binding_constraints_rhs.h"
 #include "antares/solver/simulation/sim_structure_probleme_economique.h"
 #include "antares/solver/simulation/simulation.h"
@@ -544,13 +543,6 @@ void SIM_RenseignementProblemeHebdo(const Study& study,
     for (unsigned hourInWeek = 0; hourInWeek < problem.NombreDePasDeTemps;
          ++hourInWeek, ++hourInYear)
     {
-        //        Simulation::prepareBindingConstraint(problem,
-        //                                             PasDeTempsDebut,
-        //                                             study.bindingConstraints,
-        //                                             study.bindingConstraintsGroups,
-        //                                             weekFirstDay,
-        //                                             hourInWeek);
-
         for (uint k = 0; k < nbPays; ++k)
         {
             auto& area = *(study.areas.byIndex[k]);
