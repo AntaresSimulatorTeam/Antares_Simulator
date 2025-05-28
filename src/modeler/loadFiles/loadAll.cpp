2bcd60b12e38360173506bb9a8fdcf7f6f1297cb
Jason Marechal <jason.marechal+github@gmail.com>
2025-05-28 14:23:28 +0200

    Fix

--- src/modeler/loadFiles/loadAll.cpp
+++ src/modeler/loadFiles/loadAll.cpp
@@ -22,7 +22,7 @@
 #include <antares/logs/logs.h>
 #include "antares/solver/modeler/loadFiles/loadFiles.h"
 
-#include "../modeler/include/antares/solver/modeler/data.h"
+#include "antares/solver/modeler/data.h"
 
 namespace Antares::Solver::LoadFiles
 {
