21f53767ce0649f0de4969900fda511cf4a67245
Florian OMNES <florian.omnes@rte-france.com>
2025-06-13 10:58:47 +0200

    Remove useless logging

--- src/solver/utils/ortools_utils.cpp
+++ src/solver/utils/ortools_utils.cpp
@@ -59,10 +59,6 @@ static void checkSetSolverSpecificParameters(bool status,
     {
         throw Antares::Error::InvalidSolverSpecificParameters(solverName, specificParameters);
     }
-    else
-    {
-        Antares::logs.info() << "  Successfully set " + solverName + " solver specific parameters";
-    }
 }
 
 static void TuneSolverSpecificOptions(MPSolver* solver,
