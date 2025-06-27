9322c10195acd0d15d6d74aeb7a0e725c3abd0ce
Vincent Payet <vincent.payet@rte-france.com>
2025-06-27 11:38:51 +0200

    Change var name

--- src/solver/simulation/common-eco-adq.cpp
+++ src/solver/simulation/common-eco-adq.cpp
@@ -349,9 +349,9 @@ void SetInitialHydroLevel(Data::Study& study,
     study.areas.each(
       [&problem, &firstDaySimu, &hydroVentilationResults](const Data::Area& area)
       {
-          bool TurbinageEntreBornes = area.hydro.reservoirManagement
-                                      && (!area.hydro.useHeuristicTarget || area.hydro.useLeeway);
-          if (TurbinageEntreBornes)
+          bool updatePreviousLevel = area.hydro.reservoirManagement
+                                     && (!area.hydro.useHeuristicTarget || area.hydro.useLeeway);
+          if (updatePreviousLevel)
           {
               double capacity = area.hydro.reservoirCapacity;
               problem.previousSimulationFinalLevel[area.index] = hydroVentilationResults[area.index]
