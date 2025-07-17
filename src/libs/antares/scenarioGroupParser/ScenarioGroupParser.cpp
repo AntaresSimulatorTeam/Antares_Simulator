65ec7916d205cdc738fe8102786275fbf9c6bf59
Jason Marechal <jason.marechal+github@gmail.com>
2025-07-17 10:42:43 +0200

    Handle stoi exception

--- src/libs/antares/scenarioGroupParser/ScenarioGroupParser.cpp
+++ src/libs/antares/scenarioGroupParser/ScenarioGroupParser.cpp
@@ -28,61 +28,57 @@
 #include "scenarioBuilderExpression/ScenarioBuilderLexer.h"
 #include "scenarioBuilderExpression/ScenarioBuilderParser.h"
 
-namespace Antares
-{
-class ScenarioBuilderImplVisitor: public ScenarioBuilderBaseVisitor
-{
-public:
-    std::any visitRules(ScenarioBuilderParser::RulesContext* ctx) override
-    {
-        return visitLine(ctx->line(0));
-    }
-
-    std::any visitLine(ScenarioBuilderParser::LineContext* ctx) override
-    {
-        auto group = std::any_cast<std::string>(visitGroup(ctx->group()));
-        auto year = std::any_cast<int>(visitYear(ctx->year()));
-        auto chronicle = std::any_cast<int>(visitChronicle(ctx->chronicle()));
-        return ScenarioGroupParser::Line{.groupName = group, .year = year, .chronicle = chronicle};
-    }
-
-    std::any visitGroup(ScenarioBuilderParser::GroupContext* ctx) override
-    {
-        auto groupName = ctx->IDENTIFIER()->getText();
-        return groupName;
-    }
-
-    std::any visitYear(ScenarioBuilderParser::YearContext* ctx) override
-    {
-        auto yearText = ctx->getText();
-        int year = std::stoi(yearText);
-        return year;
-    }
-
-    std::any visitChronicle(ScenarioBuilderParser::ChronicleContext* ctx) override
-    {
-        auto scenarioText = ctx->getText();
-        try
-        {
-            int scenario = std::stoi(scenarioText);
-            return scenario;
+namespace Antares {
+    class ScenarioBuilderImplVisitor : public ScenarioBuilderBaseVisitor {
+    public:
+        std::any visitRules(ScenarioBuilderParser::RulesContext *ctx) override {
+            return visitLine(ctx->line(0));
         }
-        catch (std::exception& e)
-        {
-            throw Antares::Error::RuntimeError(fmt::format(
-              "Scenario builder errror: could not parse the following text as a number \"{}\"",
-              scenarioText));
+
+        std::any visitLine(ScenarioBuilderParser::LineContext *ctx) override {
+            auto group = std::any_cast<std::string>(visitGroup(ctx->group()));
+            auto year = std::any_cast<int>(visitYear(ctx->year()));
+            auto chronicle = std::any_cast<int>(visitChronicle(ctx->chronicle()));
+            return ScenarioGroupParser::Line{.groupName = group, .year = year, .chronicle = chronicle};
         }
-    }
-};
+
+        std::any visitGroup(ScenarioBuilderParser::GroupContext *ctx) override {
+            auto groupName = ctx->IDENTIFIER()->getText();
+            return groupName;
+        }
+
+        std::any visitYear(ScenarioBuilderParser::YearContext *ctx) override {
+            auto yearText = ctx->getText();
+            try {
+                int year = std::stoi(yearText);
+                return year;
+            } catch (std::exception &e) {
+                throw Antares::Error::RuntimeError(fmt::format(
+                    "Scenario builder errror: could not parse the following text of a year as a number \"{}\"",
+                    yearText));
+            }
+        }
+
+        std::any visitChronicle(ScenarioBuilderParser::ChronicleContext *ctx) override {
+            auto scenarioText = ctx->getText();
+            try {
+                int scenario = std::stoi(scenarioText);
+                return scenario;
+            } catch (std::exception &e) {
+                throw Antares::Error::RuntimeError(fmt::format(
+                    "Scenario builder errror: could not parse the following text as a number \"{}\"",
+                    scenarioText));
+            }
+        }
+    };
 
 ScenarioGroupParser::Line ScenarioGroupParser::parseLine(const std::string& line,
                                                          antlr4::BaseErrorListener* errorListener)
 {
-    antlr4::ANTLRInputStream input(line);
-    ScenarioBuilderLexer lexer(&input);
-    antlr4::CommonTokenStream tokens(&lexer);
-    ScenarioBuilderParser parser(&tokens);
+        antlr4::ANTLRInputStream input(line);
+        ScenarioBuilderLexer lexer(&input);
+        antlr4::CommonTokenStream tokens(&lexer);
+        ScenarioBuilderParser parser(&tokens);
     if (errorListener)
     {
         lexer.removeErrorListeners();
@@ -93,14 +89,12 @@ ScenarioGroupParser::Line ScenarioGroupParser::parseLine(const std::string& line
 
     try
     {
-        auto* tree = parser.rules();
-        ScenarioBuilderImplVisitor visitor;
-        return std::any_cast<Line>(visitor.visit(tree));
+            auto *tree = parser.rules();
+            ScenarioBuilderImplVisitor visitor;
+            return std::any_cast<Line>(visitor.visit(tree));
+        } catch (const std::exception &e) {
+            throw Error::RuntimeError(
+                fmt::format("Error parsing line \"{}\".\n\tDetails: {}", line, e.what()));
+        }
     }
-    catch (const std::exception& e)
-    {
-        throw Error::RuntimeError(
-          fmt::format("Error parsing line \"{}\".\n\tDetails: {}", line, e.what()));
-    }
-}
 } // namespace Antares
