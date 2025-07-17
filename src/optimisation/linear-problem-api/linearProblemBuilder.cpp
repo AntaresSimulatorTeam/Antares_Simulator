328ab0b16fc1435d348531f1c9d8550e154fa131
Thomas Bittar <thomas.bittar@rte-france.com>
2025-07-17 18:49:12 +0200

    Clean again

--- src/optimisation/linear-problem-api/linearProblemBuilder.cpp
+++ src/optimisation/linear-problem-api/linearProblemBuilder.cpp
@@ -20,8 +20,6 @@
  */
 
 #include <algorithm>
-#include <chrono>
-#include <iostream>
 
 #include <antares/optimisation/linear-problem-api/linearProblemBuilder.h>
 
@@ -35,19 +33,12 @@ LinearProblemBuilder::LinearProblemBuilder(const std::vector<LinearProblemFiller
 
 void LinearProblemBuilder::build(ILinearProblem& pb, ILinearProblemData& data, FillContext& ctx)
 {
-    auto start = std::chrono::system_clock::now();
     std::ranges::for_each(fillers_,
                           [&](const auto& filler) { filler->addVariables(pb, data, ctx); });
     std::ranges::for_each(fillers_,
                           [&](const auto& filler) { filler->addConstraints(pb, data, ctx); });
     std::ranges::for_each(fillers_,
                           [&](const auto& filler) { filler->addObjective(pb, data, ctx); });
-    auto end = std::chrono::system_clock::now();
-
-    std::chrono::duration<double> elapsed_seconds = end - start;
-    std::cout << "LinearProblem.build in " << elapsed_seconds.count() << " seconds." << std::endl;
-    std::cout << "Total variables : " << pb.variableCount() << std::endl;
-    std::cout << "Total constraints : " << pb.constraintCount() << std::endl;
 }
 
 } // namespace Antares::Optimisation::LinearProblemApi
