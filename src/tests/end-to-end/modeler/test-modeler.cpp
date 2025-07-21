b74be53f851de8a7245c4fffb31191fba24f5405
Jason Maréchal <45510813+JasonMarechal25@users.noreply.github.com>
2025-07-21 12:00:22 +0200

    Update src/tests/end-to-end/modeler/test-modeler.cpp
    Co-authored-by: Florian Omnès <florian.omnes@rte-france.com>

--- src/tests/end-to-end/modeler/test-modeler.cpp
+++ src/tests/end-to-end/modeler/test-modeler.cpp
@@ -145,6 +145,6 @@ BOOST_AUTO_TEST_CASE(Minimal_system_minimize_to_0)
     StubWriter inMemoryWriter;
 
     const Antares::Solver::Modeler modeler(inMemoryLoader, inMemoryWriter);
-    modeler.solve();
-    BOOST_CHECK_EQUAL(inMemoryWriter.solution_.objectiveValue, 0);
+    auto solution = modeler.solve();
+    BOOST_CHECK_EQUAL(solution.objectiveValue, 0);
 }
