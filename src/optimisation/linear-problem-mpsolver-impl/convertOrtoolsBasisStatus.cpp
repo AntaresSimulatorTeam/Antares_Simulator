5461d84eb89cf70ce14a1d4a4b843a26c5a6e724
Abdoulbari Zakir <abdoulbari.zakir@gmail.com>
2025-08-07 09:55:27 +0200

    update

--- src/optimisation/linear-problem-mpsolver-impl/convertOrtoolsBasisStatus.cpp
+++ src/optimisation/linear-problem-mpsolver-impl/convertOrtoolsBasisStatus.cpp
@@ -16,6 +16,7 @@ LinearProblemApi::MipBasisStatus convertOrtoolsBasisStatus(
     case operations_research::MPSolver::BasisStatus::FIXED_VALUE:
         return LinearProblemApi::MipBasisStatus::FIXED_VALUE;
     case operations_research::MPSolver::BasisStatus::BASIC:
+    case default: /*TODO*/
         return LinearProblemApi::MipBasisStatus::BASIC;
     }
 }
