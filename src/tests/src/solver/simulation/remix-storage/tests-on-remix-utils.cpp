3471bb2dfed6792a36dc41e7d46742dbced0ce62
guilpier <guillaume.pierre_externe@rte-france.com>
2025-08-26 11:50:40 +0200

    Remix multi storage : adding a source file for tests on remix utils

---
+++ src/tests/src/solver/simulation/remix-storage/tests-on-remix-utils.cpp
@@ -0,0 +1,18 @@
+#define BOOST_TEST_MODULE remix utils
+
+#define WIN32_LEAN_AND_MEAN
+
+#include <memory>
+#include <unit_test_utils.h>
+#include <vector>
+
+#include <boost/test/unit_test.hpp>
+
+#include "antares/solver/simulation/remix-storage/remix-utils.h"
+#include "antares/solver/simulation/remix-storage/shave-peaks-by-remix-storage-gen.h"
+#include "antares/solver/simulation/remix-storage/storage-for-remix.h"
+
+BOOST_AUTO_TEST_CASE(dummy_test)
+{ 
+	BOOST_CHECK(true); 
+}
