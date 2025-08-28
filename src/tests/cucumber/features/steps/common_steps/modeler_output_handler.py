6a669762a85e53096bc0e0f702bbcd4d5c796401
Peter Mitri <peter.mitri@rte-france.com>
2025-08-28 16:26:34 +0200

    Add cucumber test step for simulation table (#2999)

--- src/tests/cucumber/features/steps/common_steps/modeler_output_handler.py
+++ src/tests/cucumber/features/steps/common_steps/modeler_output_handler.py
@@ -1,23 +1,27 @@
 # Antares modeler outputs parsing
 
-import os
 import pandas as pd
 
 
 class modeler_output_handler:
 
-    def __init__(self, study_output_path):
-        self.study_output_path = study_output_path
-        self.results = self.__read_csv("solution.csv")
+    def __init__(self, simulation_table_location):
+        self.simulation_table = self.__read_csv(simulation_table_location)
 
+    def __read_csv(self, absolute_path) -> pd.DataFrame:
+        return pd.read_csv(absolute_path, header=0, sep=',', low_memory=False)
 
-    def __read_csv(self, file_name) -> pd.DataFrame:
-        absolute_path = os.path.join(self.study_output_path, file_name.replace("/", os.sep))
-        return pd.read_csv(absolute_path, header=None, sep=' ', low_memory=False)
+    def get_simulation_table_entry(self, component : str, output : str, timestep : int, scenario : int):
+        df = self.simulation_table[(self.simulation_table["component"] == component)
+                                   & (self.simulation_table["output"] == output)
+                                   & (self.simulation_table["absolute_time_index"] == timestep)
+                                   & (self.simulation_table["scenario_index"] == scenario)]
+        if len(df) != 1:
+            raise LookupError(f"Simulation table does not contain exactly 1 row for component '{component}', output '{output}', timestep '{timestep}', scenario '{scenario}'")
+        return df["value"].iloc[0]
 
-    def get_optimal_value(self, var : str) -> float:
-
-        for row in self.results.iterrows():
-            if row[1][0] == var:
-                return row[1][1]
-        raise ValueError(f"Variable '{var}' not found")
+    def get_objective_value(self):
+        df = self.simulation_table[(self.simulation_table["output"] == "OBJECTIVE_VALUE")]
+        if len(df) != 1:
+            raise LookupError(f"Simulation table contains no or multiple objective values")
+        return df["value"].iloc[0]
