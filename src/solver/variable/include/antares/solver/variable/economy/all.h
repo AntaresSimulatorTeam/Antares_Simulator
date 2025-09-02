97f3528f4c76c611a4e006175bb60f983380dfaf
Jason Marechal <jason.marechal+github@gmail.com>
2025-09-02 11:38:59 +0200

    Fix build
    Signed-off-by: Jason Marechal <jason.marechal+github@gmail.com>

--- src/solver/variable/include/antares/solver/variable/economy/all.h
+++ src/solver/variable/include/antares/solver/variable/economy/all.h
@@ -44,7 +44,6 @@
 #include "dispatchableGeneration.h"
 #include "domesticUnsuppliedEnergy.h"
 #include "dtgMarginAfterCsr.h"
-#include "hydroCost.h"
 #include "hydrostorage.h"
 #include "inflow.h"
 #include "localMatchingRuleViolations.h"
