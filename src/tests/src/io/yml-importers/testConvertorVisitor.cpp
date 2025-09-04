59eb6d794345f0712306d807a028fef944a55724
Florian OMNES <florian.omnes@rte-france.com>
2025-09-04 14:18:39 +0200

    Fix

--- src/tests/src/io/yml-importers/testConvertorVisitor.cpp
+++ src/tests/src/io/yml-importers/testConvertorVisitor.cpp
@@ -30,6 +30,7 @@
 
 using namespace Antares::Expressions;
 using namespace Antares::IO::Inputs;
+namespace utf = boost::unit_test;
 
 class ExpressionToNodeConvertorEmptyModel
 {
@@ -126,7 +127,20 @@ BOOST_AUTO_TEST_CASE(identifierNotFound)
                           expectedMessage);
 }
 
-BOOST_FIXTURE_TEST_CASE(addThreeLiterals, ExpressionToNodeConvertorEmptyModel)
+BOOST_FIXTURE_TEST_CASE(addTwoLiterals, ExpressionToNodeConvertorEmptyModel)
+{
+    const std::string expression = "1 + 2";
+    auto expr = run(expression);
+    BOOST_CHECK_EQUAL(expr.node->name(), "SumNode");
+
+    auto* nodeSum = dynamic_cast<Nodes::SumNode*>(expr.node);
+    BOOST_REQUIRE(nodeSum);
+    const auto& operands = nodeSum->getOperands();
+    BOOST_CHECK_EQUAL(toLiteral(operands[0])->value(), 1);
+    BOOST_CHECK_EQUAL(toLiteral(operands[1])->value(), 2);
+}
+
+BOOST_FIXTURE_TEST_CASE(addThreeLiterals, ExpressionToNodeConvertorEmptyModel, *utf::disabled())
 {
     const std::string expression = "1 + 2 + 3";
     auto expr = run(expression);
