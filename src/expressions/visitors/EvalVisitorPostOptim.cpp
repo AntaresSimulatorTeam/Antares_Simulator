35c5403dcb123e3d5a8c6b4e91d44797b8fe9d97
Vincent Payet <vincent.payet@rte-france.com>
2025-09-10 09:52:19 +0200

    refactor loop over connections

--- src/expressions/visitors/EvalVisitorPostOptim.cpp
+++ src/expressions/visitors/EvalVisitorPostOptim.cpp
@@ -48,40 +48,48 @@ EvaluationResult EvalVisitorPostOptim::visit(const Nodes::PortFieldSumNode* node
 {
     std::string portId = node->getPortName();
     std::string fieldId = node->getFieldName();
-
     auto idxType = Antares::Expressions::Visitors::TimeIndexVisitor(*component_, contextProvider_)
                      .dispatch(node);
-    if (idxType == TimeIndex::CONSTANT_IN_TIME_AND_SCENARIO)
+
+    auto forEachConnection = [this](const std::string& portId,
+                                    const std::string& fieldId,
+                                    std::function<void(const EvaluationResult&)> processor)
     {
-        EvaluationResult result(0.);
         for (const auto connectionEnd: component_->componentConnectionsViaPort(portId))
         {
             auto* component = connectionEnd.component();
             auto* port = connectionEnd.port();
             EvalVisitorPostOptim visitor(contextProvider_, fillContext_, component);
             const Nodes::Node* node = component->nodeAtPortField(port->Id(), fieldId);
-            result += visitor.dispatch(node);
+            auto dispatchResult = visitor.dispatch(node);
+            processor(dispatchResult);
         }
+    };
+
+    if (idxType == TimeIndex::CONSTANT_IN_TIME_AND_SCENARIO)
+    {
+        EvaluationResult result(0.);
+        forEachConnection(portId,
+                          fieldId,
+                          [&result](const auto& dispatchResult) { result += dispatchResult; });
         return result;
     }
-
     std::vector<double> results;
     results.resize(fillContext_.getLocalNumberOfTimeSteps());
-    for (const auto connectionEnd: component_->componentConnectionsViaPort(portId))
-    {
-        auto* component = connectionEnd.component();
-        auto* port = connectionEnd.port();
-        EvalVisitorPostOptim visitor(contextProvider_, fillContext_, component);
-        const Nodes::Node* node = component->nodeAtPortField(port->Id(), fieldId);
-        auto r = visitor.dispatch(node).valuesAsVector();
-        for (auto timeStep = fillContext_.getGlobalFirstTimeStep();
-             timeStep <= fillContext_.getGlobalLastTimeStep();
-             ++timeStep)
-        {
-            logs.notice() << "PortFieldSumNode: adding value " << r[timeStep] << " at index " << timeStep;
-            results[timeStep] += r[timeStep];
-        }
-    }
+    forEachConnection(portId,
+                      fieldId,
+                      [this, &results](const auto& dispatchResult)
+                      {
+                          auto r = dispatchResult.valuesAsVector();
+                          for (auto timeStep = fillContext_.getGlobalFirstTimeStep();
+                               timeStep <= fillContext_.getGlobalLastTimeStep();
+                               ++timeStep)
+                          {
+                              logs.notice() << "PortFieldSumNode: adding value " << r[timeStep]
+                                            << " at index " << timeStep;
+                              results[timeStep] += 20;
+                          }
+                      });
     return EvaluationResult{results};
 }
 
