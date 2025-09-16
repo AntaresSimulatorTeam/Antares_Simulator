7cc9b975743c6b58fd48f73339b897fbe6ab6cf3
guilpier <guillaume.pierre_externe@rte-france.com>
2025-09-16 16:54:58 +0200

    Tests on multi storage remix : set tests environment

---
+++ src/tests/src/solver/simulation/remix-storage/tests-multi-storage-remix.cpp
@@ -0,0 +1,19 @@
+#define BOOST_TEST_MODULE hydro remix
+
+#define WIN32_LEAN_AND_MEAN
+
+#include <boost/test/unit_test.hpp>
+
+#include "antares/solver/simulation/remix-storage/create-storage-for-remix.h"
+#include "antares/solver/simulation/remix-storage/remix-utils.h"
+#include "antares/solver/simulation/remix-storage/shave-peaks-by-remix-storage-gen.h"
+
+
+BOOST_AUTO_TEST_CASE(dummy_test)
+{
+    BOOST_CHECK(true);
+}
+
+
+
+
