0776d24011bd256f4b6cb25ce2509e0374c12e31
Abdoulbari Zakir <abdoulbari.zakir@gmail.com>
2025-10-02 17:53:21 +0200

    fix unit-tests-for-modeler-data-series

--- src/tests/src/study/system-model/test_component.cpp
+++ src/tests/src/study/system-model/test_component.cpp
@@ -20,7 +20,7 @@
  */
 
 #define WIN32_LEAN_AND_MEAN
-#if 0
+
 #include <unit_test_utils.h>
 
 #include <boost/test/unit_test.hpp>
@@ -52,11 +52,10 @@ static Model createModelWithoutParameters()
 
 BOOST_FIXTURE_TEST_SUITE(_Component_, ComponentBuilderCreationFixture)
 
-std::pair<std::string, Antares::Expressions::Visitors::ParameterTypeAndValue>
-build_context_parameter_with(const std::string& id,
-                             const std::string& value,
-                             const Antares::Expressions::Visitors::ParameterType& type = Antares::
-                               Expressions::Visitors::ParameterType::CONSTANT)
+std::pair<std::string, ParameterTypeAndValue> build_context_parameter_with(
+  const std::string& id,
+  const std::string& value,
+  const ParameterType& type = ParameterType::CONSTANT)
 {
     return {id, {.id = id, .type = type, .value = value}};
 }
@@ -345,4 +344,3 @@ BOOST_AUTO_TEST_CASE(successfully_connect_area_to_port)
 }
 
 BOOST_AUTO_TEST_SUITE_END()
-#endif
