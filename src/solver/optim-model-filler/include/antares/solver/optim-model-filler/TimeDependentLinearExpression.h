214720403e52a508ae5e15241bd8f134631a66f1
Florian Omnès <florian.omnes@rte-france.com>
2025-10-09 23:51:33 +0200

    Fixes after review for #3093 (#3124)
    - Split LinearExpression.h / TimeDependentLinearExpression.h, same for
    .cpp
    - Remove `std::variant`
    - Rename `removeDuplicateCoefficients` -> `mergeDuplicateCoefficients`
    - Other minor remarks

---
+++ src/solver/optim-model-filler/include/antares/solver/optim-model-filler/TimeDependentLinearExpression.h
@@ -0,0 +1,78 @@
+/*
+ * Copyright 2007-2024, RTE (https://www.rte-france.com)
+ * See AUTHORS.txt
+ * SPDX-License-Identifier: MPL-2.0
+ * This file is part of Antares-Simulator,
+ * Adequacy and Performance assessment for interconnected energy networks.
+ *
+ * Antares_Simulator is free software: you can redistribute it and/or modify
+ * it under the terms of the Mozilla Public Licence 2.0 as published by
+ * the Mozilla Foundation, either version 2 of the License, or
+ * (at your option) any later version.
+ *
+ * Antares_Simulator is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
+ * Mozilla Public Licence 2.0 for more details.
+ *
+ * You should have received a copy of the Mozilla Public Licence 2.0
+ * along with Antares_Simulator. If not, see <https://opensource.org/license/mpl-2-0/>.
+ */
+#pragma once
+#include <span>
+
+#include "LinearExpression.h"
+
+namespace Antares::Optimization
+{
+class TimeDependentLinearExpression final
+{
+public:
+    explicit TimeDependentLinearExpression(std::size_t nbTimesteps);
+
+    explicit TimeDependentLinearExpression(const std::span<const double>& values);
+
+    // Constant expression
+    explicit TimeDependentLinearExpression(LinearExpression&& expr);
+    explicit TimeDependentLinearExpression(const std::vector<std::pair<int, double>>& coefs,
+                                           double constant);
+
+    std::vector<double> constant() const;
+
+    void mergeDuplicateCoefficients();
+
+    std::size_t size() const;
+
+    bool isConstant() const;
+
+    using iterator = std::vector<LinearExpression>::iterator;
+    iterator begin();
+    iterator end();
+
+    using const_iterator = std::vector<LinearExpression>::const_iterator;
+    const_iterator begin() const;
+    const_iterator end() const;
+
+    LinearExpression& operator[](std::size_t idx);
+
+    const LinearExpression& operator[](std::size_t idx) const;
+
+    TimeDependentLinearExpression& operator+=(const TimeDependentLinearExpression& other);
+
+    TimeDependentLinearExpression& operator-=(const TimeDependentLinearExpression& other);
+
+    void rotate(int shift);
+
+    TimeDependentLinearExpression& operator*=(double factor);
+
+    TimeDependentLinearExpression& operator*=(const TimeDependentLinearExpression& other);
+
+    TimeDependentLinearExpression operator-() const;
+
+private:
+    void expandTo(std::size_t nbTimesteps);
+
+    std::vector<LinearExpression> v_;
+};
+
+} // namespace Antares::Optimization
