dc0f5475416d83abcebfd2d36a587e367f2e0c20
Jason Marechal <jason.marechal+github@gmail.com>
2025-10-13 13:25:52 +0200

    Lold

--- src/solver/variable/include/antares/solver/variable/economy/loldCsr.h
+++ src/solver/variable/include/antares/solver/variable/economy/loldCsr.h
@@ -26,55 +26,10 @@
 */
 #pragma once
 
-#include "economy_base.h"
+#include "lold_base.h"
 
 namespace Antares::Solver::Variable::Economy
 {
-
-struct LOLD_CSRTraits
-{
-    static std::string Caption()
-    {
-        return "LOLD CSR";
-    }
-
-    static std::string Unit()
-    {
-        return "Hours";
-    }
-
-    static std::string Description()
-    {
-        return "LOLD for CSR";
-    }
-
-    typedef Results<R::AllYears::Average< // The average values throughout all years
-      R::AllYears::StdDeviation<          // The standard deviation values throughout all years
-        R::AllYears::Min<                 // The minimum values throughout all years
-          R::AllYears::Max<               // The maximum values throughout all years
-            >>>>>
-      ResultsType;
-
-    static constexpr uint8_t decimal = 4;
-
-    static constexpr uint8_t spatialAggregate = Category::spatialAggregateSum;
-
-    static double value(const State&)
-    {
-        return 1.;
-    }
-
-    static void computeStats(IntermediateValues& iv)
-    {
-        iv.computeStatisticsForTheCurrentYear();
-    }
-
-    static bool checkCondition(const State& state)
-    {
-        return state.hourlyResults->ValeursHorairesDeDefaillancePositiveCSR[state.hourInTheWeek]
-               > 0.5;
-    }
-};
 
 using VCardLOLD_CSR = VCard_Base<LOLD_CSRTraits>;
 
