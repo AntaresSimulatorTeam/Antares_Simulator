0fe9cea3fb328df698a3f3266900288ad62df467
Florian OMNES <florian.omnes@rte-france.com>
2025-10-14 14:52:55 +0200

    Scaffolding

---
+++ src/api/getProblem.cpp
@@ -0,0 +1,7 @@
+#include "antares/api/getproblem.h"
+
+namespace Antares::Solver
+{
+LpsFromAntares getProblem(WeeklyProblemId id);
+
+} // namespace Antares::Solver
---
+++ src/api/include/antares/api/getProblem.h
@@ -0,0 +1,6 @@
+#include "antares/solver/lps/LpsFromAntares.h"
+
+namespace Antares::Solver
+{
+LpsFromAntares getProblem(WeeklyProblemId id);
+} // namespace Antares::Solver
