bf40e76c1ff6628efdfd0d47d0a356f7c0417296
Florian OMNES <florian.omnes@rte-france.com>
2025-10-17 10:34:40 +0200

    Truncate output

--- src/api/main.cpp
+++ src/api/main.cpp
@@ -15,28 +15,13 @@ std::string to_string_any(const T& value)
     return oss.str();
 }
 
-// Base case: find max size among vectors
-template<typename T>
-size_t max_size(const std::vector<T>& v)
-{
-    return v.size();
-}
-
-template<typename T, typename... Rest>
-size_t max_size(const std::vector<T>& v, const Rest&... rest)
-{
-    return std::max(v.size(), max_size(rest...));
-}
-
-// Recursive printing row by row
+// Variadic print function that limits rows to n
 template<typename... Vecs>
-void print_side_by_side(const Vecs&... vecs)
+void print_side_by_side(size_t n, const Vecs&... vecs)
 {
-    size_t n = max_size(vecs...);
     for (size_t i = 0; i < n; ++i)
     {
         size_t col = 0;
-        // Fold expression to print each column
         ((std::cout << (i < vecs.size() ? to_string_any(vecs[i]) : "")
                     << (++col < sizeof...(vecs) ? "\t" : "")),
          ...);
@@ -59,7 +44,14 @@ int main(int argc, char** argv)
     auto constant = getter.getConstantData();
     auto weekly = getter.getWeeklyData({year, week});
 
-    print_side_by_side(constant.VariablesMeaning, weekly.Xmin, weekly.Xmax, weekly.LinearCost);
-    print_side_by_side(constant.ConstraintsMeaning, weekly.Direction, weekly.RHS);
+    print_side_by_side(constant.VariablesCount,
+                       constant.VariablesMeaning,
+                       weekly.Xmin,
+                       weekly.Xmax,
+                       weekly.LinearCost);
+    print_side_by_side(constant.ConstraintesCount,
+                       constant.ConstraintsMeaning,
+                       weekly.Direction,
+                       weekly.RHS);
     return 0;
 }
