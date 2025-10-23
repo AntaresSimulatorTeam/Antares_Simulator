d6d562a3482fdc2437de0db54d201ba9b08c7656
Jason Marechal <jason.marechal+github@gmail.com>
2025-10-23 09:14:40 +0200

    Wip

--- src/tests/src/io/yml-importers/testOptimConfigConverter.cpp
+++ src/tests/src/io/yml-importers/testOptimConfigConverter.cpp
@@ -19,28 +19,4 @@
  * along with Antares_Simulator. If not, see <https://opensource.org/license/mpl-2-0/>.
  */
 
-#pragma once
 
-#include <antares/study/system-model/library.h>
-#include <antares/study/system-model/system.h>
-
-#include "parser.h"
-
-namespace Antares::IO::Inputs::SystemConverter
-{
-
-struct TwoFieldsOfSameRole final: std::invalid_argument
-{
-    using std::invalid_argument::invalid_argument;
-};
-
-struct ConnectingPortToItSelf final: std::invalid_argument
-{
-    using std::invalid_argument::invalid_argument;
-};
-
-ModelerStudy::SystemModel::System convert(
-  const YmlSystem::System& ymlSystem,
-  const std::vector<ModelerStudy::SystemModel::Library>& libraries);
-
-} // namespace Antares::IO::Inputs::SystemConverter
