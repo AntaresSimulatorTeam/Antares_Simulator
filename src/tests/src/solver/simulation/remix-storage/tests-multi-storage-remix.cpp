1461eba90fc3f7585b6c7944fdfcb398743bbb95
Vincent Payet <vincent.payet@rte-france.com>
2025-11-06 11:56:42 +0100

    comment debug in tests

--- src/tests/src/solver/simulation/remix-storage/tests-multi-storage-remix.cpp
+++ src/tests/src/solver/simulation/remix-storage/tests-multi-storage-remix.cpp
@@ -76,10 +76,10 @@ struct InputFixture
         storagesForRemix.push_back(sts_1.createSTS(UnsupE, "sts1"));
         storagesForRemix.push_back(sts_2.createSTS(UnsupE, "sts2"));
 
-        std::stringstream debug;
-        shavePeaksByRemixingStorageGen(Load, UnsupE, Spillage, DTG_MRG, storagesForRemix, &debug);
-        std::ofstream outfile("debug_sts_remix.txt");
-        outfile << debug.str();
+        /*std::stringstream debug;*/
+        shavePeaksByRemixingStorageGen(Load, UnsupE, Spillage, DTG_MRG, storagesForRemix/*, &debug*/);
+        /*std::ofstream outfile("debug_sts_remix.txt");*/
+        /*outfile << debug.str();*/
     }
 
     STS_holder<nb_hours> sts_1;
