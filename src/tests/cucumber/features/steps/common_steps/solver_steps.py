8e8da3d52053fc46a29c6c86c4ee1df9359d4819
Jason Marechal <jason.marechal+github@gmail.com>
2025-11-14 14:06:12 +0100

    Revert "Add message to error"
    This reverts commit 047f182849964ab1d4eef3af0c45f02afb3a52db.

--- src/tests/cucumber/features/steps/common_steps/solver_steps.py
+++ src/tests/cucumber/features/steps/common_steps/solver_steps.py
@@ -120,7 +120,7 @@ def get_quadratic_solver(context) -> str:
 
 @then('the simulation takes less than {seconds:g} seconds')
 def check_simu_time(context, seconds):
-    assert context.soh.get_simu_time() <= seconds, f"Simulation time {context.soh.get_simu_time()} exceeds {seconds} seconds"
+    assert context.soh.get_simu_time() <= seconds
 
 
 @then('in area "{area}", during year {year:d}, loss of load lasts {lold_hours:d} hours')
