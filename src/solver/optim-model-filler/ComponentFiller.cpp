6377ccbacd1d877eb9e9d2a10030e83d0423376f
Jason Marechal <jason.marechal+github@gmail.com>
2025-11-27 10:23:57 +0100

    fix: use declared variable

--- src/solver/optim-model-filler/ComponentFiller.cpp
+++ src/solver/optim-model-filler/ComponentFiller.cpp
@@ -417,8 +417,7 @@ void ComponentFiller::addObjectives(const LinearProblemApi::FillContext& ctx)
     for (const auto& objective: model->Objectives() | locationFilter())
     {
         const auto root_node = objective.expression().RootNode();
-        const auto linearExpression = visitor.visitMergeDuplicates(
-          objective.expression().RootNode());
+        const auto linearExpression = visitor.visitMergeDuplicates(root_node);
 
         const auto timeIndex = getConstraintTimeIndex(root_node, component_);
         if (timeIndex == TimeIndex::VARYING_IN_TIME_ONLY
