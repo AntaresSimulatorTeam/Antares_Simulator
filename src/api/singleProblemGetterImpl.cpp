e05dbb52755c7835dcf7d0f4ad085a422bd1bfe6
Abdoulbari Zakir <abdoulbari.zakir@gmail.com>
2025-12-22 10:42:11 +0100

    update

--- src/api/singleProblemGetterImpl.cpp
+++ src/api/singleProblemGetterImpl.cpp
@@ -58,7 +58,7 @@ std::unique_ptr<Antares::Data::Study> loadStudy(const std::filesystem::path& stu
 
 std::string problemName(const WeeklyProblemId& id)
 {
-    return fmt::format("problem-{}-{}.txt", id.year, id.week);
+    return fmt::format("problem-{}-{}", id.year, id.week);
 }
 } // namespace
 
