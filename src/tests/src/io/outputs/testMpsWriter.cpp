3223e695b311ecbe734962e1318e206788819554
Abdoulbari Zakir <abdoulbari.zakir@gmail.com>
2025-12-24 14:10:31 +0100

    upd

--- src/tests/src/io/outputs/testMpsWriter.cpp
+++ src/tests/src/io/outputs/testMpsWriter.cpp
@@ -21,7 +21,7 @@
 
 #define WIN32_LEAN_AND_MEAN
 
-#include <algorithm>
+#include  <algorithm>
 #include <ortools/linear_solver/linear_solver.h>
 #include <ortools/linear_solver/wrappers/model_builder_helper.h>
 
