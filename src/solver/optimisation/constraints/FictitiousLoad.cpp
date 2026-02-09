a770e80894cf10d763f7e378706d50097a1aafc7
payetvin <payetvin@rte-france.com>
2026-02-09 13:54:37 +0100

    Add sts prod to fictitious load

--- src/solver/optimisation/constraints/FictitiousLoad.cpp
+++ src/solver/optimisation/constraints/FictitiousLoad.cpp
@@ -18,6 +18,8 @@ void FictitiousLoad::add(int pdt, int pays)
     ExportPaliers(data.PaliersThermiquesDuPays[pays], builder);
     auto coeff = data.DefaillanceNegativeUtiliserHydro[pays] ? -1 : 0;
     builder.HydProd(pays, coeff).NegativeUnsuppliedEnergy(pays, 1.0);
+    builder.ShortTermStorageWithdrawal(pays, coeff);
+    builder.ShortTermStorageInjection(pays, -coeff);
 
     builder.lessThan();
     builder.build();
