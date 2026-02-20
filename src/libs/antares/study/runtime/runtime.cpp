2e240d0ba30092cdb990363d28461806ba9df032
payetvin <payetvin@rte-france.com>
2026-02-20 16:05:41 +0100

    re-add warning modeler

--- src/libs/antares/study/runtime/runtime.cpp
+++ src/libs/antares/study/runtime/runtime.cpp
@@ -124,9 +124,9 @@ void StudyRuntimeInfos::initializeRangeLimits(const Study& study, StudyRangeLimi
             // We have here too much hours, the interval will be reduced
             // Log Entry
             logs.warning() << "    Partial week detected. Not allowed in "
-                        << SimulationModeToCString(study.parameters.mode);
+                           << SimulationModeToCString(study.parameters.mode);
             logs.warning() << "    Time interval that has been requested: " << (1 + a) << ".."
-                        << (1 + b);
+                           << (1 + b);
             // Reducing
             while (b > a and 0 != ((b - a + 1) % 168))
             {
