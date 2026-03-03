fc8f9a5096023bdd18e7c6438432088b3a632d69
payetvin <payetvin@rte-france.com>
2026-03-03 15:44:16 +0100

    better exception

--- src/solver/optim-model-filler/ReadLinearExpressionVisitor.cpp
+++ src/solver/optim-model-filler/ReadLinearExpressionVisitor.cpp
@@ -4,6 +4,7 @@
 #include "antares/solver/optim-model-filler/ReadLinearExpressionVisitor.h"
 
 #include <cmath>
+#include <stdexcept>
 
 #include <antares/expressions/visitors/NodeVisitor.h>
 #include <antares/optimisation/linear-problem-api/ILinearProblemData.h>
@@ -11,6 +12,7 @@
 #include "antares/exception/InvalidArgumentError.hpp"
 #include "antares/expressions/nodes/ExpressionsNodes.h"
 #include "antares/expressions/visitors/HelpVisitNode.h"
+#include "antares/expressions/visitors/PrintVisitor.h"
 #include "antares/modeler-optimisation-container/OptimEntityContainer.h"
 #include "antares/study/system-model/component.h"
 
@@ -43,6 +45,19 @@ ReadLinearExpressionVisitor::ReadLinearExpressionVisitor(
     nbtimeSteps_(fillContext.getLocalNumberOfTimeSteps())
 {
 }
+
+class TimeIndexOutOfRangeException: public std::out_of_range
+{
+public:
+    TimeIndexOutOfRangeException(unsigned timeIndex, const std::string& expr, std::size_t size):
+        std::out_of_range(fmt::format(
+          "TimeIndexNode: index {} is out of range in expression '{}' (expression size: {})",
+          timeIndex,
+          expr,
+          size))
+    {
+    }
+};
 
 TimeDependentLinearExpression ReadLinearExpressionVisitor::visitMergeDuplicates(
   const Nodes::Node* node)
@@ -217,10 +232,8 @@ TimeDependentLinearExpression ReadLinearExpressionVisitor::visit(const Nodes::Ti
 
     if (timeIndex < 0 || static_cast<std::size_t>(timeIndex) >= expression.size())
     {
-        throw std::runtime_error(
-          fmt::format("TimeIndexNode: index {} is out of range (expression size: {})",
-                      timeIndex,
-                      expression.size()));
+        Expressions::Visitors::PrintVisitor visitor;
+        throw TimeIndexOutOfRangeException(timeIndex, visitor.dispatch(node), expression.size());
     }
     return TimeDependentLinearExpression(std::move(expression[timeIndex]));
 }
