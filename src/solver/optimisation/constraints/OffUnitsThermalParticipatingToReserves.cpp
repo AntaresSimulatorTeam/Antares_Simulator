1328533d022b9e130513a49051fac68fe3d75a6f
sylvmara <symarandon@gmail.com>
2026-03-09 16:21:10 +0100

    Fix : updated licences

--- src/solver/optimisation/constraints/OffUnitsThermalParticipatingToReserves.cpp
+++ src/solver/optimisation/constraints/OffUnitsThermalParticipatingToReserves.cpp
@@ -1,3 +1,6 @@
+// Copyright 2007-2026, RTE (https://www.rte-france.com)
+// SPDX-License-Identifier: MPL-2.0
+
 #include "antares/solver/optimisation/constraints/OffUnitsThermalParticipatingToReserves.h"
 
 void OffUnitsThermalParticipatingToReserves::add(int pays, int reserve, int cluster, int pdt)
--- src/solver/optimisation/constraints/POutBounds.cpp
+++ src/solver/optimisation/constraints/POutBounds.cpp
@@ -1,3 +1,6 @@
+// Copyright 2007-2026, RTE (https://www.rte-france.com)
+// SPDX-License-Identifier: MPL-2.0
+
 #include "antares/solver/optimisation/constraints/POutBounds.h"
 
 void POutBounds::add(int pays, int cluster, int pdt)
