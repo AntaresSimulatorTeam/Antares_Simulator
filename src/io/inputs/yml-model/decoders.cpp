3c65c75b3f04ee43023a9aafe211cd7f3940f149
Jason Marechal <jason.marechal@pm.me>
2026-03-25 13:29:31 +0100

    refactor(decoders): move printPathTree to decoders_utils * Relocate printPathTree function for better organization * Remove unused decode function from decoders.cpp * Update CMakeLists.txt to include decoders_utils.cpp

--- src/io/inputs/yml-model/decoders.cpp
+++ src/io/inputs/yml-model/decoders.cpp
@@ -5,30 +5,6 @@
 
 #include <unordered_set>
 
-std::string printPathTree(const std::filesystem::path& p)
-{
-    std::string treeStr;
-    std::size_t depth = 0;
-    for (const auto& part: p)
-    {
-        if (depth == 0)
-        {
-            treeStr += part.string();
-            treeStr += '\n';
-        }
-        else
-        {
-            treeStr += std::string((depth - 1) * 4, ' ');
-            // "└── " is a u8 and it does not display correctly
-            treeStr += "|__ ";
-            treeStr += part.string();
-            treeStr += '\n';
-        }
-        ++depth;
-    }
-    return treeStr;
-}
-
 namespace YAML
 {
 
@@ -362,15 +338,4 @@ bool convert<Antares::IO::Inputs::YmlModel::Model>::decode(
     return true;
 }
 
-bool convert<Antares::IO::Inputs::YmlModel::Library>::decode(
-  const Node& node,
-  Antares::IO::Inputs::YmlModel::Library& rhs)
-{
-    rhs.id = node["id"].as<std::string>();
-    rhs.description = node["description"].as<std::string>("");
-    rhs.port_types = as_fallback_default<std::vector<Antares::IO::Inputs::YmlModel::PortType>>(
-      node["port-types"]);
-    rhs.models = node["models"].as<std::vector<Antares::IO::Inputs::YmlModel::Model>>();
-    return true;
-}
 } // namespace YAML
--- src/io/inputs/yml-model/decoders_utils.cpp
+++ src/io/inputs/yml-model/decoders_utils.cpp
@@ -1,10 +1,30 @@
 // Copyright 2007-2026, RTE (https://www.rte-france.com)
 // SPDX-License-Identifier: MPL-2.0
-
-#include <mutex>
-#include <unordered_set>
-
 #include "antares/io/inputs/yml-model/decoders.h"
+
+std::string printPathTree(const std::filesystem::path& p)
+{
+    std::string treeStr;
+    std::size_t depth = 0;
+    for (const auto& part: p)
+    {
+        if (depth == 0)
+        {
+            treeStr += part.string();
+            treeStr += '\n';
+        }
+        else
+        {
+            treeStr += std::string((depth - 1) * 4, ' ');
+            // "└── " is a u8 and it does not display correctly
+            treeStr += "|__ ";
+            treeStr += part.string();
+            treeStr += '\n';
+        }
+        ++depth;
+    }
+    return treeStr;
+}
 
 // Implement convert<Library>::decode in this translation unit to ensure all
 // other convert<> specializations (e.g., Model) are available and avoid
