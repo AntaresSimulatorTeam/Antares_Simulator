df03f1719c3d33ce530ccb78a0d77aa28f5951b9
Florian OMNES <florian.omnes@rte-france.com>
2026-03-31 14:18:25 +0200

    format

--- src/tests/src/solver/variable/test_setofareas_reports.cpp
+++ src/tests/src/solver/variable/test_setofareas_reports.cpp
@@ -234,10 +234,10 @@ BOOST_AUTO_TEST_CASE(exports_one_enabled_district)
     const auto& files = writer.getMap();
     const auto fileIt = files.find("out/areas/@ district-1/values-hourly.txt");
     BOOST_REQUIRE(fileIt != files.end());
-    BOOST_CHECK_NE(fileIt->second.find("\tGROUP1_TH_PROD\tGROUP1_TH_PROD\tGROUP1_TH_PROD\tGROUP1_TH_PROD"),
+    BOOST_CHECK_NE(fileIt->second.find(
+                     "\tGROUP1_TH_PROD\tGROUP1_TH_PROD\tGROUP1_TH_PROD\tGROUP1_TH_PROD"),
                    std::string::npos);
-    BOOST_CHECK_NE(fileIt->second.find("\tEXP\tstd\tmin\tmax"),
-                   std::string::npos);
+    BOOST_CHECK_NE(fileIt->second.find("\tEXP\tstd\tmin\tmax"), std::string::npos);
 }
 
 BOOST_AUTO_TEST_CASE(exports_two_enabled_districts)
@@ -259,15 +259,13 @@ BOOST_AUTO_TEST_CASE(exports_two_enabled_districts)
     BOOST_CHECK_NE(firstFileIt->second.find(
                      "\tGROUP1_TH_PROD\tGROUP1_TH_PROD\tGROUP1_TH_PROD\tGROUP1_TH_PROD"),
                    std::string::npos);
-    BOOST_CHECK_NE(firstFileIt->second.find("\tEXP\tstd\tmin\tmax"),
-                   std::string::npos);
+    BOOST_CHECK_NE(firstFileIt->second.find("\tEXP\tstd\tmin\tmax"), std::string::npos);
     const auto secondFileIt = files.find("out/areas/@ district-2/values-hourly.txt");
     BOOST_REQUIRE(secondFileIt != files.end());
     BOOST_CHECK_NE(secondFileIt->second.find(
                      "\tGROUP1_TH_PROD\tGROUP1_TH_PROD\tGROUP1_TH_PROD\tGROUP1_TH_PROD"),
                    std::string::npos);
-    BOOST_CHECK_NE(secondFileIt->second.find("\tEXP\tstd\tmin\tmax"),
-                   std::string::npos);
+    BOOST_CHECK_NE(secondFileIt->second.find("\tEXP\tstd\tmin\tmax"), std::string::npos);
 }
 
 BOOST_AUTO_TEST_CASE(skips_disabled_district_and_exports_enabled_one)
@@ -287,10 +285,10 @@ BOOST_AUTO_TEST_CASE(skips_disabled_district_and_exports_enabled_one)
     BOOST_CHECK(files.find("out/areas/@ district-disabled/values-hourly.txt") == files.end());
     const auto fileIt = files.find("out/areas/@ district-enabled/values-hourly.txt");
     BOOST_REQUIRE(fileIt != files.end());
-    BOOST_CHECK_NE(fileIt->second.find("\tGROUP1_TH_PROD\tGROUP1_TH_PROD\tGROUP1_TH_PROD\tGROUP1_TH_PROD"),
+    BOOST_CHECK_NE(fileIt->second.find(
+                     "\tGROUP1_TH_PROD\tGROUP1_TH_PROD\tGROUP1_TH_PROD\tGROUP1_TH_PROD"),
                    std::string::npos);
-    BOOST_CHECK_NE(fileIt->second.find("\tEXP\tstd\tmin\tmax"),
-                   std::string::npos);
+    BOOST_CHECK_NE(fileIt->second.find("\tEXP\tstd\tmin\tmax"), std::string::npos);
 }
 
 BOOST_AUTO_TEST_SUITE_END()
