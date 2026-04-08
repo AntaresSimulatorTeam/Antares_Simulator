623c42db73dc30598844ee006d3ccae543f3a435
Florian OMNES <florian.omnes@rte-france.com>
2026-04-08 10:51:21 +0200

    Fix linux build

--- src/libs/antares/writer/include/antares/writer/table_writer_factory.h
+++ src/libs/antares/writer/include/antares/writer/table_writer_factory.h
@@ -4,10 +4,12 @@
 #pragma once
 
 #include "i_table_writer.h"
+#include "simulation-table-writers_export.h"
 
 namespace Antares::Writer
 {
 // gp : here we return a shared ptr. Why not rather a unique ptr ?
-ITableWriter::Ptr makeTableWriter(bool parquetFormatRequired, std::filesystem::path& filePath);
+SIMULATION_TABLE_WRITERS_EXPORT ITableWriter::Ptr makeTableWriter(bool parquetFormatRequired,
+                                                                  std::filesystem::path& filePath);
 
 } // namespace Antares::Writer
