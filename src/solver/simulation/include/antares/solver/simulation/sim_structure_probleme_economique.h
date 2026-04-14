207890df083870fda01bee7e1d3b618fa3b05987
h-fournier <hugo.fournier@scalian.com>
2026-04-14 17:36:24 +0200

    Change pb_.OptimisationAvecCoutsDeDemarrage with the new flag OptimisationNotFastMode

--- src/solver/simulation/include/antares/solver/simulation/sim_structure_probleme_economique.h
+++ src/solver/simulation/include/antares/solver/simulation/sim_structure_probleme_economique.h
@@ -540,7 +540,6 @@ struct PROBLEME_HEBDO
     char TypeDeLissageHydraulique = PAS_DE_LISSAGE_HYDRAULIQUE;
     bool WaterValueAccurate = false;
     bool OptimisationNotFastMode = false;
-    bool OptimisationAvecCoutsDeDemarrage = false;
     bool OptimisationAvecVariablesEntieres = false;
     uint32_t NombreDePays = 0;
     std::vector<const char*> NomsDesPays;
