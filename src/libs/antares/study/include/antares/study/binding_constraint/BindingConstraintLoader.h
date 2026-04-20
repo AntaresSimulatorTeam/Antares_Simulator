7969facb46d5cb352b80d273cbbe6411792953ee
LISASI Josue (Externe) <lisasijos@gm0winl1074.bureau.si.interne>
2026-04-20 17:50:07 +0200

    fix(solver): added and return if bindingconstraints not enabled and refactor load function

--- src/libs/antares/study/include/antares/study/binding_constraint/BindingConstraintLoader.h
+++ src/libs/antares/study/include/antares/study/binding_constraint/BindingConstraintLoader.h
@@ -36,6 +36,18 @@ private:
     bool loadTimeSeries(EnvForLoading& env,
                         BindingConstraint::Operator operatorType,
                         BindingConstraint* bindingConstraint) const;
+
+    void populateConstraint(EnvForLoading& env, std::shared_ptr<BindingConstraint>& bc);
+
+    void parseWeightAndOffset(EnvForLoading& env,
+                            const IniFile::Property* p,
+                            std::shared_ptr<BindingConstraint>& bc);
+
+                            bool validate(EnvForLoading& env, const std::shared_ptr<BindingConstraint>& bc);
+
+     std::vector<std::shared_ptr<BindingConstraint>> loadByOperator(
+        EnvForLoading& env,
+        std::shared_ptr<BindingConstraint>& bc);
 };
 
 } // namespace Antares::Data
