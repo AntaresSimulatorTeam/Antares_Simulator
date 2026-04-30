a8212dde027c0ca65c8467e57692ef4c2e55b935
sylvmara <symarandon@gmail.com>
2026-04-30 10:17:35 +0200

    Attempt to pass ubuntu CI

--- src/solver/variable/include/antares/solver/variable/economy/rampingCostByDispatchablePlant.h
+++ src/solver/variable/include/antares/solver/variable/economy/rampingCostByDispatchablePlant.h
@@ -273,7 +273,7 @@ public:
       unsigned int,
       unsigned int numSpace) const
     {
-        return pValuesForTheCurrentYear[numSpace]->hour;
+        return pValuesForTheCurrentYear[numSpace].hour;
     }
 
     void localBuildAnnualSurveyReport(SurveyResults& results,
