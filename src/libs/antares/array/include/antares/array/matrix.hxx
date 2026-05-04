7d9feae377ea6ec704e0b94fe0acb71b3e829630
Florian OMNES <florian.omnes@rte-france.com>
2026-05-04 12:50:26 +0200

    Try to fix OL8's double free

--- src/libs/antares/array/include/antares/array/matrix.hxx
+++ src/libs/antares/array/include/antares/array/matrix.hxx
@@ -238,6 +238,10 @@ Matrix<T, ReadWriteT>::Matrix(const Matrix<T, ReadWriteT>& rhs):
 
 template<class T, class ReadWriteT>
 Matrix<T, ReadWriteT>::Matrix(Matrix<T, ReadWriteT>&& rhs) noexcept
+    : width(0)
+    , height(0)
+    , entry(nullptr)
+    , jit(nullptr)
 {
     // use Matrix::operator=(Matrix&& rhs)
     *this = std::move(rhs);
@@ -1512,6 +1516,11 @@ inline Matrix<T, ReadWriteT>& Matrix<T, ReadWriteT>::operator=(const Matrix<T, R
 template<class T, class ReadWriteT>
 inline Matrix<T, ReadWriteT>& Matrix<T, ReadWriteT>::operator=(Matrix<T, ReadWriteT>&& rhs) noexcept
 {
+    if (this == &rhs)
+    {
+        return *this;
+    }
+
     // Free existing resources before taking new ones
     if (entry && entry != rhs.entry)
     {
