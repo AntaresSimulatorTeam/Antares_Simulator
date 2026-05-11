f1bd08e59176b1299272d042ae317e9190810ebb
payetvin <payetvin_externe@rte-france.com>
2026-05-11 11:38:22 +0200

    comment

--- src/io/outputs/MPSGenerator.cpp
+++ src/io/outputs/MPSGenerator.cpp
@@ -295,7 +295,7 @@ void MPSGenerator::writeBounds(std::string& mps) const
 
         if (lbIsZero && ubIsPlusInfinity) // this case is the default
         {
-            // explicit bounds to avoid upper bound interpreted as 1 by some solver (binary variable)
+            // explicit bounds to avoid upper bound interpreted as 1 by some solver (binary vars)
             if (isInt)
             {
                 fmt::format_to(std::back_inserter(mps), "{}LI {} {} {}\n", pad, bnd, varName, lb);
