4c7a6b0905a864923695386c8bb6d234e00b265c
payetvin <payetvin_externe@rte-france.com>
2026-05-12 17:34:14 +0200

    generate

--- src/solver/workflow/generationAndResolutionConfig.cpp
+++ src/solver/workflow/generationAndResolutionConfig.cpp
@@ -3,13 +3,57 @@
 
 #include "antares/solver/workflow/generationAndResolutionConfig.h"
 
+#include <antares/solver/optimisation/MipDetection.h>
+#include <antares/study/system-model/component.h>
+#include <antares/study/system-model/model.h>
+#include <antares/study/system-model/optimConfig.h>
+
 namespace Antares::Solver::Workflow
 {
 
-GenerationAndResolutionConfig generate(const Data::Study& /*study*/)
+namespace
 {
+
+bool hasMasterIntegerVariables(const Solver::ModelerData* modelerData)
+{
+    if (!modelerData || !modelerData->system)
+        return false;
+
+    for (const auto& component : modelerData->system->Components())
+    {
+        for (const auto& variable : component.getModel()->Variables())
+        {
+            auto loc = variable.location();
+            if (loc != Solver::Config::Location::MASTER
+                && loc != Solver::Config::Location::MASTER_AND_SUBPROBLEMS)
+                continue;
+            if (variable.Type() != ModelerStudy::SystemModel::ValueType::FLOAT)
+                return true;
+        }
+    }
+    return false;
+}
+
+} // anonymous namespace
+
+GenerationAndResolutionConfig generate(const Data::Study& study)
+{
+    auto* modelerData = study.getModelerData();
+    const auto ucMode = study.parameters.unitCommitment.ucMode;
+
+    auto resMode = modelerData->resolutionMode;
+    bool heuristic = (ucMode == Data::ucHeuristicFast
+                      || ucMode == Data::ucHeuristicAccurate);
+    bool subpMilp = (ucMode == Data::ucMILP
+                     || Antares::Optimization::hasModelerIntegerVariables(modelerData));
+
     GenerationAndResolutionConfig config;
-    // TODO: fill from study
+    config.resolution_mode = resMode;
+    config.use_heuristic = heuristic;
+    config.subproblems = subpMilp ? SolverType::MILP : SolverType::LP;
+    config.master = (resMode == ResolutionMode::BENDERS_DECOMPOSITION)
+                        ? (hasMasterIntegerVariables(modelerData) ? SolverType::MILP : SolverType::LP)
+                        : SolverType::MILP;
     return config;
 }
 
