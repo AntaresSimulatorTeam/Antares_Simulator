56d122f4866657edfafa1bfb599f0b6cd2189dba
payetvin <payetvin_externe@rte-france.com>
2026-05-12 16:29:14 +0200

    base for workflow

---
+++ src/solver/workflow/generationAndResolutionConfig.cpp
@@ -0,0 +1,16 @@
+// Copyright 2007-2026, RTE (https://www.rte-france.com)
+// SPDX-License-Identifier: MPL-2.0
+
+#include "antares/solver/workflow/generationAndResolutionConfig.h"
+
+namespace Antares::Solver::Workflow
+{
+
+GenerationAndResolutionConfig generate(const Data::Study& /*study*/)
+{
+    GenerationAndResolutionConfig config;
+    // TODO: fill from study
+    return config;
+}
+
+} // namespace Antares::Solver::Workflow
---
+++ src/solver/workflow/include/antares/solver/workflow/generationAndResolutionConfig.h
@@ -0,0 +1,28 @@
+// Copyright 2007-2026, RTE (https://www.rte-france.com)
+// SPDX-License-Identifier: MPL-2.0
+
+#pragma once
+
+#include <antares/solver/modeler/ModelerData.h>
+#include <antares/study/study.h>
+
+namespace Antares::Solver::Workflow
+{
+
+enum class SolverType : unsigned
+{
+    LP,
+    MILP
+};
+
+struct GenerationAndResolutionConfig
+{
+    SolverType master = SolverType::MILP;
+    SolverType subproblems = SolverType::MILP;
+    bool use_heuristic = false;
+    ResolutionMode resolution_mode = ResolutionMode::SEQUENTIAL_SUBPROBLEMS;
+};
+
+GenerationAndResolutionConfig generate(const Data::Study& study);
+
+} // namespace Antares::Solver::Workflow
