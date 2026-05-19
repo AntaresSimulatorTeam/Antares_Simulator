fbc146287d0a67b7c6ae5bb7c0f6cd8987c84893
sylvmara <symarandon@gmail.com>
2026-05-19 11:54:12 +0200

    Modification of constraints 18 bis and ter

--- src/solver/optimisation/constraints/RampingIncreaseRate.cpp
+++ src/solver/optimisation/constraints/RampingIncreaseRate.cpp
@@ -13,30 +13,32 @@ void RampingIncreaseRate::add(int pays, int index, int pdt)
                                              .maxUpwardPowerRampingRate[index];
         double pmaxDUnGroupeDuPalierThermique = data.PaliersThermiquesDuPays[pays]
                                                   .PmaxDUnGroupeDuPalierThermique[index];
-        double multiplicateurStartingDispatchableUnits = -pmaxDUnGroupeDuPalierThermique
-                                                         + maxUpwardPowerRampingRate;
+        double pminDUnGroupeDuPalierThermique = data.PaliersThermiquesDuPays[pays]
+                                                  .pminDUnGroupeDuPalierThermique[index];
         // 18 (bis)
-        // Equation : P(t) <= P(t-1) - ((R^+) * M(t)) + (u * M^+(t))
+        // Equation : P(t) <= P(t-1) - ((R^+) * (M(t) - M^+(t))) + (u * M^+(t)) - (l * M^-(t))
         // P(t) : Power output of the cluster at timestep t
         // R^+ : Max ramping up for cluster
         // M(t) : number of running units
         // u : maximum unit power output when running
+        // l : minimum unit power output when running
         // M^+(t) : number of starting units this timestep
-        // constraint : P(t) - P(t-1) - ((R^+) * M(t)) - u * M^+(t) < 0
-
-        // Modif : commentaire à merger au dessus
-        // Equation : P(t) <= P(t-1) - ((R^+) * M(t-1)) + (u * M^+(t))
-        // constraint : P(t) - P(t-1) - ((R^+) * M(t)) - ((u-R^+) * M^+(t)) < 0
+        // M^-(t) : number of stopping units this timestep
+        // constraint : P(t) - P(t-1) - ((R^+) * M(t)) - ((u - R^+) * M^+(t)) + (l * M^-(t)) < 0
 
         builder.updateHourWithinWeek(pdt)
-          .DispatchableProduction(cluster, 1.0)
+          .DispatchableProduction(cluster, 1.0) // P(t)
           .DispatchableProduction(cluster,
                                   -1.0,
                                   -1,
-                                  builder.data.NombreDePasDeTempsPourUneOptimisation)
-          .NumberOfDispatchableUnits(cluster, -maxUpwardPowerRampingRate)
-          .NumberStartingDispatchableUnits(cluster, multiplicateurStartingDispatchableUnits)
-          .lessThan();
+                                  builder.data.NombreDePasDeTempsPourUneOptimisation) // - P(t-1)
+          .NumberOfDispatchableUnits(cluster, -maxUpwardPowerRampingRate) // - ((R^+) * M(t))
+          .NumberStartingDispatchableUnits(
+            cluster,
+            maxUpwardPowerRampingRate - pmaxDUnGroupeDuPalierThermique) // ((u - R^+) * M^+(t))
+          .NumberStoppingDispatchableUnits(cluster,
+                                           pminDUnGroupeDuPalierThermique) // + (l * M^-(t))
+          .lessThan();                                                     // < 0
 
         if (builder.NumberOfVariables() > 0)
         {
