32a07d518874ce14416068572587c22320265381
Josue Lisasi <josue@ignifai.net>
2026-05-22 12:09:36 +0200

    fixed issue for sum(t+a..t+b) when a >b

--- src/expressions/visitors/EvalVisitor.cpp
+++ src/expressions/visitors/EvalVisitor.cpp
@@ -425,11 +425,13 @@ EvaluationResult EvaluationResult::timeSumOnVector(int from, int to) const
 {
     const std::vector<double> values = valuesAsVector(); // Exception throw if value_ not a vector
     std::vector<double> to_return(values.size(), 0.);
-
-    for (int shift = from; shift <= to; ++shift)
+    if (from <= to)
     {
-        std::vector<double> shifted_values = shiftVector(values, shift);
-        to_return += shifted_values;
+        for (int shift = from; shift <= to; ++shift)
+        {
+            std::vector<double> shifted_values = shiftVector(values, shift);
+            to_return += shifted_values;
+        }
     }
     return EvaluationResult(to_return);
 }
