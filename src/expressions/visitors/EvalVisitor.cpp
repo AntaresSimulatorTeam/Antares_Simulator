d8bec2e5d4be9c12b0523e303f86c61a4927eb26
Josue Lisasi <josue@ignifai.net>
2026-05-26 16:52:25 +0200

    removed unnecassary if loop

--- src/expressions/visitors/EvalVisitor.cpp
+++ src/expressions/visitors/EvalVisitor.cpp
@@ -425,13 +425,10 @@ EvaluationResult EvaluationResult::timeSumOnVector(int from, int to) const
 {
     const std::vector<double> values = valuesAsVector(); // Exception throw if value_ not a vector
     std::vector<double> to_return(values.size(), 0.);
-    if (from <= to)
+    for (int shift = from; shift <= to; ++shift)
     {
-        for (int shift = from; shift <= to; ++shift)
-        {
-            std::vector<double> shifted_values = shiftVector(values, shift);
-            to_return += shifted_values;
-        }
+        std::vector<double> shifted_values = shiftVector(values, shift);
+        to_return += shifted_values;
     }
     return EvaluationResult(to_return);
 }
