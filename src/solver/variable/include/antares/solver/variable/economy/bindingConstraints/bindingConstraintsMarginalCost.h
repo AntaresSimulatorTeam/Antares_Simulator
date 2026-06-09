6c6b60129086c24b4b9b817dd52f469aab452f93
payetvin <payetvin_externe@rte-france.com>
2026-06-09 14:04:25 +0200

    format

--- src/solver/variable/include/antares/solver/variable/economy/bindingConstraints/bindingConstraintsMarginalCost.h
+++ src/solver/variable/include/antares/solver/variable/economy/bindingConstraints/bindingConstraintsMarginalCost.h
@@ -265,7 +265,9 @@ public:
     }
 
     template<class SearchVCardT, class O>
-    static void computeSpatialAggregateWith(O& /*out*/, const Data::Area* /*area*/, uint /*numSpace*/)
+    static void computeSpatialAggregateWith(O& /*out*/,
+                                            const Data::Area* /*area*/,
+                                            uint /*numSpace*/)
     {
     }
 
