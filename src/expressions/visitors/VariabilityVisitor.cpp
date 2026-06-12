dd2ca2d23b1b26589bea61a12f4c0579b0ed5703
payetvin <payetvin_externe@rte-france.com>
2026-06-12 13:02:24 +0200

    fix variability

--- src/expressions/visitors/VariabilityVisitor.cpp
+++ src/expressions/visitors/VariabilityVisitor.cpp
@@ -179,6 +179,10 @@ VariabilityType VariabilityVisitor::visit(const Nodes::FunctionNode* node)
     case Nodes::FunctionNodeType::floor:
     case Nodes::FunctionNodeType::ceil:
         return visitChildrenNodes(node);
+    case Nodes::FunctionNodeType::round:
+        return visitChildrenNodes(node);
+    case Nodes::FunctionNodeType::abs:
+        return visitChildrenNodes(node);
     case Nodes::FunctionNodeType::pow:
         return visitPow(node);
     default:
