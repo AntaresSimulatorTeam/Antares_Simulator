bcc465dcef1f86ff3ebec783f0b45e23a203e209
Jason Marechal <jason.marechal+github@gmail.com>
2026-06-16 13:02:09 +0200

    refacto: sorties design

--- src/solver/variable/include/antares/solver/variable/economy/rampingCosts.h
+++ src/solver/variable/include/antares/solver/variable/economy/rampingCosts.h
@@ -1,262 +1,75 @@
 // Copyright 2007-2026, RTE (https://www.rte-france.com)
 // SPDX-License-Identifier: MPL-2.0
 
-#ifndef __SOLVER_VARIABLE_ECONOMY_RampingCost_H__
-#define __SOLVER_VARIABLE_ECONOMY_RampingCost_H__
+#pragma once
 
-#include "antares/solver/variable/variable.h"
+#include "economy_base.h"
 
 namespace Antares::Solver::Variable::Economy
 {
-struct VCardRampingCost
+struct RampingCostTraits
 {
-    //! Caption
     static std::string Caption()
     {
         return "RAMP COST";
     }
 
-    //! Unit
     static std::string Unit()
     {
         return "Euro";
     }
 
-    //! The short description of the variable
     static std::string Description()
     {
         return "Ramping Cost throughout all MC years, of all the thermal dispatchable "
                "clusters";
     }
 
-    //! The expecte results
-    typedef Results<R::AllYears::Average<        // The average values throughout all years
-                      R::AllYears::StdDeviation< // The standard deviation values throughout all
-                                                 // years
-                        R::AllYears::Min<        // The minimum values throughout all years
-                          R::AllYears::Max<      // The maximum values throughout all years
-                            >>>>,
-                    R::AllYears::Average // The
-                    >
-      ResultsType;
+    using ResultsType = Results<
+      R::AllYears::Average<R::AllYears::StdDeviation<R::AllYears::Min<R::AllYears::Max<>>>>,
+      R::AllYears::Average>;
 
-    //! The VCard to look for for calculating spatial aggregates
-    typedef VCardRampingCost VCardForSpatialAggregate;
-
-    //! Data Level
-    static constexpr uint8_t categoryDataLevel = Category::DataLevel::area;
-    //! File level (provided by the type of the results)
-    static constexpr uint8_t categoryFileLevel = ResultsType::categoryFile
-                                                 & (Category::FileLevel::id
-                                                    | Category::FileLevel::va);
-    //! Precision (views)
-    static constexpr uint8_t precision = Category::all;
-    //! Indentation (GUI)
-    static constexpr uint8_t nodeDepthForGUI = +0;
-    //! Decimal precision
     static constexpr uint8_t decimal = 0;
-    //! Number of columns used by the variable (One ResultsType per column)
-    static constexpr int columnCount = 1;
-    //! The Spatial aggregation
     static constexpr uint8_t spatialAggregate = Category::spatialAggregateSum;
-    static constexpr uint8_t spatialAggregateMode = Category::spatialAggregateEachYear;
-    static constexpr uint8_t spatialAggregatePostProcessing = 0;
-    //! Intermediate values
-    static constexpr uint8_t hasIntermediateValues = 1;
-    //! Can this variable be non applicable (0 : no, 1 : yes)
-    static constexpr uint8_t isPossiblyNonApplicable = 0;
 
-    typedef IntermediateValues IntermediateValuesBaseType;
-    typedef std::vector<IntermediateValues> IntermediateValuesType;
-
-    using IntermediateValuesTypeForSpatialAg = std::unique_ptr<IntermediateValuesBaseType[]>;
-
-}; // class VCard
-
-/*!
-** \brief C02 Average value of the overrall OperatingCost emissions expected from all
-**   the thermal dispatchable clusters
-*/
-template<class NextT = Container::EndOfList>
-class RampingCost: public Variable::IVariable<RampingCost<NextT>, NextT, VCardRampingCost>
-{
-public:
-    //! Type of the next static variable
-    typedef NextT NextType;
-    //! VCard
-    typedef VCardRampingCost VCardType;
-    //! Ancestor
-    typedef Variable::IVariable<RampingCost<NextT>, NextT, VCardType> AncestorType;
-
-    //! List of expected results
-    typedef typename VCardType::ResultsType ResultsType;
-
-    typedef VariableAccessor<ResultsType, VCardType::columnCount> VariableAccessorType;
-
-    enum
+    static void yearEndBuildForEachThermalCluster(IntermediateValues& yearlyValues,
+                                                  const State& state,
+                                                  unsigned int /*year*/,
+                                                  unsigned int /*numSpace*/)
     {
-        //! How many items have we got
-        count = 1 + NextT::count,
-    };
-
-    template<int CDataLevel, int CFile>
-    struct Statistics
-    {
-        enum
-        {
-            count = ((VCardType::categoryDataLevel & CDataLevel
-                      && VCardType::categoryFileLevel & CFile)
-                       ? (NextType::template Statistics<CDataLevel, CFile>::count
-                          + VCardType::columnCount * ResultsType::count)
-                       : NextType::template Statistics<CDataLevel, CFile>::count),
-        };
-    };
-
-public:
-    void initializeFromStudy(Data::Study& study)
-    {
-        pNbYearsParallel = study.maxNbYearsInParallel;
-
-        InitializeResultsFromStudy(AncestorType::pResults, study);
-
-        pValuesForTheCurrentYear.resize(pNbYearsParallel);
-        for (unsigned int numSpace = 0; numSpace < pNbYearsParallel; numSpace++)
-        {
-            pValuesForTheCurrentYear[numSpace].initializeFromStudy(study);
-        }
-
-        // Next
-        NextType::initializeFromStudy(study);
-    }
-
-    template<class R>
-    static void InitializeResultsFromStudy(R& results, Data::Study& study)
-    {
-        VariableAccessorType::InitializeAndReset(results, study);
-    }
-
-    void initializeFromArea(Data::Study* study, Data::Area* area)
-    {
-        pArea = area;
-        // Next
-        NextType::initializeFromArea(study, area);
-    }
-
-    void initializeFromLink(Data::Study* study, Data::AreaLink* link)
-    {
-        // Next
-        NextType::initializeFromAreaLink(study, link);
-    }
-
-    void simulationBegin()
-    {
-        for (unsigned int numSpace = 0; numSpace < pNbYearsParallel; numSpace++)
-        {
-            pValuesForTheCurrentYear[numSpace].reset();
-        }
-        // Next
-        NextType::simulationBegin();
-    }
-
-    void simulationEnd()
-    {
-        // Next
-        NextType::simulationEnd();
-    }
-
-    void yearBegin(unsigned int year, unsigned int numSpace)
-    {
-        // Reset the values for the current year
-        pValuesForTheCurrentYear[numSpace].reset();
-
-        // Next variable
-        NextType::yearBegin(year, numSpace);
-    }
-
-    void yearEndBuildForEachThermalCluster(State& state, uint year, unsigned int numSpace)
-    {
-        // Get end year calculations
+        // Sum the ramping cost of every thermal cluster of the area over the year.
         for (unsigned int i = state.study.runtime.rangeLimits.hour[Data::rangeBegin];
              i <= state.study.runtime.rangeLimits.hour[Data::rangeEnd];
              ++i)
         {
-            pValuesForTheCurrentYear[numSpace][i] += state.thermalClusterRampingCostForYear[i];
-        }
-
-        // Next variable
-        NextType::yearEndBuildForEachThermalCluster(state, year, numSpace);
-    }
-
-    void yearEndBuild(State& state, unsigned int year)
-    {
-        // Next variable
-        NextType::yearEndBuild(state, year);
-    }
-
-    void yearEnd(unsigned int year, unsigned int numSpace)
-    {
-        // Compute all statistics for the current year (daily,weekly,monthly)
-        pValuesForTheCurrentYear[numSpace].computeStatisticsForTheCurrentYear();
-
-        // Next variable
-        NextType::yearEnd(year, numSpace);
-    }
-
-    void computeSummary(unsigned int year, unsigned int numSpace)
-    {
-        // Merge all those values with the global results
-        AncestorType::pResults.merge(year, pValuesForTheCurrentYear[numSpace]);
-
-        // Next variable
-        NextType::computeSummary(year, numSpace);
-    }
-
-    void hourBegin(unsigned int hourInTheYear)
-    {
-        // Next variable
-        NextType::hourBegin(hourInTheYear);
-    }
-
-    void hourForEachArea(State& state, unsigned int numSpace)
-    {
-        // Next variable
-        NextType::hourForEachArea(state, numSpace);
-    }
-
-    Antares::Memory::Stored<double>::ConstReturnType retrieveRawHourlyValuesForCurrentYear(
-      unsigned int,
-      unsigned int numSpace) const
-    {
-        return pValuesForTheCurrentYear[numSpace].hour;
-    }
-
-    void localBuildAnnualSurveyReport(SurveyResults& results,
-                                      int fileLevel,
-                                      int precision,
-                                      unsigned int numSpace) const
-    {
-        // Initializing external pointer on current variable non applicable status
-        results.isCurrentVarNA = AncestorType::isNonApplicable;
-
-        if (AncestorType::isPrinted[0])
-        {
-            // Write the data for the current year
-            results.variableCaption = VCardType::Caption();
-            results.variableUnit = VCardType::Unit();
-            pValuesForTheCurrentYear[numSpace]
-              .template buildAnnualSurveyReport<VCardType>(results, fileLevel, precision);
+            yearlyValues[i] += state.thermalClusterRampingCostForYear[i];
         }
     }
 
-private:
-    //! The attached area
-    Antares::Data::Area* pArea;
-    //! Intermediate values for each year
-    typename VCardType::IntermediateValuesType pValuesForTheCurrentYear;
-    unsigned int pNbYearsParallel;
+    // The ramping cost is accumulated at the end of the year (see above), not
+    // hour by hour, so no hourly value is ever stored.
+    static bool checkCondition(const State&)
+    {
+        return false;
+    }
 
-}; // class RampingCost
+    static double value(const State&)
+    {
+        return 0.;
+    }
+
+    static void computeStats(IntermediateValues& intermediateValues)
+    {
+        intermediateValues.computeStatisticsForTheCurrentYear();
+    }
+};
+
+using VCardRampingCost = VCard_Base<RampingCostTraits>;
+
+/*!
+** \brief Ramping cost expected from all the thermal dispatchable clusters
+*/
+template<class NextT = Container::EndOfList>
+using RampingCost = Economy_Base<RampingCostTraits, NextT>;
 
 } // namespace Antares::Solver::Variable::Economy
-
-#endif // __SOLVER_VARIABLE_ECONOMY_RampingCost_H__
