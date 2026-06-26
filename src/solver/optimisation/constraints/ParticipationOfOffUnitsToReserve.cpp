6b55f2ef2116537c62854c61d4a79d60f0fa6474
h-fournier <hugo.fournier@scalian.com>
2026-06-26 10:39:09 +0200

    clang format

--- src/solver/optimisation/constraints/ParticipationOfOffUnitsToReserve.cpp
+++ src/solver/optimisation/constraints/ParticipationOfOffUnitsToReserve.cpp
@@ -9,11 +9,11 @@ void ParticipationOfOffUnitsToReserve::add(int pays, int reserve, int cluster, i
     {
         // 16 ter
         // The max power of off units participating to Reserves is bounded by the number of units
-        // and max power of each unit : 
+        // and max power of each unit :
         // P^off +(Pmax^off . M)  <= (Pmax^off . M^on)
         // with P^off: total participation of turned off units to res
-        // Pmax^off : max participation for each off unit 
-        // M : Number of units in the cluster 
+        // Pmax^off : max participation for each off unit
+        // M : Number of units in the cluster
         // M^on : Number of running units in the cluster
 
         CAPACITY_RESERVATION& capacityReservation = data.areaReserves[pays]
