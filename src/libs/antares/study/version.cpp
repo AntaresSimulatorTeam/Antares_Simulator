f009a8afe80960cb936a575ef82b581052a5f362
Jason Marechal <jason.marechal@pm.me>
2026-07-02 17:02:13 +0200

    chore(version): 10.2.0
    Signed-off-by: Jason Marechal <jason.marechal@pm.me>

--- src/libs/antares/study/version.cpp
+++ src/libs/antares/study/version.cpp
@@ -28,7 +28,8 @@ constexpr auto supportedVersions = std::to_array({
   StudyVersion(9, 1),
   StudyVersion(9, 2),
   StudyVersion(9, 3),
-  StudyVersion(10, 0)
+  StudyVersion(10, 0),
+  StudyVersion(10, 2),
   // Add new versions here
 });
 
