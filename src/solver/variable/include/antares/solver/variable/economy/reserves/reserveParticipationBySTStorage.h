217c4e06e942bbba467c7c6388f5ea78431e0337
Florian OMNES <florian.omnes@rte-france.com>
2026-07-13 17:15:24 +0200

    fmt

--- src/solver/variable/include/antares/solver/variable/economy/reserves/reserveParticipationBySTStorage.h
+++ src/solver/variable/include/antares/solver/variable/economy/reserves/reserveParticipationBySTStorage.h
@@ -63,8 +63,8 @@ inline void ReserveParticipationBySTStorage::populateHourlyValues(State& state,
     if (hasIndexMapping(state.study, state.area))
     {
         const auto& resData = state.reserveData.value().at(state.area->index);
-        const auto& hourlyData =
-          resData.reserveParticipationPerSTStorageClusterForYear.at(state.hourInTheYear);
+        const auto& hourlyData = resData.reserveParticipationPerSTStorageClusterForYear.at(
+          state.hourInTheYear);
         for (const auto& [clusterName, reserveMap]: hourlyData)
         {
             for (const auto& [reserveName, reserveParticipation]: reserveMap)
