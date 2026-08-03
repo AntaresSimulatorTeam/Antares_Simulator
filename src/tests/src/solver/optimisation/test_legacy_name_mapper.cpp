a33307a8701acfc2682bb120162038cc192e94ec
LISASI Josue (Externe) <lisasijos@gm0winl1074.bureau.si.interne>
2026-08-03 17:14:10 +0200

    changed variable names in for thermal and hydro is st for legacy

--- src/tests/src/solver/optimisation/test_legacy_name_mapper.cpp
+++ src/tests/src/solver/optimisation/test_legacy_name_mapper.cpp
@@ -23,12 +23,6 @@ BOOST_AUTO_TEST_CASE(known_spillage_is_mapped)
     BOOST_CHECK_EQUAL(mapper.mapOutput("Spillage"), "spilled_energy");
 }
 
-BOOST_AUTO_TEST_CASE(unknown_name_is_returned_unchanged)
-{
-    const LegacyNameMapper mapper;
-    BOOST_CHECK_EQUAL(mapper.mapOutput("HydProd"), "HydProd");
-}
-
 BOOST_AUTO_TEST_CASE(empty_name_is_returned_unchanged)
 {
     const LegacyNameMapper mapper;
@@ -40,6 +34,54 @@ BOOST_AUTO_TEST_CASE(mapping_is_case_sensitive)
     const LegacyNameMapper mapper;
     BOOST_CHECK_EQUAL(mapper.mapOutput("unsuppliedenergy"), "unsuppliedenergy");
     BOOST_CHECK_EQUAL(mapper.mapOutput("UNSUPPLIEDENERGY"), "UNSUPPLIEDENERGY");
+}
+
+BOOST_AUTO_TEST_CASE(known_hydprod_is_mapped)
+{
+    const LegacyNameMapper mapper;
+    BOOST_CHECK_EQUAL(mapper.mapOutput("HydProd"), "withdrawal_power");
+}
+
+BOOST_AUTO_TEST_CASE(known_pumping_is_mapped)
+{
+    const LegacyNameMapper mapper;
+    BOOST_CHECK_EQUAL(mapper.mapOutput("Pumping"), "injection_power");
+}
+
+BOOST_AUTO_TEST_CASE(known_hydro_level_is_mapped)
+{
+    const LegacyNameMapper mapper;
+    BOOST_CHECK_EQUAL(mapper.mapOutput("HydroLevel"), "level");
+}
+
+BOOST_AUTO_TEST_CASE(known_overflow_is_mapped)
+{
+    const LegacyNameMapper mapper;
+    BOOST_CHECK_EQUAL(mapper.mapOutput("Overflow"), "overflow");
+}
+
+BOOST_AUTO_TEST_CASE(known_number_stopping_is_mapped)
+{
+    const LegacyNameMapper mapper;
+    BOOST_CHECK_EQUAL(mapper.mapOutput("NumberStoppingDispatchableUnits"), "num_units_stopping");
+}
+
+BOOST_AUTO_TEST_CASE(known_number_breaking_down_is_mapped)
+{
+    const LegacyNameMapper mapper;
+    BOOST_CHECK_EQUAL(mapper.mapOutput("NumberBreakingDownDispatchableUnits"), "num_units_failing");
+}
+
+BOOST_AUTO_TEST_CASE(known_cost_variation_injection_is_mapped)
+{
+    const LegacyNameMapper mapper;
+    BOOST_CHECK_EQUAL(mapper.mapOutput("CostVariationInjection"), "injection_variation");
+}
+
+BOOST_AUTO_TEST_CASE(known_cost_variation_withdrawal_is_mapped)
+{
+    const LegacyNameMapper mapper;
+    BOOST_CHECK_EQUAL(mapper.mapOutput("CostVariationWithdrawal"), "withdrawal_variation");
 }
 
 BOOST_AUTO_TEST_SUITE_END()
