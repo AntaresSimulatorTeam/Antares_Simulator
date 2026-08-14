7222857b8ba15576fac887d41ec89d588ecd24a4
Jason Marechal <jason.marechal@pm.me>
2026-08-14 08:23:36 +0200

    format
    Signed-off-by: Jason Marechal <jason.marechal@pm.me>

--- src/libs/antares/logs/logs-instance.cpp
+++ src/libs/antares/logs/logs-instance.cpp
@@ -1,9 +1,9 @@
 // Copyright 2007-2026, RTE (https://www.rte-france.com)
 // SPDX-License-Identifier: MPL-2.0
 
-#include "antares/logs/logs.h"
-
 #include <string>
+
+#include "antares/logs/logs.h"
 
 // Kept in its own translation unit, separate from Logger/LogBuffer, so that pulling in the
 // LogBuffer implementation from the static library does not also drag in this definition of
