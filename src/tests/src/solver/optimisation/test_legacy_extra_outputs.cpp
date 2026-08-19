7ab5864c2cce79a6186e95c9f24e38a94a7fb3cd
Jason Marechal <jason.marechal@pm.me>
2026-08-19 15:17:41 +0200

    fix: build and test
    Signed-off-by: Jason Marechal <jason.marechal@pm.me>

--- src/tests/src/solver/optimisation/test_legacy_extra_outputs.cpp
+++ src/tests/src/solver/optimisation/test_legacy_extra_outputs.cpp
@@ -722,7 +722,7 @@ BOOST_AUTO_TEST_CASE(
     problem.inactiveComponents = analyzer;
     fill();
 
-    BOOST_CHECK(!FindRow(table, "balance_port.flow", "area1_hydro").has_value());
+    BOOST_CHECK(!FindRow(table, "balance_port.flow", "area1_hydro_storage").has_value());
 }
 
 BOOST_AUTO_TEST_CASE(hydro_balance_port_flow_is_kept_when_reservoir_unmanaged_but_inflow_nonzero)
@@ -733,7 +733,7 @@ BOOST_AUTO_TEST_CASE(hydro_balance_port_flow_is_kept_when_reservoir_unmanaged_bu
     problem.inactiveComponents = analyzer;
     fill();
 
-    BOOST_CHECK(FindRow(table, "balance_port.flow", "area1_hydro").has_value());
+    BOOST_CHECK(FindRow(table, "balance_port.flow", "area1_hydro_storage").has_value());
 }
 
 BOOST_AUTO_TEST_CASE(hydro_balance_port_flow_is_kept_when_reservoir_managed_even_if_inflow_all_zero)
@@ -744,7 +744,7 @@ BOOST_AUTO_TEST_CASE(hydro_balance_port_flow_is_kept_when_reservoir_managed_even
     problem.inactiveComponents = analyzer;
     fill();
 
-    BOOST_CHECK(FindRow(table, "balance_port.flow", "area1_hydro").has_value());
+    BOOST_CHECK(FindRow(table, "balance_port.flow", "area1_hydro_storage").has_value());
 }
 
 BOOST_AUTO_TEST_CASE(port_field_link_flows_are_the_signed_flow_and_its_negation)
