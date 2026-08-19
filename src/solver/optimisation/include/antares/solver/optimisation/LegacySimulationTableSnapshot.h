b0edbd4f27640d00cfe25f412c75e17ff01153af
Jason Marechal <jason.marechal@pm.me>
2026-08-19 14:15:52 +0200

    format
    Signed-off-by: Jason Marechal <jason.marechal@pm.me>

--- src/solver/optimisation/include/antares/solver/optimisation/LegacySimulationTableSnapshot.h
+++ src/solver/optimisation/include/antares/solver/optimisation/LegacySimulationTableSnapshot.h
@@ -19,11 +19,10 @@ namespace Antares::Optimization
 //
 // Reads the solution through PROBLEME_ANTARES_A_RESOUDRE::X, so the caller is
 // responsible for X holding the state it wants published.
-void FillLegacySimulationTable(
-  Antares::IO::Outputs::SimulationTable& simulationTable,
-  PROBLEME_HEBDO& problemeHebdo,
-  const Antares::LinearProblem::Api::FillContext& fillContext,
-  const LegacyNameMapper& nameMapper,
-  unsigned currentBlock);
+void FillLegacySimulationTable(Antares::IO::Outputs::SimulationTable& simulationTable,
+                               PROBLEME_HEBDO& problemeHebdo,
+                               const Antares::LinearProblem::Api::FillContext& fillContext,
+                               const LegacyNameMapper& nameMapper,
+                               unsigned currentBlock);
 
 } // namespace Antares::Optimization
