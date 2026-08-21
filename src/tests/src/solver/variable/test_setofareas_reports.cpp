419e7293d7fbc531afc1d9a0c29f76b92a0469e6
payetvin <payetvin_externe@rte-france.com>
2026-08-21 15:57:33 +0200

    Dynamic variables: Remove suffixes for thermal and renew prod

--- src/tests/src/solver/variable/test_setofareas_reports.cpp
+++ src/tests/src/solver/variable/test_setofareas_reports.cpp
@@ -238,7 +238,7 @@ BOOST_AUTO_TEST_CASE(exports_one_enabled_district)
     const auto fileIt = files.find("out/areas/@ district-1/values-hourly.txt");
     BOOST_REQUIRE(fileIt != files.end());
     BOOST_CHECK_NE(fileIt->second.find(
-                     "\tGROUP1_TH_PROD\tGROUP1_TH_PROD\tGROUP1_TH_PROD\tGROUP1_TH_PROD"),
+                     "\tGROUP1\tGROUP1\tGROUP1\tGROUP1"),
                    std::string::npos);
     BOOST_CHECK_NE(fileIt->second.find("\tEXP\tstd\tmin\tmax"), std::string::npos);
 }
@@ -260,13 +260,13 @@ BOOST_AUTO_TEST_CASE(exports_two_enabled_districts)
     const auto firstFileIt = files.find("out/areas/@ district-1/values-hourly.txt");
     BOOST_REQUIRE(firstFileIt != files.end());
     BOOST_CHECK_NE(firstFileIt->second.find(
-                     "\tGROUP1_TH_PROD\tGROUP1_TH_PROD\tGROUP1_TH_PROD\tGROUP1_TH_PROD"),
+                     "\tGROUP1\tGROUP1\tGROUP1\tGROUP1"),
                    std::string::npos);
     BOOST_CHECK_NE(firstFileIt->second.find("\tEXP\tstd\tmin\tmax"), std::string::npos);
     const auto secondFileIt = files.find("out/areas/@ district-2/values-hourly.txt");
     BOOST_REQUIRE(secondFileIt != files.end());
     BOOST_CHECK_NE(secondFileIt->second.find(
-                     "\tGROUP1_TH_PROD\tGROUP1_TH_PROD\tGROUP1_TH_PROD\tGROUP1_TH_PROD"),
+                     "\tGROUP1\tGROUP1\tGROUP1\tGROUP1"),
                    std::string::npos);
     BOOST_CHECK_NE(secondFileIt->second.find("\tEXP\tstd\tmin\tmax"), std::string::npos);
 }
@@ -289,7 +289,7 @@ BOOST_AUTO_TEST_CASE(skips_disabled_district_and_exports_enabled_one)
     const auto fileIt = files.find("out/areas/@ district-enabled/values-hourly.txt");
     BOOST_REQUIRE(fileIt != files.end());
     BOOST_CHECK_NE(fileIt->second.find(
-                     "\tGROUP1_TH_PROD\tGROUP1_TH_PROD\tGROUP1_TH_PROD\tGROUP1_TH_PROD"),
+                     "\tGROUP1\tGROUP1\tGROUP1\tGROUP1"),
                    std::string::npos);
     BOOST_CHECK_NE(fileIt->second.find("\tEXP\tstd\tmin\tmax"), std::string::npos);
 }
