f4986eafaca2f68399c0a54ed559c61c556cf1ee
payetvin <payetvin_externe@rte-france.com>
2026-09-01 17:54:28 +0200

    ternaire

--- src/libs/antares/study/area/list.cpp
+++ src/libs/antares/study/area/list.cpp
@@ -714,14 +714,11 @@ static bool AreaListLoadFromFolderSingleArea(Study& study,
         if (study.parameters.unitCommitment.ucMode != UnitCommitmentMode::ucHeuristicFast
             && study.parameters.include.reserves && studyVersion >= StudyVersion(10, 1))
         {
-            fs::path reservesHydroPath = study.folderInput / "hydro" / "reserves" / area.id
-                                         / "reserve-participations.yml";
-
-            if (studyVersion < StudyVersion(10, 2))
-            {
-                reservesHydroPath = study.folderInput / "hydro" / "common" / area.id
-                                    / "reserve-participations.yml";
-            }
+            fs::path reservesHydroPath = studyVersion >= StudyVersion(10, 2)
+                                           ? study.folderInput / "hydro" / "reserves" / area.id
+                                               / "reserve-participations.yml"
+                                           : study.folderInput / "hydro" / "common" / area.id
+                                               / "reserve-participations.yml";
 
             area.hydro.loadReserveParticipations(area, reservesHydroPath);
         }
